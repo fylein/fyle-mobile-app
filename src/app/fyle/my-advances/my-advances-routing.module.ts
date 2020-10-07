4c7ed6e97215c08a72d0642837b0393f8f31f05c
Tarun <tarun.kumar@fyle.in>
2020-10-07 11:00:08 +0530

    My Advances

---
+++ src/app/fyle/my-advances/my-advances-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MyAdvancesPage } from './my-advances.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MyAdvancesPage
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MyAdvancesPageRoutingModule {}
