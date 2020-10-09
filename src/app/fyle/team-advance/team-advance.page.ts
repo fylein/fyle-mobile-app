0b20f2ab04047b3bc8632df9775f49f3299196f9
Madhav Mansuriya <madhav.mansuriya@fyle.in>
2020-10-09 08:14:42 +0530

    Team advance state fix

--- src/app/fyle/team-advance/team-advance.page.ts
+++ src/app/fyle/team-advance/team-advance.page.ts
@@ -35,8 +35,9 @@ export class TeamAdvancePage implements OnInit {
               offset: (pageNumber - 1) * 10,
               limit: 10,
               queryParams: {
+                areq_state: ['eq.APPROVAL_PENDING'],
                 areq_trip_request_id: ['is.null'],
-                areq_approval_state: ['ov.{APPROVAL_PENDING,APPROVAL_DONE,APPROVAL_REJECTED}']
+                or: ['(areq_is_sent_back.is.null,areq_is_sent_back.is.false)']
               }
             });
           }),
