08fb94ccfd2a272bf4630d62030e17fe524d6154
aiyush <aiyush.dhar@fyle.in>
2020-10-13 18:26:05 +0530

    Added fixes

--- src/app/fyle/my-expenses/add-expense-popover/add-expense-popover.component.ts
+++ src/app/fyle/my-expenses/add-expense-popover/add-expense-popover.component.ts
@@ -28,7 +28,7 @@ export class AddExpensePopoverComponent implements OnInit {
   async createExpense(event) {
     await this.popoverController.dismiss();
     this.router.navigate(['/', 'enterprise', 'add_edit_expense']);
-  }
+  } 
 
   async createMileage(event) {
     await this.popoverController.dismiss();
