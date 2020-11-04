bc93d7c8a8cc2bf59c2c7921372caf7e4b799589
Tarun Kumar Pal <iamtarun1993@gmail.com>
2020-11-04 14:10:50 +0530

    estatus fix

--- src/app/core/services/status.service.ts
+++ src/app/core/services/status.service.ts
@@ -20,16 +20,14 @@ export class StatusService {
 
   find(objectType, objectId) {
     return this.apiService.get('/' + objectType + '/' + objectId + '/estatuses').pipe(
-      map(estatuses => {
+      map((estatuses: ExtendedStatus[])=> {
         return estatuses.map(estatus => {
-          const res = this.dataTransformService.unflatten(estatus);
-          this.dateService.fixDates(res.st);
-          res.st.created_at = this.dateService.getLocalDate(res.st.created_at);
-          res.st.createdAtNew = this.datePipe.transform(res.st.created_at, 'MMM d, y');
-          return res as ExtendedStatus[];
+          estatus.st_created_at = new Date(estatus.st_created_at);
+          estatus.st_created_at = this.dateService.getLocalDate(estatus.st_created_at);
+          return estatus as ExtendedStatus;
         });
       })
-    );
+    )
   }
 
 
