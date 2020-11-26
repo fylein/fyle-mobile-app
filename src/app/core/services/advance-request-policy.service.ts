299d690f1f84b116c183d91eb8dbdc0e03dfe53c
Tarun <tarun.kumar@fyle.in>
2020-11-26 13:22:17 +0530

    resolved comment

--- src/app/core/services/advance-request-policy.service.ts
+++ src/app/core/services/advance-request-policy.service.ts
@@ -20,9 +20,6 @@ export class AdvanceRequestPolicyService {
   }
 
   servicePost(url, data, config) {
-    if (url.indexOf('remotecopy') === -1 && url.indexOf('upload_html') === -1) {
-      console.log('post ' + url + ' with data ' + JSON.stringify(data));
-    }
     return this.httpClient.post(environment.ROOT_URL + '/policy/advance_requests' + url, data);
   }
 
