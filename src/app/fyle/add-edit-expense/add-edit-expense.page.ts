c4b654c2bd984aa32499dceffc79b607e2cf123f
aiyush <aiyush.dhar@fyle.in>
2020-12-04 12:57:37 +0530

    Updated PR

--- src/app/fyle/add-edit-expense/add-edit-expense.page.ts
+++ src/app/fyle/add-edit-expense/add-edit-expense.page.ts
@@ -403,6 +403,8 @@ export class AddEditExpensePage implements OnInit {
                             };
                           })
                         );
+                  } else {
+                    return of(instaFyleImageData);
                   }
                 })
               );
