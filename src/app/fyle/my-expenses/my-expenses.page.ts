6717aec218feb6b3d1d186b8134e432ea0ff3697
aiyush <aiyush.dhar@fyle.in>
2020-12-10 14:07:08 +0530

    Save

--- src/app/fyle/my-expenses/my-expenses.page.ts
+++ src/app/fyle/my-expenses/my-expenses.page.ts
@@ -12,7 +12,6 @@ import { MyExpensesSearchFilterComponent } from './my-expenses-search-filter/my-
 import { MyExpensesSortFilterComponent } from './my-expenses-sort-filter/my-expenses-sort-filter.component';
 import { Expense } from 'src/app/core/models/expense.model';
 import { CurrencyService } from 'src/app/core/services/currency.service';
-import { OrgUserSettingsService } from 'src/app/core/services/org-user-settings.service';
 import { OrgSettingsService } from 'src/app/core/services/org-settings.service';
 import { AddExpensePopoverComponent } from './add-expense-popover/add-expense-popover.component';
 import { CategoriesService } from 'src/app/core/services/categories.service';
