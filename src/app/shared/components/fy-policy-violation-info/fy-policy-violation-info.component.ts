989f174770a6040ca412e9951dada913471495e2
Tarun <tarun.kumar@fyle.in>
2020-12-20 22:39:55 +0530

    minor

--- src/app/shared/components/fy-policy-violation-info/fy-policy-violation-info.component.ts
+++ src/app/shared/components/fy-policy-violation-info/fy-policy-violation-info.component.ts
@@ -11,10 +11,7 @@ export class FyPolicyViolationInfoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log(this.estatuses);
-
     this.policyViolations = [];
-
     this.policyViolations = this.estatuses.filter(function (estatus) {
       return estatus.st_org_user_id === 'POLICY';
     });
