b1d8093e4e6a2722469271189b6e9d78c5e84f0a
Madhav Mansuriya <madhav.mansuriya@fyle.in>
2021-02-12 20:58:32 +0530

    update

--- src/app/shared/components/fy-apporver/approver-dialog/approver-dialog.component.ts
+++ src/app/shared/components/fy-apporver/approver-dialog/approver-dialog.component.ts
@@ -102,13 +102,7 @@ export class ApproverDialogComponent implements OnInit, AfterViewInit {
         return this.orgUserService.excludeByStatus(eouc, 'DISABLED');
       }),
       map(eouc => {
-        eouc.sort((a, b) => {
-          if (a.us.email < b.us.email) {
-            return -1;
-          } else {
-            return 1;
-          }
-        });
+        eouc.sort((a, b) => a.us.email < b.us.email ? -1 : 1);
         return eouc.filter(approver => {
           return this.approverList.indexOf(approver.us.email) < 0;
         }).sort();
