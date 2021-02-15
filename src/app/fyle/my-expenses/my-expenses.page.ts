7f8601afc0ecfc3c4b27ca8d4a21ab817908aeb0
Tarun <iamtarun1993@gmail.com>
2021-02-15 12:12:36 +0530

    minor

--- src/app/fyle/my-expenses/my-expenses.page.ts
+++ src/app/fyle/my-expenses/my-expenses.page.ts
@@ -255,7 +255,7 @@ export class MyExpensesPage implements OnInit {
     this.count$ = paginatedPipe.pipe(
       map(res => res.count),
       shareReplay(1)
-    )
+    );
 
     this.isNewUser$ = this.transactionService.getPaginatedETxncCount().pipe(
       map(res => {
