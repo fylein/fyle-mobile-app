3860eb2121fd4b0655926c6cbd5fbd8c179f6744
Madhav Mansuriya <madhav.mansuriya@fyle.in>
2021-02-17 10:24:07 +0530

    inital commit for removing singup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AuthGuard } from './core/guards/auth.guard';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'auth/sign_up',
+    redirectTo: 'auth/sign_in',
     pathMatch: 'full'
   },
   {
