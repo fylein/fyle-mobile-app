1fb5663fcc7ae217a6e748ccbb609fb8d3eb2d68
lakshmisuhas1996 <lakshmisuhasn123+github@gmail.com>
2021-03-08 14:52:12 +0530

    changes

--- src/app/core/models/cost-center.model.ts
+++ src/app/core/models/cost-center.model.ts
@@ -1,7 +1,7 @@
 export interface CostCenter {
   id: number;
-  created_at: string;
-  updated_at: string;
+  created_at: Date;
+  updated_at: Date;
   name: string;
   code?: string;
   active?: boolean;
