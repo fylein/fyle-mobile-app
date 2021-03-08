74bf5d6284dc7e1e53d9c75e73fc9e1a33d3ffae
lakshmisuhas1996 <lakshmisuhasn123+github@gmail.com>
2021-03-08 14:01:18 +0530

    Changes for adding Vendor Model

---
+++ src/app/core/models/vendor.model.ts
@@ -0,0 +1,12 @@
+export interface Vendor {
+  id: number;
+  cin?: any;
+  tin?: any;
+  display_name?: string;
+  other_names?: string;
+  creator_id: string;
+  created_at: string;
+  updated_at: string;
+  default_category?: any;
+  verified?: boolean; 
+}
