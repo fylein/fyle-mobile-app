9a5e507b32b243b27854b29d27a1d822e6d6b5a2
Tarun <iamtarun1993@gmail.com>
2021-03-08 16:42:57 +0530

    Closing menu button before opening FreshDesk chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,7 +65,7 @@ export class AppComponent implements OnInit {
     private domSanitizer: DomSanitizer,
     private networkService: NetworkService,
     private alertController: AlertController,
-    private freshchatService: FreshChatService,
+    private freshChatService: FreshChatService,
     private zone: NgZone,
     private deepLinkService: DeepLinkService,
     private splashScreen: SplashScreen,
@@ -126,7 +126,8 @@ export class AppComponent implements OnInit {
   }
 
   openHelp() {
-    this.freshchatService.openLiveChatSupport();
+    this.menuController.close(); // Closing menu button before opening FreshDesk chat
+    this.freshChatService.openLiveChatSupport();
   }
 
   redirect(route) {
@@ -237,7 +238,7 @@ export class AppComponent implements OnInit {
         });
       }
 
-      this.freshchatService.setupNetworkWatcher();
+      this.freshChatService.setupNetworkWatcher();
 
       // TODO: remove nested subscribe - mini tech debt
       if (isConnected) {
@@ -491,7 +492,7 @@ export class AppComponent implements OnInit {
 
     this.userEventService.onLogout(() => {
       this.trackingService.onSignOut({Asset: 'Mobile'});
-      this.freshchatService.destory();
+      this.freshChatService.destory();
       this.router.navigate(['/', 'auth', 'sign_in']);
     });
 
