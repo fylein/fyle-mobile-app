6a7f4449742420ce8de348b37b29022781cf60ad
lakshmisuhas1996 <lakshmisuhasn123+github@gmail.com>
2021-03-09 11:06:30 +0530

    Minor change

--- src/app/core/models/vendor.model.ts
+++ src/app/core/models/vendor.model.ts
@@ -2,7 +2,7 @@ export interface Vendor {
   id: number;
   cin?: any;
   tin?: any;
-  display_name?: string;
+  display_name: string;
   other_names?: string;
   creator_id: string;
   created_at: Date;
