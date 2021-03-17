f75dd4fcbecde9eb18e749e5ca02b3007f17be80
lakshmisuhas1996 <lakshmisuhasn123+github@gmail.com>
2021-03-17 13:36:27 +0530

    changes

--- src/app/shared/directive/format-date.directive.ts
+++ src/app/shared/directive/format-date.directive.ts
@@ -1,31 +1,20 @@
 // @Link:  https://stackoverflow.com/a/31162426
 
-import { Directive, Input, ElementRef, OnChanges, OnInit, HostListener } from '@angular/core';
+import { Directive, ElementRef, OnInit, HostListener } from '@angular/core';
 import * as moment from 'moment';
-import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[appFormatDate]'
 })
 export class FormatDateDirective implements OnInit {
-  @Input() dateValue: string;
 
-  constructor(private elementRef: ElementRef, private control: NgControl) { }
+  constructor(private elementRef: ElementRef) { }
 
-  // ngOnChanges() {
-  //   const inputEl = this.elementRef.nativeElement;
-  //   if (inputEl.value) {
-  //     inputEl.setAttribute('data-date', moment(inputEl.value).format('MMM DD, YYYY'));
-  //   }
-  // }
+  ngOnInit() {}
 
-  ngOnInit() {
-
-  }
-
-  @HostListener('ngModelChange', [ '$event'] ) onChange(value) {
+  @HostListener('ngModelChange', [ '$event'] ) onChange(value: string) {
     if (value) {
-      this.elementRef.nativeElement.setAttribute('data-date', moment(this.elementRef.nativeElement.value).format('MMM DD, YYYY'));
+      this.elementRef.nativeElement.setAttribute('data-date', moment(value).format('MMM DD, YYYY'));
     }
   }
 }
