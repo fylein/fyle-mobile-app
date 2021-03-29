b630e4914fe6f96e3269725e49bd3d3bd4c41ed2
Shishira <shishira.ms@fyle.in>
2021-03-29 11:49:46 +0530

    Added more failsafes

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -5,29 +5,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class FilterPipe implements PipeTransform {
-	/**
-	 * items: list of elements that needs to be filtered
-	 * searchText: search query
-	 * param: key to get the value to be filtered if items is list of objects
-	 *  */
+  /**
+   * items: list of elements that needs to be filtered
+   * searchText: search query
+   * param (Optional): key to get the value to be filtered if items is list of objects
+   *  */
   transform(items: any[], searchText: string, param: string): any[] {
     // if there are no items, return empty list
-		if (!items) {
-			return [];
-		}
+    if (!(items && items.length > 0)) {
+      return [];
+    }
 
-		// if there is no search text, return full list
-		if (!searchText) {
-			return items; 
-		}
+    // if there is no search text, return full list
+    if (!searchText) {
+      return items; 
+    }
 
-		searchText = searchText.toLowerCase();
+    var searchTextLowerCase = searchText.toLowerCase();
 
-		if (items && items.length > 0) {
-			return items.filter ( item => {
-				let itemValue = item[param] || item;
-				return itemValue.includes(searchText);
-			});
-		}
-	}
+    if (items && items.length > 0) {
+      return items.filter ( item => {
+        // if param is passed (in case when 'items' is list of objects), set itemValue using param. Else set item itself (when list of strings is passed)
+        let itemValue = item[param] || item;
+        return itemValue && item.length > 0 && itemValue.includes(searchTextLowerCase);
+      });
+    }
+  }
 }
