b1b841fcd15e8c3d855bf7c6fcb5554f220653c7
Tarun <iamtarun1993@gmail.com>
2021-04-14 13:38:24 +0530

    fix in my exp page

--- src/app/fyle/my-expenses/my-expenses.page.ts
+++ src/app/fyle/my-expenses/my-expenses.page.ts
@@ -208,13 +208,39 @@ export class MyExpensesPage implements OnInit {
     this.simpleSearchText = '';
 
     this.currentPageNumber = 1;
-    this.loadData$ = new BehaviorSubject({
-      pageNumber: 1
-    });
+    // this.loadData$ = new BehaviorSubject({
+    //   pageNumber: 1
+    // });
 
-    this.params$ = this.loadData$.asObservable().pipe(
-      skip(1)
-    );
+    if (this.activatedRoute.snapshot.queryParams.filters) {
+      this.filters = Object.assign({}, this.filters, JSON.parse(this.activatedRoute.snapshot.queryParams.filters));
+      this.currentPageNumber = 1;
+      const params = this.addNewFiltersToParams();
+      console.log("----------1---------");
+      this.loadData$ = new BehaviorSubject(params);
+    } else if (this.activatedRoute.snapshot.params.state) {
+      let filters = {};
+      if (this.activatedRoute.snapshot.params.state.toLowerCase() === 'needsreceipt') {
+        filters = {tx_receipt_required: 'eq.true', state: 'NEEDS_RECEIPT'};
+      } else if (this.activatedRoute.snapshot.params.state.toLowerCase() === 'policyviolated') {
+        filters = {tx_policy_flag: 'eq.true', or: '(tx_policy_amount.is.null,tx_policy_amount.gt.0.0001)', state: 'POLICY_VIOLATED'};
+      } else if (this.activatedRoute.snapshot.params.state.toLowerCase() === 'cannotreport') {
+        filters = {tx_policy_amount: 'lt.0.0001', state: 'CANNOT_REPORT'};
+      }
+      this.filters = Object.assign({}, this.filters, filters);
+      this.currentPageNumber = 1;
+      const params = this.addNewFiltersToParams();
+      this.loadData$ = new BehaviorSubject(params);
+    } else {
+      this.filters = {};
+      this.currentPageNumber = 1;
+      const params = this.addNewFiltersToParams();
+      this.loadData$ = new BehaviorSubject(params);
+    }
+
+    // this.params$ = this.loadData$.asObservable().pipe(
+    //   skip(1)
+    // );
 
     this.selectionMode = false;
     this.selectedElements = [];
@@ -256,8 +282,8 @@ export class MyExpensesPage implements OnInit {
         this.loadData$.next(currentParams);
       });
 
-    const paginatedPipe = this.params$.pipe(
-      tap(console.log),
+    const paginatedPipe = this.loadData$.pipe(
+      tap((res) => console.log(JSON.stringify(res))),
       concatMap((params) => {
         if (this.currentPageNumber === 1) {
           return from(this.loaderService.showLoader('Loading Expenses...')).pipe(map(() => params));
@@ -375,27 +401,27 @@ export class MyExpensesPage implements OnInit {
     this.myExpenses$.subscribe(noop);
     this.count$.subscribe(noop);
     this.isInfiniteScrollRequired$.subscribe(noop);
-    if (this.activatedRoute.snapshot.queryParams.filters) {
-      this.filters = Object.assign({}, this.filters, JSON.parse(this.activatedRoute.snapshot.queryParams.filters));
-      this.currentPageNumber = 1;
-      const params = this.addNewFiltersToParams();
-      this.loadData$.next(params);
-    } else if (this.activatedRoute.snapshot.params.state) {
-      let filters = {};
-      if (this.activatedRoute.snapshot.params.state.toLowerCase() === 'needsreceipt') {
-        filters = {tx_receipt_required: 'eq.true', state: 'NEEDS_RECEIPT'};
-      } else if (this.activatedRoute.snapshot.params.state.toLowerCase() === 'policyviolated') {
-        filters = {tx_policy_flag: 'eq.true', or: '(tx_policy_amount.is.null,tx_policy_amount.gt.0.0001)', state: 'POLICY_VIOLATED'};
-      } else if (this.activatedRoute.snapshot.params.state.toLowerCase() === 'cannotreport') {
-        filters = {tx_policy_amount: 'lt.0.0001', state: 'CANNOT_REPORT'};
-      }
-      this.filters = Object.assign({}, this.filters, filters);
-      this.currentPageNumber = 1;
-      const params = this.addNewFiltersToParams();
-      this.loadData$.next(params);
-    } else {
-      this.clearFilters();
-    }
+    // if (this.activatedRoute.snapshot.queryParams.filters) {
+    //   this.filters = Object.assign({}, this.filters, JSON.parse(this.activatedRoute.snapshot.queryParams.filters));
+    //   this.currentPageNumber = 1;
+    //   const params = this.addNewFiltersToParams();
+    //   this.loadData$.next(params);
+    // } else if (this.activatedRoute.snapshot.params.state) {
+    //   let filters = {};
+    //   if (this.activatedRoute.snapshot.params.state.toLowerCase() === 'needsreceipt') {
+    //     filters = {tx_receipt_required: 'eq.true', state: 'NEEDS_RECEIPT'};
+    //   } else if (this.activatedRoute.snapshot.params.state.toLowerCase() === 'policyviolated') {
+    //     filters = {tx_policy_flag: 'eq.true', or: '(tx_policy_amount.is.null,tx_policy_amount.gt.0.0001)', state: 'POLICY_VIOLATED'};
+    //   } else if (this.activatedRoute.snapshot.params.state.toLowerCase() === 'cannotreport') {
+    //     filters = {tx_policy_amount: 'lt.0.0001', state: 'CANNOT_REPORT'};
+    //   }
+    //   this.filters = Object.assign({}, this.filters, filters);
+    //   this.currentPageNumber = 1;
+    //   const params = this.addNewFiltersToParams();
+    //   this.loadData$.next(params);
+    // } else {
+    //   this.clearFilters();
+    // }
   }
 
   setupNetworkWatcher() {
@@ -449,7 +475,10 @@ export class MyExpensesPage implements OnInit {
   }
 
   addNewFiltersToParams() {
-    const currentParams = this.loadData$.getValue();
+    let currentParams:{[key: string]: any} = {};
+    if (this.loadData$) {
+      currentParams = this.loadData$.getValue();
+    }
     currentParams.pageNumber = 1;
     const newQueryParams: any = {};
 
