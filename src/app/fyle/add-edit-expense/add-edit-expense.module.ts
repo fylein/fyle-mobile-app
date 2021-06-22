b31b5e3d81be51024922d9bfcde53ddeaf9d8bb7
Dimple_kh <dimple.kh@fyle.in>
2021-06-22 12:26:02 +0530

    minor

--- src/app/fyle/add-edit-expense/add-edit-expense.module.ts
+++ src/app/fyle/add-edit-expense/add-edit-expense.module.ts
@@ -17,7 +17,6 @@ import { MatRippleModule, MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FyCriticalPolicyViolationComponent } from 'src/app/shared/components/fy-critical-policy-violation/fy-critical-policy-violation.component';
 import { PolicyViolationComponent } from './policy-violation/policy-violation.component';
 import { CameraOptionsPopupComponent } from './camera-options-popup/camera-options-popup.component';
 import { PinchZoomModule } from 'ngx-pinch-zoom';
@@ -50,7 +49,6 @@ import {MatButtonModule} from '@angular/material/button';
     FyCurrencyComponent,
     FyCurrencyChooseCurrencyComponent,
     FyCurrencyExchangeRateComponent,
-    FyCriticalPolicyViolationComponent,
     PolicyViolationComponent,
     CameraOptionsPopupComponent,
     MatchTransactionComponent
