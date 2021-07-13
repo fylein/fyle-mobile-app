e6b4946a2411f08ccec39a580e95f88966ef0a94
Dimple_kh <dimple.kh@fyle.in>
2021-07-13 20:06:58 +0530

    resolved conflict

--- src/app/shared/components/comments-history/view-comment/view-comment.component.ts
+++ src/app/shared/components/comments-history/view-comment/view-comment.component.ts
@@ -37,10 +37,7 @@ export class ViewCommentComponent implements OnInit {
   userComments: any;
   type: string;
   systemEstatuses: ExtendedStatus[];
-<<<<<<< HEAD:src/app/shared/components/comments-history/view-comment/view-comment.component.ts
   showDt: boolean;
-=======
->>>>>>> master:src/app/shared/components/comments-history/view-comment/view-comment.component.ts
 
   constructor(
     private statusService: StatusService,
