57555e2435985823098f1183929b8c9a49d2c68d
Ravisankar <ravisankar.s@fyle.in>
2021-07-19 11:55:38 +0530

    Minor

--- hooks/preserve.js
+++ hooks/preserve.js
@@ -4,18 +4,19 @@ const path = require('path');
 const stat = util.promisify(fs.stat);
 
 module.exports = function (ctx) {
+  console.log(process.env);
     const stagingEnviroinent = `
     export const environment = {
         production: false,
         NAME: '',
         CLUSTER_DOMAIN: '',
-        ROOT_URL: '`+process.env.FYLE_MOBILE_ROOT_URL+`',
-        ROUTER_API_ENDPOINT: '`+process.env.FYLE_MOBILE_ROUTER_API_ENDPOINT+`',
-        ANDROID_CLIENT_ID: '`+process.env.FYLE_MOBILE_ANDROID_CLIENT_ID+`',
-        IP_FIND_KEY: '`+process.env.FYLE_MOBILE_IP_FIND_KEY+`',
-        GOOGLE_MAPS_API_KEY: '`+process.env.FYLE_MOBILE_GOOGLE_MAPS_API_KEY+`',
-        FRESHCHAT_TOKEN: '`+process.env.FYLE_MOBILE_FRESHCHAT_TOKEN+`',
-        SENTRY_DSN: '`+process.env.FYLE_MOBILE_SENTRY_DSN+`'
+        ROOT_URL: '`+process.env.FYLE_MOBILE_STAGING_ROOT_URL+`',
+        ROUTER_API_ENDPOINT: '`+process.env.FYLE_MOBILE_STAGING_ROUTER_API_ENDPOINT+`',
+        ANDROID_CLIENT_ID: '`+process.env.FYLE_MOBILE_STAGING_ANDROID_CLIENT_ID+`',
+        IP_FIND_KEY: '`+process.env.FYLE_MOBILE_STAGING_IP_FIND_KEY+`',
+        GOOGLE_MAPS_API_KEY: '`+process.env.FYLE_MOBILE_STAGING_GOOGLE_MAPS_API_KEY+`',
+        FRESHCHAT_TOKEN: '`+process.env.FYLE_MOBILE_STAGING_FRESHCHAT_TOKEN+`',
+        SENTRY_DSN: '`+process.env.FYLE_MOBILE_STAGING_SENTRY_DSN+`'
       };
 
     `;
