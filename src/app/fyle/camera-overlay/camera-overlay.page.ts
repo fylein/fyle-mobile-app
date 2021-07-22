ed9d9d544f6bddb039f9ed073e53c5f305f0d8bd
Tarun <iamtarun1993@gmail.com>
2021-07-22 16:26:24 +0530

    Minor

--- src/app/fyle/camera-overlay/camera-overlay.page.ts
+++ src/app/fyle/camera-overlay/camera-overlay.page.ts
@@ -61,7 +61,7 @@ export class CameraOverlayPage implements OnInit {
         y: 0,
         toBack: true,
         width: window.screen.width,
-        height: window.innerHeight,
+        height: window.innerHeight - 120,
         parent: 'cameraPreview'
       };
 
