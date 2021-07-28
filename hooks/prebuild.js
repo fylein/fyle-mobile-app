121ce822a03149b911e3bf4312e521b392b50322
iravisankar <86199056+iravisankar@users.noreply.github.com>
2021-07-28 16:26:20 +0530

    Android app id changed

--- hooks/prebuild.js
+++ hooks/prebuild.js
@@ -4,7 +4,8 @@ dotenv.config();
 const secrets = require('./secrets');
 
 module.exports = function (ctx) {
-  
+  console.log(ctx);
+  console.log(secrets.prodEnviroinent);
     // Creating environment.prod.ts file
     fs.writeFileSync(`${ctx.project.dir}`+ "/src/environments/environment.prod.ts", secrets.prodEnviroinent);
 
