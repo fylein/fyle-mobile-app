70f696d18e79cb2046b871b14ed71cd012d8bdac
iravisankar <86199056+iravisankar@users.noreply.github.com>
2021-07-28 22:37:11 +0530

    Hooks updated

--- hooks/secrets.js
+++ hooks/secrets.js
@@ -15,7 +15,7 @@ module.exports = {
                     "client_info": {
                     "mobilesdk_app_id": "` + process.env.FYLE_MOBILE_MOBILESDK_APP_ID + `",
                     "android_client_info": {
-                        "package_name": "com.ionicframework.fyle595781"
+                        "package_name": "` + process.env.base.bundle_id + `"
                     }
                     },
                     "oauth_client": [
@@ -40,7 +40,7 @@ module.exports = {
                             "client_id": "` + process.env.FYLE_MOBILE_CLIENT_ID_2 + `",
                             "client_type": 2,
                             "ios_info": {
-                            "bundle_id": "com.ionicframework.fyle595781"
+                            "bundle_id": "` + process.env.base.bundle_id + `"
                             }
                         }
                         ]
