f7bedd228f1d367d032d3515fff574aa09519215
iravisankar <86199056+iravisankar@users.noreply.github.com>
2021-07-29 01:59:22 +0530

    Hooks updated

--- hooks/prebuild.js
+++ hooks/prebuild.js
@@ -11,4 +11,18 @@ module.exports = function (ctx) {
     // Creating environment.prod.ts file
     fs.writeFileSync(`${ctx.project.dir}`+ "/src/environments/environment.prod.ts", secrets.prodEnviroinent);
 
+    if (ctx.build.platform === 'android') {
+        const platformRoot = path.join(ctx.project.dir, 'android');
+        FILE_PATHS = {
+          "android.cameraPreview": `${ctx.project.dir}`+ "/node_modules/@capacitor-community/camera-preview/android/src/main/java/com/ahm/capacitor/camera/preview/CameraPreview.java"
+        };
+        // Commenting Manifest.permission.RECORD_AUDIO on CameraPreview.java 
+        console.log("Commenting Manifest.permission.RECORD_AUDIO on CameraPreview.java ...");
+        var cameraPreviewPath = path.resolve(process.cwd(), FILE_PATHS["android.cameraPreview"]);
+        var cameraPreviewContents = fs.readFileSync(cameraPreviewPath).toString();
+        fs.writeFileSync(cameraPreviewPath, cameraPreviewContents.replace(/Manifest.permission.RECORD_AUDIO/g, '// Manifest.permission.RECORD_AUDIO ,'), 'utf8');
+    
+        // Creating google-services.json file
+        fs.writeFileSync(platformRoot + "/app/google-services.json", secrets.googleCredentials);
+      }
 };
