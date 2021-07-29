3c46f0a3fec69cfedebcad23c138f05cadab5811
iravisankar <86199056+iravisankar@users.noreply.github.com>
2021-07-29 11:25:47 +0530

    Actions merged

--- hooks/prebuild.js
+++ hooks/prebuild.js
@@ -7,10 +7,7 @@ dotenv.config();
 const secrets = require('./secrets');
 
 module.exports = function (ctx) {
-  console.log(process.env);
-  console.log('---------------------------');
-  console.log(ctx);
-  console.log(secrets.prodEnviroinent);
+
     // Creating environment.prod.ts file
     fs.writeFileSync(`${ctx.project.dir}`+ "/src/environments/environment.prod.ts", secrets.prodEnviroinent);
 
