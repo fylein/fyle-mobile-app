75e7388258c209c39054ff0118c7369893450ddb
iravisankar <86199056+iravisankar@users.noreply.github.com>
2021-08-18 18:12:21 +0530

    Sentry support added

--- sentry.sh
+++ sentry.sh
@@ -1,1 +1,4 @@
-echo "teting"
+VERSION=`sentry-cli releases propose-version`
+ionic capacitor sync --prod --source-map
+sentry-cli releases --org fyle-technologies-private-limi -p new-mobile-app-prod files "$VERSION" upload-sourcemaps www
+npx cap sync
