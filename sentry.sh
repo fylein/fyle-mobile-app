afa76f748ee9c08dfe45df95e72bff1e089337d9
iravisankar <86199056+iravisankar@users.noreply.github.com>
2021-08-19 11:22:36 +0530

    Minor

--- sentry.sh
+++ sentry.sh
@@ -1,4 +1,5 @@
 VERSION=`sentry-cli releases propose-version`
+echo $VERSION
 ionic capacitor sync --prod --source-map
 sentry-cli releases --org fyle-technologies-private-limi -p new-mobile-app-prod files "$VERSION" upload-sourcemaps www
 npx cap sync
