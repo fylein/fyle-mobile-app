0e84b72c8186966f157d67b2f57abda600d036b3
iravisankar <86199056+iravisankar@users.noreply.github.com>
2021-08-20 10:05:52 +0530

    Flow modified

--- build_appflow.sh
+++ build_appflow.sh
@@ -1,3 +1,3 @@
 VERSION=`sentry-cli releases propose-version`
-ionic capacitor build --prod --source-map
+ionic capacitor sync --prod --source-map
 sentry-cli releases --org fyle-technologies-private-limi -p new-mobile-app-prod files "$VERSION" upload-sourcemaps www
