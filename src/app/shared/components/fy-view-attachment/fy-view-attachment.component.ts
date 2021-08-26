29f2b002a4561f0f3aaa45ea667f56a4bca1834e
Yash <yashsolanki1709@gmail.com>
2021-08-26 22:49:12 +0530

    Update delete receipt popover design

--- src/app/shared/components/fy-view-attachment/fy-view-attachment.component.ts
+++ src/app/shared/components/fy-view-attachment/fy-view-attachment.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, PopoverController } from '@ionic/angular';
 import { DomSanitizer } from '@angular/platform-browser';
-import { PopupService } from 'src/app/core/services/popup.service';
 import { LoaderService } from 'src/app/core/services/loader.service';
 import { FileService } from 'src/app/core/services/file.service';
 import { from, of } from 'rxjs';
 import { switchMap, finalize } from 'rxjs/operators';
+import { PopupAlertComponentComponent } from 'src/app/shared/components/popup-alert-component/popup-alert-component.component';
 
 @Component({
   selector: 'app-fy-view-attachment',
@@ -13,7 +13,6 @@ import { switchMap, finalize } from 'rxjs/operators';
   styleUrls: ['./fy-view-attachment.component.scss'],
 })
 export class FyViewAttachmentComponent implements OnInit {
-
 
   @Input() attachments: any[];
 
@@ -30,8 +29,8 @@ export class FyViewAttachmentComponent implements OnInit {
   // max params shouldnt effect constructors
   constructor(
     private modalController: ModalController,
+    private popoverController: PopoverController,
     private sanitizer: DomSanitizer,
-    private popupService: PopupService,
     private loaderService: LoaderService,
     private fileService: FileService
   ) { }
@@ -77,37 +76,51 @@ export class FyViewAttachmentComponent implements OnInit {
 
   async deleteAttachment() {
     const activeIndex = await this.imageSlides.getActiveIndex();
-    const popupResult = await this.popupService.showPopup({
-      header: 'Confirm',
-      message: 'Are you sure you want to delete this attachment?',
-      primaryCta: {
-        text: 'DELETE'
-      }
+    const deletePopover = await this.popoverController.create({
+      component: PopupAlertComponentComponent,
+      componentProps: {
+        title: 'Remove Receipt',
+        message: 'Are you sure you want to remove this receipt?',
+        primaryCta: {
+          text: 'Remove',
+          action: 'remove',
+          type: 'alert'
+        },
+        secondaryCta: {
+          text: 'Cancel',
+          action: 'cancel'
+        }
+      },
+      cssClass: 'pop-up-in-center'
     });
 
-    if (popupResult === 'primary') {
-      from(this.loaderService.showLoader()).pipe(
-        switchMap(() => {
-          if (this.attachments[activeIndex].id) {
-            return this.fileService.delete(this.attachments[activeIndex].id);
+    await deletePopover.present();
+    const { data } = await deletePopover.onWillDismiss();
+
+    if(data && data.action) {
+      if (data.action === 'remove') {
+        from(this.loaderService.showLoader()).pipe(
+          switchMap(() => {
+            if (this.attachments[activeIndex].id) {
+              return this.fileService.delete(this.attachments[activeIndex].id);
+            } else {
+              return of(null);
+            }
+          }),
+          finalize(() => from(this.loaderService.hideLoader()))
+        ).subscribe(() => {
+          this.attachments.splice(activeIndex, 1);
+          if (this.attachments.length === 0) {
+            this.modalController.dismiss({ attachments: this.attachments });
           } else {
-            return of(null);
+            if (activeIndex > 0) {
+              this.goToPrevSlide();
+            } else {
+              this.goToNextSlide();
+            }
           }
-        }),
-        finalize(() => from(this.loaderService.hideLoader()))
-      ).subscribe(() => {
-        this.attachments.splice(activeIndex, 1);
-        if (this.attachments.length === 0) {
-          this.modalController.dismiss({ attachments: this.attachments });
-        } else {
-          if (activeIndex > 0) {
-            this.goToPrevSlide();
-          } else {
-            this.goToNextSlide();
-          }
-        }
-      });
+        });
+      }
     }
   }
-
 }
