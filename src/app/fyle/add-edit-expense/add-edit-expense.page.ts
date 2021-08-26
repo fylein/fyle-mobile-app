ecbd0a4178eb461482fdc8e50e352823f620cfca
Julias0 <aiyushdhar85@gmail.com>
2021-08-26 12:42:26 +0530

    Data gather2 (#557)
    * Added changes to check for add to report usage
    
    * Added trackers wherever missed
    
    * Added trackers wherever missed

--- src/app/fyle/add-edit-expense/add-edit-expense.page.ts
+++ src/app/fyle/add-edit-expense/add-edit-expense.page.ts
@@ -3565,4 +3565,22 @@ export class AddEditExpensePage implements OnInit {
       this.trackingService.viewComment({ Asset: 'Mobile' });
     }
   }
+
+  hideFields() {
+    this.trackingService.hideMoreClicked({
+      Asset: 'Mobile',
+      source: 'Add Edit Expenses page',
+    });
+
+    this.isExpandedView = false;
+  }
+
+  showFields() {
+    this.trackingService.showMoreClicked({
+      Asset: 'Mobile',
+      source: 'Add Edit Expenses page',
+    });
+
+    this.isExpandedView = true;
+  }
 }
