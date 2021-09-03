e5b7a7523f962890eee804746e4273fd8925d049
Pradeep Gupta <pradeep.gupta@fyle.in>
2021-09-03 18:21:14 +0530

    refiner implementation - lint issue fixed

--- src/app/core/services/nps.service.ts
+++ src/app/core/services/nps.service.ts
@@ -32,39 +32,17 @@ export class NpsService {
     });
   }
 
-  private async initiateRefiner() {
-    const that = this;
-    const eou = await that.authService.getEou();
-    let device = '';
-
-    const info = await Device.getInfo();
-
-    if (info.operatingSystem === 'ios') {
-      device = 'IOS';
-    } else if (info.operatingSystem === 'android') {
-      device = 'ANDROID';
-    }
-
-    _refiner('setProject', '56e693b0-0c7f-11ec-a9f2-b308f8f57190');  
-
-    _refiner('identifyUser', {
-        id: eou.ou.id, // Replace with your user ID
-        email: eou.us.email, // Replace with user Email
-        name: eou.us.full_name, // Replace with user name
-        device 
-    });
-  }
-
   async getDefaultProperties() {
     const that = this;
-    let properties = {};
     const eou = await that.authService.getEou();
+    const properties = {
+      'Admin': eou && eou.ou && eou.ou.roles && eou.ou.roles.indexOf('ADMIN') > -1 ? 'T' : 'F',
+      'Lite': 'F'
+    };
     properties['India / International'] = 'International Americas';
     properties['Delighted Email Subject'] = '';
     properties['Company ID'] = eou.ou.org_id;
     properties['Company Name'] = eou.ou.org_name;
-    properties['Admin'] = eou && eou.ou && eou.ou.roles && eou.ou.roles.indexOf('ADMIN') > -1 ? 'T' : 'F';
-    properties['Lite'] = 'F';
 
     return properties;
   }
@@ -77,11 +55,11 @@ export class NpsService {
     const that = this;
     const eou$ = from(that.authService.getEou());
     forkJoin({
-      eou: eou$, 
-      homeCurrency: this.offlineService.getHomeCurrency()
+      eou: eou$,
+      homeCurrency: this.offlineService.getHomeCurrency();
     }).subscribe((res) => {
       const eou = res.eou;
-      const homeCurrency = res.homeCurrency
+      const homeCurrency = res.homeCurrency;
       if (this.canStartSurvey()) {
         let refinerProperties = this.getDefaultProperties();
         if (properties) {
@@ -97,5 +75,28 @@ export class NpsService {
         return _refiner('showForm', '56ed8380-0c7f-11ec-89c5-effca223424b');
       }
     })
+  }
+
+  private async initiateRefiner() {
+    const that = this;
+    const eou = await that.authService.getEou();
+    let device = '';
+
+    const info = await Device.getInfo();
+
+    if (info.operatingSystem === 'ios') {
+      device = 'IOS';
+    } else if (info.operatingSystem === 'android') {
+      device = 'ANDROID';
+    }
+
+    _refiner('setProject', '56e693b0-0c7f-11ec-a9f2-b308f8f57190');
+
+    _refiner('identifyUser', {
+        id: eou.ou.id, // Replace with your user ID
+        email: eou.us.email, // Replace with user Email
+        name: eou.us.full_name, // Replace with user name
+        device
+    });
   }
 }
