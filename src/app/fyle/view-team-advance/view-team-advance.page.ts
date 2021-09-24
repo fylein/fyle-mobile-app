52fc96af133cfe07305c2d61d9db5cc4114c8366
Ayush Seth <littlebitbot@gmail.com>
2021-09-25 00:32:06 +0530

    moved the comment option to header in team advance

--- src/app/fyle/view-team-advance/view-team-advance.page.ts
+++ src/app/fyle/view-team-advance/view-team-advance.page.ts
@@ -10,7 +10,7 @@ import { FileService } from 'src/app/core/services/file.service';
 import { from, Subject, forkJoin } from 'rxjs';
 import { switchMap, finalize, shareReplay, concatMap, map, reduce, startWith, take, tap } from 'rxjs/operators';
 import { PopupService } from 'src/app/core/services/popup.service';
-import { PopoverController } from '@ionic/angular';
+import { PopoverController, ModalController } from '@ionic/angular';
 import { AdvanceActionsComponent } from './advance-actions/advance-actions.component';
 import { ApproveAdvanceComponent } from './approve-advance/approve-advance.component';
 import { SendBackAdvanceComponent } from './send-back-advance/send-back-advance.component';
@@ -18,6 +18,9 @@ import { RejectAdvanceComponent } from './reject-advance/reject-advance.componen
 import { LoaderService } from 'src/app/core/services/loader.service';
 import { AdvanceRequestsCustomFieldsService } from 'src/app/core/services/advance-requests-custom-fields.service';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { ViewCommentComponent } from 'src/app/shared/components/comments-history/view-comment/view-comment.component';
+import { TrackingService } from '../../core/services/tracking.service';
+import { ModalPropertiesService } from 'src/app/core/services/modal-properties.service';
 
 @Component({
   selector: 'app-view-team-advance',
@@ -43,6 +46,8 @@ export class ViewTeamAdvancePage implements OnInit {
 
   customFields$: Observable<any>;
 
+  isDeviceWidthSmall = window.innerWidth < 375;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private advanceRequestService: AdvanceRequestService,
@@ -52,7 +57,10 @@ export class ViewTeamAdvancePage implements OnInit {
     private popoverController: PopoverController,
     private loaderService: LoaderService,
     private advanceRequestsCustomFieldsService: AdvanceRequestsCustomFieldsService,
-    private authService: AuthService
+    private authService: AuthService,
+    private modalController: ModalController,
+    private modalProperties: ModalPropertiesService,
+    private trackingService: TrackingService
   ) {}
 
   ionViewWillEnter() {
@@ -249,6 +257,29 @@ export class ViewTeamAdvancePage implements OnInit {
 
     if (data && data.goBack) {
       this.router.navigate(['/', 'enterprise', 'team_advance']);
+    }
+  }
+
+  async openCommentsModal() {
+    const advanceRequestId = this.activatedRoute.snapshot.params.id;
+    const modal = await this.modalController.create({
+      component: ViewCommentComponent,
+      componentProps: {
+        objectType: 'advance_requests',
+        objectId: advanceRequestId,
+      },
+      presentingElement: await this.modalController.getTop(),
+      ...this.modalProperties.getModalDefaultProperties(),
+    });
+
+    await modal.present();
+    const { data } = await modal.onDidDismiss();
+
+    if (data && data.updated) {
+      this.trackingService.addComment();
+    } else {
+      console.log('hi');
+      this.trackingService.viewComment();
     }
   }
 
