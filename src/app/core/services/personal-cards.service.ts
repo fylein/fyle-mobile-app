f3680e91a4cb255ddf1b6bdab213b2a3e4919029
iravisankar <86199056+iravisankar@users.noreply.github.com>
2021-10-27 09:49:39 +0530

    Minor

--- src/app/core/services/personal-cards.service.ts
+++ src/app/core/services/personal-cards.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { map, shareReplay, take } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { PersonalCard } from '../models/personal_card.model';
 import { ApiV2Service } from './api-v2.service';
 import { ApiService } from './api.service';
 import { ExpenseAggregationService } from './expense-aggregation.service';
-import { Cacheable, CacheBuster } from 'ts-cacheable';
 import { ISODateString } from '@capacitor/core';
 import { Expense } from '../models/expense.model';
 
 const tripRequestsCacheBuster$ = new Subject<void>();
+
+type matchExpenseResponse = Partial<{
+  external_expense_id: string;
+  id: string;
+  transaction_split_group_id: string;
+}>;
 @Injectable({
   providedIn: 'root',
 })
@@ -65,19 +70,14 @@ export class PersonalCardsService {
   }
 
   getExpenseDetails(transactionSplitGroupId: string): Observable<Expense> {
-    return this.apiv2Service
-      .get('/expenses', {
-        params: {
-          tx_split_group_id: `eq.${transactionSplitGroupId}`,
-        },
-      })
-      .pipe(map((res) => res.data[0]));
+    return this.apiv2Service.get('/expenses', {
+      params: {
+        tx_split_group_id: `eq.${transactionSplitGroupId}`,
+      },
+    });
   }
 
-  matchExpense(
-    transactionSplitGroupId: string,
-    externalExpenseId: string
-  ): Observable<Partial<{ external_expense_id: string; id: string; transaction_split_group_id: string }>> {
+  matchExpense(transactionSplitGroupId: string, externalExpenseId: string): Observable<matchExpenseResponse> {
     return this.apiService.post('/transactions/external_expense/match', {
       transaction_split_group_id: transactionSplitGroupId,
       external_expense_id: externalExpenseId,
