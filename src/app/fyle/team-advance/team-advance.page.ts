35fa471a19d144d11007a6d89816835f329d714e
Ayush Seth <littlebitbot@gmail.com>
2021-11-17 23:04:57 +0530

    some more cleanup; implementation of filtering and filter pill functions

--- src/app/fyle/team-advance/team-advance.page.ts
+++ src/app/fyle/team-advance/team-advance.page.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+
 import { Observable, Subject, from, noop } from 'rxjs';
+import { concatMap, switchMap, finalize, map, scan, shareReplay, take } from 'rxjs/operators';
+
 import { AdvanceRequestService } from 'src/app/core/services/advance-request.service';
 import { LoaderService } from 'src/app/core/services/loader.service';
-import { concatMap, switchMap, finalize, map, scan, shareReplay, tap, take } from 'rxjs/operators';
-import { ExtendedAdvanceRequest } from 'src/app/core/models/extended_advance_request.model';
-import { Router } from '@angular/router';
+import { FiltersHelperService } from 'src/app/core/services/filters-helper.service';
 
+import { FilterOptions } from 'src/app/shared/components/fy-filters/filter-options.interface';
+import { ExtendedAdvanceRequest } from 'src/app/core/models/extended_advance_request.model';
+import { AdvancesStates } from 'src/app/core/models/advances-states.model';
+import { FilterOptionType } from 'src/app/shared/components/fy-filters/filter-option-type.enum';
+import { FyFiltersComponent } from 'src/app/shared/components/fy-filters/fy-filters.component';
+
+type Filters = Partial<{
+  state: AdvancesStates[];
+  sortParam: string;
+  sortDir: string;
+}>;
 @Component({
   selector: 'app-team-advance',
   templateUrl: './team-advance.page.html',
   styleUrls: ['./team-advance.page.scss'],
 })
-export class TeamAdvancePage implements OnInit {
+export class TeamAdvancePage {
   teamAdvancerequests$: Observable<any[]>;
 
-  loadData$: Subject<{ pageNumber: number; state: string }> = new Subject();
+  loadData$: Subject<{ pageNumber: number; state: AdvancesStates[] }> = new Subject();
 
   count$: Observable<number>;
 
@@ -22,46 +36,39 @@ export class TeamAdvancePage implements OnInit {
 
   isInfiniteScrollRequired$: Observable<boolean>;
 
-  state = 'PENDING';
+  filters: Filters;
+
+  filterPills = [];
 
   constructor(
     private advanceRequestService: AdvanceRequestService,
     private loaderService: LoaderService,
-    private router: Router
+    private router: Router,
+    private filtersHelperService: FiltersHelperService,
+    private modalController: ModalController
   ) {}
 
-  ngOnInit() {}
-
   ionViewWillEnter() {
-    this.currentPageNumber = 1;
-    this.teamAdvancerequests$ = this.loadData$.pipe(
-      concatMap(({ pageNumber, state }) => {
-        const extraParams =
-          state === 'PENDING'
-            ? {
-                areq_state: ['eq.APPROVAL_PENDING'],
-                areq_trip_request_id: ['is.null'],
-                or: ['(areq_is_sent_back.is.null,areq_is_sent_back.is.false)'],
-              }
-            : {
-                areq_trip_request_id: ['is.null'],
-                areq_approval_state: ['ov.{APPROVAL_PENDING,APPROVAL_DONE}'],
-              };
+    this.setupDefaultFilters();
 
-        return from(this.loaderService.showLoader()).pipe(
+    this.currentPageNumber = 1;
+
+    this.teamAdvancerequests$ = this.loadData$.pipe(
+      concatMap(({ pageNumber, state }) =>
+        from(this.loaderService.showLoader()).pipe(
           switchMap(() =>
-            this.advanceRequestService.getTeamadvanceRequests({
+            this.advanceRequestService.getTeamAdvanceRequests({
               offset: (pageNumber - 1) * 10,
               limit: 10,
               queryParams: {
-                ...extraParams,
+                ...this.getExtraParams(state),
               },
               filter: state,
             })
           ),
           finalize(() => from(this.loaderService.hideLoader()))
-        );
-      }),
+        )
+      ),
       map((res) => res.data),
       scan((acc, curr) => {
         if (this.currentPageNumber === 1) {
@@ -73,26 +80,14 @@ export class TeamAdvancePage implements OnInit {
     );
 
     this.count$ = this.loadData$.pipe(
-      switchMap(({ state }) => {
-        const extraParams =
-          state === 'PENDING'
-            ? {
-                areq_state: ['eq.APPROVAL_PENDING'],
-                areq_trip_request_id: ['is.null'],
-                or: ['(areq_is_sent_back.is.null,areq_is_sent_back.is.false)'],
-              }
-            : {
-                areq_trip_request_id: ['is.null'],
-                areq_approval_state: ['ov.{APPROVAL_PENDING,APPROVAL_DONE}'],
-              };
-
-        return this.advanceRequestService.getTeamAdvanceRequestsCount(
+      switchMap(({ state }) =>
+        this.advanceRequestService.getTeamAdvanceRequestsCount(
           {
-            ...extraParams,
+            ...this.getExtraParams(state),
           },
           state
-        );
-      }),
+        )
+      ),
       shareReplay(1)
     );
 
@@ -109,28 +104,116 @@ export class TeamAdvancePage implements OnInit {
     this.teamAdvancerequests$.subscribe(noop);
     this.count$.subscribe(noop);
     this.isInfiniteScrollRequired$.subscribe(noop);
-    this.loadData$.next({ pageNumber: this.currentPageNumber, state: this.state });
-  }
-
-  loadData(event) {
-    this.currentPageNumber = this.currentPageNumber + 1;
-    this.loadData$.next({ pageNumber: this.currentPageNumber, state: this.state });
-    event.target.complete();
-  }
-
-  doRefresh(event) {
-    this.currentPageNumber = 1;
-    this.loadData$.next({ pageNumber: this.currentPageNumber, state: this.state });
-    event.target.complete();
+    this.loadData$.next({ pageNumber: this.currentPageNumber, state: this.filters.state || [] });
   }
 
   onAdvanceClick(areq: ExtendedAdvanceRequest) {
     this.router.navigate(['/', 'enterprise', 'view_team_advance', { id: areq.areq_id }]);
   }
 
-  changeState(state) {
-    this.currentPageNumber = 1;
-    this.state = state;
-    this.loadData$.next({ pageNumber: this.currentPageNumber, state: this.state });
+  changeState(event?: any, incrementPageNumber = false) {
+    this.currentPageNumber = incrementPageNumber ? this.currentPageNumber + 1 : 1;
+    this.loadData$.next({ pageNumber: this.currentPageNumber, state: this.filters.state || [] });
+    if (event) {
+      event.target.complete();
+    }
+  }
+
+  async openFilters(activeFilterInitialName?: string) {
+    const filterPopover = await this.modalController.create({
+      component: FyFiltersComponent,
+      componentProps: {
+        filterOptions: [
+          {
+            name: 'State',
+            optionType: FilterOptionType.multiselect,
+            options: [
+              {
+                label: 'Approval Pending',
+                value: AdvancesStates.pending,
+              },
+
+              {
+                label: 'Approved',
+                value: AdvancesStates.approved,
+              },
+            ],
+          } as FilterOptions<string>,
+        ],
+        selectedFilterValues: this.filtersHelperService.generateSelectedFilters(this.filters),
+        activeFilterInitialName,
+      },
+      cssClass: 'dialog-popover',
+    });
+
+    await filterPopover.present();
+
+    const { data } = await filterPopover.onWillDismiss();
+    if (data) {
+      this.filters = this.filtersHelperService.convertDataToFilters(data);
+      this.filterPills = this.filtersHelperService.generateFilterPills(this.filters);
+      this.changeState();
+    }
+  }
+
+  onFilterClose(filterType: string) {
+    if (filterType === 'sort') {
+      this.filters = {
+        ...this.filters,
+        sortParam: null,
+        sortDir: null,
+      };
+    } else if (filterType === 'state') {
+      this.filters = {
+        ...this.filters,
+        state: null,
+      };
+    }
+    this.filterPills = this.filtersHelperService.generateFilterPills(this.filters);
+    this.changeState();
+  }
+
+  async onFilterClick(filterType: string) {
+    const filterTypes = {
+      state: 'State',
+      sort: 'Sort By',
+    };
+    await this.openFilters(filterTypes[filterType]);
+  }
+
+  onFilterPillsClearAll() {
+    this.filters = {};
+    this.filterPills = this.filtersHelperService.generateFilterPills(this.filters);
+    this.changeState();
+  }
+
+  setupDefaultFilters() {
+    this.filters = {
+      state: [AdvancesStates.pending],
+    };
+    this.filterPills = this.filtersHelperService.generateFilterPills(this.filters);
+  }
+
+  getExtraParams(state: AdvancesStates[]) {
+    const isPending = state.includes(AdvancesStates.pending);
+    const isApproved = state.includes(AdvancesStates.approved);
+    let extraParams: any;
+
+    if ((isPending && isApproved) || (!isPending && !isApproved)) {
+      extraParams = { areq_trip_request_id: ['is.null'] };
+    } else if (isPending) {
+      extraParams = {
+        areq_state: ['eq.APPROVAL_PENDING'],
+        areq_trip_request_id: ['is.null'],
+        or: ['(areq_is_sent_back.is.null,areq_is_sent_back.is.false)'],
+      };
+    } else if (isApproved) {
+      extraParams = {
+        areq_trip_request_id: ['is.null'],
+        areq_approval_state: ['ov.{APPROVAL_PENDING,APPROVAL_DONE}'],
+      };
+    }
+
+    return extraParams;
   }
 }
