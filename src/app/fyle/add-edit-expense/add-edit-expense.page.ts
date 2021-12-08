5efb90a125031d4c7bd4f4f6961ed76a950d6f25
iravisankar <86199056+iravisankar@users.noreply.github.com>
2021-12-08 15:40:09 +0530

    minor

--- src/app/fyle/add-edit-expense/add-edit-expense.page.ts
+++ src/app/fyle/add-edit-expense/add-edit-expense.page.ts
@@ -2185,7 +2185,7 @@ export class AddEditExpensePage implements OnInit {
               }
 
               if (etxn.tx.extracted_data.date) {
-                etxn.tx.txn_dt = etxn.tx.extracted_data.date.slice(0, -1);
+                etxn.tx.txn_dt = this.dateService.getUTCDate(new Date(etxn.tx.extracted_data.date));
               }
 
               if (etxn.tx.extracted_data.invoice_dt) {
