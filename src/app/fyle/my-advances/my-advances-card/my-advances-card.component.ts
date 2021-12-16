2826cff637711bb3ec031bb9260fd048dd8987d3
siddharth0-0 <agrawalsid05@gmail.com>
2021-12-16 17:46:20 +0530

    New Advances Design

--- src/app/fyle/my-advances/my-advances-card/my-advances-card.component.ts
+++ src/app/fyle/my-advances/my-advances-card/my-advances-card.component.ts
@@ -1,3 +1,4 @@
+import { getCurrencySymbol } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ExtendedAdvanceRequest } from 'src/app/core/models/extended_advance_request.model';
 import { AdvanceRequestService } from 'src/app/core/services/advance-request.service';
@@ -10,14 +11,24 @@ import { AdvanceRequestService } from 'src/app/core/services/advance-request.ser
 export class MyAdvancesCardComponent implements OnInit {
   @Input() advanceRequest: any;
 
+  @Input() prevDate: Date;
+
   @Output() advanceClick: EventEmitter<{ advanceRequest: ExtendedAdvanceRequest; internalState: any }> =
     new EventEmitter();
 
   internalState: { name: string; state: string };
 
+  currencySymbol = '';
+
+  showDate = true;
+
   constructor(private advanceRequestService: AdvanceRequestService) {}
 
   ngOnInit() {
+    this.showDate =
+      (this.advanceRequest && new Date(this.advanceRequest.created_at).toDateString()) !==
+      (this.prevDate && new Date(this.prevDate).toDateString());
+    this.currencySymbol = getCurrencySymbol(this.advanceRequest.currency, 'wide');
     this.internalState =
       this.advanceRequest.type === 'request'
         ? this.advanceRequestService.getInternalStateAndDisplayName(this.advanceRequest)
--- src/app/fyle/team-advance/team-adv-card/team-adv-card.component.ts
+++ src/app/fyle/team-advance/team-adv-card/team-adv-card.component.ts
@@ -1,3 +1,4 @@
+import { getCurrencySymbol } from '@angular/common';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ExtendedAdvanceRequest } from 'src/app/core/models/extended_advance_request.model';
 import { AdvanceRequestService } from 'src/app/core/services/advance-request.service';
@@ -10,15 +11,26 @@ import { AdvanceRequestService } from 'src/app/core/services/advance-request.ser
 export class TeamAdvCardComponent implements OnInit {
   @Input() advanceRequest: ExtendedAdvanceRequest;
 
+  @Input() prevDate: Date;
+
   @Output() gotoAdvance: EventEmitter<ExtendedAdvanceRequest> = new EventEmitter();
 
   internalState: { name: string; state: string };
+
+  currencySymbol = '';
+
+  showDate = true;
 
   actionOpened = false;
 
   constructor(private advanceRequestService: AdvanceRequestService) {}
 
   ngOnInit() {
+    console.log('sam', this.advanceRequest);
+    this.showDate =
+      (this.advanceRequest && new Date(this.advanceRequest.areq_created_at).toDateString()) !==
+      (this.prevDate && new Date(this.prevDate).toDateString());
+    this.currencySymbol = getCurrencySymbol(this.advanceRequest.areq_currency, 'wide');
     this.internalState = this.advanceRequestService.getInternalStateAndDisplayName(this.advanceRequest);
   }
 
