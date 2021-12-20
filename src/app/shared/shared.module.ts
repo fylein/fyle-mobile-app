820a0eed09436e841df8883896e629bef9bef503
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2021-12-20 17:02:24 +0530

    FYLE-dzjwz3: View report page - Fyler- All changes (#692)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -12,7 +12,6 @@ import { HumanizeCurrencyPipe } from './pipes/humanize-currency.pipe';
 import { HighlightPipe } from './pipes/highlight.pipe';
 import { ReportState } from './pipes/report-state.pipe';
 import { SnakeCaseToSpaceCase } from './pipes/snake-case-to-space-case.pipe';
-import { ToLowerCase } from './pipes/to-lowercase.pipe';
 import { TripState } from './pipes/trip-state.pipe';
 import { FySelectComponent } from './components/fy-select/fy-select.component';
 import { FySelectModalComponent } from './components/fy-select/fy-select-modal/fy-select-modal.component';
@@ -117,7 +116,6 @@ import { PersonalCardTransactionComponent } from './components/personal-card-tra
     HumanizeCurrencyPipe,
     ReportState,
     SnakeCaseToSpaceCase,
-    ToLowerCase,
     TripState,
     DateFormatPipe,
     FySelectComponent,
@@ -234,7 +232,6 @@ import { PersonalCardTransactionComponent } from './components/personal-card-tra
     FyDuplicateDetectionComponent,
     AdvanceState,
     SnakeCaseToSpaceCase,
-    ToLowerCase,
     TripState,
     InitialsPipe,
     DelegatedAccMessageComponent,
