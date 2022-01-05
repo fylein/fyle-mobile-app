a7ca61edfa380d91e0bf6de62fe6624db8dd36e4
Yash Solanki <yashsolanki1709@gmail.com>
2022-01-05 16:44:22 +0530

    Add Receipt crop feature (#804)
    * image-cropper test
    
    * Remove rotate, improve crop interaction and code refactoring
    
    * Fix unit test run issue
    
    * Add loader, tracker and fix bg
    
    * Fix ios image alignment
    
    * Minor fix
    
    * Create separate modal for crop
    
    * Minor fix
    
    * Fix image center issue
    
    * Fix image alignment
    
    * Fix lower image not visible
    
    * Remove comments

---
+++ src/app/fyle/capture-receipt/crop-receipt/crop-receipt.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { CropReceiptComponent } from './crop-receipt.component';
+
+describe('CropReceiptComponent', () => {
+  let component: CropReceiptComponent;
+  let fixture: ComponentFixture<CropReceiptComponent>;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [CropReceiptComponent],
+        imports: [IonicModule.forRoot()],
+      }).compileComponents();
+
+      fixture = TestBed.createComponent(CropReceiptComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
