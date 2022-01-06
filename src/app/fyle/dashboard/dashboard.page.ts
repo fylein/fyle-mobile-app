d99af550a76ca14a04a96fdef6b0ec9d6bbf1553
Yash <yashsolanki1709@gmail.com>
2022-01-06 20:38:18 +0530

    Make modal wrapper transparent

--- src/app/fyle/dashboard/dashboard.page.ts
+++ src/app/fyle/dashboard/dashboard.page.ts
@@ -189,6 +189,7 @@ export class DashboardPage implements OnInit {
       componentProps: {
         isNewExpense: true,
       },
+      cssClass: 'hide-modal-test',
     });
 
     await captureReceiptModal.present();
