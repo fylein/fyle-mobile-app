037ec3abac33e6c871ed22e003b5baa4ef75bc23
Yash <yashsolanki1709@gmail.com>
2022-01-10 22:11:11 +0530

    Minor fix

--- src/app/shared/components/capture-receipt/receipt-preview/receipt-preview.component.ts
+++ src/app/shared/components/capture-receipt/receipt-preview/receipt-preview.component.ts
@@ -6,7 +6,7 @@ import { from } from 'rxjs';
 import { PopupAlertComponentComponent } from 'src/app/shared/components/popup-alert-component/popup-alert-component.component';
 import { AddMorePopupComponent } from '../add-more-popup/add-more-popup.component';
 import { TrackingService } from 'src/app/core/services/tracking.service';
-import { CropReceiptComponent } from 'src/app/fyle/capture-receipt/crop-receipt/crop-receipt.component';
+import { CropReceiptComponent } from '../crop-receipt/crop-receipt.component';
 
 type Image = Partial<{
   source: string;
