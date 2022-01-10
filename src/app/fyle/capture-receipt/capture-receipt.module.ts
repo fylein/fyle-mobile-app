b6e7f6a806620827e6f6d9ae4e4e92145371c577
Yash <yashsolanki1709@gmail.com>
2022-01-10 20:35:30 +0530

    Remove unnecessary code

--- src/app/fyle/capture-receipt/capture-receipt.module.ts
+++ src/app/fyle/capture-receipt/capture-receipt.module.ts
@@ -1,35 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ImageCropperModule } from 'ngx-image-cropper';
 
 import { IonicModule } from '@ionic/angular';
 
 import { CaptureReceiptPageRoutingModule } from './capture-receipt-routing.module';
 
 import { CaptureReceiptPage } from './capture-receipt.page';
-import { MatIconModule } from '@angular/material/icon';
-import { ReceiptPreviewComponent } from './receipt-preview/receipt-preview.component';
-import { PinchZoomModule } from 'ngx-pinch-zoom';
-import { ImagePicker } from '@ionic-native/image-picker/ngx';
-import { AddMorePopupComponent } from './add-more-popup/add-more-popup.component';
-import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
-import { CropReceiptComponent } from './crop-receipt/crop-receipt.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    CaptureReceiptPageRoutingModule,
-    MatIconModule,
-    PinchZoomModule,
-    MatBottomSheetModule,
-    ImageCropperModule,
-    SharedModule,
-  ],
-  declarations: [CaptureReceiptPage, ReceiptPreviewComponent, AddMorePopupComponent, CropReceiptComponent],
-  providers: [ImagePicker],
+  imports: [CommonModule, FormsModule, IonicModule, CaptureReceiptPageRoutingModule, SharedModule],
+  declarations: [CaptureReceiptPage],
 })
 export class CaptureReceiptPageModule {}
