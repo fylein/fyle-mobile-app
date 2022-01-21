8cf1e48034c30dc171bcbc48a08898e95736e085
siddharth0-0 <agrawalsid05@gmail.com>
2022-01-21 16:09:12 +0530

    fixx

--- src/app/shared/components/fy-popover/fy-popover.component.ts
+++ src/app/shared/components/fy-popover/fy-popover.component.ts
@@ -21,7 +21,7 @@ export class FyPopoverComponent implements AfterViewInit {
     const formInput = this.simpleFormInput.nativeElement as HTMLInputElement;
     setTimeout(() => {
       formInput.focus();
-    }, 200);
+    }, 400);
   }
 
   dismiss() {
