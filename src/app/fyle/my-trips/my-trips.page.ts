e805c17ff79d742c4020b2bdfd56d9c6113e56ab
Yash Solanki <yashsolanki1709@gmail.com>
2022-02-07 19:14:05 +0530

    Disable New trip request cta and update deprecation msg (#878)
    * Disable New trip request cta and update deprecation msg
    
    * Minor fix

--- src/app/fyle/my-trips/my-trips.page.ts
+++ src/app/fyle/my-trips/my-trips.page.ts
@@ -29,6 +29,8 @@ export class MyTripsPage implements OnInit {
 
   deprecationMsg$: Observable<string>;
 
+  isCreateTripRequestDisabled = true;
+
   constructor(
     private tripRequestsService: TripRequestsService,
     private loaderService: LoaderService,
@@ -41,6 +43,10 @@ export class MyTripsPage implements OnInit {
     this.navigateBack = !!this.activatedRoute.snapshot.params.navigateBack;
 
     this.deprecationMsg$ = this.tripRequestsService.getTripDeprecationMsg('individual');
+
+    this.tripRequestsService
+      .doesOrgHaveExtension()
+      .subscribe((doesOrgHaveExtension) => (this.isCreateTripRequestDisabled = !doesOrgHaveExtension));
 
     this.currentPageNumber = 1;
 
