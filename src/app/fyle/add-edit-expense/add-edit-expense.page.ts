52377af55c551f8d5dfbeffed0967f70c9cb9311
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2022-02-09 11:07:45 +0530

    FYLE-1z55me7: Prompt report submission mobile app changes (#829)

--- src/app/fyle/add-edit-expense/add-edit-expense.page.ts
+++ src/app/fyle/add-edit-expense/add-edit-expense.page.ts
@@ -311,6 +311,10 @@ export class AddEditExpensePage implements OnInit {
 
   canDeleteExpense = true;
 
+  isReportMandatory = false;
+
+  saveWithCriticalPolicyViolation = false;
+
   policyDetails;
 
   source = 'MOBILE';
@@ -2506,6 +2510,15 @@ export class AddEditExpensePage implements OnInit {
 
     this.isExpandedView = this.mode !== 'add';
 
+    if (this.mode === 'add') {
+      this.isConnected$.pipe(take(1)).subscribe((isConnected) => {
+        if (isConnected) {
+          this.fg.controls.report.setValidators(Validators.required);
+          this.isReportMandatory = true;
+        }
+      });
+    }
+
     this.activeIndex = parseInt(this.activatedRoute.snapshot.params.activeIndex, 10);
     this.reviewList =
       this.activatedRoute.snapshot.params.txnIds && JSON.parse(this.activatedRoute.snapshot.params.txnIds);
@@ -2912,7 +2925,7 @@ export class AddEditExpensePage implements OnInit {
 
   async reloadCurrentRoute() {
     await this.router.navigateByUrl('/enterprise/my_expenses', { skipLocationChange: true });
-    await this.router.navigate(['/', 'enterprise', 'add_edit_expense']);
+    await this.router.navigate(['/', 'enterprise', 'add_edit_expense', { rp_id: this.fg.controls.report.value.rp.id }]);
   }
 
   addToNewReport(txnId: string) {
@@ -2961,9 +2974,19 @@ export class AddEditExpensePage implements OnInit {
               that.saveAndMatchWithPersonalCardTxn();
             } else {
               that.addExpense('SAVE_EXPENSE').subscribe((res: any) => {
-                if (that.fg.controls.add_to_new_report.value && res && res.transaction) {
+                if (
+                  !this.saveWithCriticalPolicyViolation &&
+                  that.fg.controls.add_to_new_report.value &&
+                  res &&
+                  res.transaction
+                ) {
                   this.addToNewReport(res.transaction.id);
-                } else if (that.fg.value.report && that.fg.value.report.rp && that.fg.value.report.rp.id) {
+                } else if (
+                  !this.saveWithCriticalPolicyViolation &&
+                  that.fg.value.report &&
+                  that.fg.value.report.rp &&
+                  that.fg.value.report.rp.id
+                ) {
                   that.goBack();
                   this.showAddToReportSuccessToast(that.fg.value.report.rp.id);
                 } else {
@@ -2974,9 +2997,14 @@ export class AddEditExpensePage implements OnInit {
           } else {
             // to do edit
             that.editExpense('SAVE_EXPENSE').subscribe((res) => {
-              if (that.fg.controls.add_to_new_report.value && res && res.id) {
+              if (!this.saveWithCriticalPolicyViolation && that.fg.controls.add_to_new_report.value && res && res.id) {
                 this.addToNewReport(res.id);
-              } else if (that.fg.value.report && that.fg.value.report.rp && that.fg.value.report.rp.id) {
+              } else if (
+                !this.saveWithCriticalPolicyViolation &&
+                that.fg.value.report &&
+                that.fg.value.report.rp &&
+                that.fg.value.report.rp.id
+              ) {
                 that.goBack();
                 this.showAddToReportSuccessToast(that.fg.value.report.rp.id);
               } else {
@@ -3126,6 +3154,9 @@ export class AddEditExpensePage implements OnInit {
     await fyCriticalPolicyViolationPopOver.present();
 
     const { data } = await fyCriticalPolicyViolationPopOver.onWillDismiss();
+    if (data) {
+      this.saveWithCriticalPolicyViolation = true;
+    }
     return !!data;
   }
 
@@ -3297,7 +3328,7 @@ export class AddEditExpensePage implements OnInit {
               switchMap((tx) => {
                 const selectedReportId = this.fg.value.report && this.fg.value.report.rp && this.fg.value.report.rp.id;
                 const criticalPolicyViolated = isNumber(etxn.tx_policy_amount) && etxn.tx_policy_amount < 0.0001;
-                if (!criticalPolicyViolated) {
+                if (!this.saveWithCriticalPolicyViolation && !criticalPolicyViolated) {
                   if (!txnCopy.tx.report_id && selectedReportId) {
                     return this.reportService.addTransactions(selectedReportId, [tx.id]).pipe(
                       tap(() => this.trackingService.addToExistingReportAddEditExpense()),
@@ -3551,12 +3582,15 @@ export class AddEditExpensePage implements OnInit {
             }
 
             let reportId;
-            if (
-              this.fg.value.report &&
-              (etxn.tx.policy_amount === null || (etxn.tx.policy_amount && !(etxn.tx.policy_amount < 0.0001)))
-            ) {
-              reportId = this.fg.value.report.rp.id;
+            if (!this.saveWithCriticalPolicyViolation) {
+              if (
+                this.fg.value.report &&
+                (etxn.tx.policy_amount === null || (etxn.tx.policy_amount && !(etxn.tx.policy_amount < 0.0001)))
+              ) {
+                reportId = this.fg.value.report.rp.id;
+              }
             }
+
             let entry;
             if (this.fg.value.add_to_new_report) {
               entry = {
