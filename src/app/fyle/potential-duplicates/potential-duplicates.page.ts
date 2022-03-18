69a83d207f0a968ff32fba15783de1c370c69328
iravisankar <86199056+iravisankar@users.noreply.github.com>
2022-03-18 15:34:46 +0530

    minor

--- src/app/fyle/potential-duplicates/potential-duplicates.page.ts
+++ src/app/fyle/potential-duplicates/potential-duplicates.page.ts
@@ -121,9 +121,18 @@ export class PotentialDuplicatesPage implements OnInit {
       tx_id: `in.(${selectedTxnIds.toString()})`,
     };
     this.transaction.getETxnc({ offset: 0, limit: 10, params }).subscribe((selectedExpenses) => {
-      this.router.navigate(['/', 'enterprise', 'merge_expense'], {
-        state: { selectedElements: selectedExpenses, from: 'TASK' },
-      });
+      // this.router.navigate(['/', 'enterprise', 'merge_expense'], {
+      //   state: { selectedElements: selectedExpenses, from: 'TASK' },
+      // });
+      this.router.navigate([
+        '/',
+        'enterprise',
+        'merge_expense',
+        {
+          selectedElements: JSON.stringify(this.selectedElements),
+          from: 'TASK',
+        },
+      ]);
     });
   }
 
