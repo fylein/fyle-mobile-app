af436816a5cee4d89c51001f6e36e02bacc0f418
Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
2022-04-02 10:29:56 +0530

    Added changes

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -2,7 +2,7 @@ describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/');
     cy.contains('Sign In');
-    cy.get('.sign-in--form-field').type('ajain@fyle.in');
+    cy.get('#sign-in--email').type('ajain@fyle.in');
     cy.get('#sign-in--continue').click();
     cy.get('#sign-in--password').type('KalaChashma');
     cy.get('#sign-in--btn-sign-in').click();
