70d56a22bcab08357acea1b5e6b7d0197e858c96
Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
2022-04-02 10:52:46 +0530

    Added changes

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -2,7 +2,8 @@ describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/');
     cy.contains('Sign In');
-    cy.get('#sign-in--email', { timeout: 100000 }).type('ajain@fyle.in');
+    cy.wait(10000);
+    cy.get('#sign-in--email').should('be.visible').type('ajain@fyle.in');
     cy.get('#sign-in--continue').click();
     cy.get('#sign-in--password').type('KalaChashma');
     cy.get('#sign-in--btn-sign-in').click();
