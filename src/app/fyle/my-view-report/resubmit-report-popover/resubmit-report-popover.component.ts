2008671a4901520ba89a2d46a43f125e53662b53
Omkar Joshi <65808188+OmkarJ13@users.noreply.github.com>
2022-04-14 12:02:57 +0530

    Fixed missing NPS survey triggers in Submit Report Flow. (#1034)
    Co-authored-by: Omkar Joshi <omkar@Omkars-MacBook-Air.local>

--- src/app/fyle/my-view-report/resubmit-report-popover/resubmit-report-popover.component.ts
+++ src/app/fyle/my-view-report/resubmit-report-popover/resubmit-report-popover.component.ts
@@ -7,6 +7,7 @@ import { TripRequestsService } from 'src/app/core/services/trip-requests.service
 import { TransactionService } from 'src/app/core/services/transaction.service';
 import { ReportService } from 'src/app/core/services/report.service';
 import { map } from 'rxjs/operators';
+import { RefinerService } from 'src/app/core/services/refiner.service';
 
 @Component({
   selector: 'app-resubmit-report-popover',
@@ -28,7 +29,8 @@ export class ResubmitReportPopoverComponent implements OnInit {
     private popoverController: PopoverController,
     private offlineService: OfflineService,
     private tripRequestService: TripRequestsService,
-    private reportService: ReportService
+    private reportService: ReportService,
+    private refinerService: RefinerService
   ) {}
 
   ngOnInit() {
@@ -105,6 +107,8 @@ export class ResubmitReportPopoverComponent implements OnInit {
       this.popoverController.dismiss({
         goBack: true,
       });
+
+      this.refinerService.startSurvey({ actionName: 'Resubmit Report ' });
     });
   }
 }
