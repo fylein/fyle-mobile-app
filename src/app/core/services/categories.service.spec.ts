647e680b7f84c891b10f9597e9c3ff1a73e1d16a
Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
2022-05-10 14:07:53 +0530

    Added unit tests for category service

--- src/app/core/services/categories.service.spec.ts
+++ src/app/core/services/categories.service.spec.ts
@@ -1,16 +1,422 @@
 import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs/internal/observable/of';
 import { CategoriesService } from './categories.service';
+import { SpenderPlatformApiService } from './spender-platform-api.service';
 
-xdescribe('CategoriesService', () => {
-  let service: CategoriesService;
+const categoryPlatformResponse = {
+  count: 4,
+  data: [
+    {
+      code: null,
+      created_at: '2020-05-26T09:41:48.997882+00:00',
+      display_name: 'Activity',
+      id: 110221,
+      is_enabled: true,
+      name: 'Activity',
+      org_id: 'orDjkSfq43i2',
+      restricted_project_ids: [],
+      sub_category: null,
+      system_category: 'Activity',
+      updated_at: '2022-05-05T17:47:34.313143+00:00',
+    },
+    {
+      code: null,
+      created_at: '2020-05-26T09:41:49.013496+00:00',
+      display_name: 'Train',
+      id: 110222,
+      is_enabled: true,
+      name: 'Train',
+      org_id: 'orDjkSfq43i2',
+      restricted_project_ids: [290502, 290501],
+      sub_category: null,
+      system_category: 'Train',
+      updated_at: '2022-05-05T17:47:34.313143+00:00',
+    },
+    {
+      code: null,
+      created_at: '2020-05-26T09:41:49.025697+00:00',
+      display_name: 'Fuel',
+      id: 110223,
+      is_enabled: true,
+      name: 'Fuel',
+      org_id: 'orDjkSfq43i2',
+      restricted_project_ids: [290501],
+      sub_category: null,
+      system_category: 'Fuel',
+      updated_at: '2022-05-05T17:47:34.313143+00:00',
+    },
+    {
+      code: null,
+      created_at: '2020-05-26T09:41:49.034454+00:00',
+      display_name: 'Snacks',
+      id: 110224,
+      is_enabled: true,
+      name: 'Snacks',
+      org_id: 'orDjkSfq43i2',
+      restricted_project_ids: [290501],
+      sub_category: null,
+      system_category: 'Snacks',
+      updated_at: '2022-05-05T17:47:34.313143+00:00',
+    },
+  ],
+  offset: 0,
+};
+
+const transformedCategories = [
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: null,
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.013496+00:00'),
+    displayName: 'Train',
+    id: 110222,
+    enabled: true,
+    name: 'Train',
+    org_id: 'orDjkSfq43i2',
+    sub_category: null,
+    fyle_category: 'Train',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.025697+00:00'),
+    displayName: 'Fuel',
+    id: 110223,
+    enabled: true,
+    name: 'Fuel',
+    org_id: 'orDjkSfq43i2',
+    sub_category: null,
+    fyle_category: 'Fuel',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.034454+00:00'),
+    displayName: 'Snacks',
+    id: 110224,
+    enabled: true,
+    name: 'Snacks',
+    org_id: 'orDjkSfq43i2',
+    sub_category: null,
+    fyle_category: 'Snacks',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+];
+
+const unsortedCategories1 = [
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'temp activity 2',
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'temp activity 1',
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'Activity',
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'temp activity 3',
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'temp activity 3',
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+];
+
+const sortedCategories1 = [
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'Activity',
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'temp activity 1',
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'temp activity 2',
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'temp activity 3',
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'temp activity 3',
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+];
+
+const categoriesForFilterTest = [
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:48.997882+00:00'),
+    displayName: 'Activity',
+    id: 110221,
+    enabled: true,
+    name: 'Activity',
+    org_id: 'orDjkSfq43i2',
+    sub_category: null,
+    fyle_category: 'Activity',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.013496+00:00'),
+    displayName: 'Train',
+    id: 110222,
+    enabled: true,
+    name: 'Train',
+    org_id: 'orDjkSfq43i2',
+    sub_category: null,
+    fyle_category: 'Train',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.013496+00:00'),
+    displayName: 'Polka',
+    id: 110222,
+    enabled: true,
+    name: 'Polka',
+    org_id: 'orDjkSfq43i2',
+    sub_category: null,
+    fyle_category: null,
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.025697+00:00'),
+    displayName: 'Fuel',
+    id: 110223,
+    enabled: true,
+    name: 'Fuel',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'Fuel',
+    fyle_category: 'Fuel',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.025697+00:00'),
+    displayName: 'Fuel / Fuel2',
+    id: 110223,
+    enabled: true,
+    name: 'Fuel',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'Fuel2',
+    fyle_category: 'Fuel',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.034454+00:00'),
+    displayName: 'Snacks',
+    id: 110224,
+    enabled: true,
+    name: 'Snacks',
+    org_id: 'orDjkSfq43i2',
+    sub_category: null,
+    fyle_category: 'Snacks',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+];
+
+const categoriesAfterFilterTest = [
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.013496+00:00'),
+    displayName: 'Train',
+    id: 110222,
+    enabled: true,
+    name: 'Train',
+    org_id: 'orDjkSfq43i2',
+    sub_category: null,
+    fyle_category: 'Train',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.013496+00:00'),
+    displayName: 'Polka',
+    id: 110222,
+    enabled: true,
+    name: 'Polka',
+    org_id: 'orDjkSfq43i2',
+    sub_category: null,
+    fyle_category: null,
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.025697+00:00'),
+    displayName: 'Fuel',
+    id: 110223,
+    enabled: true,
+    name: 'Fuel',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'Fuel',
+    fyle_category: 'Fuel',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.025697+00:00'),
+    displayName: 'Fuel / Fuel2',
+    id: 110223,
+    enabled: true,
+    name: 'Fuel',
+    org_id: 'orDjkSfq43i2',
+    sub_category: 'Fuel2',
+    fyle_category: 'Fuel',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+  {
+    code: null,
+    created_at: new Date('2020-05-26T09:41:49.034454+00:00'),
+    displayName: 'Snacks',
+    id: 110224,
+    enabled: true,
+    name: 'Snacks',
+    org_id: 'orDjkSfq43i2',
+    sub_category: null,
+    fyle_category: 'Snacks',
+    updated_at: new Date('2022-05-05T17:47:34.313143+00:00'),
+  },
+];
+
+describe('CategoriesService', () => {
+  let categoriesService: CategoriesService;
+  let spenderPlatformApiService: jasmine.SpyObj<SpenderPlatformApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CategoriesService);
+    const spenderPlatformApiServiceSpy = jasmine.createSpyObj('SpenderPlatformApiService', ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CategoriesService,
+        {
+          provide: SpenderPlatformApiService,
+          useValue: spenderPlatformApiServiceSpy,
+        },
+      ],
+    });
+    categoriesService = TestBed.inject(CategoriesService);
+    spenderPlatformApiService = TestBed.inject(SpenderPlatformApiService) as jasmine.SpyObj<SpenderPlatformApiService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(categoriesService).toBeTruthy();
+  });
+
+  it('should return proper response from api and transform it into proper model', (done) => {
+    spenderPlatformApiService.get.and.returnValue(of(categoryPlatformResponse));
+    const sortedCategories = categoriesService.sortCategories(transformedCategories);
+    categoriesService.getAll().subscribe((categories) => {
+      expect(categories).toEqual(sortedCategories);
+      done();
+    });
+  });
+
+  it('should be able to sort categories properly', () => {
+    const sortedCategories = categoriesService.sortCategories(unsortedCategories1);
+    expect(sortedCategories).toEqual(sortedCategories1);
+  });
+
+  it('should be able to filter categories for creation of expenses properly', () => {
+    const filteredCategories = categoriesService.filterRequired(categoriesForFilterTest);
+    expect(filteredCategories).toEqual(categoriesAfterFilterTest);
   });
 });
--- src/app/core/services/categories.service.ts
+++ src/app/core/services/categories.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Cacheable } from 'ts-cacheable';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { SpenderPlatformApiService } from './spender-platform-api.service';
 import { PlatformCategory } from '../models/platform/platform-category.model';
 import { PlatformCategoryData } from '../models/platform/platform-category-data.model';
@@ -13,18 +13,15 @@ const categoriesCacheBuster$ = new Subject<void>();
   providedIn: 'root',
 })
 export class CategoriesService {
-  skipCategories = ['activity', 'mileage', 'per diem', 'unspecified'];
-
   constructor(private spenderPlatformApiService: SpenderPlatformApiService) {}
 
   @Cacheable({
     cacheBusterObserver: categoriesCacheBuster$,
   })
-  getAll() {
-    return this.spenderPlatformApiService.get('/categories').pipe(
-      map((res: PlatformCategory) => this.transformFrom(res.data)),
-      map(this.sortCategories),
-      map(this.addDisplayName)
+  getAll(): Observable<OrgCategory[]> {
+    return this.spenderPlatformApiService.get<PlatformCategory>('/categories').pipe(
+      map((res) => this.transformFrom(res.data)),
+      map((categories) => this.sortCategories(categories))
     );
   }
 
@@ -32,7 +29,7 @@ export class CategoriesService {
     let oldCategory = [];
     oldCategory = platformCategory.map((category) => ({
       code: category.code,
-      created_at: category.created_at,
+      created_at: new Date(category.created_at),
       displayName: category.display_name,
       enabled: category.is_enabled,
       fyle_category: category.system_category,
@@ -40,13 +37,13 @@ export class CategoriesService {
       name: category.name,
       org_id: category.org_id,
       sub_category: category.sub_category,
-      updated_at: category.updated_at,
+      updated_at: new Date(category.updated_at),
     }));
 
     return oldCategory;
   }
 
-  sortCategories(categories) {
+  sortCategories(categories: OrgCategory[]): OrgCategory[] {
     return categories.sort((a, b) => {
       const category1 = a.name.toUpperCase();
       const category2 = b.name.toUpperCase();
@@ -67,10 +64,6 @@ export class CategoriesService {
         return -1;
       }
 
-      if (category2 === subCategory2) {
-        return 1;
-      }
-
       if (subCategory1 < subCategory2) {
         return -1;
       }
@@ -79,29 +72,17 @@ export class CategoriesService {
         return 1;
       }
 
-      return 0;
+      return 1;
     });
   }
 
-  addDisplayName(categories: any[]) {
-    return categories.map((category) => {
-      let displayName = category.name;
-
-      if (category.sub_category && category.sub_category.toLowerCase() !== displayName.toLowerCase()) {
-        displayName += ' / ' + category.sub_category;
-      }
-      category.displayName = displayName;
-      return category;
-    });
-  }
-
-  filterRequired(categoryList) {
+  filterRequired(categoryList: OrgCategory[]): OrgCategory[] {
     return categoryList.filter((category) => {
       if (!category.fyle_category) {
         return true;
       }
 
-      return this.skipCategories.indexOf(category.fyle_category.toLowerCase()) === -1;
+      return ['activity', 'mileage', 'per diem', 'unspecified'].indexOf(category.fyle_category.toLowerCase()) === -1;
     });
   }
 }
