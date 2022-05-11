c368e474fa54617db1bcbc5f351a1a431c41b1fa
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2022-05-11 17:22:27 +0530

    Revert "FYLE-29ptkye: Added platform costcenter models and moved costcenter service methods to platform (#1070)"
    This reverts commit 8f69e43808cba29855ee69dcbdcf8569f5a32af8.

--- src/app/core/models/platform/platform-cost-center.model.ts
+++
@@ -1,7 +0,0 @@
-import { PlatformCostCenterData } from '../../models/platform/platform-cost-center-data.model';
-
-export interface PlatformCostCenter {
-  count: number;
-  offset: number;
-  data: PlatformCostCenterData[];
-}
