9fa2ed9c35a8991f017bcbac8a64b2d04db9520f
Dimple <dimple.kh@fyle.in>
2022-05-18 15:00:58 +0530

    added txn model

--- src/app/core/models/undo-merge.model.ts
+++ src/app/core/models/undo-merge.model.ts
@@ -1,4 +1,5 @@
+import { Transaction } from '../models/v1/transaction.model';
 export interface UndoMerge {
-  user_created_expense: Object;
-  auto_created_expense: Object;
+  user_created_expense: Transaction;
+  auto_created_expense: Transaction;
 }
