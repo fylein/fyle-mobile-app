a061c2ced456e8097dc89c37f20d5b0fad0e329c
Dimple <dimple.kh@fyle.in>
2022-05-19 18:54:11 +0530

    added pagination for cost centers api

--- src/app/core/services/cost-centers.service.ts
+++ src/app/core/services/cost-centers.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Cacheable } from 'ts-cacheable';
-import { Subject } from 'rxjs';
+import { Observable, range, Subject } from 'rxjs';
 import { SpenderPlatformApiService } from './spender-platform-api.service';
 import { CostCenter } from '../models/v1/cost-center.model';
 import { PlatformCostCenterData } from '../models/platform/platform-cost-center-data.model';
-import { map } from 'rxjs/operators';
+import { concatMap, map, reduce, switchMap } from 'rxjs/operators';
 import { PlatformCostCenter } from '../models/platform/platform-cost-center.model';
 
 const costCentersCacheBuster$ = new Subject<void>();
@@ -15,13 +15,23 @@ const costCentersCacheBuster$ = new Subject<void>();
 export class CostCentersService {
   constructor(private spenderPlatformApiService: SpenderPlatformApiService) {}
 
-  @Cacheable({
-    cacheBusterObserver: costCentersCacheBuster$,
-  })
-  getAllActive() {
+  getActiveCostCentersCount(): Observable<number> {
     const data = {
       params: {
         is_enabled: 'eq.' + true,
+        offset: 0,
+        limit: 1,
+      },
+    };
+    return this.spenderPlatformApiService.get('/cost_centers', data).pipe(map((res: PlatformCostCenter) => res.count));
+  }
+
+  getCostCenters(config: { offset: number; limit: number }): Observable<CostCenter[]> {
+    const data = {
+      params: {
+        is_enabled: 'eq.' + true,
+        offset: config.offset,
+        limit: config.limit,
       },
     };
     return this.spenderPlatformApiService
@@ -43,5 +53,20 @@ export class CostCentersService {
     }));
 
     return oldCostCenter;
+  }
+
+  @Cacheable({
+    cacheBusterObserver: costCentersCacheBuster$,
+  })
+  getAllActive(): Observable<CostCenter[]> {
+    return this.getActiveCostCentersCount().pipe(
+      switchMap((count) => {
+        count = count > 50 ? count / 50 : 1;
+        return range(0, count);
+      }),
+      concatMap((page) => this.getCostCenters({ offset: 50 * page, limit: 50 })),
+      map((res) => res),
+      reduce((acc, curr) => acc.concat(curr), [] as any[])
+    );
   }
 }
