bf9f0f39bd81b7ce47e3624a85e677161d39f44c
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2022-05-31 19:26:47 +0530

    Revert "FYLE-2dmkmkj: Moved per diem service to platform and added per diem models (#1134)"
    This reverts commit 64ac4a0d5832ed67fa9fd5aa90a20bbd09c28f3c.

--- src/app/core/services/per-diem.service.ts
+++ src/app/core/services/per-diem.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
 import { Cacheable } from 'ts-cacheable';
-import { Observable, range, Subject } from 'rxjs';
-import { PlatformPerDiemRates } from '../models/platform/platform-per-diem-rates.model';
-import { PerDiemRates } from '../models/v1/per-diem-rates.model';
-import { SpenderPlatformApiService } from './spender-platform-api.service';
-import { PlatformApiResponse } from '../models/platform/platform-api-response.model';
-import { switchMap, concatMap, tap, map, reduce } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 const perDiemsCacheBuster$ = new Subject<void>();
 
@@ -13,73 +9,16 @@ const perDiemsCacheBuster$ = new Subject<void>();
   providedIn: 'root',
 })
 export class PerDiemService {
-  constructor(private spenderPlatformApiService: SpenderPlatformApiService) {}
+  constructor(private apiService: ApiService) {}
 
   @Cacheable({
     cacheBusterObserver: perDiemsCacheBuster$,
   })
-  getRates(): Observable<PerDiemRates[]> {
-    return this.getActivePerDiemRatesCount().pipe(
-      switchMap((count) => {
-        count = count > 50 ? count / 50 : 1;
-        return range(0, count);
-      }),
-      concatMap((page) => this.getPerDiemRates({ offset: 50 * page, limit: 50 })),
-      map((res) => res),
-      reduce((acc, curr) => acc.concat(curr), [] as PerDiemRates[])
-    );
+  getRates() {
+    return this.apiService.get('/per_diem_rates');
   }
 
-  getRate(id: number): Observable<PerDiemRates> {
-    const data = {
-      params: {
-        id: 'eq.' + id,
-      },
-    };
-    return this.spenderPlatformApiService.get<PlatformApiResponse<PlatformPerDiemRates>>('/per_diem_rates', data).pipe(
-      map((res) => this.transformFrom(res.data)),
-      map((res) => res[0])
-    );
-  }
-
-  getPerDiemRates(config: { offset: number; limit: number }): Observable<PerDiemRates[]> {
-    const data = {
-      params: {
-        is_enabled: 'eq.' + true,
-        offset: config.offset,
-        limit: config.limit,
-      },
-    };
-    return this.spenderPlatformApiService
-      .get<PlatformApiResponse<PlatformPerDiemRates>>('/per_diem_rates', data)
-      .pipe(map((res) => this.transformFrom(res.data)));
-  }
-
-  getActivePerDiemRatesCount(): Observable<number> {
-    const data = {
-      params: {
-        is_enabled: 'eq.' + true,
-        offset: 0,
-        limit: 1,
-      },
-    };
-    return this.spenderPlatformApiService
-      .get<PlatformApiResponse<PlatformPerDiemRates>>('/per_diem_rates', data)
-      .pipe(map((res) => res.count));
-  }
-
-  transformFrom(platformPerDiemRates: PlatformPerDiemRates[]): PerDiemRates[] {
-    const oldPerDiemRates = platformPerDiemRates.map((perDiemRate) => ({
-      active: perDiemRate.is_enabled,
-      created_at: perDiemRate.created_at,
-      currency: perDiemRate.currency,
-      id: perDiemRate.id,
-      name: perDiemRate.name,
-      org_id: perDiemRate.org_id,
-      rate: perDiemRate.rate,
-      updated_at: perDiemRate.updated_at,
-    }));
-
-    return oldPerDiemRates;
+  getRate(id: number) {
+    return this.apiService.get('/per_diem_rates/' + id);
   }
 }
