098b6429671304c71eb94d0434113ca7f4c083e7
Yash Solanki <yashsolankli1709@gmail.com>
2022-06-03 17:26:48 +0530

    Add prebuild hook and comment sentry and camera plugin changes

--- hooks/prebuild.js
+++ hooks/prebuild.js
@@ -10,20 +10,20 @@ module.exports = function (ctx) {
   // Creating environment.prod.ts file
   fs.writeFileSync(`${ctx.project.dir}` + "/src/environments/environment.prod.ts", secrets.prodEnviroinent);
 
-  const platformRoot = path.join(ctx.project.dir, 'android');
-  FILE_PATHS = {
-    "android.cameraPreview": `${ctx.project.dir}` + "/node_modules/@capacitor-community/camera-preview/android/src/main/java/com/ahm/capacitor/camera/preview/CameraPreview.java"
-  };
+  // const platformRoot = path.join(ctx.project.dir, 'android');
+  // FILE_PATHS = {
+  //   "android.cameraPreview": `${ctx.project.dir}` + "/node_modules/@capacitor-community/camera-preview/android/src/main/java/com/ahm/capacitor/camera/preview/CameraPreview.java"
+  // };
 
   // Adding GIT_COMMIT_SHA for sentry
-  var mainPath = path.resolve(process.cwd(), "src/main.ts");
-  var mainPathContent = fs.readFileSync(mainPath).toString();
-  fs.writeFileSync(mainPath, mainPathContent.replace(/please-replace-your-git-commit-version/g, process.env.CI_GIT_COMMIT_SHA), 'utf8');
+  // var mainPath = path.resolve(process.cwd(), "src/main.ts");
+  // var mainPathContent = fs.readFileSync(mainPath).toString();
+  // fs.writeFileSync(mainPath, mainPathContent.replace(/please-replace-your-git-commit-version/g, process.env.CI_GIT_COMMIT_SHA), 'utf8');
 
   // Commenting Manifest.permission.RECORD_AUDIO on CameraPreview.java 
-  var cameraPreviewPath = path.resolve(process.cwd(), FILE_PATHS["android.cameraPreview"]);
-  var cameraPreviewContents = fs.readFileSync(cameraPreviewPath).toString();
-  fs.writeFileSync(cameraPreviewPath, cameraPreviewContents.replace(/Manifest.permission.RECORD_AUDIO/g, '// Manifest.permission.RECORD_AUDIO ,'), 'utf8');
+  // var cameraPreviewPath = path.resolve(process.cwd(), FILE_PATHS["android.cameraPreview"]);
+  // var cameraPreviewContents = fs.readFileSync(cameraPreviewPath).toString();
+  // fs.writeFileSync(cameraPreviewPath, cameraPreviewContents.replace(/Manifest.permission.RECORD_AUDIO/g, '// Manifest.permission.RECORD_AUDIO ,'), 'utf8');
  
   // updating app version
   var buildGradlePath = path.resolve(process.cwd(),"android/app/build.gradle");
