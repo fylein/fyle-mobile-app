8b160652db5c904fd00dc352e1328a024c7f613f
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2022-06-07 18:20:42 +0530

    FYLE-29pu6en-2: Platform changes - Category service (#1154)

---
+++ src/app/core/models/platform/platform-category.model.ts
@@ -0,0 +1,13 @@
+export interface PlatformCategory {
+  id: number;
+  org_id: string;
+  created_at: Date;
+  updated_at: Date;
+  name: string;
+  sub_category: string;
+  is_enabled: boolean;
+  display_name: string;
+  system_category: string;
+  code: string;
+  restricted_project_ids?: number[];
+}
