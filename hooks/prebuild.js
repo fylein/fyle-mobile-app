54ca8f996becd0dc3825b2c6a32efd41443acdf5
yksolanki9 <yashsolankli1709@gmail.com>
2022-06-14 13:31:26 +0530

    Throw error from child function

--- hooks/prebuild.js
+++ hooks/prebuild.js
@@ -1,65 +1,59 @@
 const fs = require('fs');
-const util = require('util');
 const path = require('path');
-const stat = util.promisify(fs.stat);
 const dotenv = require('dotenv');
 dotenv.config();
 const secrets = require('./secrets');
 
 module.exports = function (ctx) {
-  try {
-    // Creating environment.prod.ts file
-    fs.writeFileSync(`${ctx.project.dir}` + "/src/environments/environment.prod.ts", secrets.prodEnvironment);
-  
-    // Adding LIVEUPDATE_CHANNEL in strings.xml
-    var androidStringsPath = path.resolve(process.cwd(),'android/app/src/main/res/values/strings.xml');
-    var androidStrings = fs.readFileSync(androidStringsPath).toString();
-    fs.writeFileSync(androidStringsPath, androidStrings.replace('LIVEUPDATE_CHANNEL', process.env.LIVEUPDATE_CHANNEL), 'utf8');
-  
-    if (!process.env.NATIVE_CONFIG) {
-      return;
-    }
+  // Creating environment.prod.ts file
+  fs.writeFileSync(`${ctx.project.dir}` + "/src/environments/environment.prod.ts", secrets.prodEnvironment);
 
-    //Check if live update and actual app version have same major and minor versions
-    compareAppVersion(process.env.LIVEUPDATE_APP_VERSION, ctx.env.FYLE_MOBILE_RELEASE_VERSION);
-    
-    const platformRoot = path.join(ctx.project.dir, 'android');
-    FILE_PATHS = {
-      "android.cameraPreview": `${ctx.project.dir}` + "/node_modules/@capacitor-community/camera-preview/android/src/main/java/com/ahm/capacitor/camera/preview/CameraPreview.java"
-    };
-  
-    // Adding GIT_COMMIT_SHA for sentry
-    var mainPath = path.resolve(process.cwd(), "src/main.ts");
-    var mainPathContent = fs.readFileSync(mainPath).toString();
-    fs.writeFileSync(mainPath, mainPathContent.replace(/please-replace-your-git-commit-version/g, process.env.CI_GIT_COMMIT_SHA), 'utf8');
-  
-    // Commenting Manifest.permission.RECORD_AUDIO on CameraPreview.java 
-    var cameraPreviewPath = path.resolve(process.cwd(), FILE_PATHS["android.cameraPreview"]);
-    var cameraPreviewContents = fs.readFileSync(cameraPreviewPath).toString();
-    fs.writeFileSync(cameraPreviewPath, cameraPreviewContents.replace(/Manifest.permission.RECORD_AUDIO/g, '// Manifest.permission.RECORD_AUDIO ,'), 'utf8');
-  
-    // updating app version
-    var buildGradlePath = path.resolve(process.cwd(),"android/app/build.gradle");
-    var buildGradlePathContents = fs.readFileSync(buildGradlePath).toString();
-    fs.writeFileSync(buildGradlePath, buildGradlePathContents.replace(/51000/g, ctx.env.FYLE_MOBILE_BUILD_VERSION), 'utf8');
-    buildGradlePathContents = fs.readFileSync(buildGradlePath).toString();
-    fs.writeFileSync(buildGradlePath, buildGradlePathContents.replace(/5.10.0/g, ctx.env.FYLE_MOBILE_RELEASE_VERSION), 'utf8');
-  
-    var pbxprojPath = path.resolve(process.cwd(),"ios/App/App.xcodeproj/project.pbxproj");
-    var pbxprojPathContents = fs.readFileSync(pbxprojPath).toString();
-    fs.writeFileSync(pbxprojPath, pbxprojPathContents.replace(/5.10.0/g, ctx.env.FYLE_MOBILE_RELEASE_VERSION), 'utf8');
-  
-    // Creating google-services.json file
-    fs.writeFileSync("android/app/google-services.json", secrets.googleCredentialsAndroid);
-  
-    // Creating GoogleService-Info.plist file
-    fs.writeFileSync("ios/App/App/GoogleService-Info.plist", secrets.googleCredentialsIos);  
-  
-    // Creating Info.plist file
-    fs.writeFileSync("ios/App/App/Info.plist", secrets.iosInfo);
-  } catch(err) {
-    throw Error(err);
+  // Adding LIVEUPDATE_CHANNEL in strings.xml
+  var androidStringsPath = path.resolve(process.cwd(),'android/app/src/main/res/values/strings.xml');
+  var androidStrings = fs.readFileSync(androidStringsPath).toString();
+  fs.writeFileSync(androidStringsPath, androidStrings.replace('LIVEUPDATE_CHANNEL', process.env.LIVEUPDATE_CHANNEL), 'utf8');
+
+  if (!process.env.NATIVE_CONFIG) {
+    return;
   }
+
+  //Check if live update and actual app version have same major and minor versions
+  compareAppVersion(process.env.LIVEUPDATE_APP_VERSION, ctx.env.FYLE_MOBILE_RELEASE_VERSION);
+  
+  const platformRoot = path.join(ctx.project.dir, 'android');
+  FILE_PATHS = {
+    "android.cameraPreview": `${ctx.project.dir}` + "/node_modules/@capacitor-community/camera-preview/android/src/main/java/com/ahm/capacitor/camera/preview/CameraPreview.java"
+  };
+
+  // Adding GIT_COMMIT_SHA for sentry
+  var mainPath = path.resolve(process.cwd(), "src/main.ts");
+  var mainPathContent = fs.readFileSync(mainPath).toString();
+  fs.writeFileSync(mainPath, mainPathContent.replace(/please-replace-your-git-commit-version/g, process.env.CI_GIT_COMMIT_SHA), 'utf8');
+
+  // Commenting Manifest.permission.RECORD_AUDIO on CameraPreview.java 
+  var cameraPreviewPath = path.resolve(process.cwd(), FILE_PATHS["android.cameraPreview"]);
+  var cameraPreviewContents = fs.readFileSync(cameraPreviewPath).toString();
+  fs.writeFileSync(cameraPreviewPath, cameraPreviewContents.replace(/Manifest.permission.RECORD_AUDIO/g, '// Manifest.permission.RECORD_AUDIO ,'), 'utf8');
+
+  // updating app version
+  var buildGradlePath = path.resolve(process.cwd(),"android/app/build.gradle");
+  var buildGradlePathContents = fs.readFileSync(buildGradlePath).toString();
+  fs.writeFileSync(buildGradlePath, buildGradlePathContents.replace(/51000/g, ctx.env.FYLE_MOBILE_BUILD_VERSION), 'utf8');
+  buildGradlePathContents = fs.readFileSync(buildGradlePath).toString();
+  fs.writeFileSync(buildGradlePath, buildGradlePathContents.replace(/5.10.0/g, ctx.env.FYLE_MOBILE_RELEASE_VERSION), 'utf8');
+
+  var pbxprojPath = path.resolve(process.cwd(),"ios/App/App.xcodeproj/project.pbxproj");
+  var pbxprojPathContents = fs.readFileSync(pbxprojPath).toString();
+  fs.writeFileSync(pbxprojPath, pbxprojPathContents.replace(/5.10.0/g, ctx.env.FYLE_MOBILE_RELEASE_VERSION), 'utf8');
+
+  // Creating google-services.json file
+  fs.writeFileSync("android/app/google-services.json", secrets.googleCredentialsAndroid);
+
+  // Creating GoogleService-Info.plist file
+  fs.writeFileSync("ios/App/App/GoogleService-Info.plist", secrets.googleCredentialsIos);  
+
+  // Creating Info.plist file
+  fs.writeFileSync("ios/App/App/Info.plist", secrets.iosInfo);
 };
 
 function compareAppVersion(liveUpdateVersion, currentVersion) {
@@ -72,4 +66,5 @@ function compareAppVersion(liveUpdateVersion, currentVersion) {
   if((currentVersionArray[0] !== liveUpdateVersionArray[0]) || (currentVersionArray[1] !== liveUpdateVersionArray[1])) {
     throw Error(`Live update version ${liveUpdateVersion} is different from current version ${currentVersion}`);
   }
+  return true;
 }
