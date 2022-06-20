585d6fa18ed1146d50c118602d055fb026e04af5
Vaishnavi Mohan <vaishnavi.mohan@fyle.in>
2022-06-20 15:16:54 +0530

    intermediate commit

--- src/app/fyle/add-edit-mileage/add-edit-mileage.page.ts
+++ src/app/fyle/add-edit-mileage/add-edit-mileage.page.ts
@@ -28,6 +28,7 @@ import { ProjectsService } from 'src/app/core/services/projects.service';
 import { TransactionService } from 'src/app/core/services/transaction.service';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { MileageService } from 'src/app/core/services/mileage.service';
+import { MileageRatesService } from 'src/app/core/services/mileage-rates.service';
 import { TransactionsOutboxService } from 'src/app/core/services/transactions-outbox.service';
 import { PolicyService } from 'src/app/core/services/policy.service';
 import { StatusService } from 'src/app/core/services/status.service';
@@ -206,6 +207,7 @@ export class AddEditMileagePage implements OnInit {
     private fb: FormBuilder,
     private projectService: ProjectsService,
     private mileageService: MileageService,
+    private mileageRatesService: MileageRatesService,
     private transactionsOutboxService: TransactionsOutboxService,
     private policyService: PolicyService,
     private statusService: StatusService,
@@ -876,7 +878,7 @@ export class AddEditMileagePage implements OnInit {
     this.navigateBack = this.activatedRoute.snapshot.params.navigate_back;
     this.expenseStartTime = new Date().getTime();
     this.fg = this.fb.group({
-      mileage_vehicle_type: [],
+      mileage_rate_name: [],
       dateOfSpend: [, this.customDateValidator],
       route: [],
       paymentMode: [, Validators.required],
