85842ae9eb2cdcf6554fef3ab5cdfe630da44ac3
Aditya Shrivastav <adityashrivastava992001@gmail.com>
2022-06-24 18:22:51 +0530

    removed commented code

--- src/app/auth/new-header/new-header.template.ts
+++ src/app/auth/new-header/new-header.template.ts
@@ -3,6 +3,5 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-header',
   templateUrl: './new-header.template.html',
-  // styleUrls: ['./sign-in.page.scss'],
 })
 export class NewHeaderComponent {}
