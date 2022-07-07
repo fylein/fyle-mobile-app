503653775ac5b761dd1d6b7f76c87d14d4a32855
OmkarJ13 <oajoshi13@gmail.com>
2022-07-07 20:13:18 +0530

    Added test cases

--- src/app/shared/pipes/fy-currency.pipe.spec.ts
+++ src/app/shared/pipes/fy-currency.pipe.spec.ts
@@ -2,9 +2,18 @@ import { CurrencyPipe } from '@angular/common';
 import { FyCurrencyPipe } from './fy-currency.pipe';
 
 describe('FyCurrencyPipe', () => {
+  const currencyPipe = new CurrencyPipe('en');
+  const pipe = new FyCurrencyPipe(currencyPipe);
+
   it('create an instance', () => {
-    const currencyPipe = new CurrencyPipe('en');
-    const pipe = new FyCurrencyPipe(currencyPipe);
     expect(pipe).toBeTruthy();
+  });
+
+  it('transforms 5 and USD to $5.00', () => {
+    expect(pipe.transform(5, 'USD')).toEqual('$5.00');
+  });
+
+  it('transforms 5 and OMR to OMR 5.000', () => {
+    expect(pipe.transform(5, 'OMR')).toEqual('OMR 5.000');
   });
 });
