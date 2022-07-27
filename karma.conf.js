a903371e28e16f76bb254587bfc7e7b16d7358fd
Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
2022-07-27 14:14:44 +0530

    Added fixes

--- karma.conf.js
+++ karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    concurrency: 1
   });
 };
--- src/app/core/services/transactions-outbox.service.spec.ts
+++ src/app/core/services/transactions-outbox.service.spec.ts
@@ -12,474 +12,13 @@ import { TrackingService } from './tracking.service';
 import { TransactionService } from './transaction.service';
 
 import { TransactionsOutboxService } from './transactions-outbox.service';
-
-const dataExtractionQueueBulkFyleSample = [
-  {
-    transaction: {
-      created_at: '2022-07-22T08:52:18.244Z',
-      updated_at: '2022-07-22T08:52:18.244Z',
-      id: 'txxpF3eoBNaK',
-      source: 'MOBILE_DASHCAM_BULK',
-      org_user_id: 'ouCI4UQ2G0K1',
-      creator_id: 'ouCI4UQ2G0K1',
-      txn_dt: '2022-07-22T06:30:00.000Z',
-      status_id: null,
-      vendor: null,
-      platform_vendor: null,
-      category: null,
-      amount: null,
-      currency: null,
-      admin_amount: null,
-      user_amount: null,
-      orig_amount: null,
-      orig_currency: null,
-      tax: null,
-      tax_amount: null,
-      tax_group_id: null,
-      report_id: null,
-      reported_at: null,
-      num_files: 0,
-      invoice_number: null,
-      purpose: null,
-      vendor_id: null,
-      platform_vendor_id: null,
-      project_id: null,
-      billable: null,
-      skip_reimbursement: false,
-      state: 'DRAFT',
-      external_id: null,
-      cost_center_id: null,
-      payment_id: null,
-      source_account_id: 'acconhTEKPHHR',
-      org_category_id: 5380,
-      physical_bill: null,
-      policy_state: null,
-      manual_flag: null,
-      policy_flag: null,
-      expense_number: 'E/2022/07/T/47',
-      split_group_id: 'txxpF3eoBNaK',
-      split_group_user_amount: null,
-      extracted_data: null,
-      proposed_exchange_rate: null,
-      exchange_rate: null,
-      exchange_rate_diff_percentage: null,
-      mandatory_fields_present: false,
-      user_reason_for_duplicate_expenses: null,
-      distance: null,
-      distance_unit: null,
-      from_dt: null,
-      to_dt: null,
-      num_days: null,
-      fyle_category: 'Unspecified',
-      mileage_calculated_distance: null,
-      mileage_calculated_amount: null,
-      mileage_vehicle_type: null,
-      mileage_rate: null,
-      mileage_is_round_trip: null,
-      hotel_is_breakfast_provided: null,
-      flight_journey_travel_class: null,
-      flight_return_travel_class: null,
-      train_travel_class: null,
-      bus_travel_class: null,
-      taxi_travel_class: null,
-      per_diem_rate_id: null,
-      activity_policy_pending: null,
-      activity_details: null,
-      custom_properties: null,
-      physical_bill_at: null,
-      policy_amount: null,
-      locations: [],
-    },
-    dataUrls: [
-      {
-        thumbnail: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-        type: 'image',
-        url: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-      },
-    ],
-  },
-  {
-    transaction: {
-      created_at: '2022-07-22T08:52:18.554Z',
-      updated_at: '2022-07-22T08:52:18.554Z',
-      id: 'txaFCMravH6E',
-      source: 'MOBILE_DASHCAM_BULK',
-      org_user_id: 'ouCI4UQ2G0K1',
-      creator_id: 'ouCI4UQ2G0K1',
-      txn_dt: '2022-07-22T06:30:00.000Z',
-      status_id: null,
-      vendor: null,
-      platform_vendor: null,
-      category: null,
-      amount: null,
-      currency: null,
-      admin_amount: null,
-      user_amount: null,
-      orig_amount: null,
-      orig_currency: null,
-      tax: null,
-      tax_amount: null,
-      tax_group_id: null,
-      report_id: null,
-      reported_at: null,
-      num_files: 0,
-      invoice_number: null,
-      purpose: null,
-      vendor_id: null,
-      platform_vendor_id: null,
-      project_id: null,
-      billable: null,
-      skip_reimbursement: false,
-      state: 'DRAFT',
-      external_id: null,
-      cost_center_id: null,
-      payment_id: null,
-      source_account_id: 'acconhTEKPHHR',
-      org_category_id: 5380,
-      physical_bill: null,
-      policy_state: null,
-      manual_flag: null,
-      policy_flag: null,
-      expense_number: 'E/2022/07/T/48',
-      split_group_id: 'txaFCMravH6E',
-      split_group_user_amount: null,
-      extracted_data: null,
-      proposed_exchange_rate: null,
-      exchange_rate: null,
-      exchange_rate_diff_percentage: null,
-      mandatory_fields_present: false,
-      user_reason_for_duplicate_expenses: null,
-      distance: null,
-      distance_unit: null,
-      from_dt: null,
-      to_dt: null,
-      num_days: null,
-      fyle_category: 'Unspecified',
-      mileage_calculated_distance: null,
-      mileage_calculated_amount: null,
-      mileage_vehicle_type: null,
-      mileage_rate: null,
-      mileage_is_round_trip: null,
-      hotel_is_breakfast_provided: null,
-      flight_journey_travel_class: null,
-      flight_return_travel_class: null,
-      train_travel_class: null,
-      bus_travel_class: null,
-      taxi_travel_class: null,
-      per_diem_rate_id: null,
-      activity_policy_pending: null,
-      activity_details: null,
-      custom_properties: null,
-      physical_bill_at: null,
-      policy_amount: null,
-      locations: [],
-    },
-    dataUrls: [
-      {
-        thumbnail: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-        type: 'image',
-        url: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-      },
-    ],
-  },
-  {
-    transaction: {
-      created_at: '2022-07-22T08:52:19.162Z',
-      updated_at: '2022-07-22T08:52:19.162Z',
-      id: 'txclGDPfFv9b',
-      source: 'MOBILE_DASHCAM_BULK',
-      org_user_id: 'ouCI4UQ2G0K1',
-      creator_id: 'ouCI4UQ2G0K1',
-      txn_dt: '2022-07-22T06:30:00.000Z',
-      status_id: null,
-      vendor: null,
-      platform_vendor: null,
-      category: null,
-      amount: null,
-      currency: null,
-      admin_amount: null,
-      user_amount: null,
-      orig_amount: null,
-      orig_currency: null,
-      tax: null,
-      tax_amount: null,
-      tax_group_id: null,
-      report_id: null,
-      reported_at: null,
-      num_files: 0,
-      invoice_number: null,
-      purpose: null,
-      vendor_id: null,
-      platform_vendor_id: null,
-      project_id: null,
-      billable: null,
-      skip_reimbursement: false,
-      state: 'DRAFT',
-      external_id: null,
-      cost_center_id: null,
-      payment_id: null,
-      source_account_id: 'acconhTEKPHHR',
-      org_category_id: 5380,
-      physical_bill: null,
-      policy_state: null,
-      manual_flag: null,
-      policy_flag: null,
-      expense_number: 'E/2022/07/T/52',
-      split_group_id: 'txclGDPfFv9b',
-      split_group_user_amount: null,
-      extracted_data: null,
-      proposed_exchange_rate: null,
-      exchange_rate: null,
-      exchange_rate_diff_percentage: null,
-      mandatory_fields_present: false,
-      user_reason_for_duplicate_expenses: null,
-      distance: null,
-      distance_unit: null,
-      from_dt: null,
-      to_dt: null,
-      num_days: null,
-      fyle_category: 'Unspecified',
-      mileage_calculated_distance: null,
-      mileage_calculated_amount: null,
-      mileage_vehicle_type: null,
-      mileage_rate: null,
-      mileage_is_round_trip: null,
-      hotel_is_breakfast_provided: null,
-      flight_journey_travel_class: null,
-      flight_return_travel_class: null,
-      train_travel_class: null,
-      bus_travel_class: null,
-      taxi_travel_class: null,
-      per_diem_rate_id: null,
-      activity_policy_pending: null,
-      activity_details: null,
-      custom_properties: null,
-      physical_bill_at: null,
-      policy_amount: null,
-      locations: [],
-    },
-    dataUrls: [
-      {
-        thumbnail: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-        type: 'image',
-        url: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-      },
-    ],
-  },
-  {
-    transaction: {
-      created_at: '2022-07-22T08:52:18.989Z',
-      updated_at: '2022-07-22T08:52:18.989Z',
-      id: 'txD3hsVZHrKr',
-      source: 'MOBILE_DASHCAM_BULK',
-      org_user_id: 'ouCI4UQ2G0K1',
-      creator_id: 'ouCI4UQ2G0K1',
-      txn_dt: '2022-07-22T06:30:00.000Z',
-      status_id: null,
-      vendor: null,
-      platform_vendor: null,
-      category: null,
-      amount: null,
-      currency: null,
-      admin_amount: null,
-      user_amount: null,
-      orig_amount: null,
-      orig_currency: null,
-      tax: null,
-      tax_amount: null,
-      tax_group_id: null,
-      report_id: null,
-      reported_at: null,
-      num_files: 0,
-      invoice_number: null,
-      purpose: null,
-      vendor_id: null,
-      platform_vendor_id: null,
-      project_id: null,
-      billable: null,
-      skip_reimbursement: false,
-      state: 'DRAFT',
-      external_id: null,
-      cost_center_id: null,
-      payment_id: null,
-      source_account_id: 'acconhTEKPHHR',
-      org_category_id: 5380,
-      physical_bill: null,
-      policy_state: null,
-      manual_flag: null,
-      policy_flag: null,
-      expense_number: 'E/2022/07/T/49',
-      split_group_id: 'txD3hsVZHrKr',
-      split_group_user_amount: null,
-      extracted_data: null,
-      proposed_exchange_rate: null,
-      exchange_rate: null,
-      exchange_rate_diff_percentage: null,
-      mandatory_fields_present: false,
-      user_reason_for_duplicate_expenses: null,
-      distance: null,
-      distance_unit: null,
-      from_dt: null,
-      to_dt: null,
-      num_days: null,
-      fyle_category: 'Unspecified',
-      mileage_calculated_distance: null,
-      mileage_calculated_amount: null,
-      mileage_vehicle_type: null,
-      mileage_rate: null,
-      mileage_is_round_trip: null,
-      hotel_is_breakfast_provided: null,
-      flight_journey_travel_class: null,
-      flight_return_travel_class: null,
-      train_travel_class: null,
-      bus_travel_class: null,
-      taxi_travel_class: null,
-      per_diem_rate_id: null,
-      activity_policy_pending: null,
-      activity_details: null,
-      custom_properties: null,
-      physical_bill_at: null,
-      policy_amount: null,
-      locations: [],
-    },
-    dataUrls: [
-      {
-        thumbnail: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-        type: 'image',
-        url: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-      },
-    ],
-  },
-  {
-    transaction: {
-      created_at: '2022-07-22T08:52:18.991Z',
-      updated_at: '2022-07-22T08:52:18.991Z',
-      id: 'txZvLH5TgNZ6',
-      source: 'MOBILE_DASHCAM_BULK',
-      org_user_id: 'ouCI4UQ2G0K1',
-      creator_id: 'ouCI4UQ2G0K1',
-      txn_dt: '2022-07-22T06:30:00.000Z',
-      status_id: null,
-      vendor: null,
-      platform_vendor: null,
-      category: null,
-      amount: null,
-      currency: null,
-      admin_amount: null,
-      user_amount: null,
-      orig_amount: null,
-      orig_currency: null,
-      tax: null,
-      tax_amount: null,
-      tax_group_id: null,
-      report_id: null,
-      reported_at: null,
-      num_files: 0,
-      invoice_number: null,
-      purpose: null,
-      vendor_id: null,
-      platform_vendor_id: null,
-      project_id: null,
-      billable: null,
-      skip_reimbursement: false,
-      state: 'DRAFT',
-      external_id: null,
-      cost_center_id: null,
-      payment_id: null,
-      source_account_id: 'acconhTEKPHHR',
-      org_category_id: 5380,
-      physical_bill: null,
-      policy_state: null,
-      manual_flag: null,
-      policy_flag: null,
-      expense_number: 'E/2022/07/T/50',
-      split_group_id: 'txZvLH5TgNZ6',
-      split_group_user_amount: null,
-      extracted_data: null,
-      proposed_exchange_rate: null,
-      exchange_rate: null,
-      exchange_rate_diff_percentage: null,
-      mandatory_fields_present: false,
-      user_reason_for_duplicate_expenses: null,
-      distance: null,
-      distance_unit: null,
-      from_dt: null,
-      to_dt: null,
-      num_days: null,
-      fyle_category: 'Unspecified',
-      mileage_calculated_distance: null,
-      mileage_calculated_amount: null,
-      mileage_vehicle_type: null,
-      mileage_rate: null,
-      mileage_is_round_trip: null,
-      hotel_is_breakfast_provided: null,
-      flight_journey_travel_class: null,
-      flight_return_travel_class: null,
-      train_travel_class: null,
-      bus_travel_class: null,
-      taxi_travel_class: null,
-      per_diem_rate_id: null,
-      activity_policy_pending: null,
-      activity_details: null,
-      custom_properties: null,
-      physical_bill_at: null,
-      policy_amount: null,
-      locations: [],
-    },
-    dataUrls: [
-      {
-        thumbnail: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-        type: 'image',
-        url: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-      },
-    ],
-  },
-];
-
-const sampleBulkFyleQueue = [
-  {
-    transaction: {
-      source_account_id: 'acconhTEKPHHR',
-      skip_reimbursement: true,
-      source: 'MOBILE_DASHCAM_BULK',
-      txn_dt: '2022-07-22T14:43:25.092Z',
-      currency: 'INR',
-    },
-    dataUrls: [
-      {
-        thumbnail: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-        type: 'image',
-        url: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-      },
-    ],
-    comments: null,
-    reportId: null,
-    applyMagic: true,
-  },
-  {
-    transaction: {
-      source_account_id: 'acconhTEKPHHR',
-      skip_reimbursement: true,
-      source: 'MOBILE_DASHCAM_BULK',
-      txn_dt: '2022-07-22T14:43:25.102Z',
-      currency: 'INR',
-    },
-    dataUrls: [
-      {
-        thumbnail: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-        type: 'image',
-        url: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
-      },
-    ],
-    comments: null,
-    reportId: null,
-    applyMagic: true,
-  },
-];
+import { transactionOutboxData } from './data-extraction-queue-bulk-fyle-sample';
 
 fdescribe('TransactionsOutboxService', () => {
   let service: TransactionsOutboxService;
   let storageService: jasmine.SpyObj<StorageService>;
+  let dataExtractionQueueBulkFyleSample = transactionOutboxData.dataQueue;
+  let sampleBulkFyleQueue = transactionOutboxData.sampleBulkFyleQueue;
 
   beforeEach(() => {
     const storageServiceSpy = jasmine.createSpyObj('StorageService', ['get', 'set']);
@@ -495,6 +34,9 @@ fdescribe('TransactionsOutboxService', () => {
       'addToExistingReportAddEditExpense',
       'syncError',
     ]);
+
+    dataExtractionQueueBulkFyleSample = transactionOutboxData.dataQueue;
+    sampleBulkFyleQueue = transactionOutboxData.sampleBulkFyleQueue;
 
     TestBed.configureTestingModule({
       providers: [
@@ -557,55 +99,78 @@ fdescribe('TransactionsOutboxService', () => {
 
   it('can check if data extraction is pending for a particular expense', () => {
     service.dataExtractionQueue = dataExtractionQueueBulkFyleSample;
+
     expect(service.isDataExtractionPending('txxpF3eoBNaK')).toBeTrue();
     expect(service.isDataExtractionPending('txxpF3eosNaK')).toBeFalse();
   });
 
-  it('should be able to save the data extraction queue', (done) => {
+  it('should be able to save the data extraction queue', async () => {
     service.dataExtractionQueue = dataExtractionQueueBulkFyleSample;
-    service.saveDataExtractionQueue().then(() => {
-      expect(storageService.set).toHaveBeenCalledWith('data_extraction_queue', dataExtractionQueueBulkFyleSample);
-      done();
-    });
+    await service.saveDataExtractionQueue();
+    expect(storageService.set).toHaveBeenCalledWith('data_extraction_queue', dataExtractionQueueBulkFyleSample);
   });
 
-  it('can remove entries from data extraction queue', (done) => {
+  it('can remove entries from data extraction queue', async () => {
     service.dataExtractionQueue = dataExtractionQueueBulkFyleSample;
     let clonedQueue = cloneDeep(dataExtractionQueueBulkFyleSample);
     clonedQueue = clonedQueue.filter((c) => c.transaction.id !== dataExtractionQueueBulkFyleSample[0].transaction.id);
-    service.removeDataExtractionEntry(dataExtractionQueueBulkFyleSample[0].transaction.id).then(() => {
-      expect(storageService.set).toHaveBeenCalledWith('data_extraction_queue', clonedQueue);
-      done();
-    });
+    await service.removeDataExtractionEntry(dataExtractionQueueBulkFyleSample[0].transaction.id);
+    expect(storageService.set).toHaveBeenCalledWith('data_extraction_queue', clonedQueue);
   });
 
-  it('can save to transaction outbox', (done) => {
+  it('can save to transaction outbox', async () => {
     service.queue = sampleBulkFyleQueue;
-    service.saveQueue().then(() => {
-      expect(storageService.set).toHaveBeenCalledWith('outbox', sampleBulkFyleQueue);
-      done();
-    });
+    await service.saveQueue();
+    expect(storageService.set).toHaveBeenCalledWith('outbox', sampleBulkFyleQueue);
   });
 
-  it('can restore upload queue and data extraction queue from local storage', (done) => {
+  it('can restore upload queue and data extraction queue from local storage', async () => {
     storageService.get.withArgs('outbox').and.returnValue(Promise.resolve(sampleBulkFyleQueue));
     storageService.get
       .withArgs('data_extraction_queue')
       .and.returnValue(Promise.resolve(dataExtractionQueueBulkFyleSample));
-    service.restoreQueue().then(() => {
-      expect(service.queue).toEqual(sampleBulkFyleQueue);
-      expect(service.dataExtractionQueue).toEqual(dataExtractionQueueBulkFyleSample);
-      done();
-    });
+    await service.restoreQueue();
+    expect(service.queue).toEqual(sampleBulkFyleQueue);
+    expect(service.dataExtractionQueue).toEqual(dataExtractionQueueBulkFyleSample);
   });
 
-  it('can set upload queue and data extraction queue to empty arrays if they are undefined', (done) => {
+  it('can set upload queue and data extraction queue to empty arrays if they are undefined', async () => {
     service.queue = [];
     service.dataExtractionQueue = [];
-    service.restoreQueue().then(() => {
-      expect(service.queue).toEqual([]);
-      expect(service.dataExtractionQueue).toEqual([]);
-      done();
-    });
+    await service.restoreQueue();
+    expect(service.queue).toEqual([]);
+    expect(service.dataExtractionQueue).toEqual([]);
+  });
+
+  it('can set root properly', () => {
+    service.setRoot('https://app.fylehq.com');
+    expect(service.ROOT_ENDPOINT).toEqual('https://app.fylehq.com');
+  });
+
+  it('should be able to add data extraction entry', async () => {
+    service.queue = [];
+    service.dataExtractionQueue = [];
+    const transaction = {
+      source_account_id: 'acconhTEKPHHR',
+      skip_reimbursement: true,
+      source: 'MOBILE_DASHCAM_BULK',
+      txn_dt: '2022-07-22T14:43:25.102Z',
+      currency: 'INR',
+    };
+
+    const dataUrls = [
+      {
+        thumbnail: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
+        type: 'image',
+        url: 'data:image/jpeg;base64,iVBORw0KGgoAAAANS',
+      },
+    ];
+    await service.addDataExtractionEntry(transaction, dataUrls);
+    expect(storageService.set).toHaveBeenCalledWith('data_extraction_queue', [
+      {
+        transaction,
+        dataUrls,
+      },
+    ]);
   });
 });
