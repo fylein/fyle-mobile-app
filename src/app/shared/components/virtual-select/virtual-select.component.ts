c347b07d451e7c3eb4fb50a623313354d43fcbc3
Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
2022-08-19 10:27:42 +0530

    Added fix for virtual scroll

---
+++ src/app/shared/components/virtual-select/virtual-select.component.ts
@@ -0,0 +1,165 @@
+import { Component, OnInit, forwardRef, Input, TemplateRef, Injector } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor, NgControl } from '@angular/forms';
+import { noop } from 'rxjs';
+import { ModalController } from '@ionic/angular';
+import { VirtualSelectModalComponent } from './virtual-select-modal/virtual-select-modal.component';
+import { isEqual } from 'lodash';
+import { ModalPropertiesService } from 'src/app/core/services/modal-properties.service';
+
+@Component({
+  selector: 'app-virtual-select',
+  templateUrl: './virtual-select.component.html',
+  styleUrls: ['./virtual-select.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => VirtualSelectComponent),
+      multi: true,
+    },
+  ],
+})
+export class VirtualSelectComponent implements ControlValueAccessor, OnInit {
+  @Input() options: { label: string; value: any }[] = [];
+
+  @Input() disabled = false;
+
+  @Input() label = '';
+
+  @Input() mandatory = false;
+
+  @Input() selectionElement: TemplateRef<any>;
+
+  @Input() nullOption = true;
+
+  @Input() cacheName = '';
+
+  @Input() subheader = 'All';
+
+  @Input() enableSearch = true;
+
+  @Input() selectModalHeader = '';
+
+  @Input() showSaveButton = false;
+
+  @Input() placeholder: string;
+
+  @Input() defaultLabelProp;
+
+  @Input() recentlyUsed: { label: string; value: any; selected?: boolean }[];
+
+  displayValue;
+
+  private innerValue;
+
+  private ngControl: NgControl;
+
+  private onTouchedCallback: () => void = noop;
+
+  private onChangeCallback: (_: any) => void = noop;
+
+  constructor(
+    private modalController: ModalController,
+    private injector: Injector,
+    private modalProperties: ModalPropertiesService
+  ) {}
+
+  get valid() {
+    if (this.ngControl.touched) {
+      return this.ngControl.valid;
+    } else {
+      return true;
+    }
+  }
+
+  get value(): any {
+    return this.innerValue;
+  }
+
+  set value(v: any) {
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      if (this.options) {
+        const selectedOption = this.options.find((option) => isEqual(option.value, this.innerValue));
+        if (selectedOption && selectedOption.label) {
+          this.displayValue = selectedOption.label;
+        } else if (typeof this.innerValue === 'string') {
+          this.displayValue = this.innerValue;
+        } else if (this.innerValue && this.defaultLabelProp) {
+          this.displayValue = this.innerValue[this.defaultLabelProp];
+        } else {
+          this.displayValue = '';
+        }
+      }
+
+      this.onChangeCallback(v);
+    }
+  }
+
+  ngOnInit() {
+    this.ngControl = this.injector.get(NgControl);
+  }
+
+  async openModal() {
+    const cssClass = this.label === 'Payment Mode' ? 'payment-mode-modal' : 'virtual-modal';
+
+    const selectionModal = await this.modalController.create({
+      component: VirtualSelectModalComponent,
+      componentProps: {
+        options: this.options,
+        currentSelection: this.value,
+        selectionElement: this.selectionElement,
+        nullOption: this.nullOption,
+        cacheName: this.cacheName,
+        subheader: this.subheader,
+        enableSearch: this.enableSearch,
+        selectModalHeader: this.selectModalHeader || 'Select Item',
+        placeholder: this.placeholder,
+        showSaveButton: this.showSaveButton,
+        defaultLabelProp: this.defaultLabelProp,
+        recentlyUsed: this.recentlyUsed,
+        label: this.label,
+      },
+      mode: 'ios',
+      ...this.modalProperties.getModalDefaultProperties(cssClass),
+    });
+
+    await selectionModal.present();
+
+    const { data } = await selectionModal.onWillDismiss();
+
+    if (data) {
+      this.value = data.value;
+    }
+  }
+
+  onBlur() {
+    this.onTouchedCallback();
+  }
+
+  writeValue(value: any): void {
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+      if (this.options) {
+        const selectedOption = this.options.find((option) => isEqual(option.value, this.innerValue));
+
+        if (selectedOption && selectedOption.label) {
+          this.displayValue = selectedOption.label;
+        } else if (typeof this.innerValue === 'string') {
+          this.displayValue = this.innerValue;
+        } else if (this.innerValue && this.defaultLabelProp) {
+          this.displayValue = this.innerValue[this.defaultLabelProp];
+        } else {
+          this.displayValue = '';
+        }
+      }
+    }
+  }
+
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
+  }
+}
