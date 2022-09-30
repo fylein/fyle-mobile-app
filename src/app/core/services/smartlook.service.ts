ae32d069559772762095f4a2f729cb05ec50e353
yksolanki9 <yashsolanki1709@gmail.com>
2022-09-30 15:45:05 +0530

    Add check to see if user is connected to internet

--- src/app/core/services/smartlook.service.ts
+++ src/app/core/services/smartlook.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import { forkJoin, from } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { Injectable, EventEmitter } from '@angular/core';
+import { forkJoin, from, concat, Observable } from 'rxjs';
+import { filter, take } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 import { CurrencyService } from './currency.service';
 import { DeviceService } from './device.service';
@@ -10,25 +10,44 @@ import {
   SmartlookUserIdentifier,
 } from '@awesome-cordova-plugins/smartlook/ngx';
 import { environment } from 'src/environments/environment';
+import { NetworkService } from './network.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SmartlookService {
+  isConnected$: Observable<boolean>;
+
   constructor(
     private currencyService: CurrencyService,
     private authService: AuthService,
     private deviceService: DeviceService,
+    private networkService: NetworkService,
     private smartlook: Smartlook
-  ) {}
+  ) {
+    this.setupNetworkWatcher();
+  }
+
+  setupNetworkWatcher() {
+    const that = this;
+    const networkWatcherEmitter = new EventEmitter<boolean>();
+    this.networkService.connectivityWatcher(networkWatcherEmitter);
+    this.isConnected$ = concat(that.networkService.isOnline(), networkWatcherEmitter.asObservable());
+  }
 
   init() {
     forkJoin({
+      isConnected: this.isConnected$.pipe(take(1)),
       homeCurrency: this.currencyService.getHomeCurrency(),
       eou: from(this.authService.getEou()),
       deviceInfo: this.deviceService.getDeviceInfo(),
     })
-      .pipe(filter(({ homeCurrency, eou }) => eou && !eou.us.email.includes('fyle') && homeCurrency === 'USD'))
+      .pipe(
+        filter(
+          ({ isConnected, homeCurrency, eou }) =>
+            isConnected && eou && !eou.us.email.includes('fyle') && homeCurrency === 'USD'
+        )
+      )
       .subscribe(({ eou, deviceInfo }) => {
         const setupConfig = new SmartlookSetupConfigBuilder(environment.SMARTLOOK_API_KEY);
         this.smartlook.setup(setupConfig.build());
