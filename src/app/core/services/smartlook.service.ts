be42417115531a71b7af60adbf0ab9a923ce1322
Yash Solanki <yashsolanki1709@gmail.com>
2022-10-07 12:19:15 +0530

    Add session recording support with Smartlook (#1396)
    * Added support for smartlook
    
    * Hide only password field in session recordings
    
    * Initialize rendering
    
    * Add smartlook-show class to some files
    
    * Add smartlook-show class to remaining input fields
    
    * Move smartlook related code inside a service
    
    * Get Smartlook api key from env vars
    
    * Minor refactoring
    
    * Set api key in prebuild hook
    
    * Fix unit test
    
    * Add check to exclude demo accounts and resolve comments
    
    * Add check to see if user is connected to internet
    
    Co-authored-by: Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>

---
+++ src/app/core/services/smartlook.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { forkJoin, from, concat, Observable } from 'rxjs';
+import { filter, take } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+import { CurrencyService } from './currency.service';
+import { DeviceService } from './device.service';
+import {
+  Smartlook,
+  SmartlookSetupConfigBuilder,
+  SmartlookUserIdentifier,
+} from '@awesome-cordova-plugins/smartlook/ngx';
+import { environment } from 'src/environments/environment';
+import { NetworkService } from './network.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SmartlookService {
+  isConnected$: Observable<boolean>;
+
+  constructor(
+    private currencyService: CurrencyService,
+    private authService: AuthService,
+    private deviceService: DeviceService,
+    private networkService: NetworkService,
+    private smartlook: Smartlook
+  ) {
+    this.setupNetworkWatcher();
+  }
+
+  setupNetworkWatcher() {
+    const that = this;
+    const networkWatcherEmitter = new EventEmitter<boolean>();
+    this.networkService.connectivityWatcher(networkWatcherEmitter);
+    this.isConnected$ = concat(that.networkService.isOnline(), networkWatcherEmitter.asObservable());
+  }
+
+  init() {
+    forkJoin({
+      isConnected: this.isConnected$.pipe(take(1)),
+      homeCurrency: this.currencyService.getHomeCurrency(),
+      eou: from(this.authService.getEou()),
+      deviceInfo: this.deviceService.getDeviceInfo(),
+    })
+      .pipe(
+        filter(
+          ({ isConnected, homeCurrency, eou }) =>
+            isConnected && eou && !eou.us.email.includes('fyle') && homeCurrency === 'USD'
+        )
+      )
+      .subscribe(({ eou, deviceInfo }) => {
+        const setupConfig = new SmartlookSetupConfigBuilder(environment.SMARTLOOK_API_KEY);
+        this.smartlook.setup(setupConfig.build());
+
+        this.smartlook.setUserIdentifier(
+          new SmartlookUserIdentifier(eou.us.id, {
+            id: eou.us.id,
+            email: eou.us.email,
+            name: eou.us.full_name,
+            org_id: eou.ou.org_id,
+            org_name: eou.ou.org_name,
+            devicePlatform: deviceInfo.platform,
+            deviceModel: deviceInfo.model,
+            deviceOS: deviceInfo.osVersion,
+            is_approver: eou.ou.roles.includes('APPROVER') ? 'true' : 'false',
+          })
+        );
+        this.smartlook.startRecording();
+      });
+  }
+}
