df1e024d4af49dcb4eddada9fb9d0f9d57519902
yksolanki9 <yashsolanki1709@gmail.com>
2022-10-18 15:34:32 +0530

    Refactor HTML and fix css convention in capture receipt component, and new new empty component

---
+++ src/app/shared/components/capture-receipt/camera-preview/camera-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-camera-preview',
+  templateUrl: './camera-preview.component.html',
+  styleUrls: ['./camera-preview.component.scss'],
+})
+export class CameraPreviewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
