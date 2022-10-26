43531f1c7ee4ddf9430bf4a8370f1742003f5e49
Yash Solanki <yashsolanki1709@gmail.com>
2022-10-26 19:28:48 +0530

    Assign value to the observable before it is used (#1444)

--- src/app/fyle/add-edit-expense/add-edit-expense.page.ts
+++ src/app/fyle/add-edit-expense/add-edit-expense.page.ts
@@ -2633,6 +2633,10 @@ export class AddEditExpensePage implements OnInit {
 
     const editExpensePipe$ = this.getEditExpenseObservable();
 
+    this.etxn$ = iif(() => this.activatedRoute.snapshot.params.id, editExpensePipe$, newExpensePipe$).pipe(
+      shareReplay(1)
+    );
+
     this.attachments$ = this.loadAttachments$.pipe(
       switchMap(() =>
         this.etxn$.pipe(
@@ -2652,10 +2656,6 @@ export class AddEditExpensePage implements OnInit {
           reduce((acc, curr) => acc.concat(curr), [])
         )
       )
-    );
-
-    this.etxn$ = iif(() => this.activatedRoute.snapshot.params.id, editExpensePipe$, newExpensePipe$).pipe(
-      shareReplay(1)
     );
 
     this.paymentModes$ = this.getPaymentModes();
