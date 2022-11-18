59e4c158fef9c795a921cfb803014d39ca9789bc
yksolanki9 <yashsolanki1709@gmail.com>
2022-11-18 12:08:51 +0530

    Add explanation for isInstant proprty

--- src/app/core/services/transaction.service.ts
+++ src/app/core/services/transaction.service.ts
@@ -68,6 +68,11 @@ export class TransactionService {
     });
   }
 
+  /*
+    'isInstant' clears the cache before the method returns a value.
+    If we don't pass that property, then the cache is not cleared in our case.
+    Ref: https://www.npmjs.com/package/ts-cacheable#:~:text=need%20to%20set-,isInstant%3A%20true,-on%20CacheBuster%20configuration
+  */
   @CacheBuster({
     cacheBusterNotifier: transactionsCacheBuster$,
     isInstant: true,
