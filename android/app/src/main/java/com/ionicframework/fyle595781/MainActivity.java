0dd3060ea91e90a660ffef7912fa616fd4b18fea
yksolanki9 <yashsolanki1709@gmail.com>
2022-11-30 15:16:26 +0530

    Ignore user's device font size in android

--- android/app/src/main/java/com/ionicframework/fyle595781/MainActivity.java
+++ android/app/src/main/java/com/ionicframework/fyle595781/MainActivity.java
@@ -1,12 +1,18 @@
 package com.ionicframework.fyle595781;
-import com.ahm.capacitor.camera.preview.CameraPreview;
-import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import com.whitestein.securestorage.SecureStoragePlugin;
-
-import java.util.ArrayList;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends BridgeActivity {
+  /*
+  * Use the app font size irrespective of the user's device font settings.
+  * Ref - https://forum.ionicframework.com/t/font-size-difference-caused-by-android-font-size-settings/49200/6
+  */
+  public void onResume() {
+    super.onResume();
+    WebSettings settings = bridge.getWebView().getSettings();
+    settings.setTextZoom(100);
+    settings.setSupportZoom(false);
+  }
 }
