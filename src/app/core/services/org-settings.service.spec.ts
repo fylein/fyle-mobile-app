92162cbfb4e1756d18733cd94f6a7e353391325d
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2022-11-30 12:07:23 +0530

    Code coverage: Org settings service (#1484)

--- src/app/core/services/org-settings.service.spec.ts
+++ src/app/core/services/org-settings.service.spec.ts
@@ -1,16 +1,135 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import {
+  AccountingExportSettings,
+  IncomingAccountObject,
+  OrgSettings,
+  OrgSettingsResponse,
+} from '../models/org-settings.model';
+import { ApiService } from './api.service';
+import {
+  orgSettingsGetData,
+  incomingQuickbooksAccoutingObj,
+  incomingTallyAccoutingObj,
+  outgoingQuickbooksAccountObj,
+  outgoingTallyAccountObj,
+  orgSettingsPostData,
+  incomingAccountSettingsObj,
+  outgoingAccountSettingsObj,
+  incomingAccountingObj,
+  incomingTallyAccountingObjWithoutSettings,
+  incomingQuickbooksAccountingObjWithoutSettings,
+  outgoingAccountingQuickbooksObjWithoutSettings,
+  outgoingAccountingTallyObjWithoutSettings,
+} from '../models/test-data/org-settings-data';
 
 import { OrgSettingsService } from './org-settings.service';
 
-xdescribe('OrgSettingsService', () => {
-  let service: OrgSettingsService;
+const getApiData: OrgSettings = orgSettingsGetData;
+const postApiData: OrgSettingsResponse = orgSettingsPostData;
+const incomingTallyAccountObject: IncomingAccountObject = incomingTallyAccoutingObj;
+const incomingQuickBooksAccountObject: IncomingAccountObject = incomingQuickbooksAccoutingObj;
+const incomingAccountSettingsObject: IncomingAccountObject = incomingAccountSettingsObj;
+const outgoingTallyAccountObject: AccountingExportSettings = outgoingTallyAccountObj;
+const outgoingQuickbooksAccountObject: AccountingExportSettings = outgoingQuickbooksAccountObj;
+const outgoingAccountSettingsObject: AccountingExportSettings = outgoingAccountSettingsObj;
+const incomingAccountingObject: IncomingAccountObject = incomingAccountingObj;
+const outgoingAccountingTallyObjectWithoutSettings: AccountingExportSettings =
+  outgoingAccountingTallyObjWithoutSettings;
+const outgoingAccountingQuickbooksObjectWithoutSettings: AccountingExportSettings =
+  outgoingAccountingQuickbooksObjWithoutSettings;
+const incomingTallyAccountingObjectWithoutSettings: IncomingAccountObject = incomingTallyAccountingObjWithoutSettings;
+const incomingQuickbooksAccountingObjectWithoutSettings: IncomingAccountObject =
+  incomingQuickbooksAccountingObjWithoutSettings;
+
+describe('OrgSettingsService', () => {
+  let orgSettingsService: OrgSettingsService;
+  let apiService: jasmine.SpyObj<ApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(OrgSettingsService);
+    const apiServiceSpy = jasmine.createSpyObj('ApiService', ['get', 'post']);
+    TestBed.configureTestingModule({
+      providers: [
+        OrgSettingsService,
+        {
+          provide: ApiService,
+          useValue: apiServiceSpy,
+        },
+      ],
+    });
+    orgSettingsService = TestBed.inject(OrgSettingsService);
+    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(orgSettingsService).toBeTruthy();
+  });
+
+  it('should be able to get the org settings properly', (done) => {
+    apiService.get.and.returnValue(of(postApiData));
+    orgSettingsService.get().subscribe((res) => {
+      expect(res).toEqual(getApiData);
+      done();
+    });
+  });
+
+  it('should be able to update the org settings properly', (done) => {
+    apiService.post.and.returnValue(of(postApiData));
+    orgSettingsService.post(getApiData).subscribe((res) => {
+      expect(res).toEqual(postApiData);
+      done();
+    });
+  });
+
+  it('should be able to get incoming tally account object', () => {
+    expect(orgSettingsService.getIncomingAccountingObject(outgoingTallyAccountObject)).toEqual(
+      incomingTallyAccountObject
+    );
+  });
+
+  it('should be able to set outgoing tally account object', () => {
+    expect(orgSettingsService.setOutgoingAccountingObject(incomingTallyAccountObject)).toEqual(
+      outgoingTallyAccountObject
+    );
+  });
+
+  it('should be able to get incoming quick books account object', () => {
+    expect(orgSettingsService.getIncomingAccountingObject(outgoingQuickbooksAccountObject)).toEqual(
+      incomingQuickBooksAccountObject
+    );
+  });
+
+  it('should be able to set outgoing quickbooks account object', () => {
+    expect(orgSettingsService.setOutgoingAccountingObject(incomingQuickBooksAccountObject)).toEqual(
+      outgoingQuickbooksAccountObject
+    );
+  });
+
+  it('should be able to get incoming account settings object', () => {
+    expect(orgSettingsService.getIncomingAccountingObject(outgoingAccountSettingsObject)).toEqual(
+      incomingAccountSettingsObject
+    );
+  });
+
+  it('should be able to set outgoing account settings object', () => {
+    expect(orgSettingsService.setOutgoingAccountingObject(incomingAccountSettingsObject)).toEqual(
+      outgoingAccountSettingsObject
+    );
+  });
+
+  it('should be able to get incoming accounting object when accounting export is passed as null', () => {
+    expect(orgSettingsService.getIncomingAccountingObject(null)).toEqual(incomingAccountingObject);
+  });
+
+  it('should be able to set outgoing tally accounting object with empty settings', () => {
+    expect(orgSettingsService.setOutgoingAccountingObject(incomingTallyAccountingObjectWithoutSettings)).toEqual(
+      outgoingAccountingTallyObjectWithoutSettings
+    );
+  });
+
+  it('should be able to set outgoing quickbooks accounting object with empty settings', () => {
+    expect(orgSettingsService.setOutgoingAccountingObject(incomingQuickbooksAccountingObjectWithoutSettings)).toEqual(
+      outgoingAccountingQuickbooksObjectWithoutSettings
+    );
   });
 });
