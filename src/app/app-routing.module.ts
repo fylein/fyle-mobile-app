6c455fc51bd6df8330729a5201e1d4ba2168c94d
Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
2022-12-13 16:40:04 +0530

    Added preloading back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: NoPreloading })],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
