5dd761bcfdcb16bc1550810b34c8babcc490f77e
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2022-12-21 12:08:02 +0530

    changed date data

--- src/app/core/models/v2/extended-project.model.ts
+++ src/app/core/models/v2/extended-project.model.ts
@@ -7,13 +7,13 @@ export interface ExtendedProject {
   project_approver1_id: string;
   project_approver2_id: string;
   project_code: string;
-  project_created_at?: Date;
+  project_created_at: Date;
   project_description: string;
   project_id: number;
   project_name: string;
   project_org_category_ids: number[];
   project_org_id: string;
-  project_updated_at?: Date;
+  project_updated_at: Date;
   projectv2_name: string;
   sub_project_name: string;
   org_category_ids?: number[];
