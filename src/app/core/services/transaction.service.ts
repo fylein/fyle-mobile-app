8a44259adb79c0b37c5c05d37e9800bd0ff7b8ad
Yash Solanki <yashsolanki1709@gmail.com>
2023-01-04 17:18:44 +0530

    Revert "Content Changes for fix_implicit_merge_errors (#1579)"
    This reverts commit 575c4bc812a49725bdc122068dca8ba4e1b1c5af.

--- src/app/core/services/transaction.service.ts
+++ src/app/core/services/transaction.service.ts
@@ -642,22 +642,22 @@ export class TransactionService {
     return dialogBody;
   }
 
-  getRemoveCardExpenseDialogBody(isSplitExpensesPresent: boolean): string {
+  getUnlinkDialogBody(isSplitExpensesPresent: boolean): string {
     const dialogBody = isSplitExpensesPresent
       ? `<ul class="text-left">
-    <li>Since this is a split expense, clicking on <strong>Confirm</strong> will remove the card details from all the related split expenses.</li>
-    <li>A new expense will be created from the card expense removed here.</li>
-    <li>Are you sure to remove your card expense from this expense?</li>
+    <li>If you're sure that your expense is linked with the wrong card details, you can proceed to unlink the card details by clicking on <strong>Confirm.</strong></li>
+    <li>It removes the card details from the expense and creates a new card expense under the Expenses section.</li>
+    <li>Since this is a split expense, clicking on Confirm will remove the card details from all the related split expenses.</li>
     </ul>`
       : `<ul class="text-left">
-    <li>A new expense will be created from the card expense removed here.</li>
-    <li>Are you sure to remove your card expense from this expense?</li>
+    <li>If you're sure that your expense is linked with the wrong card details, you can proceed to unlink the card details by clicking on <strong>Confirm.</strong></li>
+    <li>It removes the card details from the expense and creates a new card expense under the Expenses section.</li>
     </ul>`;
 
     return dialogBody;
   }
 
-  removeCorporateCardExpense(txnId: string): Observable<UndoMerge> {
+  unlinkCorporateCardExpense(txnId: string): Observable<UndoMerge> {
     const data: Object = {
       txn_id: txnId,
     };
