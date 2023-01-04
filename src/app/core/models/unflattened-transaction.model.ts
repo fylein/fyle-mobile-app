51c7c32aeb0fb1ba40db7a2cb4db3aab3805a62d
Dimple <dimple.kh@fyle.in>
2023-01-04 16:23:25 +0530

    replaced optional keys and used partial interface types

--- src/app/core/models/unflattened-transaction.model.ts
+++ src/app/core/models/unflattened-transaction.model.ts
@@ -4,8 +4,8 @@ import { User } from './user.model';
 import { Transaction } from './v1/transaction.model';
 
 export interface UnflattenedTransaction {
-  ou: OrgUser;
-  tx: Transaction;
+  ou: Partial<OrgUser>;
+  tx: Partial<Transaction>;
   us: User;
   source: {
     account_type: string;
