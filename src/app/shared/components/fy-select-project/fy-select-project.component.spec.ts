633ad451781899a5aa481e6ff913e94c5c273cb0
Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
2023-01-11 17:25:37 +0530

    Added unit tests for select project

--- src/app/shared/components/fy-select-project/fy-select-project.component.spec.ts
+++ src/app/shared/components/fy-select-project/fy-select-project.component.spec.ts
@@ -1,26 +1,165 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { FormsModule, NgControl, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { By } from '@angular/platform-browser';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { ModalPropertiesService } from 'src/app/core/services/modal-properties.service';
 
 import { FySelectProjectComponent } from './fy-select-project.component';
 
-xdescribe('FySelectProjectComponent', () => {
+describe('FySelectProjectComponent', () => {
   let component: FySelectProjectComponent;
   let fixture: ComponentFixture<FySelectProjectComponent>;
+  let el: DebugElement;
+  let modalController: jasmine.SpyObj<ModalController>;
+  let modalPropertiesService: jasmine.SpyObj<ModalPropertiesService>;
 
   beforeEach(
     waitForAsync(() => {
+      const modalControllerSpy = jasmine.createSpyObj('ModalController', ['create']);
+      const modalPropertiesServiceSpy = jasmine.createSpyObj('ModalPropertiesService', ['getModalDefaultProperties']);
+      const ngControlSpy = jasmine.createSpyObj('NgControl', ['touched', 'valid']);
+
       TestBed.configureTestingModule({
         declarations: [FySelectProjectComponent],
-        imports: [IonicModule.forRoot()],
+        imports: [IonicModule.forRoot(), FormsModule, ReactiveFormsModule, MatIconModule],
+        providers: [
+          {
+            provide: ModalController,
+            useValue: modalControllerSpy,
+          },
+          {
+            provide: ModalPropertiesService,
+            useValue: modalPropertiesServiceSpy,
+          },
+          {
+            provide: NgControl,
+            useValue: ngControlSpy,
+          },
+        ],
       }).compileComponents();
+
+      modalController = TestBed.inject(ModalController) as jasmine.SpyObj<ModalController>;
+      modalPropertiesService = TestBed.inject(ModalPropertiesService) as jasmine.SpyObj<ModalPropertiesService>;
 
       fixture = TestBed.createComponent(FySelectProjectComponent);
       component = fixture.componentInstance;
+      el = fixture.debugElement;
+      component.ngOnInit();
       fixture.detectChanges();
     })
   );
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be able to display proper label', () => {
+    component.label = 'Project Field';
+    fixture.detectChanges();
+    const label = el.query(By.css('.fy-select-project--label-content'));
+    expect(label.nativeElement.textContent.trim()).toEqual('Project Field');
+  });
+
+  it('should be able to set a proper project value', async () => {
+    const project = {
+      ap1_email: null,
+      ap1_full_name: null,
+      ap2_email: null,
+      ap2_full_name: null,
+      project_active: true,
+      project_approver1_id: null,
+      project_approver2_id: null,
+      project_code: '1184',
+      project_created_at: new Date('2021-05-12T10:28:40.834844'),
+      project_description: 'Sage Intacct Project - Customer Mapped Project, Id - 1184',
+      project_id: 257528,
+      project_name: 'Customer Mapped Project',
+      project_org_category_ids: [122269, 122270, 122271, null],
+      project_org_id: 'orFdTTTNcyye',
+      project_updated_at: new Date('2021-07-08T10:28:27.686886'),
+      projectv2_name: 'Customer Mapped Project',
+      sub_project_name: null,
+    };
+
+    component.writeValue(project);
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const inputDisplayField = el.query(By.css('.fy-select-project--input'));
+    expect(inputDisplayField.nativeElement.value).toEqual('Customer Mapped Project');
+  });
+
+  it('should be able to set touched value properly whenever the input field is touched', () => {
+    const callBackTestObject = jasmine.createSpyObj('touchedTested', ['touchedTested']);
+    component.registerOnTouched(callBackTestObject.touchedTested);
+
+    expect(callBackTestObject.touchedTested).not.toHaveBeenCalled();
+
+    const inputDisplayField = el.query(By.css('.fy-select-project--input'));
+    inputDisplayField.triggerEventHandler('blur', null);
+
+    fixture.detectChanges();
+
+    expect(callBackTestObject.touchedTested).toHaveBeenCalled();
+  });
+
+  it('should be able to trigger on change value properly & set display value whenever the input field is changed', async () => {
+    const sampleProject = {
+      ap1_email: null,
+      ap1_full_name: null,
+      ap2_email: null,
+      ap2_full_name: null,
+      project_active: true,
+      project_approver1_id: null,
+      project_approver2_id: null,
+      project_code: '1184',
+      project_created_at: new Date('2021-05-12T10:28:40.834844'),
+      project_description: 'Sage Intacct Project - Customer Mapped Project, Id - 1184',
+      project_id: 257528,
+      project_name: 'Customer Mapped Project',
+      project_org_category_ids: [122269, 122270, 122271, null],
+      project_org_id: 'orFdTTTNcyye',
+      project_updated_at: new Date('2021-07-08T10:28:27.686886'),
+      projectv2_name: 'Customer Mapped Project',
+      sub_project_name: null,
+    };
+
+    const changeTestCallback = jasmine.createSpyObj('changeTested', ['test']);
+    component.registerOnChange(changeTestCallback.test);
+
+    expect(changeTestCallback.test).not.toHaveBeenCalled();
+
+    const modalControllerMockReturnValue = Promise.resolve({
+      present: () => Promise.resolve({}),
+      onWillDismiss: () =>
+        Promise.resolve({
+          data: {
+            value: sampleProject,
+          },
+        }),
+    });
+
+    modalController.create.and.returnValue(modalControllerMockReturnValue as any); // there are too many methods to mock
+    modalPropertiesService.getModalDefaultProperties.and.returnValue({
+      cssClass: 'fy-modal',
+      showBackdrop: true,
+      canDismiss: true,
+      backdropDismiss: true,
+      animated: true,
+      initialBreakpoint: 1,
+      breakpoints: [0, 1],
+      handle: false,
+    });
+
+    await component.openModal();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(changeTestCallback.test).toHaveBeenCalled();
+    const inputDisplayField = el.query(By.css('.fy-select-project--input'));
+    expect(inputDisplayField.nativeElement.value).toEqual('Customer Mapped Project');
   });
 });
