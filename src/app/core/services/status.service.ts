6f8ff2f912c6bb72e89cda93e503867ae272f32a
Manjiri Chavande <manjiri.c@fyle.in>
2023-01-11 12:23:42 +0530

    task FYLE-85zrh6q1u change the category content from Expense added to the report -> Expense added

--- src/app/core/services/status.service.ts
+++ src/app/core/services/status.service.ts
@@ -81,7 +81,7 @@ export class StatusService {
         break;
       case lowerCaseComment.indexOf('added to the report') > -1:
         statusCategory = {
-          category: 'Expense added to report',
+          category: 'Expense added',
           icon: 'circle',
         };
         break;
