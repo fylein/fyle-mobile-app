fbc2803c6e5a3ad47fc82d60131b53fc23517f5e
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-01-12 13:55:32 +0530

    added data file

---
+++ src/app/core/test-data/dashboard.service.spec.data.ts
@@ -0,0 +1,16 @@
+export const apiTransactionStatsRes = [
+  {
+    aggregates: [
+      {
+        function_name: 'count(tx_id)',
+        function_value: 6,
+      },
+      {
+        function_name: 'sum(tx_amount)',
+        function_value: 16748.73,
+      },
+    ],
+    dimensions: [],
+    name: 'scalar_stat',
+  },
+];
