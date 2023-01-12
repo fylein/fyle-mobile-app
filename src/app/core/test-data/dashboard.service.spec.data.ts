0faa48053bd59dc04d601e3e2093fb12850cee02
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-01-12 17:04:10 +0530

    code coverage at 94

--- src/app/core/test-data/dashboard.service.spec.data.ts
+++ src/app/core/test-data/dashboard.service.spec.data.ts
@@ -1,4 +1,4 @@
-export const apiTransactionStatsRes = [
+export const apiTransactionUnreportedStatsRes = [
   {
     aggregates: [
       {
@@ -14,3 +14,169 @@ export const apiTransactionStatsRes = [
     name: 'scalar_stat',
   },
 ];
+
+export const apiTransactionIncompleteStatsRes = [
+  {
+    aggregates: [
+      {
+        function_name: 'count(tx_id)',
+        function_value: 1130,
+      },
+      {
+        function_name: 'sum(tx_amount)',
+        function_value: 1148487.57555147,
+      },
+    ],
+    dimensions: [],
+    name: 'scalar_stat',
+  },
+];
+
+export const apiReportStatsRes = {
+  data: [
+    {
+      dimensions: ['rp_state'],
+      name: '1',
+      value: [
+        {
+          aggregates: [
+            {
+              function_name: 'sum(rp_amount)',
+              function_value: 28758273650702.816,
+            },
+            {
+              function_name: 'count(rp_id)',
+              function_value: 56,
+            },
+          ],
+          key: [
+            {
+              column_name: 'rp_state',
+              column_value: 'APPROVED',
+            },
+          ],
+        },
+        {
+          aggregates: [
+            {
+              function_name: 'sum(rp_amount)',
+              function_value: 642147.297922,
+            },
+            {
+              function_name: 'count(rp_id)',
+              function_value: 2,
+            },
+          ],
+          key: [
+            {
+              column_name: 'rp_state',
+              column_value: 'APPROVER_INQUIRY',
+            },
+          ],
+        },
+        {
+          aggregates: [
+            {
+              function_name: 'sum(rp_amount)',
+              function_value: 5177243929.65219,
+            },
+            {
+              function_name: 'count(rp_id)',
+              function_value: 45,
+            },
+          ],
+          key: [
+            {
+              column_name: 'rp_state',
+              column_value: 'APPROVER_PENDING',
+            },
+          ],
+        },
+        {
+          aggregates: [
+            {
+              function_name: 'sum(rp_amount)',
+              function_value: 93165.91,
+            },
+            {
+              function_name: 'count(rp_id)',
+              function_value: 6,
+            },
+          ],
+          key: [
+            {
+              column_name: 'rp_state',
+              column_value: 'DRAFT',
+            },
+          ],
+        },
+        {
+          aggregates: [
+            {
+              function_name: 'sum(rp_amount)',
+              function_value: 1221297040.1711,
+            },
+            {
+              function_name: 'count(rp_id)',
+              function_value: 33,
+            },
+          ],
+          key: [
+            {
+              column_name: 'rp_state',
+              column_value: 'PAID',
+            },
+          ],
+        },
+        {
+          aggregates: [
+            {
+              function_name: 'sum(rp_amount)',
+              function_value: 501602.12,
+            },
+            {
+              function_name: 'count(rp_id)',
+              function_value: 4,
+            },
+          ],
+          key: [
+            {
+              column_name: 'rp_state',
+              column_value: 'PAYMENT_PENDING',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  url: '/v2/reports/stats',
+};
+
+export const expectedUnreportedExpStats = {
+  count: 6,
+  sum: 16748.73,
+};
+
+export const expectedIncompleteExpStats = {
+  count: 1130,
+  sum: 1148487.57555147,
+};
+
+export const expectedReportStats = {
+  draft: {
+    count: 6,
+    sum: 93165.91,
+  },
+  report: {
+    count: 45,
+    sum: 5177243929.65219,
+  },
+  approved: {
+    count: 56,
+    sum: 28758273650702.816,
+  },
+  paymentPending: {
+    count: 4,
+    sum: 501602.12,
+  },
+};
--- src/app/fyle/dashboard/dashboard.service.spec.ts
+++ src/app/fyle/dashboard/dashboard.service.spec.ts
@@ -4,6 +4,16 @@ import { ReportService } from 'src/app/core/services/report.service';
 import { TransactionService } from 'src/app/core/services/transaction.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DashboardService } from './dashboard.service';
+import {
+  apiTransactionUnreportedStatsRes,
+  apiTransactionIncompleteStatsRes,
+  apiReportStatsRes,
+  expectedUnreportedExpStats,
+  expectedIncompleteExpStats,
+  expectedReportStats,
+} from '../../core/test-data/dashboard.service.spec.data';
+import { of } from 'rxjs';
+import { StatsResponse } from 'src/app/core/models/v2/stats-response.model';
 
 describe('DashboardService', () => {
   let dashboardService: DashboardService;
@@ -43,5 +53,52 @@ describe('DashboardService', () => {
 
   it('should be created', () => {
     expect(dashboardService).toBeTruthy();
+  });
+
+  it('should get UNREPORTED expense stats', (done) => {
+    transactionService.getTransactionStats.and.returnValue(of(apiTransactionUnreportedStatsRes));
+
+    const apiParams = {
+      scalar: true,
+      tx_state: 'in.(COMPLETE)',
+      or: '(tx_policy_amount.is.null,tx_policy_amount.gt.0.0001)',
+      tx_report_id: 'is.null',
+    };
+
+    dashboardService.getUnreportedExpensesStats().subscribe((res) => {
+      expect(res).toEqual(expectedUnreportedExpStats);
+      expect(transactionService.getTransactionStats).toHaveBeenCalledWith('count(tx_id),sum(tx_amount)', apiParams);
+      done();
+    });
+  });
+
+  it('should get INCOMPLETE expense stats', (done) => {
+    transactionService.getTransactionStats.and.returnValue(of(apiTransactionIncompleteStatsRes));
+
+    const apiParams = {
+      scalar: true,
+      tx_state: 'in.(DRAFT)',
+      tx_report_id: 'is.null',
+    };
+
+    dashboardService.getIncompleteExpensesStats().subscribe((res) => {
+      expect(res).toEqual(expectedIncompleteExpStats);
+      expect(transactionService.getTransactionStats).toHaveBeenCalledWith('count(tx_id),sum(tx_amount)', apiParams);
+      done();
+    });
+  });
+
+  it('should get Report stats', (done) => {
+    reportService.getReportStats.and.returnValue(of(new StatsResponse(apiReportStatsRes)));
+    const apiParams = {
+      scalar: false,
+      dimension_1_1: 'rp_state',
+      aggregates: 'sum(rp_amount),count(rp_id)',
+    };
+
+    dashboardService.getReportsStats().subscribe((res) => {
+      expect(res).toEqual(expectedReportStats);
+      done();
+    });
   });
 });
