9bdd68a29c3360fd7ed1837f4af324fabd5777f4
jayfyle <115472256+jayfyle@users.noreply.github.com>
2023-01-17 12:06:47 +0530

    Code Coverage: Dashboard Service #2 (#1595)
    * added data file
    
    * code coverage at 94
    
    * added data files as per models
    
    * comments resolved -1
    
    * comments resolved -2
    
    * renamed response data
    
    * added params to model and data files
    
    * renamed types and created local declarations
    
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>

---
+++ src/app/core/mock-data/stats-dimension-response.data.ts
@@ -0,0 +1,48 @@
+import { StatsDimensionResponse } from '../models/stats-dimension-response.model';
+
+export const apiTxnUnreportedStatsRes: StatsDimensionResponse[] = [
+  {
+    aggregates: [
+      {
+        function_name: 'count(tx_id)',
+        function_value: 6,
+      },
+      {
+        function_name: 'sum(tx_amount)',
+        function_value: 16748.73,
+      },
+    ],
+    dimensions: [],
+    name: 'scalar_stat',
+  },
+];
+
+export const apiTxnIncompleteStatsRes: StatsDimensionResponse[] = [
+  {
+    aggregates: [
+      {
+        function_name: 'count(tx_id)',
+        function_value: 1130,
+      },
+      {
+        function_name: 'sum(tx_amount)',
+        function_value: 1148487.57555147,
+      },
+    ],
+    dimensions: [],
+    name: 'scalar_stat',
+  },
+];
+
+export const apiUnreportedParams: Partial<StatsDimensionResponse> = {
+  scalar: true,
+  tx_state: 'in.(COMPLETE)',
+  or: '(tx_policy_amount.is.null,tx_policy_amount.gt.0.0001)',
+  tx_report_id: 'is.null',
+};
+
+export const apiIncompleteParams: Partial<StatsDimensionResponse> = {
+  scalar: true,
+  tx_state: 'in.(DRAFT)',
+  tx_report_id: 'is.null',
+};
