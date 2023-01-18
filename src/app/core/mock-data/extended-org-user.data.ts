a86fb567e55e79319956ace1c3afdf33354f32b5
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-01-18 19:24:46 +0530

    testbench setup and initial tests added

---
+++ src/app/core/mock-data/extended-org-user.data.ts
@@ -0,0 +1,103 @@
+import { ExtendedOrgUser } from '../models/extended-org-user.model';
+
+export const apiEouRes: ExtendedOrgUser = {
+  ou: {
+    id: 'ouX8dwsbLCLv',
+    created_at: new Date('2018-02-01T02:32:25.267Z'),
+    org_id: 'orNVthTo2Zyo',
+    user_id: 'usvKA4X8Ugcr',
+    employee_id: '',
+    location: 'Mumbai',
+    level: '123',
+    level_id: 'lvlPtroPaClQy',
+    band: 'Very Long Level name Very Long Level name Very Long Level name Very Long Level name Very Long Level name Very Long Level name Very Long Level name Very Long Level name',
+    rank: 1121212121,
+    business_unit:
+      'A very long Business Unit indeed A very long Business Unit indeed A very long Business Unit indeed A very long Business Unit indeed',
+    department_id: 'deptpmQ0SsMO0S',
+    department: '0000000',
+    sub_department: null,
+    roles: ['FINANCE', 'ADMIN', 'APPROVER', 'FYLER', 'VERIFIER', 'PAYMENT_PROCESSOR', 'AUDITOR', 'HOP', 'HOD', 'OWNER'],
+    approver1_id: null,
+    approver2_id: 'oubQzXeZbwbS',
+    approver3_id: null,
+    delegatee_id: null,
+    delegation_start_at: null,
+    delegation_end_at: null,
+    title: 'director',
+    status: 'ACTIVE',
+    branch_ifsc: 'ICIC0002322',
+    branch_account: null,
+    mobile: '123456',
+    mobile_verified: false,
+    mobile_verified_at: null,
+    is_primary: true,
+    owner: false,
+    joining_dt: '2017-07-25T00:00:00.000+0000',
+    special_email: 'receipts+ajain_6@fyle.ai',
+    custom_field_values: [
+      {
+        id: 415,
+        name: 'nj',
+        value: 2222,
+      },
+      {
+        id: 790,
+        name: 'nj1233',
+        value: 'hvgv',
+      },
+      {
+        id: 792,
+        name: 'nj12331',
+        value: 'asdf',
+      },
+      {
+        id: 793,
+        name: 'nj123312',
+        value: 'asd',
+      },
+      {
+        id: 685,
+        name: 'Hybrid human',
+        value: null,
+      },
+    ],
+    org_name: 'Staging Loaded',
+    settings_id: 'ousS9MgDNQ6NB',
+    default_cost_center_id: null,
+    default_cost_center_name: null,
+    default_cost_center_code: null,
+    cost_center_ids: [13792, 13793, 13794, 14018, 13795],
+  },
+  org: {
+    domain: 'fyle.in',
+    currency: 'INR',
+  },
+  us: {
+    id: 'usvKA4X8Ugcr',
+    created_at: new Date('2016-06-13T12:21:16.803Z'),
+    full_name: 'Abhishek Jain',
+    email: 'ajain@fyle.in',
+    email_verified_at: new Date('2022-09-06T05:26:19.898Z'),
+    onboarded: true,
+  },
+  ap1: {
+    full_name: null,
+    email: null,
+  },
+  ap2: {
+    full_name: 'AA23',
+    email: 'ajain+12+12+1@fyle.in',
+  },
+  ap3: {
+    full_name: null,
+    email: null,
+  },
+  bb: {
+    bank_name: 'ICICI BANK LIMITED',
+  },
+  dwolla: {
+    customer_id: 'dwcJzfwZCgwkdfG',
+    bank_account_added: true,
+  },
+};
--- src/app/core/services/report.service.spec.ts
+++ src/app/core/services/report.service.spec.ts
@@ -1,16 +1,205 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ReportService } from './report.service';
+import { NetworkService } from './network.service';
+import { ApiService } from './api.service';
+import { ApiV2Service } from './api-v2.service';
+import { AuthService } from './auth.service';
+import { DateService } from './date.service';
+import { DataTransformService } from './data-transform.service';
+import { TransactionService } from './transaction.service';
+import { StorageService } from './storage.service';
+import { SpenderPlatformApiService } from './spender-platform-api.service';
+import { DatePipe } from '@angular/common';
+import { UserEventService } from './user-event.service';
+import { PermissionsService } from './permissions.service';
+import { LaunchDarklyService } from './launch-darkly.service';
+import { PAGINATION_SIZE } from 'src/app/constants';
+import { of } from 'rxjs';
 
-xdescribe('ReportService', () => {
-  let service: ReportService;
+import { apiEouRes } from '../mock-data/extended-org-user.data';
+import { apiReportRes1, apiReportRes2, apiReportRes } from '../mock-data/api-reports.data';
+import { apiReportActions } from '../mock-data/report-actions.data';
+import { apiExpenseRes } from '../mock-data/expense.data';
+
+describe('ReportService', () => {
+  let reportService: ReportService;
+  let dataTransformService: DataTransformService;
+  let dateService: DateService;
+  let apiService: jasmine.SpyObj<ApiService>;
+  let apiv2Service: jasmine.SpyObj<ApiV2Service>;
+  let authService: jasmine.SpyObj<AuthService>;
+  let storageService: jasmine.SpyObj<StorageService>;
+  let userEventService: jasmine.SpyObj<UserEventService>;
+  let spenderPlatformApiService: jasmine.SpyObj<SpenderPlatformApiService>;
+  let permissionsService: jasmine.SpyObj<PermissionsService>;
+  let transactionService: jasmine.SpyObj<TransactionService>;
+  let networkService: jasmine.SpyObj<NetworkService>;
+  let launchDarklyService: LaunchDarklyService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ReportService);
+    const apiServiceSpy = jasmine.createSpyObj('ApiService', ['get', 'post']);
+    const apiv2ServiceSpy = jasmine.createSpyObj('ApiV2Service', ['get', 'post']);
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['getEou']);
+    const networkServiceSpy = jasmine.createSpyObj('NetworkSpy', ['isOnline']);
+    const storageServiceSpy = jasmine.createSpyObj('StorageService', ['set', 'get']);
+    const transactionServiceSpy = jasmine.createSpyObj('TransactionService', ['clearCache']);
+    const userEventServiceSpy = jasmine.createSpyObj('UserEventServive', ['clearTaskCache', 'onLogout']);
+    const spenderPlatformApiServiceSpy = jasmine.createSpyObj('SpenderPlatformService', ['post']);
+    const permissionsServiceSpy = jasmine.createSpyObj('PermissionService', ['getAllowedActions']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        ReportService,
+        DatePipe,
+        DataTransformService,
+        LaunchDarklyService,
+        {
+          provide: ApiService,
+          useValue: apiServiceSpy,
+        },
+        {
+          provide: ApiV2Service,
+          useValue: apiv2ServiceSpy,
+        },
+        {
+          provide: AuthService,
+          useValue: authServiceSpy,
+        },
+        {
+          provide: NetworkService,
+          useValue: networkServiceSpy,
+        },
+        {
+          provide: StorageService,
+          useValue: storageServiceSpy,
+        },
+        {
+          provide: TransactionService,
+          useValue: transactionServiceSpy,
+        },
+        {
+          provide: UserEventService,
+          useValue: userEventServiceSpy,
+        },
+        {
+          provide: SpenderPlatformApiService,
+          useValue: spenderPlatformApiServiceSpy,
+        },
+        {
+          provide: PermissionsService,
+          useValue: permissionsServiceSpy,
+        },
+        {
+          provide: PAGINATION_SIZE,
+          useValue: 2,
+        },
+      ],
+    });
+
+    reportService = TestBed.inject(ReportService);
+    dateService = TestBed.inject(DateService);
+    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
+    apiv2Service = TestBed.inject(ApiV2Service) as jasmine.SpyObj<ApiV2Service>;
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    dataTransformService = TestBed.inject(DataTransformService);
+    networkService = TestBed.inject(NetworkService) as jasmine.SpyObj<NetworkService>;
+    storageService = TestBed.inject(StorageService) as jasmine.SpyObj<StorageService>;
+    transactionService = TestBed.inject(TransactionService) as jasmine.SpyObj<TransactionService>;
+    userEventService = TestBed.inject(UserEventService) as jasmine.SpyObj<UserEventService>;
+    spenderPlatformApiService = TestBed.inject(SpenderPlatformApiService) as jasmine.SpyObj<SpenderPlatformApiService>;
+    permissionsService = TestBed.inject(PermissionsService) as jasmine.SpyObj<PermissionsService>;
+    launchDarklyService = TestBed.inject(LaunchDarklyService) as jasmine.SpyObj<LaunchDarklyService>;
   });
 
+  function fixDates(res) {
+    return {
+      ...res,
+      data: dateService.fixDates(res.data),
+    };
+  }
+
+  function getExtendedOrgUser() {
+    authService.getEou.and.returnValue(Promise.resolve(apiEouRes));
+  }
+
+  function getReports() {
+    apiv2Service.get.and.returnValue(of(apiReportRes));
+  }
+
+  function getPaginatedReports() {
+    apiv2Service.get.and.returnValue(of(apiReportRes1));
+    apiv2Service.get.and.returnValue(of(apiReportRes2));
+  }
+
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(reportService).toBeTruthy();
+  });
+
+  it('getMyReports(): should get reports from API as specified by params', (done) => {
+    getExtendedOrgUser();
+    getReports();
+
+    const params = {
+      offset: 0,
+      limit: 10,
+      queryParams: {
+        or: [],
+      },
+      order: 'rp_created_at.desc',
+    };
+
+    reportService.getMyReports(params).subscribe((res) => {
+      expect(res).toEqual(fixDates(res));
+      done();
+    });
+  });
+
+  it('getMyReportsCount(): should get reports count', (done) => {
+    getExtendedOrgUser();
+    getReports();
+
+    reportService.getMyReportsCount({}).subscribe((res) => {
+      expect(res).toEqual(4);
+      done();
+    });
+  });
+
+  it('actions(): should get related actions for a report', (done) => {
+    apiService.get.and.returnValue(of(apiReportActions));
+
+    const reportID = 'rpxtbiLXQZUm';
+
+    reportService.actions(reportID).subscribe((res) => {
+      expect(res).toEqual(apiReportActions);
+      expect(apiService.get).toHaveBeenCalledWith(`/reports/${reportID}/actions`);
+      done();
+    });
+  });
+
+  it('getExports(): should get export actions for a report', (done) => {
+    apiService.get.and.returnValue(of([]));
+
+    const reportID = 'rphLXGFVbDaJ';
+
+    reportService.getExports(reportID).subscribe(() => {
+      expect(apiService.get).toHaveBeenCalledWith(`/reports/${reportID}/exports`);
+      done();
+    });
+  });
+
+  it('getReportETxnc(): should get report transactions', (done) => {
+    apiService.get.and.returnValue(of(apiExpenseRes));
+    const reportID = 'rp1xCiq5WA1R';
+    const orgUserID = 'ouCI4UQ2G0K1';
+
+    reportService.getReportETxnc(reportID, orgUserID).subscribe((res) => {
+      expect(apiService.get).toHaveBeenCalledWith(`/erpts/${reportID}/etxns`, {
+        params: {
+          approver_id: orgUserID,
+        },
+      });
+      done();
+    });
   });
 });
