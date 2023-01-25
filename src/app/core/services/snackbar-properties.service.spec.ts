f45588c5c5bf1a704b1136853f9faca5041de6b3
Manjiri Chavande <82377810+CODEX108@users.noreply.github.com>
2023-01-25 13:50:49 +0530

    Code Coverage subtask 85zrja8dj : Snackbar properties service (#1630)
    * Code Coverage subtask: Snackbar properties service
    
    * renamed a test case condition, a small change in naming a spec

--- src/app/core/services/snackbar-properties.service.spec.ts
+++ src/app/core/services/snackbar-properties.service.spec.ts
@@ -2,15 +2,52 @@ import { TestBed } from '@angular/core/testing';
 
 import { SnackbarPropertiesService } from './snackbar-properties.service';
 
-xdescribe('SnackbarPropertiesService', () => {
+describe('SnackbarPropertiesService', () => {
   let service: SnackbarPropertiesService;
 
   beforeEach(() => {
+    //arrange
     TestBed.configureTestingModule({});
     service = TestBed.inject(SnackbarPropertiesService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return the correct icon for a success toast message', () => {
+    //act
+    const properties = service.setSnackbarProperties('success', { message: 'Success message' });
+    //assert
+    expect(properties.data.icon).toEqual('tick-square-filled');
+  });
+
+  it('should return the correct icon for a failure toast message', () => {
+    const properties = service.setSnackbarProperties('failure', { message: 'Failure message' });
+    expect(properties.data.icon).toEqual('danger');
+  });
+
+  it('should return correct icon for a information toast message', () => {
+    const properties = service.setSnackbarProperties('information', { message: 'Information message' });
+    expect(properties.data.icon).toEqual('');
+  });
+
+  it('should return the correct duration', () => {
+    const properties = service.setSnackbarProperties('success', { message: 'Success message' });
+    expect(properties.duration).toEqual(3000);
+  });
+
+  it('should return redirection text in data', () => {
+    const properties = service.setSnackbarProperties('success', {
+      message: 'Success message',
+      redirectiontext: 'redirection',
+    });
+    expect(properties.data.message).toEqual('Success message');
+    expect(properties.data.redirectiontext).toEqual('redirection');
+  });
+
+  it('should return showCloseButton as true', () => {
+    const properties = service.setSnackbarProperties('success', { message: 'Success message' });
+    expect(properties.data.showCloseButton).toEqual(true);
   });
 });
