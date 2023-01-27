954d35dec0be0cfde0f6779744835def857de8ec
Manjiri Chavande <manjiri.c@fyle.in>
2023-01-27 16:40:09 +0530

    Code Coverage : Cost Center Services subtask 2 : 85zrk1m8b Add params and return types with models

--- src/app/core/services/cost-centers.service.spec.ts
+++ src/app/core/services/cost-centers.service.spec.ts
@@ -1,16 +1,32 @@
 import { TestBed } from '@angular/core/testing';
-
+import { PAGINATION_SIZE } from 'src/app/constants';
+import { SpenderPlatformApiService } from './spender-platform-api.service';
 import { CostCentersService } from './cost-centers.service';
 
-xdescribe('CostCentersService', () => {
-  let service: CostCentersService;
+describe('CostCentersService', () => {
+  let costCentersService: CostCentersService;
+  let spenderPlatformApiService: jasmine.SpyObj<SpenderPlatformApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CostCentersService);
+    const spenderPlatformApiServiceSpy = jasmine.createSpyObj('SpenderPlatformApiService', ['get']);
+    TestBed.configureTestingModule({
+      providers: [
+        CostCentersService,
+        {
+          provide: SpenderPlatformApiService,
+          useValue: spenderPlatformApiServiceSpy,
+        },
+        {
+          provide: PAGINATION_SIZE,
+          useValue: 2,
+        },
+      ],
+    });
+    costCentersService = TestBed.inject(CostCentersService);
+    spenderPlatformApiService = TestBed.inject(SpenderPlatformApiService) as jasmine.SpyObj<SpenderPlatformApiService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(costCentersService).toBeTruthy();
   });
 });
