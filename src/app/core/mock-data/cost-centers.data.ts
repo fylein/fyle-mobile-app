dfb60e2449c75e705e1da1ea066962609364cb35
Manjiri Chavande <manjiri.c@fyle.in>
2023-01-28 15:22:12 +0530

    Code coverage subtask 7-85zrk1m9u :Cost Centers Service

---
+++ src/app/core/mock-data/cost-centers.data.ts
@@ -0,0 +1,117 @@
+export const apiCostCenterSingleResponse = {
+  count: 4,
+  data: [
+    {
+      code: 'code',
+      created_at: '2018-02-01T07:23:14.321866+00:00',
+      description: null,
+      id: 37,
+      is_enabled: true,
+      name: 'Marketing',
+      org_id: 'orNVthTo2Zyo',
+      updated_at: '2019-01-08T10:51:37.721398+00:00',
+    },
+  ],
+  offset: 0,
+};
+
+export const apiCostServiceFirst = {
+  count: 4,
+  data: [
+    {
+      code: null,
+      created_at: '2019-06-24T15:12:04.002242+00:00',
+      description: null,
+      id: 2411,
+      is_enabled: true,
+      name: 'SMS1',
+      org_id: 'orNVthTo2Zyo',
+      updated_at: '2019-06-24T15:12:05.002242+00:00',
+    },
+    {
+      code: 'aadsf',
+      created_at: '2019-08-13T14:18:54.500829+00:00',
+      description: 'adsf',
+      id: 2428,
+      is_enabled: true,
+      name: 'test cost',
+      org_id: 'orNVthTo2Zyo',
+      updated_at: '2019-08-13T14:18:55.500829+00:00',
+    },
+  ],
+  offset: 0,
+};
+
+export const apiCostserviceSecond = {
+  count: 4,
+  data: [
+    {
+      code: null,
+      created_at: '2019-10-30T07:16:37.975193+00:00',
+      description: null,
+      id: 3247,
+      is_enabled: true,
+      name: 'asdbusakhdm',
+      org_id: 'orNVthTo2Zyo',
+      updated_at: '2019-10-30T07:16:38.975193+00:00',
+    },
+    {
+      code: 'ad',
+      created_at: '2019-10-30T08:25:54.040980+00:00',
+      description: 'ad',
+      id: 3260,
+      is_enabled: true,
+      name: 'ab',
+      org_id: 'orNVthTo2Zyo',
+      updated_at: '2022-05-19T11:58:29.364759+00:00',
+    },
+  ],
+  offset: 2,
+};
+
+export const ActiveCostCenter = {
+  count: 4,
+  data: [
+    {
+      active: true,
+      code: 'code',
+      created_at: '2018-02-01T07:23:14.321866+00:00',
+      description: null,
+      id: 37,
+      name: 'Marketing',
+      org_id: 'orNVthTo2Zyo',
+      updated_at: '2019-01-08T10:51:37.721398+00:00',
+    },
+    {
+      active: true,
+      code: 'Administrion',
+      created_at: '2019-02-01T06:42:26.089771+00:00',
+      description: 'Administar4rtion',
+      id: 85,
+      name: 'Administration',
+      org_id: 'orNVthTo2Zyo',
+      updated_at: '2019-02-01T06:58:37.247798+00:00',
+    },
+    {
+      active: true,
+      code: 'Administration',
+      created_at: '2019-02-01T06:58:11.496848+00:00',
+      description: 'Administartion1',
+      id: 86,
+      name: 'Administeoerration',
+      org_id: 'orNVthTo2Zyo',
+      updated_at: '2019-08-24T23:10:28.798154+00:00',
+    },
+    {
+      active: true,
+      code: 'Administriondcfvgbhnj',
+      created_at: '2019-02-01T06:58:49.388927+00:00',
+      description: 'Administar4rtion',
+      id: 88,
+      name: 'Administr2232a-44tion',
+      org_id: 'orNVthTo2Zyo',
+      updated_at: '2020-02-17T08:30:32.603683+00:00',
+    },
+  ],
+  offset: 0,
+};
