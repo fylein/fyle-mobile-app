7e60b06ee733c9fc5b73e51fbd2a112924f4f82d
Manjiri Chavande <manjiri.c@fyle.in>
2023-01-30 13:04:47 +0530

    Code coverage: Advance request custom fields service

--- src/app/core/services/advance-requests-custom-fields.service.spec.ts
+++ src/app/core/services/advance-requests-custom-fields.service.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed } from '@angular/core/testing';
-
+import { advanceRequestCustomField } from '../mock-data/advance-requests-custom-fields.data';
 import { AdvanceRequestsCustomFieldsService } from './advance-requests-custom-fields.service';
+import { of } from 'rxjs';
+import { ApiService } from './api.service';
 
-xdescribe('AdvanceRequestsCustomFieldsService', () => {
-  let service: AdvanceRequestsCustomFieldsService;
+describe('AdvanceRequestsCustomFieldsService', () => {
+  let advanceRequestsCustomFieldsService: AdvanceRequestsCustomFieldsService;
+  let apiServiceSpy: jasmine.SpyObj<ApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AdvanceRequestsCustomFieldsService);
+    apiServiceSpy = jasmine.createSpyObj('ApiService', ['get']);
+    advanceRequestsCustomFieldsService = new AdvanceRequestsCustomFieldsService(apiServiceSpy);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(advanceRequestsCustomFieldsService).toBeTruthy();
+  });
+
+  it('should return all advance requests custom fields', () => {
+    apiServiceSpy.get.and.returnValue(of(advanceRequestCustomField));
+
+    advanceRequestsCustomFieldsService.getAll().subscribe((res) => {
+      expect(res).toEqual(advanceRequestCustomField);
+    });
+    expect(apiServiceSpy.get).toHaveBeenCalledWith('/advance_request_custom_fields');
   });
 });
