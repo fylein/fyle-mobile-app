0a43ae1665955b1cf37e953302ef85a341323417
Manjiri Chavande <manjiri.c@fyle.in>
2023-02-02 01:19:42 +0530

    FYLE-85zrk1mt8 Code coverage : Singular pipe

--- src/app/shared/pipes/singular.pipe.spec.ts
+++ src/app/shared/pipes/singular.pipe.spec.ts
@@ -1,8 +1,28 @@
 import { SingularPipe } from './singular.pipe';
 
 describe('SingularPipe', () => {
+  let pipe: SingularPipe;
+
+  beforeEach(() => {
+    pipe = new SingularPipe();
+  });
+
+  const string1 = 'Locations';
+  const string2 = 'Testing';
+
   it('create an instance', () => {
-    const pipe = new SingularPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it(`should remove "s" from the end of a "${string1}"`, () => {
+    expect(pipe.transform(string1)).toBe('Location');
+  });
+
+  it('should return the same string if it does not end with "s"', () => {
+    expect(pipe.transform(string2)).toBe('Testing');
+  });
+
+  it('should return an empty string if the input is falsy', () => {
+    expect(pipe.transform('')).toBe('');
   });
 });
