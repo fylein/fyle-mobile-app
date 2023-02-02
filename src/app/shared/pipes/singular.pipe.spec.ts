dad6ce94d47468c0c6c064ca255b0809102d5d98
Manjiri Chavande <manjiri.c@fyle.in>
2023-02-02 12:01:22 +0530

    resolved the comments in the PR

--- src/app/shared/pipes/singular.pipe.spec.ts
+++ src/app/shared/pipes/singular.pipe.spec.ts
@@ -1,28 +1,23 @@
 import { SingularPipe } from './singular.pipe';
 
 describe('SingularPipe', () => {
-  let pipe: SingularPipe;
-
-  beforeEach(() => {
-    pipe = new SingularPipe();
-  });
-
+  const singularPipe = new SingularPipe();
   const string1 = 'Locations';
   const string2 = 'Testing';
 
   it('create an instance', () => {
-    expect(pipe).toBeTruthy();
+    expect(singularPipe).toBeTruthy();
   });
 
-  it(`should remove "s" from the end of a "${string1}"`, () => {
-    expect(pipe.transform(string1)).toBe('Location');
+  it('SingularPipe transform() : should remove s from the end of a string', () => {
+    expect(singularPipe.transform(string1)).toBe('Location');
   });
 
-  it('should return the same string if it does not end with "s"', () => {
-    expect(pipe.transform(string2)).toBe('Testing');
+  it('SingularPipe transform() : should return the same string if it does not end with "s"', () => {
+    expect(singularPipe.transform(string2)).toBe('Testing');
   });
 
-  it('should return an empty string if the input is falsy', () => {
-    expect(pipe.transform('')).toBe('');
+  it('SingularPipe transform() : should return an empty string if the input is falsy', () => {
+    expect(singularPipe.transform('')).toBe('');
   });
 });
