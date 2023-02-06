a55cb8337898c4fc5514b3cdf1687bc2d00139e6
Manjiri Chavande <manjiri.c@fyle.in>
2023-02-06 17:15:19 +0530

    resolved comments

--- src/app/core/services/handle-duplicates.service.spec.ts
+++ src/app/core/services/handle-duplicates.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { ApiService } from './api.service';
 import { HandleDuplicatesService } from './handle-duplicates.service';
 import { of } from 'rxjs';
-import { handleDuplicatesData } from '../mock-data/handle-duplicates.service.data';
+import { handleDuplicatesDataResponse } from '../mock-data/handle-duplicates.service.data';
 
 describe('HandleDuplicatesService', () => {
   let handleDuplicatesService: HandleDuplicatesService;
@@ -28,43 +28,39 @@ describe('HandleDuplicatesService', () => {
   });
 
   it('getDuplicateSets() : should get all the duplicate sets', (done) => {
-    apiService.get.and.returnValue(of(handleDuplicatesData));
+    apiService.get.and.returnValue(of(handleDuplicatesDataResponse));
     handleDuplicatesService.getDuplicateSets().subscribe((res) => {
-      expect(handleDuplicatesData).toEqual(res);
-      expect(apiService.get).toHaveBeenCalledWith('/transactions/duplicates/sets');
+      expect(handleDuplicatesDataResponse).toEqual(res);
+      expect(apiService.get).toHaveBeenCalledOnceWith('/transactions/duplicates/sets');
       done();
     });
   });
 
   it('getDuplicatesByExpense() : should call the API  with the correct parameters and return the expected data from the API', (done) => {
     const txnId = 'txaiCW1efU0n';
-    handleDuplicatesService.getDuplicatesByExpense(txnId);
-
-    expect(apiService.get).toHaveBeenCalledWith('/transactions/duplicates/sets', {
-      params: {
-        txn_id: txnId,
-      },
-    });
-    expect(apiService.get).toHaveBeenCalledTimes(1);
-
-    apiService.get.and.returnValue(of(handleDuplicatesData));
+    apiService.get.and.returnValue(of(handleDuplicatesDataResponse));
     handleDuplicatesService.getDuplicatesByExpense(txnId).subscribe((res) => {
-      expect(res).toEqual(handleDuplicatesData);
+      expect(res).toEqual(handleDuplicatesDataResponse);
+      expect(apiService.get).toHaveBeenCalledOnceWith('/transactions/duplicates/sets', {
+        params: {
+          txn_id: txnId,
+        },
+      });
     });
-    expect(apiService.get).toHaveBeenCalledTimes(2);
     done();
   });
 
-  it('dismissAll() : should call the API service with correct params and dismiss all the duplicates', (done) => {
+  it('dismissAll() : should dismiss all duplicates', (done) => {
+    apiService.post.and.returnValue(of(null));
     const duplicateSetTransactionIds = ['tx5CixxvxeAF', 'txCyV7WCXXqD'];
     const transactionIds = ['tx5CixxvxeAF', 'txCyV7WCXXqD'];
-    handleDuplicatesService.dismissAll(duplicateSetTransactionIds, transactionIds);
-    expect(apiService.post).toHaveBeenCalledWith('/transactions/duplicates/dismiss', {
-      duplicate_set_transaction_ids: duplicateSetTransactionIds,
-      transaction_ids: transactionIds,
+    handleDuplicatesService.dismissAll(duplicateSetTransactionIds, transactionIds).subscribe((res) => {
+      expect(res).toBeNull();
+      expect(apiService.post).toHaveBeenCalledOnceWith('/transactions/duplicates/dismiss', {
+        duplicate_set_transaction_ids: duplicateSetTransactionIds,
+        transaction_ids: transactionIds,
+      });
+      done();
     });
-    apiService.post.and.returnValue(of(null));
-    expect(apiService.post).toHaveBeenCalledTimes(1);
-    done();
   });
 });
