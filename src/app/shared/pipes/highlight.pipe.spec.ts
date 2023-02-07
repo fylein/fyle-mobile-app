64a4e3087910f2c692f21a05a8c4f912b87256da
Manjiri Chavande <manjiri.c@fyle.in>
2023-02-07 16:27:16 +0530

    added test for special characters

--- src/app/shared/pipes/highlight.pipe.spec.ts
+++ src/app/shared/pipes/highlight.pipe.spec.ts
@@ -6,7 +6,7 @@ describe('HighlightPipe', () => {
     expect(highlightPipe).toBeTruthy();
   });
 
-  it('highlightPipe transform() : should highlight the search term in the text', () => {
+  it('highlightPipe transform() : should highlight the search query in the text', () => {
     const text = 'Staging Project';
     const search = 'Staging';
     const expectedResult = '<span class="highlight">Staging</span> Project';
@@ -16,18 +16,17 @@ describe('HighlightPipe', () => {
     expect(result).toBe(expectedResult);
   });
 
-  it('highlightPipe transform() : should handle a search term with multiple words', () => {
-    const text = 'This is some text that we want to highlight multiple words';
-    const search = 'highlight multiple';
-    const expectedResult =
-      'This is some text that we want to <span class="highlight">highlight</span> <span class="highlight">multiple</span> words';
+  it('highlightPipe transform() : should handle a search query with multiple words', () => {
+    const text = 'Project Dummy Test-1';
+    const search = 'Dummy Test-1';
+    const expectedResult = 'Project <span class="highlight">Dummy</span> <span class="highlight">Test-1</span>';
 
     const result = highlightPipe.transform(text, search);
 
     expect(result).toBe(expectedResult);
   });
 
-  it('highlightPipe transform() : should handle a search term with special character', () => {
+  it('highlightPipe transform() : should handle a search query with special character', () => {
     const text = 'Testing Project FAE 1/Test 1';
     const search = '/';
     const expectedResult = 'Testing Project FAE 1<span class="highlight">/</span>Test 1';
@@ -37,7 +36,17 @@ describe('HighlightPipe', () => {
     expect(result).toBe(expectedResult);
   });
 
-  it('highlightPipe should handle an empty text input', () => {
+  it('highlightPipe transform() : should handle a search query other with special characters', () => {
+    const text = 'This is some text with special characters: @#$%^+-[]{}()*';
+    const search = '@#$%^+-[]{}()*';
+    const expectedResult = 'This is some text with special characters: <span class="highlight">@#$%^+-[]{}()*</span>';
+
+    const result = highlightPipe.transform(text, search);
+
+    expect(result).toBe(expectedResult);
+  });
+
+  it('highlightPipe transform() : should handle an empty text input', () => {
     const text = '';
     const search = 'None';
     const expectedResult = '';
