442374f5a751292d98e2c6343f27d9b845207ca1
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-02-10 18:04:20 +0530

    intial tests

---
+++ src/app/core/mock-data/api-category.data.ts
@@ -0,0 +1,81 @@
+import { ApiV2Response } from '../models/api-v2.model';
+import { PlatformCategory } from '../models/platform/platform-category.model';
+
+export const apiCategoryCountRes: ApiV2Response<PlatformCategory> = {
+  count: 318,
+  data: [
+    {
+      created_at: new Date('2017-05-09T06:09:47.765967+00:00'),
+      display_name: 'Activity',
+      id: 130,
+      code: '42',
+      is_enabled: true,
+      name: 'Activity',
+      org_id: 'orrjqbDbeP9p',
+      restricted_project_ids: [519, 589, 3837, 243580, 243581],
+      sub_category: null,
+      system_category: 'Activity',
+      updated_at: new Date('2022-05-05T17:45:33.737507+00:00'),
+    },
+  ],
+  offset: 0,
+};
+
+export const apiAllCategoryRes: ApiV2Response<PlatformCategory> = {
+  count: 4,
+  data: [
+    {
+      code: '93',
+      created_at: new Date('2021-05-18T11:40:38.576068+00:00'),
+      display_name: 'Business',
+      id: 141295,
+      is_enabled: true,
+      name: 'Business',
+      org_id: 'orrjqbDbeP9p',
+      restricted_project_ids: [518, 519, 520, 589],
+      sub_category: null,
+      system_category: null,
+      updated_at: new Date('2022-07-01T05:51:31.800155+00:00'),
+    },
+    {
+      code: '98',
+      created_at: new Date('2021-05-18T11:40:38.576068+00:00'),
+      display_name: 'Pager',
+      id: 141300,
+      is_enabled: true,
+      name: 'Pager',
+      org_id: 'orrjqbDbeP9p',
+      restricted_project_ids: [518, 519, 520, 589, 599],
+      sub_category: null,
+      system_category: null,
+      updated_at: new Date('2022-05-05T17:47:06.951957+00:00'),
+    },
+    {
+      code: '43',
+      created_at: new Date('2023-01-09T16:54:09.929285+00:00'),
+      display_name: 'samp category',
+      id: 226646,
+      is_enabled: true,
+      name: 'samp category',
+      org_id: 'orrjqbDbeP9p',
+      restricted_project_ids: null,
+      sub_category: null,
+      system_category: null,
+      updated_at: new Date('2023-01-09T16:54:09.929285+00:00'),
+    },
+    {
+      code: '42',
+      created_at: new Date('2023-01-09T16:54:09.929285+00:00'),
+      display_name: 'Marketing outreach',
+      id: 226659,
+      is_enabled: true,
+      name: 'Marketing outreach',
+      org_id: 'orrjqbDbeP9p',
+      restricted_project_ids: null,
+      sub_category: null,
+      system_category: null,
+      updated_at: new Date('2023-01-09T16:54:09.929285+00:00'),
+    },
+  ],
+  offset: 0,
+};
--- src/app/core/services/categories.service.spec.ts
+++ src/app/core/services/categories.service.spec.ts
@@ -1,16 +1,92 @@
 import { TestBed } from '@angular/core/testing';
-
+import { PAGINATION_SIZE } from 'src/app/constants';
 import { CategoriesService } from './categories.service';
+import { SpenderPlatformV1BetaApiService } from './spender-platform-v1-beta-api.service';
+import { apiCategoryCountRes, apiAllCategoryRes } from '../mock-data/api-category.data';
+import { of } from 'rxjs';
+import { sortedCategory, transformDataCategory } from '../mock-data/org-category.data';
 
-xdescribe('CategoriesService', () => {
-  let service: CategoriesService;
+describe('CategoriesService', () => {
+  let categoriesService: CategoriesService;
+  let spenderPlatformV1BetaApiService: jasmine.SpyObj<SpenderPlatformV1BetaApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CategoriesService);
+    const spenderPlatformV1BetaApiServiceSpy = jasmine.createSpyObj('SpenderPlatformV1BetaApiService', ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CategoriesService,
+        {
+          provide: SpenderPlatformV1BetaApiService,
+          useValue: spenderPlatformV1BetaApiServiceSpy,
+        },
+        {
+          provide: PAGINATION_SIZE,
+          useValue: 2,
+        },
+      ],
+    });
+    categoriesService = TestBed.inject(CategoriesService);
+    spenderPlatformV1BetaApiService = TestBed.inject(
+      SpenderPlatformV1BetaApiService
+    ) as jasmine.SpyObj<SpenderPlatformV1BetaApiService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(categoriesService).toBeTruthy();
+  });
+
+  it('getActiveCategoriesCount(): should get category count', (done) => {
+    spenderPlatformV1BetaApiService.get.and.returnValue(of(apiCategoryCountRes));
+
+    const apiParam = {
+      params: {
+        is_enabled: 'eq.' + true,
+        offset: 0,
+        limit: 1,
+      },
+    };
+
+    categoriesService.getActiveCategoriesCount().subscribe((res) => {
+      expect(res).toEqual(318);
+      expect(spenderPlatformV1BetaApiService.get).toHaveBeenCalledOnceWith('/categories', apiParam);
+      done();
+    });
+  });
+
+  it('sortCategories(): should sort categories', () => {
+    const result = categoriesService.sortCategories(transformDataCategory);
+
+    expect(result).toEqual(sortedCategory);
+  });
+
+  it('getSystemCategories(): should get system categories', () => {
+    const result = categoriesService.getSystemCategories();
+
+    expect(result).toEqual(['Bus', 'Airlines', 'Lodging', 'Train']);
+  });
+
+  it('getSystemCategoriesWithTaxi(): should get system categories with taxi', () => {
+    const result = categoriesService.getSystemCategoriesWithTaxi();
+
+    expect(result).toEqual(['Taxi', 'Bus', 'Airlines', 'Lodging', 'Train']);
+  });
+
+  it('getBreakfastSystemCategories(): should get breakfast system categories', () => {
+    const result = categoriesService.getBreakfastSystemCategories();
+
+    expect(result).toEqual(['Lodging']);
+  });
+
+  it('getTravelSystemCategories(): should get travel system categories', () => {
+    const result = categoriesService.getTravelSystemCategories();
+
+    expect(result).toEqual(['Bus', 'Airlines', 'Train']);
+  });
+
+  it('getFlightSystemCategories(): should get flight system categories', () => {
+    const result = categoriesService.getFlightSystemCategories();
+
+    expect(result).toEqual(['Airlines']);
   });
 });
