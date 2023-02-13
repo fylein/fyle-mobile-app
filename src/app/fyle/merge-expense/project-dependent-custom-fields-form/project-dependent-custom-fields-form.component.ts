9fe8e276fd46794a0888acd4f515a3e9a9fcbce3
yksolanki9 <yashsolanki1709@gmail.com>
2023-02-13 15:19:17 +0530

    Pass dependentInputs to the new component

--- src/app/fyle/merge-expense/project-dependent-custom-fields-form/project-dependent-custom-fields-form.component.ts
+++ src/app/fyle/merge-expense/project-dependent-custom-fields-form/project-dependent-custom-fields-form.component.ts
@@ -1,22 +1,12 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CustomProperty } from 'src/app/core/models/custom-properties.model';
 @Component({
   selector: 'app-project-dependent-custom-fields-form',
   templateUrl: './project-dependent-custom-fields-form.component.html',
   styleUrls: ['./project-dependent-custom-fields-form.component.scss'],
 })
-export class ProjectDependentCustomFieldsFormComponent implements OnChanges {
-  @Input() projectDependentFieldsMapping: { [projectId: number]: CustomProperty<string>[] };
-
-  @Input() selectedProjectId: number;
-
-  dependentInputs: CustomProperty<string>[];
+export class ProjectDependentCustomFieldsFormComponent {
+  @Input() dependentInputs: CustomProperty<string>[] = [];
 
   constructor() {}
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.selectedProjectId?.currentValue) {
-      this.dependentInputs = this.projectDependentFieldsMapping[changes.selectedProjectId.currentValue];
-    }
-  }
 }
