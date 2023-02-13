d252c54b497a4dbdc4b5a8fc1548d84e09929944
yksolanki9 <yashsolanki1709@gmail.com>
2023-02-13 15:53:58 +0530

    Remove unused project dependent field component

--- src/app/fyle/merge-expense/merge-expense.module.ts
+++ src/app/fyle/merge-expense/merge-expense.module.ts
@@ -19,7 +19,6 @@ import { CardTransactionPreviewComponent } from './card-transaction-preview/card
 import { GenericFieldsFormComponent } from './generic-fields-form/generic-fields-form.component';
 import { CategoryDependentFieldsFormComponent } from './category-dependent-fields-form/category-dependent-fields-form.component';
 import { CustomInputsFieldsFormComponent } from './custom-inputs-fields-form/custom-inputs-fields-form.component';
-import { ProjectDependentCustomFieldsFormComponent } from './project-dependent-custom-fields-form/project-dependent-custom-fields-form.component';
 
 @NgModule({
   imports: [
@@ -47,7 +46,6 @@ import { ProjectDependentCustomFieldsFormComponent } from './project-dependent-c
     GenericFieldsFormComponent,
     CategoryDependentFieldsFormComponent,
     CustomInputsFieldsFormComponent,
-    ProjectDependentCustomFieldsFormComponent,
   ],
 })
 export class MergeExpensePageModule {}
