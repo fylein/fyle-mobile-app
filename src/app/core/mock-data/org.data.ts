2f7b60bddb9ef43bb6311f2a892fe51774d4ee22
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2023-02-16 19:49:20 +0530

    Code coverage: Org service - subtask3 (#1675)
    Co-authored-by: Dimple <dimple.kh@fyle.in>

---
+++ src/app/core/mock-data/org.data.ts
@@ -0,0 +1,24 @@
+import { Org } from '../models/org.model';
+
+export const orgData1: Org[] = [
+  {
+    id: 'orNVthTo2Zyo',
+    created_at: new Date('2018-01-31T23:50:27.216Z'),
+    updated_at: new Date('2023-02-16T13:05:08.547Z'),
+    name: 'Staging Loaded',
+    domain: 'fyle.in',
+    currency: 'USD',
+    branch_ifsc: 'ICIC0003440',
+    branch_account: '82382398239239',
+    tally_bank_ledger: null,
+    tally_default_category: null,
+    tally_default_user: null,
+    corporate_credit_card_details: {
+      bank_name: null,
+      number_of_cards: null,
+    },
+    verified: true,
+    lite: false,
+    dwolla_customers_metadata_id: 'dcmbDcwO330Wn',
+  },
+];
--- src/app/core/services/org.service.spec.ts
+++ src/app/core/services/org.service.spec.ts
@@ -1,7 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { globalCacheBusterNotifier } from 'ts-cacheable';
+import { orgData1 } from '../mock-data/org.data';
 import { ApiService } from './api.service';
 import { AuthService } from './auth.service';
-
 import { OrgService } from './org.service';
 
 describe('OrgService', () => {
@@ -33,5 +35,55 @@ describe('OrgService', () => {
 
   it('should be created', () => {
     expect(orgService).toBeTruthy();
+  });
+
+  it('getCurrentOrg(): should get current org', (done) => {
+    apiService.get.and.returnValue(of(orgData1));
+
+    orgService.getCurrentOrg().subscribe((res) => {
+      expect(res).toEqual(orgData1[0]);
+      expect(apiService.get).toHaveBeenCalledOnceWith('/orgs', {
+        params: {
+          is_current: true,
+        },
+      });
+      done();
+    });
+  });
+
+  it('getPrimaryOrg(): should get primary org', (done) => {
+    apiService.get.and.returnValue(of(orgData1[0]));
+
+    orgService.getPrimaryOrg().subscribe((res) => {
+      expect(res).toEqual(orgData1[0]);
+      expect(apiService.get).toHaveBeenCalledOnceWith('/orgs', {
+        params: {
+          is_primary: true,
+        },
+      });
+      done();
+    });
+  });
+
+  it('getPrimaryOrg(): should get orgs', (done) => {
+    apiService.get.and.returnValue(of(orgData1));
+
+    orgService.getOrgs().subscribe((res) => {
+      expect(res).toEqual(orgData1);
+      expect(apiService.get).toHaveBeenCalledTimes(1);
+      done();
+    });
+  });
+
+  it('updateOrg(): should update org', (done) => {
+    apiService.post.and.returnValue(of(orgData1[0]));
+    spyOn(globalCacheBusterNotifier, 'next');
+
+    orgService.updateOrg(orgData1[0]).subscribe((res) => {
+      expect(res).toEqual(orgData1[0]);
+      expect(globalCacheBusterNotifier.next).toHaveBeenCalledBefore(apiService.post);
+      expect(apiService.post).toHaveBeenCalledOnceWith('/orgs', orgData1[0]);
+      done();
+    });
   });
 });
