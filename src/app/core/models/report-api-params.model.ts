6024ca8be40a330d4fac71dcf28dabe359fa3b89
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-02-16 11:59:41 +0530

    resolved comments -5

--- src/app/core/models/report-api-params.model.ts
+++ src/app/core/models/report-api-params.model.ts
@@ -1,3 +1,9 @@
+export interface ReportQueryParams {
+  rp_state?: string;
+  or?: string | string[];
+  rp_id?: string;
+}
+
 export interface ReportApiParams {
   offset?: number;
   limit?: number;
