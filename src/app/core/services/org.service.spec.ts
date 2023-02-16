fa1ef08f2098a5ff874e51aeb068ac48ee006404
Dimple <dimple.kh@fyle.in>
2023-02-16 18:52:34 +0530

    28.57% statements coverage

--- src/app/core/services/org.service.spec.ts
+++ src/app/core/services/org.service.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
+import { ApiService } from './api.service';
+import { AuthService } from './auth.service';
 
 import { OrgService } from './org.service';
 
-xdescribe('OrgService', () => {
-  let service: OrgService;
+describe('OrgService', () => {
+  let orgService: OrgService;
+  let apiService: jasmine.SpyObj<ApiService>;
+  let authService: jasmine.SpyObj<AuthService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(OrgService);
+    apiService = jasmine.createSpyObj('ApiService', ['get', 'post']);
+    authService = jasmine.createSpyObj('AuthService', ['newRefreshToken']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        OrgService,
+        {
+          provide: ApiService,
+          useValue: apiService,
+        },
+        {
+          provide: AuthService,
+          useValue: authService,
+        },
+      ],
+    });
+    orgService = TestBed.inject(OrgService);
+    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(orgService).toBeTruthy();
   });
 });
