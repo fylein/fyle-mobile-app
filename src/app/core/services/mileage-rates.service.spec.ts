cc05496f213a7c0636ba5f4e35caddbeaa508c29
Manjiri Chavande <manjiri.c@fyle.in>
2023-02-17 14:02:49 +0530

    Code coverage: Initial setup and single method coverage 35% covered

--- src/app/core/services/mileage-rates.service.spec.ts
+++ src/app/core/services/mileage-rates.service.spec.ts
@@ -1,16 +1,48 @@
 import { TestBed } from '@angular/core/testing';
-
+import { CurrencyPipe } from '@angular/common';
 import { MileageRatesService } from './mileage-rates.service';
+import { SpenderPlatformV1BetaApiService } from './spender-platform-v1-beta-api.service';
+import { PAGINATION_SIZE } from 'src/app/constants';
+import { filterEnabledMileageRatesData, mileageRatesData } from '../mock-data/mileage-rate.data';
 
-xdescribe('MileageRatesService', () => {
-  let service: MileageRatesService;
+describe('MileageRatesService', () => {
+  let mileageRatesService: MileageRatesService;
+  let spenderPlatformV1BetaApiService: jasmine.SpyObj<SpenderPlatformV1BetaApiService>;
+  let currencyPipe: jasmine.SpyObj<CurrencyPipe>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MileageRatesService);
+    const spenderPlatformV1BetaApiServiceSpy = jasmine.createSpyObj('SpenderPlatformV1BetaApiService', ['get']);
+    TestBed.configureTestingModule({
+      providers: [
+        MileageRatesService,
+        {
+          provide: SpenderPlatformV1BetaApiService,
+          useValue: spenderPlatformV1BetaApiServiceSpy,
+        },
+        {
+          provide: PAGINATION_SIZE,
+          useValue: 2,
+        },
+        {
+          provide: CurrencyPipe,
+          useValue: currencyPipe,
+        },
+      ],
+    });
+    mileageRatesService = TestBed.inject(MileageRatesService);
+    spenderPlatformV1BetaApiService = TestBed.inject(
+      SpenderPlatformV1BetaApiService
+    ) as jasmine.SpyObj<SpenderPlatformV1BetaApiService>;
+    currencyPipe = TestBed.inject(CurrencyPipe) as jasmine.SpyObj<CurrencyPipe>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(mileageRatesService).toBeTruthy();
+  });
+
+  it('filterEnabledMileageRates(): should retutn enabled mileage rates', () => {
+    const result = mileageRatesService.filterEnabledMileageRates(mileageRatesData);
+    expect(result.length).toEqual(filterEnabledMileageRatesData.length);
+    expect(result).toEqual(filterEnabledMileageRatesData);
   });
 });
