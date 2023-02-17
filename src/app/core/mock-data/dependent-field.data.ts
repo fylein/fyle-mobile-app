eff87bd77d01947425ddbcaab7db8e3ba03e623b
yksolanki9 <yashsolanki1709@gmail.com>
2023-02-17 17:24:33 +0530

    Create new model for merge expense custom input

--- src/app/core/mock-data/dependent-field.data.ts
+++ src/app/core/mock-data/dependent-field.data.ts
@@ -1,14 +1,4 @@
-interface CustomInput {
-  id: number;
-  mandatory: boolean;
-  name: string;
-  options: string[];
-  placeholder: string;
-  prefix: string;
-  type: string;
-  value: string;
-  parent_field_id: number;
-}
+import { CustomInput } from '../models/merge-expense-custom-input.model';
 
 export const dependentFields: CustomInput[] = [
   {
