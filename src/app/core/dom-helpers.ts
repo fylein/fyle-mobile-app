5efa380fd02ed599e1223b6372c5fe0b48193413
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2023-02-20 17:08:22 +0530

    Code coverage: Sidemenu header component - 100% (#1685)
    Co-authored-by: Dimple <dimple.kh@fyle.in>

---
+++ src/app/core/dom-helpers.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture } from '@angular/core/testing';
+
+/**
+ * Gets the textContent value in a component fixture using the Element object.
+ *
+ * @template T
+ * @param element - The element in the fixture component.
+ * @returns The text content of the element object.
+ *
+ */
+export const getTextContent = <T>(element: Element): string => (element ? element.textContent.trim() : '');
+
+/**
+ * Finds an element in a component fixture using its query selector.
+ *
+ * @template T
+ * @param fixture - The component fixture to search in.
+ * @param selector - The query selector of the element to find.
+ * @returns The found element represented as an Element object.
+ *
+ */
+export const getElementBySelector = <T>(fixture: ComponentFixture<T>, selector: string): Element =>
+  fixture.nativeElement.querySelector(selector);
--- src/app/shared/components/sidemenu/sidemenu-header/sidemenu-header.component.spec.ts
+++ src/app/shared/components/sidemenu/sidemenu-header/sidemenu-header.component.spec.ts
@@ -1,14 +1,22 @@
+import { TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatRippleModule } from '@angular/material/core';
+import { IonicModule } from '@ionic/angular';
+import { getElementBySelector, getTextContent } from 'src/app/core/dom-helpers';
+import { apiEouRes } from 'src/app/core/mock-data/extended-org-user.data';
+import { orgData1 } from 'src/app/core/mock-data/org.data';
+import { InitialsPipe } from 'src/app/shared/pipes/initials.pipe';
 
 import { SidemenuHeaderComponent } from './sidemenu-header.component';
 
-xdescribe('SidemenuHeaderComponent', () => {
+describe('SidemenuHeaderComponent', () => {
   let component: SidemenuHeaderComponent;
   let fixture: ComponentFixture<SidemenuHeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SidemenuHeaderComponent],
+      declarations: [SidemenuHeaderComponent, InitialsPipe, UpperCasePipe, TitleCasePipe],
+      imports: [IonicModule.forRoot(), MatRippleModule],
     }).compileComponents();
   });
 
@@ -20,5 +28,27 @@ xdescribe('SidemenuHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit the profileClicked event when onProfileClicked is called', () => {
+    const spy = spyOn(component.profileClicked, 'emit');
+    const event = new Event('click');
+
+    component.onProfileClicked(event);
+    expect(spy).toHaveBeenCalledWith(event);
+  });
+
+  it('should display the user name and organization name', () => {
+    component.eou = apiEouRes;
+    component.activeOrg = orgData1[0];
+    fixture.detectChanges();
+
+    const userNameInitialEl = getElementBySelector(fixture, '.sidemenu-header__icon-container__text');
+    const activeOrgEl = getElementBySelector(fixture, '.sidemenu-header__content-container__sub-title');
+
+    expect(userNameInitialEl).toBeTruthy();
+    expect(getTextContent(userNameInitialEl)).toContain('AJ');
+    expect(activeOrgEl).toBeTruthy();
+    expect(getTextContent(activeOrgEl)).toContain('Staging Loaded');
   });
 });
