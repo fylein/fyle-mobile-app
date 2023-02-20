3ba480f355dcbf6b75d45a82bd1f99396918bc0a
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2023-02-20 17:25:30 +0530

    Code coverage: Custom fields service 92%  (#1690)
    Co-authored-by: Dimple <dimple.kh@fyle.in>

--- src/app/core/models/txn-custom-properties.model.ts
+++ src/app/core/models/txn-custom-properties.model.ts
@@ -2,9 +2,9 @@ export interface TxnCustomProperties {
   id?: number;
   mandatory?: boolean;
   name: string;
-  options?: { label: string; value: string }[] | [];
+  options?: { label: string; value: string }[] | string[];
   placeholder?: string;
   prefix?: string;
   type?: string;
-  value: string | string[] | Date;
+  value: any;
 }
--- src/app/core/services/custom-fields.service.spec.ts
+++ src/app/core/services/custom-fields.service.spec.ts
@@ -1,16 +1,90 @@
 import { TestBed } from '@angular/core/testing';
+import { customPropertiesData } from '../mock-data/custom-property.data';
+import { customExpensefields } from '../mock-data/expense-field.data';
+import {
+  txnCustomPropertiesData,
+  txnCustomPropertiesData3,
+  txnCustomPropertiesData4,
+} from '../mock-data/txn-custom-properties.data';
+import { customInputData, platformApiResponse } from '../test-data/custom-inputs.spec.data';
 
 import { CustomFieldsService } from './custom-fields.service';
 
-xdescribe('CustomFieldsService', () => {
-  let service: CustomFieldsService;
+describe('CustomFieldsService', () => {
+  let customFieldsService: CustomFieldsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(CustomFieldsService);
+    customFieldsService = TestBed.inject(CustomFieldsService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(customFieldsService).toBeTruthy();
+  });
+
+  describe('sortcustomFieldsByType():', () => {
+    it('should sort custom fields by type in descending order', () => {
+      const result = customFieldsService.sortcustomFieldsByType(txnCustomPropertiesData[0], txnCustomPropertiesData[2]);
+      expect(result).toBe(-1);
+    });
+
+    it('should sort custom fields by type in ascending order', () => {
+      const result = customFieldsService.sortcustomFieldsByType(txnCustomPropertiesData[2], txnCustomPropertiesData[0]);
+      expect(result).toBe(1);
+    });
+
+    it('should return 0 for custom fields with the same type', () => {
+      const result = customFieldsService.sortcustomFieldsByType(txnCustomPropertiesData[0], txnCustomPropertiesData[1]);
+      expect(result).toBe(0);
+    });
+  });
+
+  describe('setDefaultValue():', () => {
+    it('should set default value to false for boolean type', () => {
+      const result = customFieldsService.setDefaultValue(txnCustomPropertiesData3[0], 'BOOLEAN');
+      expect(result.value).toBe(false);
+    });
+
+    it('should set default value to empty string for select type', () => {
+      const result = customFieldsService.setDefaultValue(txnCustomPropertiesData3[1], 'SELECT');
+      expect(result.value).toBe('');
+    });
+
+    it('should set default value to empty string for multi select type', () => {
+      const result = customFieldsService.setDefaultValue(txnCustomPropertiesData[2], 'MULTI_SELECT');
+      expect(result.value).toBe('');
+    });
+
+    it('should set default value to empty string for user select type', () => {
+      const result = customFieldsService.setDefaultValue(txnCustomPropertiesData[0], 'USER_LIST');
+      expect(result.value).toEqual([]);
+    });
+  });
+
+  describe('setProperty():', () => {
+    it('should set property for expense custom fields', () => {
+      const result = customFieldsService.setProperty(null, customInputData[3], []);
+      expect(result).toEqual(txnCustomPropertiesData3[2]);
+    });
+
+    it('should set property for custom fields without is_mandatory', () => {
+      const result = customFieldsService.setProperty(null, customInputData[1], []);
+      expect(result).toEqual(txnCustomPropertiesData3[4]);
+    });
+
+    it('should set property for custom fields with custom properties', () => {
+      const result = customFieldsService.setProperty(null, customInputData[3], customPropertiesData);
+      expect(result).toEqual(txnCustomPropertiesData3[2]);
+    });
+
+    it('should set property for custom fields with custom properties of type Date', () => {
+      const result = customFieldsService.setProperty(null, customInputData[2], customPropertiesData);
+      expect(result).toEqual(txnCustomPropertiesData3[5]);
+    });
+  });
+
+  it('standardizeCustomFields(): should standardize custom fields', () => {
+    const result = customFieldsService.standardizeCustomFields([], customExpensefields);
+    expect(result).toEqual(txnCustomPropertiesData4);
   });
 });
