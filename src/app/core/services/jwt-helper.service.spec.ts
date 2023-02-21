aac4fa45dcaf6149deb65fa48120ac9245b5e85e
Dimple <dimple.kh@fyle.in>
2023-02-21 10:31:00 +0530

    minor

--- src/app/core/services/jwt-helper.service.spec.ts
+++ src/app/core/services/jwt-helper.service.spec.ts
@@ -4,19 +4,19 @@ import { JwtHelperService as AngularJwtHelper } from '@auth0/angular-jwt';
 import { JwtHelperService } from './jwt-helper.service';
 
 describe('JwtHelperService', () => {
-  let service: JwtHelperService;
+  let jwtHelperService: JwtHelperService;
   let angularJwtHelper: AngularJwtHelper;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [JwtHelperService],
     });
-    service = TestBed.inject(JwtHelperService);
+    jwtHelperService = TestBed.inject(JwtHelperService);
     angularJwtHelper = new AngularJwtHelper();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(jwtHelperService).toBeTruthy();
   });
 
   it('decodeToken(): should decode token', () => {
@@ -24,7 +24,7 @@ describe('JwtHelperService', () => {
     const rawToken =
       'eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzI5MTcyMDAsImlzcyI6IkZ5bGVBcHAiLCJ1c2VyX2lkIjoidXNNakxpYm15ZTdzIiwib3JnX3VzZXJfaWQiOiJvdXJ3N0hpNG1tcE8iLCJvcmdfaWQiOiJvck5WdGhUbzJaeW8iLCJyb2xlcyI6IltcIkZZTEVSXCIsXCJGSU5BTkNFXCIsXCJBRE1JTlwiLFwiQVBQUk9WRVJcIixcIlZFUklGSUVSXCIsXCJQQVlNRU5UX1BST0NFU1NPUlwiLFwiSE9QXCJdIiwic2NvcGVzIjoiW10iLCJhbGxvd2VkX0NJRFJzIjoiW10iLCJ2ZXJzaW9uIjoiMyIsImNsdXN0ZXJfZG9tYWluIjoiXCJodHRwczovL3N0YWdpbmcuZnlsZS50ZWNoXCIiLCJleHAiOjE2NzI5MjA4MDB9.hTMJ56cPH_HgKhZSKNCOIEGAzaAXCfIgbEYcaudhXwk';
     const expectedToken = angularJwtHelper.decodeToken(rawToken);
-    expect(service.decodeToken(rawToken)).toEqual(expectedToken as Date);
+    expect(jwtHelperService.decodeToken(rawToken)).toEqual(expectedToken as Date);
   });
 
   it('getExpirationDate(): should get expiration date', () => {
@@ -32,6 +32,6 @@ describe('JwtHelperService', () => {
     const rawToken =
       'eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzI5MTcyMDAsImlzcyI6IkZ5bGVBcHAiLCJ1c2VyX2lkIjoidXNNakxpYm15ZTdzIiwib3JnX3VzZXJfaWQiOiJvdXJ3N0hpNG1tcE8iLCJvcmdfaWQiOiJvck5WdGhUbzJaeW8iLCJyb2xlcyI6IltcIkZZTEVSXCIsXCJGSU5BTkNFXCIsXCJBRE1JTlwiLFwiQVBQUk9WRVJcIixcIlZFUklGSUVSXCIsXCJQQVlNRU5UX1BST0NFU1NPUlwiLFwiSE9QXCJdIiwic2NvcGVzIjoiW10iLCJhbGxvd2VkX0NJRFJzIjoiW10iLCJ2ZXJzaW9uIjoiMyIsImNsdXN0ZXJfZG9tYWluIjoiXCJodHRwczovL3N0YWdpbmcuZnlsZS50ZWNoXCIiLCJleHAiOjE2NzI5MjA4MDB9.hTMJ56cPH_HgKhZSKNCOIEGAzaAXCfIgbEYcaudhXwk';
     const expectedExpirationDate = angularJwtHelper.getTokenExpirationDate(rawToken);
-    expect(service.getExpirationDate(rawToken)).toEqual(expectedExpirationDate as Date);
+    expect(jwtHelperService.getExpirationDate(rawToken)).toEqual(expectedExpirationDate as Date);
   });
 });
