b8f3f1a9623ed1902a2fdfb3aabd0adec20d5173
Dimple <dimple.kh@fyle.in>
2023-02-23 22:00:16 +0530

    70% branches

--- src/app/core/mock-data/file-object.data.ts
+++ src/app/core/mock-data/file-object.data.ts
@@ -57,3 +57,52 @@ export const thumbnailUrlMockData: FileObject[] = [
     url: 'mock-url-1',
   },
 ];
+
+export const fileObjectAdv: FileObject[] = [
+  {
+    id: 'fiSSsy2Bf4Se',
+    org_user_id: 'ouCI4UQ2G0K1',
+    created_at: new Date('2023-02-23T09:45:34.026Z'),
+    name: '000.jpeg',
+    s3url: '2023-02-23/orrjqbDbeP9p/receipts/fiSSsy2Bf4Se.000.jpeg',
+    transaction_id: null,
+    invoice_id: null,
+    advance_request_id: 'areqspMJTHN4Yk',
+    purpose: 'ORIGINAL',
+    password: null,
+    receipt_coordinates: null,
+    email_meta_data: null,
+    fyle_sub_url: '/api/files/fiSSsy2Bf4Se/download',
+    file_type: 'image',
+    url: 'https://fyle-storage-mumbai-3.s3.amazonaws.com/2023-02-23/orrjqbDbeP9p/receipts/fiSSsy2Bf4Se.000.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20230223T151537Z&X-Amz-SignedHeaders=host&X-Amz-Expires=604800&X-Amz-Credential=AKIA54Z3LIXTX6CFH4VG%2F20230223%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Signature=d79c2711892e7cb3f072e223b7b416408c252da38e7df0995e3d256cd8509fee',
+    type: 'image',
+    thumbnail:
+      'https://fyle-storage-mumbai-3.s3.amazonaws.com/2023-02-23/orrjqbDbeP9p/receipts/fiSSsy2Bf4Se.000.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20230223T151537Z&X-Amz-SignedHeaders=host&X-Amz-Expires=604800&X-Amz-Credential=AKIA54Z3LIXTX6CFH4VG%2F20230223%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Signature=d79c2711892e7cb3f072e223b7b416408c252da38e7df0995e3d256cd8509fee',
+  },
+];
+
+export const fileObjectAdv1: FileObject = {
+  ...fileObjectAdv[0],
+  name: '000.pdf',
+  file_type: 'pdf',
+  url: 'https://fyle-storage-mumbai-3.s3.amazonaws.com/2023-02-23/orrjqbDbeP9p/receipts/fiSSsy2Bf4Se.000.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20230223T151537Z&X-Amz-SignedHeaders=host&X-Amz-Expires=604800&X-Amz-Credential=AKIA54Z3LIXTX6CFH4VG%2F20230223%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Signature=d79c2711892e7cb3f072e223b7b416408c252da38e7df0995e3d256cd8509fee',
+  type: 'pdf',
+  thumbnail:
+    'https://fyle-storage-mumbai-3.s3.amazonaws.com/2023-02-23/orrjqbDbeP9p/receipts/fiSSsy2Bf4Se.000.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20230223T151537Z&X-Amz-SignedHeaders=host&X-Amz-Expires=604800&X-Amz-Credential=AKIA54Z3LIXTX6CFH4VG%2F20230223%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Signature=d79c2711892e7cb3f072e223b7b416408c252da38e7df0995e3d256cd8509fee',
+};
+
+export const fileObjectData3: FileObject = {
+  id: 'fiXpfkKFhf6w',
+  org_user_id: 'ouCI4UQ2G0K1',
+  created_at: new Date('2023-02-23T16:22:03.264Z'),
+  name: '000.jpeg',
+  s3url: '2023-02-23/orrjqbDbeP9p/receipts/fiXpfkKFhf6w.000.jpeg',
+  transaction_id: null,
+  invoice_id: null,
+  advance_request_id: null,
+  purpose: 'ORIGINAL',
+  password: null,
+  receipt_coordinates: null,
+  email_meta_data: null,
+  fyle_sub_url: '/api/files/fiXpfkKFhf6w/download',
+};
