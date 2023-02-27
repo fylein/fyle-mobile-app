71b754c18369d84565859df8f5c260ec08f37ecc
Manjiri Chavande <manjiri.c@fyle.in>
2023-02-27 12:05:09 +0530

    Initial setup

--- src/app/core/services/advance.service.spec.ts
+++ src/app/core/services/advance.service.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
-
+import { ApiV2Service } from './api-v2.service';
+import { AuthService } from './auth.service';
 import { AdvanceService } from './advance.service';
+import { extendedAdvancesData } from '../mock-data/extended-advance.data';
+import { of } from 'rxjs';
 
-xdescribe('AdvanceService', () => {
-  let service: AdvanceService;
+describe('AdvanceService', () => {
+  let advanceService: AdvanceService;
+  let apiv2Service: jasmine.SpyObj<ApiV2Service>;
+  let authService: jasmine.SpyObj<AuthService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AdvanceService);
+    const apiv2ServiceSpy = jasmine.createSpyObj('ApiV2Service', ['get']);
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['getEou']);
+    TestBed.configureTestingModule({
+      providers: [
+        AdvanceService,
+        {
+          provide: ApiV2Service,
+          useValue: apiv2ServiceSpy,
+        },
+        {
+          provide: AuthService,
+          useValue: authServiceSpy,
+        },
+      ],
+    });
+    advanceService = TestBed.inject(AdvanceService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(advanceService).toBeTruthy();
   });
 });
