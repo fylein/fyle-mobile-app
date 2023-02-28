e6fb67b927474cfbcfc4945551830cbe80ec5e5f
jayfyle <115472256+jayfyle@users.noreply.github.com>
2023-02-28 21:14:33 +0530

    Code Coverage : Personal Card Service #1 (#1732)
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>

--- src/app/core/models/personal_card.model.ts
+++ src/app/core/models/personal_card.model.ts
@@ -2,15 +2,18 @@ export interface PersonalCard {
   account_number: string;
   account_type: string;
   bank_name: string;
-  created_at: any;
+  created_at: Date;
   currency: string;
   id: string;
-  last_synced_at: any;
+  fastlink_params?: string;
+  last_synced_at: Date;
   mask: string;
+  mfa_enabled?: boolean;
   nickname: string;
   official_name: string;
   sync_type: string;
+  update_credentials?: boolean;
   unclassified_amount: number;
   unclassified_count: number;
-  updated_at: any;
+  updated_at: Date;
 }
