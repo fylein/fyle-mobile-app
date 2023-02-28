2f64d3b2b8930727329ae5a7965d5278ee405425
Manjiri Chavande <manjiri.c@fyle.in>
2023-02-28 13:14:04 +0530

    initial setup 1

--- src/app/core/services/recently-used-items.service.spec.ts
+++ src/app/core/services/recently-used-items.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
-
+import { ApiService } from './api.service';
+import { ProjectsService } from './projects.service';
 import { RecentlyUsedItemsService } from './recently-used-items.service';
+import { recentlyUsedRes } from '../mock-data/recently-used.data';
+import { of } from 'rxjs';
 
-xdescribe('RecentlyUsedItemsService', () => {
-  let service: RecentlyUsedItemsService;
-
+describe('RecentlyUsedItemsService', () => {
+  let recentlyUsedItemsService: RecentlyUsedItemsService;
+  let apiService: jasmine.SpyObj<ApiService>;
+  let projectService: jasmine.SpyObj<ProjectsService>;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RecentlyUsedItemsService);
+    TestBed.configureTestingModule({
+      providers: [
+        RecentlyUsedItemsService,
+        {
+          provide: ApiService,
+          useValue: jasmine.createSpyObj('ApiService', ['get']),
+        },
+        {
+          provide: ProjectsService,
+          useValue: jasmine.createSpyObj('ProjectsService', ['getByParamsUnformatted']),
+        },
+      ],
+    });
+    recentlyUsedItemsService = TestBed.inject(RecentlyUsedItemsService);
+    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
+    projectService = TestBed.inject(ProjectsService) as jasmine.SpyObj<ProjectsService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(recentlyUsedItemsService).toBeTruthy();
+  });
+
+  it('getRecentlyUsed(): should get recently used items', (done) => {
+    apiService.get.and.returnValue(of(recentlyUsedRes));
+    recentlyUsedItemsService.getRecentlyUsed().subscribe((res) => {
+      expect(res).toEqual(recentlyUsedRes);
+      expect(apiService.get).toHaveBeenCalledWith('/recently_used');
+      done();
+    });
   });
 });
