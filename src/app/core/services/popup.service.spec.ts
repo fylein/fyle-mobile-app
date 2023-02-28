997b8c00ba6f44e9fdbbdb42e2c82c230afa9987
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-02-28 12:44:58 +0530

    coverage 100

--- src/app/core/services/popup.service.spec.ts
+++ src/app/core/services/popup.service.spec.ts
@@ -1,16 +1,56 @@
 import { TestBed } from '@angular/core/testing';
-
+import { PopoverController } from '@ionic/angular';
 import { PopupService } from './popup.service';
 
-xdescribe('PopupService', () => {
-  let service: PopupService;
+describe('PopupService', () => {
+  let popupService: PopupService;
+  let popoverController: jasmine.SpyObj<PopoverController>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PopupService);
+    const popoverControllerSpy = jasmine.createSpyObj('PopoverController', ['create']);
+    TestBed.configureTestingModule({
+      providers: [
+        PopupService,
+        {
+          provide: PopoverController,
+          useValue: popoverControllerSpy,
+        },
+      ],
+    });
+    popupService = TestBed.inject(PopupService);
+    popoverController = TestBed.inject(PopoverController) as jasmine.SpyObj<PopoverController>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(popupService).toBeTruthy();
+  });
+
+  it('showPopup(): should show popup to the user', async () => {
+    popoverController.create.and.returnValue(
+      new Promise((resolve) => {
+        const popoverSpy = jasmine.createSpyObj('popup', ['onWillDismiss', 'present']) as any;
+        popoverSpy.onWillDismiss.and.returnValue(
+          new Promise((resInt) => {
+            const data = {
+              action: 'primary',
+            };
+            resInt({
+              data,
+            });
+          })
+        );
+        resolve(popoverSpy);
+      })
+    );
+
+    const result = await popupService.showPopup({
+      header: 'Delete Expense',
+      message: 'Are you sure you want to delete this expense?',
+      primaryCta: {
+        text: 'Delete',
+      },
+    });
+
+    expect(result).toEqual('primary');
   });
 });
