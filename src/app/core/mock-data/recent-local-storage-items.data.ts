1743005c8e02941718f88d817dae919f72e66705
Manjiri Chavande <manjiri.c@fyle.in>
2023-03-02 20:32:56 +0530

    code coverage at 96%

---
+++ src/app/core/mock-data/recent-local-storage-items.data.ts
@@ -0,0 +1,139 @@
+import { RecentLocalStorageItems } from '../models/recent-local-storage-items.model';
+
+export const recentLocalStorageItemsRes: RecentLocalStorageItems[] = [
+  {
+    label: '12232',
+    value: {
+      code: null,
+      created_at: new Date('2019-06-11T04:48:22.469845+00:00'),
+      displayName: 'Mileage / 12232',
+      enabled: true,
+      fyle_category: 'Mileage',
+      id: 58349,
+      name: 'Mileage',
+      org_id: 'orNVthTo2Zyo',
+      sub_category: '12232',
+      updated_at: new Date('2022-10-04T05:23:38.831460+00:00'),
+    },
+    selected: false,
+    custom: false,
+  },
+];
+
+export const recentItemsRes: RecentLocalStorageItems[] = [
+  {
+    label: 'New New Mileage',
+    value: {
+      code: null,
+      created_at: new Date('2020-11-16T16:36:05.955705+00:00'),
+      displayName: 'Mileage / New New Mileage',
+      enabled: true,
+      fyle_category: 'Mileage',
+      id: 117013,
+      name: 'Mileage',
+      org_id: 'orNVthTo2Zyo',
+      sub_category: 'New New Mileage',
+      updated_at: new Date('2022-10-04T05:23:38.831460+00:00'),
+    },
+    selected: false,
+    custom: false,
+  },
+  {
+    label: '12232',
+    value: {
+      code: null,
+      created_at: new Date('2019-06-11T04:48:22.469845+00:00'),
+      displayName: 'Mileage / 12232',
+      enabled: true,
+      fyle_category: 'Mileage',
+      id: 58349,
+      name: 'Mileage',
+      org_id: 'orNVthTo2Zyo',
+      sub_category: '12232',
+      updated_at: new Date('2022-10-04T05:23:38.831460+00:00'),
+    },
+    selected: false,
+    custom: false,
+  },
+];
+
+export const itemsRes: RecentLocalStorageItems[] = [
+  {
+    label: 'New New Mileage',
+    value: {
+      code: null,
+      created_at: new Date('2020-11-16T16:36:05.955705+00:00'),
+      displayName: 'Mileage / New New Mileage',
+      enabled: true,
+      fyle_category: 'Mileage',
+      id: 117013,
+      name: 'Mileage',
+      org_id: 'orNVthTo2Zyo',
+      sub_category: 'New New Mileage',
+      updated_at: new Date('2022-10-04T05:23:38.831460+00:00'),
+    },
+    selected: false,
+    custom: false,
+  },
+];
+
+export const propertyRes = {
+  property: 'label',
+};
+
+export const postRecentItemsRes: RecentLocalStorageItems[] = [
+  {
+    label: 'New New Mileage',
+    value: {
+      code: null,
+      created_at: new Date('2020-11-16T16:36:05.955705+00:00'),
+      displayName: 'Mileage / New New Mileage',
+      enabled: true,
+      fyle_category: 'Mileage',
+      id: 117013,
+      name: 'Mileage',
+      org_id: 'orNVthTo2Zyo',
+      sub_category: 'New New Mileage',
+      updated_at: new Date('2022-10-04T05:23:38.831460+00:00'),
+    },
+    selected: false,
+    custom: false,
+  },
+  {
+    label: 'duowigw',
+    value: {
+      code: null,
+      created_at: new Date('2021-02-11T05:01:21.324060+00:00'),
+      displayName: 'Mileage / duowigw',
+      enabled: true,
+      fyle_category: 'Mileage',
+      id: 123032,
+      name: 'Mileage',
+      org_id: 'orNVthTo2Zyo',
+      sub_category: 'duowigw',
+      updated_at: new Date('2022-10-04T05:23:38.831460+00:00'),
+    },
+    selected: false,
+    custom: false,
+  },
+  {
+    label: '12232',
+    value: {
+      code: null,
+      created_at: new Date('2019-06-11T04:48:22.469845+00:00'),
+      displayName: 'Mileage / 12232',
+      enabled: true,
+      fyle_category: 'Mileage',
+      id: 58349,
+      name: 'Mileage',
+      org_id: 'orNVthTo2Zyo',
+      sub_category: '12232',
+      updated_at: new Date('2022-10-04T05:23:38.831460+00:00'),
+    },
+    selected: false,
+    custom: false,
+  },
+];
+
+// property
+//  label
