95540838c4769c2fee4057f11067e24d502b030d
Manjiri Chavande <82377810+CODEX108@users.noreply.github.com>
2023-03-03 11:25:13 +0530

    Fyle 85zrnrbyr Code Coverage 100% : Invitation Requests Service (#1740)

--- src/app/core/services/invitation-requests.service.spec.ts
+++ src/app/core/services/invitation-requests.service.spec.ts
@@ -1,16 +1,30 @@
-import { TestBed } from '@angular/core/testing';
-
 import { InvitationRequestsService } from './invitation-requests.service';
+import { RouterApiService } from './router-api.service';
+import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
-xdescribe('InvitationRequestsService', () => {
-  let service: InvitationRequestsService;
+describe('InvitationRequestsService', () => {
+  let invitationRequestsService: InvitationRequestsService;
+  let routerApiServiceSpy: jasmine.SpyObj<RouterApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(InvitationRequestsService);
+    const spy = jasmine.createSpyObj('RouterApiService', ['post']);
+    TestBed.configureTestingModule({
+      providers: [InvitationRequestsService, { provide: RouterApiService, useValue: spy }],
+    });
+    invitationRequestsService = TestBed.inject(InvitationRequestsService);
+    routerApiServiceSpy = TestBed.inject(RouterApiService) as jasmine.SpyObj<RouterApiService>;
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('upsertRouter(): should call the post method with the correct endpoint and email', () => {
+    const mockEmail = 'manjiri.c@fyle.in';
+    const expectedEndpoint = '/invitation_requests/invite';
+    const expectedRequestBody = { email: mockEmail };
+
+    routerApiServiceSpy.post.and.returnValue(of(null));
+
+    invitationRequestsService.upsertRouter(mockEmail).subscribe(() => {
+      expect(routerApiServiceSpy.post).toHaveBeenCalledOnceWith(expectedEndpoint, expectedRequestBody);
+    });
   });
 });
