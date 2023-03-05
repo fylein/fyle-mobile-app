7603eefe302967db852394e971d66ea23c3258ec
Manjiri Milind Chavande <manjiri.c@fyle.in>
2023-03-06 00:27:17 +0530

    initial setup and coverage of set method

--- src/app/core/services/api-v2.service.spec.ts
+++ src/app/core/services/api-v2.service.spec.ts
@@ -1,16 +1,54 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { ApiV2Service } from './api-v2.service';
+import { of } from 'rxjs';
 
-xdescribe('ApiV2Service', () => {
-  let service: ApiV2Service;
+describe('ApiV2Service', () => {
+  const rootUrl = 'https://staging.fyle.tech';
+  let apiV2Service: ApiV2Service;
+  let httpClient: jasmine.SpyObj<HttpClient>;
+
+  const requestObj = {
+    someKey: 'someValue',
+  };
+
+  const apiResponse = {
+    message: 'SUCCESS',
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiV2Service);
+    const httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        ApiV2Service,
+        {
+          provide: HttpClient,
+          useValue: httpClientSpy,
+        },
+      ],
+    });
+
+    apiV2Service = TestBed.inject(ApiV2Service);
+    httpClient = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
+    apiV2Service.setRoot(rootUrl);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(apiV2Service).toBeTruthy();
+  });
+
+  it('setRoot(): should set root url', () => {
+    expect(apiV2Service.ROOT_ENDPOINT).toBe(rootUrl);
+  });
+
+  it('get():should make GET request without params', (done) => {
+    httpClient.get.and.returnValue(of(apiResponse));
+
+    apiV2Service.get('/reports').subscribe((res) => {
+      expect(res).toEqual(apiResponse);
+      expect(httpClient.get).toHaveBeenCalledWith('https://staging.fyle.tech/v2/reports', {});
+      done();
+    });
   });
 });
