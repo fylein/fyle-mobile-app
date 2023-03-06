4a50f053f2486026fa7303a5e4f55790e90063ae
Manjiri Chavande <manjiri.c@fyle.in>
2023-03-06 19:14:16 +0530

    one branch pending in the get method

--- src/app/core/services/router-api.service.spec.ts
+++ src/app/core/services/router-api.service.spec.ts
@@ -3,6 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { RouterApiService } from './router-api.service';
 import { of } from 'rxjs';
 
+const requestObj = {
+  someKey: 'someValue',
+};
+
+const apiResponse = {
+  message: 'SUCCESS',
+};
+
 describe('RouterApiService', () => {
   let routerApiService: RouterApiService;
   let httpClient: jasmine.SpyObj<HttpClient>;
@@ -25,5 +33,40 @@ describe('RouterApiService', () => {
 
   it('should be created', () => {
     expect(routerApiService).toBeTruthy();
+  });
+
+  it('should make POST call with params', (done) => {
+    httpClient.post.and.returnValue(of(apiResponse));
+    routerApiService.post('/invitation_requests', requestObj).subscribe((res) => {
+      expect(res).toEqual(apiResponse);
+      expect(httpClient.post).toHaveBeenCalledWith('/routerapi/invitation_requests', requestObj);
+      done();
+    });
+  });
+
+  describe('get():', () => {
+    it('should make GET request without params', (done) => {
+      httpClient.get.and.returnValue(of(apiResponse));
+      routerApiService.get('/invitation_requests', {}).subscribe((res) => {
+        expect(res).toEqual(apiResponse);
+        expect(httpClient.get).toHaveBeenCalledWith('/routerapi/invitation_requests', {});
+        done();
+      });
+    });
+
+    it('should make GET request with params', (done) => {
+      httpClient.get.and.returnValue(of(apiResponse));
+      routerApiService
+        .get('/invitation_requests', {
+          params: requestObj,
+        })
+        .subscribe((res) => {
+          expect(res).toEqual(apiResponse);
+          expect(httpClient.get).toHaveBeenCalledWith('/routerapi/invitation_requests', {
+            params: requestObj,
+          });
+          done();
+        });
+    });
   });
 });
