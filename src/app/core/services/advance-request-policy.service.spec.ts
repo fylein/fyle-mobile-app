d781792ae91c2a988d7d4f536344a2e92e06f52b
Manjiri Chavande <manjiri.c@fyle.in>
2023-03-06 20:43:01 +0530

    initial setup and code coverage 40%

--- src/app/core/services/advance-request-policy.service.spec.ts
+++ src/app/core/services/advance-request-policy.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { AdvanceRequestPolicyService } from './advance-request-policy.service';
+import { of } from 'rxjs';
 
-xdescribe('AdvanceRequestPolicyService', () => {
-  let service: AdvanceRequestPolicyService;
+const requestObj = {
+  someKey: 'someValue',
+};
+
+const apiResponse = {
+  message: 'SUCCESS',
+};
+
+describe('AdvanceRequestPolicyService', () => {
+  const rootUrl = 'https://staging.fyle.tech';
+  let advanceRequestPolicyService: AdvanceRequestPolicyService;
+  let httpClient: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AdvanceRequestPolicyService);
+    const httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        AdvanceRequestPolicyService,
+        {
+          provide: HttpClient,
+          useValue: httpClientSpy,
+        },
+      ],
+    });
+    advanceRequestPolicyService = TestBed.inject(AdvanceRequestPolicyService);
+    httpClient = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
+
+    advanceRequestPolicyService.setRoot(rootUrl);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(advanceRequestPolicyService).toBeTruthy();
+  });
+
+  it('setRoot(): should set root url', () => {
+    expect(advanceRequestPolicyService.ROOT_ENDPOINT).toBe(rootUrl);
   });
 });
