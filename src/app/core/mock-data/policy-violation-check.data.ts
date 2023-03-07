e90ffa58e8eff3948a9cc33a13916fc0e5f16a7c
Manjiri Chavande <manjiri.c@fyle.in>
2023-03-07 13:39:19 +0530

    code coverage 100%

--- src/app/core/mock-data/policy-violation-check.data.ts
+++ src/app/core/mock-data/policy-violation-check.data.ts
@@ -11,3 +11,15 @@ export const checkPolicyData: PolicyViolationCheck = {
   },
   advance_request_policy_rule_desired_states: [],
 };
+
+export const checkPolicyWithRulesData: PolicyViolationCheck = {
+  advance_request_desired_state: {
+    action_description: null,
+    policy_rule_descriptions: [],
+    policy_approver_ids: [],
+    flag: false,
+    currency: null,
+    advance_request_id: 'areq4YujEm52Ub',
+  },
+  advance_request_policy_rule_desired_states: ['policy desc 1', null, 'policy desc 2'],
+};
--- src/app/core/services/advance-request-policy.service.spec.ts
+++ src/app/core/services/advance-request-policy.service.spec.ts
@@ -2,14 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClient } from '@angular/common/http';
 import { AdvanceRequestPolicyService } from './advance-request-policy.service';
 import { of } from 'rxjs';
-
-const requestObj = {
-  someKey: 'someValue',
-};
-
-const apiResponse = {
-  message: 'SUCCESS',
-};
+import { checkPolicyWithRulesData } from '../mock-data/policy-violation-check.data';
 
 describe('AdvanceRequestPolicyService', () => {
   const rootUrl = 'https://staging.fyle.tech';
@@ -40,5 +33,33 @@ describe('AdvanceRequestPolicyService', () => {
 
   it('setRoot(): should set root url', () => {
     expect(advanceRequestPolicyService.ROOT_ENDPOINT).toBe(rootUrl);
+  });
+
+  it('getPolicyRules() : shoulg get the poilcy rules', () => {
+    const expectedRules = ['policy desc 1', 'policy desc 2'];
+
+    const result = advanceRequestPolicyService.getPolicyRules(checkPolicyWithRulesData);
+
+    expect(result).toEqual(expectedRules);
+  });
+
+  it('servicePost(): should make POST request', (done) => {
+    const requestObj = {
+      someKey: 'someValue',
+    };
+
+    const apiResponse = {
+      message: 'SUCCESS',
+    };
+    httpClient.post.and.returnValue(of(apiResponse));
+
+    advanceRequestPolicyService.servicePost('/policy_check', requestObj, {}).subscribe((res) => {
+      expect(res).toEqual(apiResponse);
+      expect(httpClient.post).toHaveBeenCalledWith(
+        'https://staging.fyle.tech/policy/advance_requests/policy_check',
+        requestObj
+      );
+      done();
+    });
   });
 });
