4270967554337dc3fa9e4cd2eed7cb4ec2fd1963
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2023-03-10 11:14:49 +0530

    Fix for issues coming up while running tests involving svg icons and fixed a test warning (#1765)
    Co-authored-by: Dimple <dimple.kh@fyle.in>

--- src/app/fyle/merge-expense/card-transaction-preview/card-transaction-preview.component.spec.ts
+++ src/app/fyle/merge-expense/card-transaction-preview/card-transaction-preview.component.spec.ts
@@ -1,6 +1,7 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { IonicModule } from '@ionic/angular';
 import { getElementBySelector, getTextContent } from 'src/app/core/dom-helpers';
 import { MatchedCCCTransaction } from 'src/app/core/models/matchedCCCTransaction.model';
@@ -15,7 +16,7 @@ describe('CardTransactionPreviewComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [CardTransactionPreviewComponent, EllipsisPipe, DatePipe, CurrencyPipe],
-      imports: [IonicModule.forRoot(), MatIconModule],
+      imports: [IonicModule.forRoot(), MatIconModule, MatIconTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CardTransactionPreviewComponent);
