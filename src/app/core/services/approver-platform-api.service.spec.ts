624f3b0c2bb6a559da8d875ce009d707d85442bd
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-03-10 17:01:38 +0530

    coverage at 100

--- src/app/core/services/approver-platform-api.service.spec.ts
+++ src/app/core/services/approver-platform-api.service.spec.ts
@@ -1,16 +1,50 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ApproverPlatformApiService } from './approver-platform-api.service';
+import { HttpClient } from '@angular/common/http';
+import { ApproverExpensePolicyStatesData } from '../mock-data/platform-policy-expense.data';
+import { of } from 'rxjs';
 
-xdescribe('ApproverPlatformApiService', () => {
-  let service: ApproverPlatformApiService;
+describe('ApproverPlatformApiService', () => {
+  let approverPlatformService: ApproverPlatformApiService;
+  let httpClient: jasmine.SpyObj<HttpClient>;
+  const rootUrl = 'https://staging.fyle.tech';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApproverPlatformApiService);
+    const httpSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    TestBed.configureTestingModule({
+      providers: [
+        ApproverPlatformApiService,
+        {
+          provide: HttpClient,
+          useValue: httpSpy,
+        },
+      ],
+    });
+    approverPlatformService = TestBed.inject(ApproverPlatformApiService);
+    httpClient = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
+
+    approverPlatformService.setRoot(rootUrl);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(approverPlatformService).toBeTruthy();
+  });
+
+  it('setRoot(): should set root url', () => {
+    expect(approverPlatformService.ROOT_ENDPOINT).toBe(rootUrl);
+  });
+
+  it('get(): should get data from the API', (done) => {
+    httpClient.get.and.returnValue(of(ApproverExpensePolicyStatesData));
+    const url = '/expense_policy_states';
+
+    const params = {
+      expense_id: 'eq.txRNWeQRXhso',
+    };
+
+    approverPlatformService.get(url, params).subscribe((res) => {
+      expect(res).toEqual(ApproverExpensePolicyStatesData);
+      done();
+    });
   });
 });
