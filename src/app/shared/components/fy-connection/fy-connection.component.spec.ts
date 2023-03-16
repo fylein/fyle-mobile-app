6d961c0ca663536cec65b0ec39b4208778351869
Manjiri Chavande <manjiri.c@fyle.in>
2023-03-16 11:13:24 +0530

    initial setup

--- src/app/shared/components/fy-connection/fy-connection.component.spec.ts
+++ src/app/shared/components/fy-connection/fy-connection.component.spec.ts
@@ -1,24 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { ConnectionMessageStatus } from './connection-status.enum';
+import { NetworkService } from '../../../core/services/network.service';
 import { FyConnectionComponent } from './fy-connection.component';
+import { of } from 'rxjs';
 
-xdescribe('FyConnectionComponent', () => {
-  let component: FyConnectionComponent;
+describe('FyConnectionComponent', () => {
+  let fyConnectionComponent: FyConnectionComponent;
   let fixture: ComponentFixture<FyConnectionComponent>;
+  let networkServiceSpy: jasmine.SpyObj<NetworkService>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    networkServiceSpy = jasmine.createSpyObj('NetworkService', [
+      'isOnline',
+      'connectivityWatcher',
+      'getConnectionStatus',
+    ]);
     TestBed.configureTestingModule({
       declarations: [FyConnectionComponent],
       imports: [IonicModule.forRoot()],
+      providers: [{ provide: NetworkService, useValue: networkServiceSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FyConnectionComponent);
-    component = fixture.componentInstance;
+    fyConnectionComponent = fixture.componentInstance;
+    networkServiceSpy.isOnline.and.returnValue(of(true));
+    networkServiceSpy.getConnectionStatus.and.returnValue(of(ConnectionMessageStatus.onlineMessageShown));
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it(' should create', () => {
+    expect(fyConnectionComponent).toBeTruthy();
+  });
+
+  it('should set up network watcher', () => {
+    expect(networkServiceSpy.connectivityWatcher).toHaveBeenCalled();
+  });
+
+  it('should update the connection status when network status changes', () => {
+    networkServiceSpy.getConnectionStatus.and.returnValue(of(ConnectionMessageStatus.onlineMessageShown));
+    fyConnectionComponent.ngOnInit();
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('.connection--online-message')).toBeTruthy();
   });
 });
