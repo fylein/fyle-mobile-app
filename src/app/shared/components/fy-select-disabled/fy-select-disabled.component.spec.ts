1c22919848db30a7b4e623474ee42113a730be88
jayfyle <115472256+jayfyle@users.noreply.github.com>
2023-03-20 18:24:28 +0530

    coverage at 100 (#1808)
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>

--- src/app/shared/components/fy-select-disabled/fy-select-disabled.component.spec.ts
+++ src/app/shared/components/fy-select-disabled/fy-select-disabled.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { FySelectDisabledComponent } from './fy-select-disabled.component';
+import { getTextContent, getElementBySelector } from 'src/app/core/dom-helpers';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
 
-xdescribe('FySelectDisabledComponent', () => {
+describe('FySelectDisabledComponent', () => {
   let component: FySelectDisabledComponent;
   let fixture: ComponentFixture<FySelectDisabledComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [FySelectDisabledComponent],
-      imports: [IonicModule.forRoot()],
+      imports: [IonicModule.forRoot(), MatIconModule, MatIconTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FySelectDisabledComponent);
@@ -20,5 +22,21 @@ xdescribe('FySelectDisabledComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the correct text', () => {
+    component.label = 'Label';
+    fixture.detectChanges();
+
+    const labelText = getElementBySelector(fixture, '.fy-select--label-content');
+    expect(getTextContent(labelText)).toEqual(component.label);
+  });
+
+  it('should check if mandatory is displayed', () => {
+    component.mandatory = true;
+    fixture.detectChanges();
+
+    const mandatoryText = getElementBySelector(fixture, '.fy-select--mandatory');
+    expect(getTextContent(mandatoryText)).toEqual('*');
   });
 });
