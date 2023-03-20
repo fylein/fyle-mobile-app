0e1f3a391492b6b43ef32ae51690755d3e87f5a7
Manjiri Chavande <82377810+CODEX108@users.noreply.github.com>
2023-03-20 23:01:22 +0530

    test: Code coverage add more popup component 100% (#1810)
    * code coverage 100%
    
    * removed unnecessary imports

--- src/app/shared/components/capture-receipt/add-more-popup/add-more-popup.component.spec.ts
+++ src/app/shared/components/capture-receipt/add-more-popup/add-more-popup.component.spec.ts
@@ -1,26 +1,48 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { AddMorePopupComponent } from './add-more-popup.component';
+import { getElementBySelector, getTextContent, getAllElementsBySelector } from 'src/app/core/dom-helpers';
 
-xdescribe('AddMorePopupComponent', () => {
-  let component: AddMorePopupComponent;
+describe('AddMorePopupComponent', () => {
+  let addMorePopupComponent: AddMorePopupComponent;
   let fixture: ComponentFixture<AddMorePopupComponent>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [AddMorePopupComponent],
-        imports: [IonicModule.forRoot()],
-      }).compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [AddMorePopupComponent],
+      imports: [IonicModule.forRoot(), MatBottomSheetModule],
+    }).compileComponents();
 
-      fixture = TestBed.createComponent(AddMorePopupComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
-  );
+    fixture = TestBed.createComponent(AddMorePopupComponent);
+    addMorePopupComponent = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be created', () => {
+    expect(addMorePopupComponent).toBeTruthy();
+  });
+
+  it('should display the heading correctly', () => {
+    const headingElement = getElementBySelector(fixture, '.add-more--heading');
+    expect(getTextContent(headingElement)).toContain('Add more Using');
+  });
+
+  it('should initialize actionButtons correctly', () => {
+    const containerElements = getAllElementsBySelector(fixture, '.add-more--container');
+    expect(containerElements.length).toBe(2); // Check that there are 2 action buttons
+    expect(addMorePopupComponent.actionButtons).toEqual([
+      { icon: 'fy-camera', title: 'Capture Receipts', mode: 'camera' },
+      { icon: 'fy-gallery', title: 'Upload from Gallery', mode: 'gallery' },
+    ]);
+  });
+
+  it('should dismiss the bottom sheet when an action button is clicked', () => {
+    const matBottomSheet = TestBed.inject(MatBottomSheet);
+    const Matspy = spyOn(matBottomSheet, 'dismiss');
+    const mode = 'camera';
+    const containerElement = fixture.nativeElement.querySelector('.add-more--container');
+    containerElement.click();
+    expect(Matspy).toHaveBeenCalledWith({ mode });
   });
 });
