871955c349605c4e7e7de9479d8ccca01c923200
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-03-20 18:50:39 +0530

    inital test

--- src/app/core/services/launch-darkly.service.spec.ts
+++ src/app/core/services/launch-darkly.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LaunchDarklyService } from './launch-darkly.service';
-
-xdescribe('LaunchDarklyService', () => {
-  let service: LaunchDarklyService;
+import { UserEventService } from './user-event.service';
+import * as LDClient from 'launchdarkly-js-client-sdk';
+import { StorageService } from './storage.service';
+import { lDUser } from '../mock-data/ld-client-user.data';
+describe('LaunchDarklyService', () => {
+  let launchDarklyService: LaunchDarklyService;
+  let userEventService: jasmine.SpyObj<UserEventService>;
+  let storageService: jasmine.SpyObj<StorageService>;
+  let ldClient: LDClient.LDClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LaunchDarklyService);
+    const userEventServiceSpy = jasmine.createSpyObj('UserEventService', ['onLogout']);
+    const storageServiceSpy = jasmine.createSpyObj('StorageService', ['get', 'set']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        LaunchDarklyService,
+        {
+          provide: UserEventService,
+          useValue: userEventServiceSpy,
+        },
+        {
+          provide: StorageService,
+          useValue: storageServiceSpy,
+        },
+      ],
+    });
+    launchDarklyService = TestBed.inject(LaunchDarklyService);
+    userEventService = TestBed.inject(UserEventService) as jasmine.SpyObj<UserEventService>;
+    storageService = TestBed.inject(StorageService) as jasmine.SpyObj<StorageService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(launchDarklyService).toBeTruthy();
+  });
+
+  it('isTheSameUser(): should check if the user is same', () => {
+    //@ts-ignore
+    expect(launchDarklyService.isTheSameUser(lDUser)).toBeFalse();
   });
 });
