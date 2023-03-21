9fd5de0794061d7ff99697ad15370501c5d0c4a3
Manjiri Chavande <82377810+CODEX108@users.noreply.github.com>
2023-03-21 18:47:45 +0530

    test: Code coverage Refiner Service 77.5% (#1813)
    * code coverage 77% startSurvey() method pending
    
    * resolved PR comments- added return type for the canStartSurvey() method

--- src/app/core/services/refiner.service.spec.ts
+++ src/app/core/services/refiner.service.spec.ts
@@ -6,6 +6,7 @@ import { NetworkService } from './network.service';
 import { OrgUserService } from './org-user.service';
 import { of } from 'rxjs';
 import { apiEouRes } from '../mock-data/extended-org-user.data';
+import { ExtendedOrgUser } from '../models/extended-org-user.model';
 
 describe('RefinerService', () => {
   let refinerService: RefinerService;
@@ -67,6 +68,43 @@ describe('RefinerService', () => {
     it('should return false for demo orgs', () => {
       const orgName = 'Fyle for Acme Corp';
       expect(refinerService.isNonDemoOrg(orgName)).toBeFalse();
+    });
+  });
+
+  describe('canStartSurvey():', () => {
+    it('should return true for non-demo orgs and when not switched to delegator', (done) => {
+      spyOn(refinerService, 'isNonDemoOrg').and.returnValue(true);
+      const switchedToDelegator = false;
+      orgUserService.isSwitchedToDelegator.and.returnValue(Promise.resolve(switchedToDelegator));
+      const homeCurrency = 'INR';
+      const eou = apiEouRes;
+      refinerService.canStartSurvey(homeCurrency, eou).subscribe((res) => {
+        expect(res).toBeTrue();
+        expect(orgUserService.isSwitchedToDelegator).toHaveBeenCalledTimes(1);
+        expect(refinerService.isNonDemoOrg).toHaveBeenCalledOnceWith('Staging Loaded');
+        done();
+      });
+    });
+
+    it('should return false for demo orgs and when switched to delegator', (done) => {
+      const demoOrgRes: ExtendedOrgUser = {
+        ...apiEouRes,
+        ou: {
+          ...apiEouRes.ou,
+          org_name: 'Fyle for Acme Corp',
+        },
+      };
+      spyOn(refinerService, 'isNonDemoOrg').and.returnValue(false);
+      const switchedToDelegator = true;
+      orgUserService.isSwitchedToDelegator.and.returnValue(Promise.resolve(switchedToDelegator));
+      const homeCurrency = 'INR';
+      const eou = demoOrgRes;
+      refinerService.canStartSurvey(homeCurrency, eou).subscribe((res) => {
+        expect(res).toBeFalse();
+        expect(orgUserService.isSwitchedToDelegator).toHaveBeenCalledTimes(1);
+        expect(refinerService.isNonDemoOrg).toHaveBeenCalledOnceWith('Fyle for Acme Corp');
+        done();
+      });
     });
   });
 });
