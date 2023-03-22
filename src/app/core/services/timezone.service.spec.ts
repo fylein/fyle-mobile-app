d3ff1372caa435fc77acfb621ce687bf634fb450
Manjiri Chavande <manjiri.c@fyle.in>
2023-03-22 19:40:37 +0530

    code coverage 70%

--- src/app/core/services/timezone.service.spec.ts
+++ src/app/core/services/timezone.service.spec.ts
@@ -3,10 +3,8 @@ import { CurrencyService } from './currency.service';
 
 import { TimezoneService } from './timezone.service';
 import { UtilityService } from './utility.service';
-import { txnCustomPropertiesData } from '../mock-data/txn-custom-properties.data';
-import { TxnCustomProperties } from '../models/txn-custom-properties.model';
 
-fdescribe('TimezoneService', () => {
+describe('TimezoneService', () => {
   let timezoneService: TimezoneService;
   let currencyService: jasmine.SpyObj<CurrencyService>;
   let utilityService: jasmine.SpyObj<UtilityService>;
@@ -29,71 +27,6 @@ fdescribe('TimezoneService', () => {
   it('should be created', () => {
     expect(timezoneService).toBeTruthy();
   });
-
-  //    describe('convertAllDatesToProperLocale(): ', () => {
-  //         const objData: TxnCustomProperties[] = [
-  //             {
-  //                 id: 206198,
-  //                 mandatory: false,
-  //                 name: '2232323',
-  //                 options: [],
-  //                 placeholder: 'adsf',
-  //                 prefix: '',
-  //                 type: 'DATE',
-  //                 value: null,
-  //             },
-  //             {
-  //                 id: 211326,
-  //                 mandatory: false,
-  //                 name: 'select all 2',
-  //                 options: [],
-  //                 placeholder: 'helo date',
-  //                 prefix: '',
-  //                 type: 'DATE',
-  //                 value: '2023-02-13T17:00:00.000Z',
-  //             }
-  //         ];
-
-  //         const expectedRes = [
-  //             {
-  //               id: 206198,
-  //               mandatory: false,
-  //               name: '2232323',
-  //               options: [],
-  //               placeholder: 'adsf',
-  //               prefix: '',
-  //               type: 'DATE',
-  //               value: null,
-  //             },
-  //             {
-  //               id: 211326,
-  //               mandatory: false,
-  //               name: 'select all 2',
-  //               options: [],
-  //               placeholder: 'helo date',
-  //               prefix: '',
-  //               type: 'DATE',
-  //               value: '2023-02-13T22:30:00.000Z', // converted to UTC with offset '-05:30:00'
-  //             },
-  //           ];
-  //        const offset = '-05:30:00';
-
-  //        it('should not modify non-Date properties', () => {
-  //         const result = timezoneService.convertAllDatesToProperLocale(objData, offset);
-  //         expect(result[0]).toEqual(objData[0]);
-  //       });
-
-  //       it('should convert Date properties to UTC with the specified offset', () => {
-  //         const result = timezoneService.convertAllDatesToProperLocale(objData, offset);
-  //         console.log(result[1].value);
-  //         expect(result[1].value).toEqual('2023-02-13T22:30:00.000Z');
-  //       });
-
-  //       it('should not modify the original object', () => {
-  //         const result = timezoneService.convertAllDatesToProperLocale(objData, offset);
-  //         expect(result).not.toBe(objData);
-  //       });
-  //     });
 
   describe('convertToTimezone(): ', () => {
     it('should convert the date to UTC with the specified offset when "toUtc" is true', () => {
@@ -125,14 +58,11 @@ fdescribe('TimezoneService', () => {
     });
   });
 
-  it('should call convertToTimezone with the correct parameters', () => {
-    const date = new Date('2022-10-31T00:00:00.000Z');
+  it('convertToUtc(): should call convertToTimezone with the correct parameters', () => {
+    const date = new Date('2022-01-01T00:00:00Z');
     const offset = '05:30:00';
-    spyOn(timezoneService, 'convertToTimezone').and.callThrough();
-
-    const result = timezoneService.convertToUtc(date, offset);
-
+    spyOn(timezoneService, 'convertToTimezone').and.returnValue(date);
+    timezoneService.convertToUtc(date, offset);
     expect(timezoneService.convertToTimezone).toHaveBeenCalledWith(date, offset, true);
-    expect(result).toEqual(new Date('2022-10-30T21:00:00.000Z'));
   });
 });
