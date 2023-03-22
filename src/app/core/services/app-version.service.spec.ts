b134425b4cd58d89f49ac80eac75f0bdd5d160a5
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-03-22 17:49:55 +0530

    initial test

--- src/app/core/services/app-version.service.spec.ts
+++ src/app/core/services/app-version.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AppVersionService } from './app-version.service';
+import { ApiService } from './api.service';
+import { RouterApiService } from './router-api.service';
+import { LoginInfoService } from './login-info.service';
+import { AuthService } from './auth.service';
 
-xdescribe('AppVersionService', () => {
-  let service: AppVersionService;
+describe('AppVersionService', () => {
+  let appVersionService: AppVersionService;
+  let apiService: jasmine.SpyObj<ApiService>;
+  let routerApiService: jasmine.SpyObj<RouterApiService>;
+  let loginInfoService: jasmine.SpyObj<LoginInfoService>;
+  let authService: jasmine.SpyObj<AuthService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AppVersionService);
+    const apiServiceSpy = jasmine.createSpyObj('ApiService', ['get', 'post']);
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['getEou']);
+    const routerApiServiceSpy = jasmine.createSpyObj('RouterApiService', ['post']);
+    const loginInfoServiceSpy = jasmine.createSpyObj('LoginInfoService', ['getLastLoggedInVersion']);
+    TestBed.configureTestingModule({
+      providers: [
+        AppVersionService,
+        {
+          provide: ApiService,
+          useValue: apiServiceSpy,
+        },
+        {
+          provide: AuthService,
+          useValue: authServiceSpy,
+        },
+        {
+          provide: RouterApiService,
+          useValue: routerApiServiceSpy,
+        },
+        {
+          provide: LoginInfoService,
+          useValue: loginInfoServiceSpy,
+        },
+      ],
+    });
+    appVersionService = TestBed.inject(AppVersionService);
+    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    loginInfoService = TestBed.inject(LoginInfoService) as jasmine.SpyObj<LoginInfoService>;
+    routerApiService = TestBed.inject(RouterApiService) as jasmine.SpyObj<RouterApiService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(appVersionService).toBeTruthy();
   });
 });
