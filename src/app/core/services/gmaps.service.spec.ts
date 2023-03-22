e01784a1b000a6fd52026d9f504af672e8286a55
Dimple <dimple.kh@fyle.in>
2023-03-22 11:47:43 +0530

    Initial test

--- src/app/core/services/gmaps.service.spec.ts
+++ src/app/core/services/gmaps.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import { MapDirectionsService, MapGeocoder } from '@angular/google-maps';
 
 import { GmapsService } from './gmaps.service';
 
-xdescribe('GmapsService', () => {
-  let service: GmapsService;
+describe('GmapsService', () => {
+  let gmapsService: GmapsService;
+  let mapDirectionsService: jasmine.SpyObj<MapDirectionsService>;
+  let mapGeocoder: jasmine.SpyObj<MapGeocoder>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GmapsService);
+    const mapDirectionsServiceSpy = jasmine.createSpyObj('MapDirectionsService', ['route']);
+    const mapGeocoderSpy = jasmine.createSpyObj('MapGeocoder', ['geocode']);
+    TestBed.configureTestingModule({
+      providers: [
+        GmapsService,
+        { provide: MapDirectionsService, useValue: mapDirectionsServiceSpy },
+        { provide: MapGeocoder, useValue: mapGeocoderSpy },
+      ],
+    });
+    gmapsService = TestBed.inject(GmapsService);
+    mapDirectionsService = TestBed.inject(MapDirectionsService) as jasmine.SpyObj<MapDirectionsService>;
+    mapGeocoder = TestBed.inject(MapGeocoder) as jasmine.SpyObj<MapGeocoder>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(gmapsService).toBeTruthy();
   });
 });
