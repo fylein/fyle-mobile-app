eb8f6a0257246cbc6d08cadb3320e766833f0797
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-03-22 12:34:46 +0530

    coverage at 71

--- src/app/core/services/launch-darkly.service.spec.ts
+++ src/app/core/services/launch-darkly.service.spec.ts
@@ -4,6 +4,8 @@ import { UserEventService } from './user-event.service';
 import * as LDClient from 'launchdarkly-js-client-sdk';
 import { StorageService } from './storage.service';
 import { lDUser } from '../mock-data/ld-client-user.data';
+import { ldAllFlagsRes } from '../mock-data/ld-all-flags.data';
+import { of } from 'rxjs';
 describe('LaunchDarklyService', () => {
   let launchDarklyService: LaunchDarklyService;
   let userEventService: jasmine.SpyObj<UserEventService>;
@@ -39,5 +41,54 @@ describe('LaunchDarklyService', () => {
   it('isTheSameUser(): should check if the user is same', () => {
     //@ts-ignore
     expect(launchDarklyService.isTheSameUser(lDUser)).toBeFalse();
+  });
+
+  it('updateCache(): should update cache', () => {
+    storageService.set.and.callThrough();
+    //@ts-ignore
+    launchDarklyService.updateCache();
+  });
+
+  it('initializeUser(): should initialize user', () => {
+    //@ts-ignore
+    spyOn(launchDarklyService, 'updateCache').and.callThrough();
+    launchDarklyService.initializeUser(lDUser);
+  });
+
+  describe('getVariation():', () => {
+    it('should get variation', (done) => {
+      const key = 'keyboard_plugin_enabled';
+      storageService.get.and.returnValue(Promise.resolve(ldAllFlagsRes));
+
+      launchDarklyService.getVariation(key, true).subscribe((res) => {
+        expect(res).toBeTrue();
+        done();
+      });
+    });
+
+    it('should return default value when flags are not available', (done) => {
+      const key = 'keyboard_plugin_enabled';
+      storageService.get.and.returnValue(Promise.resolve(null));
+
+      launchDarklyService.getVariation(key, true).subscribe((res) => {
+        expect(res).toBeTrue();
+        done();
+      });
+    });
+  });
+
+  it('shutDownClient(): should shut down client', () => {
+    launchDarklyService.shutDownClient();
+  });
+
+  it('checkIfKeyboardPluginIsEnabled(): check if keyboard plugin is enabled', (done) => {
+    spyOn(launchDarklyService, 'getVariation').and.returnValue(of(true));
+    const key = 'keyboard_plugin_enabled';
+
+    launchDarklyService.checkIfKeyboardPluginIsEnabled().subscribe((res) => {
+      expect(res).toBeTrue();
+      expect(launchDarklyService.getVariation).toHaveBeenCalledOnceWith('keyboard_plugin_enabled', true);
+      done();
+    });
   });
 });
