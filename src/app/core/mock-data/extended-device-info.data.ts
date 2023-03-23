92a01ef69e5951c3c2c1b4b857adf2215e6adecd
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2023-03-23 16:53:11 +0530

    test: Code coverage: Smartlook Service (#1827)
    Co-authored-by: Dimple <dimple.kh@fyle.in>

---
+++ src/app/core/mock-data/extended-device-info.data.ts
@@ -0,0 +1,14 @@
+import { ExtendedDeviceInfo } from '../models/extended-device-info.model';
+
+export const extendedDeviceInfoMockData: ExtendedDeviceInfo = {
+  uuid: 'mockuuid123',
+  appVersion: '5.50.0',
+  liveUpdateAppVersion: '5.50.0',
+  model: 'iPhone 11 Pro Max',
+  platform: 'ios',
+  operatingSystem: 'ios',
+  osVersion: '16.0.1',
+  manufacturer: 'Apple',
+  isVirtual: false,
+  webViewVersion: 'mockwebviewversion123',
+};
