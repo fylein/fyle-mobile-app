9ac40e7db9ad03de64538f7caef7689cf179f216
Manjiri Chavande <manjiri.c@fyle.in>
2023-03-23 09:57:03 +0530

    code coverage 100%

--- src/app/shared/components/toast-message/toast-message.component.spec.ts
+++ src/app/shared/components/toast-message/toast-message.component.spec.ts
@@ -1,26 +1,83 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { MatSnackBarModule, MatSnackBarRef, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 import { ToastMessageComponent } from './toast-message.component';
+import { MatIconModule } from '@angular/material/icon';
+import { click, getElementBySelector, getTextContent } from 'src/app/core/dom-helpers';
 
-xdescribe('ToastMessageComponent', () => {
-  let component: ToastMessageComponent;
+describe('ToastMessageComponent', () => {
+  let toastMessageComponent: ToastMessageComponent;
   let fixture: ComponentFixture<ToastMessageComponent>;
+  let snackbarRefSpy: jasmine.SpyObj<MatSnackBarRef<ToastMessageComponent>>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [ToastMessageComponent],
-        imports: [IonicModule.forRoot()],
-      }).compileComponents();
+  beforeEach(waitForAsync(() => {
+    snackbarRefSpy = jasmine.createSpyObj('MatSnackBarRef', ['dismiss', 'dismissWithAction']);
 
-      fixture = TestBed.createComponent(ToastMessageComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
-  );
+    TestBed.configureTestingModule({
+      declarations: [ToastMessageComponent],
+      imports: [IonicModule.forRoot(), MatIconModule, MatSnackBarModule],
+      providers: [
+        { provide: MAT_SNACK_BAR_DATA, useValue: {} },
+        { provide: MatSnackBarRef, useValue: snackbarRefSpy },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ToastMessageComponent);
+    toastMessageComponent = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(toastMessageComponent).toBeTruthy();
+  });
+
+  it('should display message', () => {
+    const message = 'Test message';
+    toastMessageComponent.data = {
+      icon: 'warning',
+      message: 'Test message',
+      redirectionText: null,
+      showCloseButton: false,
+    };
+    fixture.detectChanges();
+    const element = getElementBySelector(fixture, '.toast-message--body');
+    expect(getTextContent(element)).toContain(message);
+  });
+
+  it('should not display redirection text if not provided', () => {
+    toastMessageComponent.data.redirectionText = '';
+    fixture.detectChanges();
+    const redirectionEl = getElementBySelector(fixture, '.toast-message--redirection');
+    expect(redirectionEl).toBeFalsy();
+  });
+
+  it('should dismiss the snackbar with action on redirection text click', () => {
+    toastMessageComponent.data = {
+      icon: '',
+      message: '',
+      redirectionText: 'Go to home',
+      showCloseButton: false,
+    };
+    fixture.detectChanges();
+
+    const redirectionEl = getElementBySelector(fixture, '.toast-message--redirection') as HTMLElement;
+    expect(redirectionEl).toBeTruthy();
+    click(redirectionEl);
+    expect(snackbarRefSpy.dismissWithAction).toHaveBeenCalledTimes(1);
+  });
+
+  it('should dismiss the snackbar on close button click', () => {
+    toastMessageComponent.data = {
+      icon: '',
+      message: '',
+      redirectionText: '',
+      showCloseButton: true,
+    };
+    fixture.detectChanges();
+
+    const closeButtonEl = getElementBySelector(fixture, '.toast-message--right-icon') as HTMLElement;
+    expect(closeButtonEl).toBeTruthy();
+    click(closeButtonEl);
+    expect(snackbarRefSpy.dismiss).toHaveBeenCalledTimes(1);
   });
 });
