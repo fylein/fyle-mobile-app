535788f0cf4f9aaada248563dcebc4860b8867ea
Manjiri Chavande <manjiri.c@fyle.in>
2023-03-24 13:47:11 +0530

    code coverage 90%

--- src/app/core/services/timezone.service.spec.ts
+++ src/app/core/services/timezone.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { TxnCustomProperties } from '../models/txn-custom-properties.model';
 import { CurrencyService } from './currency.service';
 
 import { TimezoneService } from './timezone.service';
@@ -27,14 +28,74 @@ describe('TimezoneService', () => {
     expect(timezoneService).toBeTruthy();
   });
 
+  describe('convertToLocal(): ', () => {
+    it('should convert all dates to proper locale', () => {
+      utilityService.traverse.and.callFake((object, callback) => {
+        const date = new Date('2023-02-13T17:00:00.000Z');
+        date.setHours(12);
+        date.setMinutes(0);
+        date.setSeconds(0);
+        date.setMilliseconds(0);
+        return callback(date);
+      });
+
+      const data: TxnCustomProperties[] = [
+        {
+          id: 206198,
+          mandatory: false,
+          name: '2232323',
+          options: [],
+          placeholder: 'adsf',
+          prefix: '',
+          type: 'DATE',
+          value: null,
+        },
+        {
+          id: 211326,
+          mandatory: false,
+          name: 'select all 2',
+          options: [],
+          placeholder: 'helo date',
+          prefix: '',
+          type: 'DATE',
+          value: '2023-02-13T17:00:00.000Z',
+        },
+      ];
+      const result = timezoneService.convertAllDatesToProperLocale(data, '05:30:00');
+
+      expect(result).toEqual(new Date('2023-02-13T06:30:00.000Z'));
+    });
+
+    it('should return the data as it is if not a date instance', () => {
+      const data: TxnCustomProperties[] = [
+        {
+          id: 206198,
+          mandatory: false,
+          name: '2232323',
+          options: [],
+          placeholder: 'adsf',
+          prefix: '',
+          type: 'MULTI_SELECT',
+          value: 'some non-date value',
+        },
+      ];
+
+      utilityService.traverse.and.callFake((object, callback) => {
+        const nonDateProp = data[0].value;
+        return callback(nonDateProp);
+      });
+
+      const result = timezoneService.convertAllDatesToProperLocale(data, '05:30:00');
+      expect(result).toEqual('some non-date value');
+    });
+  });
+
   it('convertToTimezone(): should not modify the original date object', () => {
     const date = new Date('2023-03-22T12:00:00.000');
     const offset = '-05:30:00';
     const toUtc = true;
     const originalDate = new Date(date);
-
     timezoneService.convertToTimezone(date, offset, toUtc);
-
     expect(date).toEqual(originalDate);
   });
 
