e9d509a26a2bef845e3fc7202e8dee51974d0b93
jayfyle <115472256+jayfyle@users.noreply.github.com>
2023-03-27 12:34:12 +0530

    test: Code Coverage: App Version Service (#1835)
    * initial test
    
    * coverage at 91
    
    * resolved comments
    
    ---------
    
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>

---
+++ src/app/core/mock-data/app-version.data.ts
@@ -0,0 +1,10 @@
+import { AppVersion } from '../models/app_version.model';
+
+export const appVersionData1: AppVersion = {
+  app_version: '5.50.0',
+  created_at: new Date('2023-02-23T13:59:29.526Z'),
+  device_platform: 'ios',
+  id: 'mockuuid123',
+  platform_version: '5.50.0',
+  user_id: 'usvKA4X8Ugcr',
+};
