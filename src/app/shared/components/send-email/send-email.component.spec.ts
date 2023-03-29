8f5fd9273bd514d7baad5bc04147d029c01c1fdd
Dimple K H <31147415+Dimple16@users.noreply.github.com>
2023-03-29 18:54:38 +0530

    test: Code coverage: Send email component (#1850)

--- src/app/shared/components/send-email/send-email.component.spec.ts
+++ src/app/shared/components/send-email/send-email.component.spec.ts
@@ -1,26 +1,59 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { SendEmailComponent } from './send-email.component';
 
-xdescribe('SendEmailComponent', () => {
+describe('SendEmailComponent', () => {
   let component: SendEmailComponent;
   let fixture: ComponentFixture<SendEmailComponent>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [SendEmailComponent],
-        imports: [IonicModule.forRoot()],
-      }).compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [SendEmailComponent],
+      imports: [IonicModule.forRoot(), MatFormFieldModule, FormsModule, RouterModule, RouterTestingModule],
+      providers: [
+        FormBuilder,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: {
+                email: 'test@test.com',
+              },
+            },
+          },
+        },
+      ],
+    }).compileComponents();
 
-      fixture = TestBed.createComponent(SendEmailComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
-  );
+    fixture = TestBed.createComponent(SendEmailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the form with email parameter', () => {
+    expect(component.fg.controls.email.value).toEqual('test@test.com');
+  });
+
+  it('should emit the email address if valid', () => {
+    const sendEmailSpy = spyOn(component.sendEmail, 'emit');
+    component.fg.controls.email.setValue('test@test.com');
+    component.onClickSend();
+    expect(sendEmailSpy).toHaveBeenCalledOnceWith('test@test.com');
+  });
+
+  it('should not emit the email address if invalid', () => {
+    const sendEmailSpy = spyOn(component.sendEmail, 'emit');
+    component.fg.controls.email.setValue('not_an_email');
+    component.onClickSend();
+    expect(sendEmailSpy).not.toHaveBeenCalled();
   });
 });
