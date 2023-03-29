f28c138e6895cbd31a7bbe23c52fe7e215e55bb1
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-03-29 16:34:23 +0530

    test setup

--- src/app/auth/reset-password/reset-password.page.spec.ts
+++ src/app/auth/reset-password/reset-password.page.spec.ts
@@ -1,24 +1,57 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { SendEmailComponent } from 'src/app/shared/components/send-email/send-email.component';
 import { ResetPasswordPage } from './reset-password.page';
+import { RouterAuthService } from 'src/app/core/services/router-auth.service';
+import { Router, Routes } from '@angular/router';
+import { Location } from '@angular/common';
+import { appRoutes } from 'src/app/app-routing.module';
+import { fyleRoutes } from 'src/app/fyle/fyle-routing.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-xdescribe('ResetPasswordPage', () => {
+describe('ResetPasswordPage', () => {
   let component: ResetPasswordPage;
   let fixture: ComponentFixture<ResetPasswordPage>;
+  let router: jasmine.SpyObj<Router>;
+  let routerAuthService: jasmine.SpyObj<RouterAuthService>;
+  let location: jasmine.SpyObj<Location>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [ResetPasswordPage],
-        imports: [IonicModule.forRoot()],
-      }).compileComponents();
+  const routes: Routes = {
+    ...appRoutes,
+    ...fyleRoutes,
+  };
 
-      fixture = TestBed.createComponent(ResetPasswordPage);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
-  );
+  beforeEach(waitForAsync(() => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    const locationSpy = jasmine.createSpyObj('Location', ['path']);
+    const routerAuthServiceSpy = jasmine.createSpyObj('RouterAuthService', ['sendResetPassword']);
+    TestBed.configureTestingModule({
+      declarations: [ResetPasswordPage, SendEmailComponent],
+      imports: [IonicModule.forRoot(), RouterTestingModule.withRoutes(routes), FormsModule, ReactiveFormsModule],
+      providers: [
+        {
+          provide: RouterAuthService,
+          useValue: routerAuthServiceSpy,
+        },
+        {
+          provide: Router,
+          useValue: routerSpy,
+        },
+        {
+          provide: Location,
+          useValue: locationSpy,
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ResetPasswordPage);
+    component = fixture.componentInstance;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    location = TestBed.inject(Location) as jasmine.SpyObj<Location>;
+    routerAuthService = TestBed.inject(RouterAuthService) as jasmine.SpyObj<RouterAuthService>;
+    fixture.detectChanges();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
