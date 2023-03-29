967603d097a8ac27504f0295cca9e51a815dc45b
Manjiri Chavande <manjiri.c@fyle.in>
2023-03-29 17:12:04 +0530

    initial setup

--- src/app/fyle/view-team-report/share-report/share-report.component.spec.ts
+++ src/app/fyle/view-team-report/share-report/share-report.component.spec.ts
@@ -1,26 +1,41 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
+import { MatRippleModule } from '@angular/material/core';
+import { IonicModule, PopoverController } from '@ionic/angular';
+import { click, getAllElementsBySelector, getElementBySelector, getTextContent } from 'src/app/core/dom-helpers';
+import { FormsModule } from '@angular/forms';
+//import { MatFormFieldModule } from '@angular/material/form-field';
 import { ShareReportComponent } from './share-report.component';
 
-xdescribe('ShareReportComponent', () => {
+fdescribe('ShareReportComponent', () => {
   let component: ShareReportComponent;
   let fixture: ComponentFixture<ShareReportComponent>;
+  let popoverControllerSpy: PopoverController;
+  beforeEach(waitForAsync(() => {
+    popoverControllerSpy = jasmine.createSpyObj('PopoverController', ['dismiss']);
+    TestBed.configureTestingModule({
+      declarations: [ShareReportComponent],
+      imports: [IonicModule.forRoot(), MatRippleModule, FormsModule],
+      providers: [
+        {
+          provide: PopoverController,
+          useValue: popoverControllerSpy,
+        },
+      ],
+    }).compileComponents();
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [ShareReportComponent],
-        imports: [IonicModule.forRoot()],
-      }).compileComponents();
-
-      fixture = TestBed.createComponent(ShareReportComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
-  );
+    fixture = TestBed.createComponent(ShareReportComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the correct title and details', () => {
+    const shareReportTitle = getElementBySelector(fixture, '.share-report--title');
+    expect(getTextContent(shareReportTitle)).toContain('Share Report');
+    const shareReportDesc = getAllElementsBySelector(fixture, '.share-report--details');
+    expect(getTextContent(shareReportDesc[0])).toContain('Share report via email.');
   });
 });
