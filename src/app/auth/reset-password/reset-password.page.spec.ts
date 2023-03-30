77ab2e49bf06fcc3bc2337bad60245c9f4c55775
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-03-30 14:36:10 +0530

    coverage at 100

--- src/app/auth/reset-password/reset-password.page.spec.ts
+++ src/app/auth/reset-password/reset-password.page.spec.ts
@@ -5,10 +5,10 @@ import { ResetPasswordPage } from './reset-password.page';
 import { RouterAuthService } from 'src/app/core/services/router-auth.service';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { Location } from '@angular/common';
-import { appRoutes } from 'src/app/app-routing.module';
-import { fyleRoutes } from 'src/app/fyle/fyle-routing.module';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PageState } from 'src/app/core/models/page-state.enum';
+import { of, throwError } from 'rxjs';
 
 describe('ResetPasswordPage', () => {
   let component: ResetPasswordPage;
@@ -17,17 +17,12 @@ describe('ResetPasswordPage', () => {
   let routerAuthService: jasmine.SpyObj<RouterAuthService>;
   let location: jasmine.SpyObj<Location>;
 
-  const routes: Routes = {
-    ...appRoutes,
-    ...fyleRoutes,
-  };
-
   beforeEach(waitForAsync(() => {
     const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
     const locationSpy = jasmine.createSpyObj('Location', ['path']);
     const routerAuthServiceSpy = jasmine.createSpyObj('RouterAuthService', ['sendResetPassword']);
     TestBed.configureTestingModule({
-      declarations: [ResetPasswordPage, SendEmailComponent],
+      declarations: [ResetPasswordPage],
       imports: [IonicModule.forRoot(), RouterTestingModule, RouterModule, FormsModule, ReactiveFormsModule],
       providers: [
         {
@@ -57,6 +52,32 @@ describe('ResetPasswordPage', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should set current page state on component creation', () => {
+    component.ionViewWillEnter();
+    expect(component.currentPageState).toEqual(PageState.notSent);
+  });
+
+  describe('sendResetLink():', () => {
+    it('should send reset password link, change loading and page state', () => {
+      routerAuthService.sendResetPassword.and.returnValue(of({}));
+
+      const email = 'jay.b@fyle.in';
+      component.sendResetLink(email);
+      expect(component.isLoading).toEqual(false);
+      expect(component.currentPageState).toEqual(PageState.success);
+    });
+
+    it('should send reset password link, change loading and page state', () => {
+      routerAuthService.sendResetPassword.and.returnValue(throwError(() => new Error('Error message')));
+      spyOn(component, 'handleError').and.callThrough();
+
+      const email = 'jay.b@fyle.in';
+      component.sendResetLink(email);
+      expect(component.isLoading).toEqual(true);
+      expect(component.handleError).toHaveBeenCalledTimes(1);
+    });
+  });
+
   describe('handleError():', () => {
     it('should navigate to disabled auth', () => {
       component.handleError({
@@ -65,6 +86,15 @@ describe('ResetPasswordPage', () => {
       });
 
       expect(router.navigate).toHaveBeenCalledOnceWith(['/', 'auth', 'disabled']);
+    });
+
+    it('should change page state if auth not disabled', () => {
+      component.handleError({
+        status: 400,
+        message: 'Error message',
+      });
+
+      expect(component.currentPageState).toEqual(PageState.failure);
     });
   });
 });
