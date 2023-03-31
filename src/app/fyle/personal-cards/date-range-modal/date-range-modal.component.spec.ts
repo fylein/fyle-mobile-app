34ba51908bd98ae7876066a8675a1df461550826
Manjiri Chavande <manjiri.c@fyle.in>
2023-03-31 20:23:19 +0530

    code coverage 100%

--- src/app/fyle/personal-cards/date-range-modal/date-range-modal.component.spec.ts
+++ src/app/fyle/personal-cards/date-range-modal/date-range-modal.component.spec.ts
@@ -4,8 +4,9 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 import { DateRangeModalComponent } from './date-range-modal.component';
+import { click, getElementBySelector } from 'src/app/core/dom-helpers';
 
-fdescribe('DateRangeModalComponent', () => {
+describe('DateRangeModalComponent', () => {
   let component: DateRangeModalComponent;
   let fixture: ComponentFixture<DateRangeModalComponent>;
   let modalController: jasmine.SpyObj<ModalController>;
@@ -27,30 +28,56 @@ fdescribe('DateRangeModalComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('selectRange(): should call modalController.dismiss with correct range value', () => {
+  it('selectRange(): should dismiss the modal with correct range value', () => {
     const range = 'This Month';
     component.selectRange(range);
-    expect(modalController.dismiss).toHaveBeenCalledWith({ range });
+    expect(modalController.dismiss).toHaveBeenCalledOnceWith({ range });
   });
 
   it('should dismiss modal when selecting a pre-defined range', () => {
-    const button = fixture.nativeElement.querySelector('.date-range-modal--label');
-    button.click();
+    const button = getElementBySelector(fixture, '.date-range-modal--label') as HTMLElement;
+    click(button);
     fixture.detectChanges();
-    expect(modalController.dismiss).toHaveBeenCalled();
+    expect(modalController.dismiss).toHaveBeenCalledTimes(1);
   });
 
-  it('should dismiss modal when selecting a custom range', () => {
-    const startDateInput = fixture.nativeElement.querySelector('input[placeholder="Start date"]');
-    startDateInput.value = '2022-01-01';
-    startDateInput.dispatchEvent(new Event('input'));
-    const endDateInput = fixture.nativeElement.querySelector('input[placeholder="End date"]');
-    endDateInput.value = '2022-01-31';
-    endDateInput.dispatchEvent(new Event('input'));
+  it('should dismiss the modal with custom range dates', () => {
+    const startDate = (component.dateRangeStart.nativeElement.value = '2022-01-01');
+    const endDate = (component.dateRangeEnd.nativeElement.value = '2022-01-15');
     fixture.detectChanges();
-    const button = fixture.nativeElement.querySelector('.mat-calendar-body-cell');
-    button.click();
+    component.datePicked();
+    expect(modalController.dismiss).toHaveBeenCalledOnceWith({
+      range: 'Custom Range',
+      startDate,
+      endDate,
+    });
+  });
+
+  it('calenderOpened() should set isCalenderVisible to true on calenderOpened', () => {
     fixture.detectChanges();
-    expect(modalController.dismiss).toHaveBeenCalled();
+    component.calenderOpened();
+    expect(component.isCalenderVisible).toBe(true);
+  });
+
+  it('calenderClosed(): should set isCalenderVisible to false on calenderClosed', () => {
+    fixture.detectChanges();
+    component.calenderClosed();
+    expect(component.isCalenderVisible).toBe(false);
+  });
+
+  it('should set isCalenderVisible to true when the date range picker is opened', () => {
+    component.isCalenderVisible = false;
+    const dateRangePickerEl = getElementBySelector(fixture, 'mat-date-range-picker');
+    fixture.detectChanges();
+    dateRangePickerEl.dispatchEvent(new Event('opened'));
+    expect(component.isCalenderVisible).toBeTrue();
+  });
+
+  it('should set isCalenderVisible to false when the date range picker is closed', () => {
+    component.isCalenderVisible = true;
+    const dateRangePickerEl = getElementBySelector(fixture, 'mat-date-range-picker');
+    fixture.detectChanges();
+    dateRangePickerEl.dispatchEvent(new Event('closed'));
+    expect(component.isCalenderVisible).toBeFalse();
   });
 });
