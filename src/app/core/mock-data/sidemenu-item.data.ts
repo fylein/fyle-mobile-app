69dc25a358393113fa1eeceee31235e9d0892625
Manjiri Chavande <82377810+CODEX108@users.noreply.github.com>
2023-03-31 12:15:09 +0530

    test: Code coverage 100% Sidemenu ContentItem Component (#1867)
    * initial setup
    
    * code coverage 100%
    
    * removed unused data

---
+++ src/app/core/mock-data/sidemenu-item.data.ts
@@ -0,0 +1,34 @@
+import { SidemenuItem } from '../models/sidemenu-item.model';
+
+export const sidemenuItemData1: SidemenuItem = {
+  title: 'Dashboard',
+  isVisible: true,
+  icon: 'fy-dashboard-new',
+  route: ['/', 'enterprise', 'my_dashboard'],
+  disabled: false,
+  isDropdownOpen: false,
+  dropdownOptions: [],
+  openLiveChat: false,
+};
+
+export const sidemenuItemData2: SidemenuItem = {
+  title: 'Teams',
+  isVisible: true,
+  icon: 'teams',
+  isDropdownOpen: false,
+  disabled: false,
+  dropdownOptions: [
+    {
+      title: 'Team Reports',
+      isVisible: true,
+      route: ['/', 'enterprise', 'team_reports'],
+    },
+    {
+      title: 'Team Advances',
+      isVisible: true,
+      route: ['/', 'enterprise', 'team_advance'],
+    },
+  ],
+  route: [],
+  openLiveChat: false,
+};
--- src/app/shared/components/sidemenu/sidemenu-content/sidemenu-content-item/sidemenu-content-item.component.spec.ts
+++ src/app/shared/components/sidemenu/sidemenu-content/sidemenu-content-item/sidemenu-content-item.component.spec.ts
@@ -1,26 +1,64 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
 import { SidemenuContentItemComponent } from './sidemenu-content-item.component';
+import { sidemenuItemData1, sidemenuItemData2 } from 'src/app/core/mock-data/sidemenu-item.data';
+import { getElementBySelector, getTextContent } from 'src/app/core/dom-helpers';
 
-xdescribe('SidemenuContentItemComponent', () => {
-  let component: SidemenuContentItemComponent;
+describe('SidemenuContentItemComponent', () => {
+  let sidemenuContentItemComponent: SidemenuContentItemComponent;
   let fixture: ComponentFixture<SidemenuContentItemComponent>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [SidemenuContentItemComponent],
-        imports: [IonicModule.forRoot()],
-      }).compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [SidemenuContentItemComponent],
+      imports: [IonicModule.forRoot(), RouterModule, RouterTestingModule],
+    }).compileComponents();
 
-      fixture = TestBed.createComponent(SidemenuContentItemComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
-  );
+    fixture = TestBed.createComponent(SidemenuContentItemComponent);
+    sidemenuContentItemComponent = fixture.componentInstance;
+    sidemenuContentItemComponent.sidemenuItem = sidemenuItemData1;
+    fixture.detectChanges();
+  }));
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(sidemenuContentItemComponent).toBeTruthy();
+  });
+
+  it('should set isRoute to false if dropdownOptions is empty and openLiveChat is not present', () => {
+    sidemenuContentItemComponent.sidemenuItem = sidemenuItemData1;
+    fixture.detectChanges();
+    sidemenuContentItemComponent.ngOnInit();
+    expect(sidemenuContentItemComponent.isRoute).toBeFalse();
+  });
+
+  it('should set dropdownHeight to 0 if dropdownOptions is empty', () => {
+    sidemenuContentItemComponent.sidemenuItem = sidemenuItemData1;
+    fixture.detectChanges();
+    sidemenuContentItemComponent.ngOnInit();
+    expect(sidemenuContentItemComponent.dropdownHeight).toBe(0);
+  });
+
+  it('should set appropriate dropdown height is dropdownOptions are present', () => {
+    sidemenuContentItemComponent.sidemenuItem = sidemenuItemData2;
+    fixture.detectChanges();
+    sidemenuContentItemComponent.ngOnInit();
+    expect(sidemenuContentItemComponent.dropdownHeight).toBe(100);
+  });
+
+  it('should have the correct title displayed', () => {
+    sidemenuContentItemComponent.sidemenuItem = sidemenuItemData1;
+    fixture.detectChanges();
+    const labelElement = getElementBySelector(fixture, '.sidemenu-item__label-container__label');
+    expect(getTextContent(labelElement)).toEqual(sidemenuContentItemComponent.sidemenuItem.title);
+  });
+
+  it('goToRoute(): should emit redirect event', () => {
+    const sidemenuItem = sidemenuItemData2;
+    const contentItemSpy = spyOn(sidemenuContentItemComponent.redirect, 'emit');
+    fixture.detectChanges();
+    sidemenuContentItemComponent.goToRoute(sidemenuItem);
+    expect(contentItemSpy).toHaveBeenCalledOnceWith(sidemenuItem);
   });
 });
