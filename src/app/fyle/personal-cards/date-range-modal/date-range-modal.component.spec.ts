c011e641b5e9e5a9e9ab1b679ad1fe670ff5ff74
Manjiri Chavande <82377810+CODEX108@users.noreply.github.com>
2023-04-04 10:33:07 +0530

    test: Code coverage dateRangeModal component (#1872)
    * initial setup
    
    * code coverage 100%
    
    * resolved PR comments

--- src/app/fyle/personal-cards/date-range-modal/date-range-modal.component.spec.ts
+++ src/app/fyle/personal-cards/date-range-modal/date-range-modal.component.spec.ts
@@ -1,24 +1,83 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
 import { DateRangeModalComponent } from './date-range-modal.component';
+import { click, getElementBySelector } from 'src/app/core/dom-helpers';
 
-xdescribe('DateRangeModalComponent', () => {
+describe('DateRangeModalComponent', () => {
   let component: DateRangeModalComponent;
   let fixture: ComponentFixture<DateRangeModalComponent>;
+  let modalController: jasmine.SpyObj<ModalController>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    modalController = jasmine.createSpyObj('ModalController', ['dismiss']);
+    TestBed.configureTestingModule({
       declarations: [DateRangeModalComponent],
+      imports: [IonicModule.forRoot(), FormsModule, MatDatepickerModule, MatNativeDateModule],
+      providers: [{ provide: ModalController, useValue: modalController }],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(DateRangeModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('selectRange(): should dismiss the modal with correct range value', () => {
+    const range = 'This Month';
+    component.selectRange(range);
+    expect(modalController.dismiss).toHaveBeenCalledOnceWith({ range });
+  });
+
+  it('should dismiss modal when selecting a pre-defined range', () => {
+    const button = getElementBySelector(fixture, '.date-range-modal--label') as HTMLElement;
+    click(button);
+    fixture.detectChanges();
+    expect(modalController.dismiss).toHaveBeenCalledTimes(1);
+  });
+
+  it('should dismiss the modal with custom range dates', () => {
+    component.dateRangeStart.nativeElement.value = '2022-01-01';
+    component.dateRangeEnd.nativeElement.value = '2022-01-15';
+    fixture.detectChanges();
+    component.datePicked();
+    expect(modalController.dismiss).toHaveBeenCalledOnceWith({
+      range: 'Custom Range',
+      startDate: '2022-01-01',
+      endDate: '2022-01-15',
+    });
+  });
+
+  it('calenderOpened() should set isCalenderVisible to true on calenderOpened', () => {
+    fixture.detectChanges();
+    component.calenderOpened();
+    expect(component.isCalenderVisible).toBe(true);
+  });
+
+  it('calenderClosed(): should set isCalenderVisible to false on calenderClosed', () => {
+    fixture.detectChanges();
+    component.calenderClosed();
+    expect(component.isCalenderVisible).toBe(false);
+  });
+
+  it('should set isCalenderVisible to true when the date range picker is opened', () => {
+    component.isCalenderVisible = false;
+    const dateRangePickerEl = getElementBySelector(fixture, 'mat-date-range-picker');
+    fixture.detectChanges();
+    dateRangePickerEl.dispatchEvent(new Event('opened'));
+    expect(component.isCalenderVisible).toBeTrue();
+  });
+
+  it('should set isCalenderVisible to false when the date range picker is closed', () => {
+    component.isCalenderVisible = true;
+    const dateRangePickerEl = getElementBySelector(fixture, 'mat-date-range-picker');
+    fixture.detectChanges();
+    dateRangePickerEl.dispatchEvent(new Event('closed'));
+    expect(component.isCalenderVisible).toBeFalse();
   });
 });
