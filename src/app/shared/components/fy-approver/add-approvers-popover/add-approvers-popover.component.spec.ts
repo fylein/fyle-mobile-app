dce218364144eb99f8ca03764b9790730656eb25
Manjiri Chavande <manjiri.c@fyle.in>
2023-04-10 11:01:25 +0530

    code coverage 60%

--- src/app/shared/components/fy-approver/add-approvers-popover/add-approvers-popover.component.spec.ts
+++ src/app/shared/components/fy-approver/add-approvers-popover/add-approvers-popover.component.spec.ts
@@ -1,24 +1,116 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
+import { IonicModule, ModalController, PopoverController } from '@ionic/angular';
 import { AddApproversPopoverComponent } from './add-approvers-popover.component';
+import { LoaderService } from 'src/app/core/services/loader.service';
+import { ApproverDialogComponent } from './approver-dialog/approver-dialog.component';
+import { ModalPropertiesService } from 'src/app/core/services/modal-properties.service';
+import { AdvanceRequestService } from 'src/app/core/services/advance-request.service';
+import { ReportService } from 'src/app/core/services/report.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { FormsModule } from '@angular/forms';
+import { apiAllApproverRes1 } from 'src/app/core/mock-data/approver.data';
+import { getElementBySelector, getTextContent } from 'src/app/core/dom-helpers';
+import { of } from 'rxjs';
 
-xdescribe('AddApproversPopoverComponent', () => {
+fdescribe('AddApproversPopoverComponent', () => {
   let component: AddApproversPopoverComponent;
   let fixture: ComponentFixture<AddApproversPopoverComponent>;
+  let modalController: jasmine.SpyObj<ModalController>;
+  let modalProperties: jasmine.SpyObj<ModalPropertiesService>;
+  let popoverController: jasmine.SpyObj<PopoverController>;
+  let advanceRequestService: jasmine.SpyObj<AdvanceRequestService>;
+  let reportService: jasmine.SpyObj<ReportService>;
+  let loaderService: jasmine.SpyObj<LoaderService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AddApproversPopoverComponent],
+  beforeEach(waitForAsync(() => {
+    const modalControllerSpy = jasmine.createSpyObj('ModalController', ['create']);
+    const modalPropertiesSpy = jasmine.createSpyObj('ModalPropertiesService', ['getModalDefaultProperties']);
+    const popoverControllerSpy = jasmine.createSpyObj('PopoverController', ['dismiss']);
+    const advanceRequestServiceSpy = jasmine.createSpyObj('AdvanceRequestService', ['getAdvanceRequests']);
+    const reportServiceSpy = jasmine.createSpyObj('ReportService', ['getReports']);
+    const loaderServiceSpy = jasmine.createSpyObj('LoaderService', ['showLoader', 'hideLoader']);
+
+    TestBed.configureTestingModule({
+      declarations: [ApproverDialogComponent],
+      imports: [IonicModule.forRoot(), MatIconModule, MatIconTestingModule, FormsModule],
+      providers: [
+        {
+          provide: ModalController,
+          useValue: modalControllerSpy,
+        },
+        {
+          provide: ModalPropertiesService,
+          useValue: modalPropertiesSpy,
+        },
+        {
+          provide: PopoverController,
+          useValue: popoverControllerSpy,
+        },
+        {
+          provide: AdvanceRequestService,
+          useValue: advanceRequestServiceSpy,
+        },
+        {
+          provide: ReportService,
+          useValue: reportServiceSpy,
+        },
+        {
+          provide: LoaderService,
+          useValue: loaderServiceSpy,
+        },
+      ],
     }).compileComponents();
-  });
+    modalController = TestBed.inject(ModalController) as jasmine.SpyObj<ModalController>;
+    modalProperties = TestBed.inject(ModalPropertiesService) as jasmine.SpyObj<ModalPropertiesService>;
+    popoverController = TestBed.inject(PopoverController) as jasmine.SpyObj<PopoverController>;
+    advanceRequestService = TestBed.inject(AdvanceRequestService) as jasmine.SpyObj<AdvanceRequestService>;
+    reportService = TestBed.inject(ReportService) as jasmine.SpyObj<ReportService>;
+    loaderService = TestBed.inject(LoaderService) as jasmine.SpyObj<LoaderService>;
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AddApproversPopoverComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('openCurrencyModal(): should open the modal', fakeAsync(() => {
+    const modalSpy = jasmine.createSpyObj('HTMLIonModalElement', ['present', 'onWillDismiss']);
+    component.approverEmailsList = ['ajain@fyle.in'];
+    component.id = 'rpFE5X1Pqi9P';
+    component.type = 'report';
+    component.ownerEmail = 'jay.b@fyle.in';
+    const selectedApproversList = ['ajain@fyle.in', 'aiyush.dhar@fylein', 'chethan.m+90@fyle.in', 'ashutosh.m@fyle.in'];
+    component.selectedApproversList = selectedApproversList;
+    modalController.create.and.returnValue(Promise.resolve(modalSpy));
+    modalSpy.onWillDismiss.and.returnValue(Promise.resolve({ data: { selectedApproversList } } as any));
+
+    component.openModal();
+    tick();
+    expect(modalController.create).toHaveBeenCalledOnceWith({
+      component: ApproverDialogComponent,
+      componentProps: {
+        approverEmailsList: component.approverEmailsList,
+        initialApproverList: selectedApproversList,
+        id: component.id,
+        type: component.type,
+        ownerEmail: component.ownerEmail,
+      },
+      mode: 'ios',
+      ...modalProperties.getModalDefaultProperties(),
+    });
+    expect(modalSpy.present).toHaveBeenCalledTimes(1);
+    expect(modalSpy.onWillDismiss).toHaveBeenCalledTimes(1);
+  }));
+
+  it('closeAddApproversPopover(): should close popover', fakeAsync(() => {
+    popoverController.dismiss.and.returnValue(Promise.resolve(true));
+
+    tick();
+    component.closeAddApproversPopover();
+    expect(popoverController.dismiss).toHaveBeenCalledTimes(1);
+  }));
 });
