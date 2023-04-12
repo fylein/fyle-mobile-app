7b720b7e49fed3b8396a6968ed200b46e140be61
Yash Solanki <yashsolanki1709@gmail.com>
2023-04-12 10:55:30 +0530

    feat: Show cost center dependent fields in view expense page (#1913)
    * Create method in service to get dependent fields based on id of base field
    
    * Add CC depenedent fields on view expense page
    
    * Move logic for getting dependent fields in view expense page
    
    * Show cost center code in the new section, and update icon accordingly
    
    * feat: Add CC dependent fields to view mileage/per-diem page (#1916)
    
    * Add cost center to view mileage oage
    
    * Show cc dependent fields in view per-diem page
    
    * feat: Split expense with cost center dependent fields (#1914)
    
    * Filter project dependent field values based on new method by generating the mappings
    
    * Split expense based on cost center dependent expense field
    
    * feat: Add cost center dependent field changes to merge expense page (#1918)
    
    * Added changes
    
    * Add support for cost center dependent fields in merge expense page
    
    * POST updated value of cost center
    
    * Minor refactoring
    
    ---------
    
    Co-authored-by: Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
    
    ---------
    
    Co-authored-by: Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
    
    ---------
    
    Co-authored-by: Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>

--- src/app/shared/components/view-dependent-fields/view-dependent-fields.component.ts
+++ src/app/shared/components/view-dependent-fields/view-dependent-fields.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CustomField } from 'src/app/core/models/custom_field.model';
+import { CustomProperty } from 'src/app/core/models/custom-properties.model';
 
 @Component({
   selector: 'app-view-dependent-fields',
@@ -7,13 +7,21 @@ import { CustomField } from 'src/app/core/models/custom_field.model';
   styleUrls: ['./view-dependent-fields.component.scss'],
 })
 export class ViewDependentFieldsComponent implements OnInit {
+  @Input() parentFieldType: 'PROJECT' | 'COST_CENTER';
+
   @Input() parentFieldName: string;
 
   @Input() parentFieldValue: string;
 
-  @Input() customProperties: CustomField[];
+  @Input() costCenterCode: string;
+
+  @Input() dependentCustomProperties: CustomProperty<string>[];
+
+  parentFieldIcon: string;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.parentFieldIcon = this.parentFieldType === 'PROJECT' ? 'list' : 'building';
+  }
 }
