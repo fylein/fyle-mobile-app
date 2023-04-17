3ced42ce082f0aab33679897b84b8a2db821e26f
Aiyush Dhar <aiyushdhar@Aiyushs-MacBook-Air.local>
2023-04-17 16:39:16 +0530

    Added a few more test cases

--- src/app/shared/components/fy-view-attachment/fy-view-attachment.component.spec.ts
+++ src/app/shared/components/fy-view-attachment/fy-view-attachment.component.spec.ts
@@ -6,6 +6,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { FileService } from 'src/app/core/services/file.service';
 import { LoaderService } from 'src/app/core/services/loader.service';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
 
 fdescribe('FyViewAttachmentComponent', () => {
   let component: FyViewAttachmentComponent;
@@ -76,6 +77,9 @@ fdescribe('FyViewAttachmentComponent', () => {
         url: 'http://example.com/attachment3.pdf',
       },
     ];
+    loaderService.hideLoader.and.returnValue(Promise.resolve());
+    loaderService.showLoader.and.returnValue(Promise.resolve());
+
     component.attachments = mockAttachments;
     fixture.detectChanges();
   }));
@@ -129,4 +133,174 @@ fdescribe('FyViewAttachmentComponent', () => {
     component.onDoneClick();
     expect(modalController.dismiss).toHaveBeenCalledOnceWith({ attachments: component.attachments });
   });
+
+  it('goToNextSlide(): should trigger appropiate methods on the swiper component on event', () => {
+    const mockSwiperRef = jasmine.createSpyObj('swiperRef', ['slideNext']);
+    component.imageSlides = {
+      swiperRef: mockSwiperRef,
+    } as any;
+    component.goToNextSlide();
+    expect(mockSwiperRef.slideNext).toHaveBeenCalledTimes(1);
+  });
+
+  it('goToPrevSlide(): should trigger appropiate methods on the swiper component on event', () => {
+    const mockSwiperRef = jasmine.createSpyObj('swiperRef', ['slidePrev']);
+    component.imageSlides = {
+      swiperRef: mockSwiperRef,
+    } as any;
+    component.goToPrevSlide();
+    expect(mockSwiperRef.slidePrev).toHaveBeenCalledTimes(1);
+  });
+
+  it('getActiveIndex(): should be able to set the proper activeIndex', () => {
+    component.imageSlides = {
+      swiperRef: {
+        activeIndex: 6,
+      },
+    } as any;
+    component.getActiveIndex();
+    expect(component.activeIndex).toBe(6);
+  });
+
+  it('deleteAttachment(): should be able to show delete attachment popover and perform deletion', fakeAsync(() => {
+    component.imageSlides = {
+      swiperRef: {
+        activeIndex: 1,
+      },
+    } as any;
+
+    popoverController.create.and.returnValue(
+      Promise.resolve({
+        present: () => Promise.resolve(),
+        onWillDismiss: () =>
+          Promise.resolve({
+            data: {
+              action: 'remove',
+            },
+          }),
+      }) as any
+    );
+
+    fileService.delete.and.returnValue(of(null));
+    spyOn(component, 'goToPrevSlide');
+    spyOn(component, 'goToNextSlide');
+    component.deleteAttachment();
+    tick(1000);
+
+    expect(component.goToPrevSlide).toHaveBeenCalledTimes(1);
+    expect(component.attachments).toEqual([
+      {
+        id: '1',
+        type: 'pdf',
+        url: 'http://example.com/attachment1.pdf',
+      },
+      {
+        id: '3',
+        type: 'pdf',
+        url: 'http://example.com/attachment3.pdf',
+      },
+    ]);
+    expect(fileService.delete).toHaveBeenCalledOnceWith('2');
+  }));
+
+  it('deleteAttachment(): should be able to show delete first attachment popover and perform deletion', fakeAsync(() => {
+    component.imageSlides = {
+      swiperRef: {
+        activeIndex: 0,
+      },
+    } as any;
+
+    popoverController.create.and.returnValue(
+      Promise.resolve({
+        present: () => Promise.resolve(),
+        onWillDismiss: () =>
+          Promise.resolve({
+            data: {
+              action: 'remove',
+            },
+          }),
+      }) as any
+    );
+
+    fileService.delete.and.returnValue(of(null));
+    spyOn(component, 'goToPrevSlide');
+    spyOn(component, 'goToNextSlide');
+    component.deleteAttachment();
+    tick(1000);
+
+    expect(component.goToNextSlide).toHaveBeenCalledTimes(1);
+    expect(component.attachments).toEqual([
+      {
+        id: '2',
+        type: 'image',
+        url: 'http://example.com/attachment2.jpg',
+      },
+      {
+        id: '3',
+        type: 'pdf',
+        url: 'http://example.com/attachment3.pdf',
+      },
+    ]);
+    expect(fileService.delete).toHaveBeenCalledOnceWith('1');
+  }));
+
+  it('deleteAttachment(): should not make api calls for attachments which have not been persisted yet', fakeAsync(() => {
+    component.attachments = [
+      {
+        id: '1',
+        type: 'pdf',
+        url: 'http://example.com/attachment1.pdf',
+      },
+      {
+        id: null,
+        type: 'jpeg',
+        url: 'http://example.com/attachment2.jpeg',
+      },
+      {
+        id: '3',
+        type: 'pdf',
+        url: 'http://example.com/attachment3.pdf',
+      },
+    ];
+
+    component.imageSlides = {
+      swiperRef: {
+        activeIndex: 1,
+      },
+    } as any;
+
+    popoverController.create.and.returnValue(
+      Promise.resolve({
+        present: () => Promise.resolve(),
+        onWillDismiss: () =>
+          Promise.resolve({
+            data: {
+              action: 'remove',
+            },
+          }),
+      }) as any
+    );
+
+    fileService.delete.and.returnValue(of(null));
+    spyOn(component, 'goToPrevSlide');
+    spyOn(component, 'goToNextSlide');
+
+    component.deleteAttachment();
+    tick(1000);
+
+    expect(component.goToPrevSlide).toHaveBeenCalledTimes(1);
+    expect(component.attachments).toEqual([
+      {
+        id: '1',
+        type: 'pdf',
+        url: 'http://example.com/attachment1.pdf',
+      },
+      {
+        id: '3',
+        type: 'pdf',
+        url: 'http://example.com/attachment3.pdf',
+      },
+    ]);
+    expect(fileService.delete).not.toHaveBeenCalledOnceWith('2');
+  }));
 });
