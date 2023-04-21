e94b80aba1d81a11861ce726c15346783a419473
yksolanki9 <yashsolanki1709@gmail.com>
2023-04-21 18:10:56 +0530

    Replace commit id with app version when uploading sourcemap to sentry

--- build_appflow.sh
+++ build_appflow.sh
@@ -1,3 +1,3 @@
-VERSION=`sentry-cli releases propose-version`
+VERSION=$(grep "LIVE_UPDATE_APP_VERSION" .env | cut -d "=" -f 2)
 ionic capacitor sync --prod --source-map
 sentry-cli releases --org fyle-technologies-private-limi -p "$SENTRY_PROJECT_NAME" files "$VERSION" upload-sourcemaps www
