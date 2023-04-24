3d3345c4a9a8e169677c7c246b831b29001c6c69
yksolanki9 <yashsolanki1709@gmail.com>
2023-04-24 17:00:11 +0530

    Initial setup

--- src/app/shared/components/dependent-fields/dependent-field/dependent-field-modal/dependent-field-modal.component.spec.ts
+++ src/app/shared/components/dependent-fields/dependent-field/dependent-field-modal/dependent-field-modal.component.spec.ts
@@ -1,16 +1,44 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 import { DependentFieldModalComponent } from './dependent-field-modal.component';
+import { DependentFieldsService } from 'src/app/core/services/dependent-fields.service';
+import { FormsModule } from '@angular/forms';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-xdescribe('DependentFieldModalComponent', () => {
+fdescribe('DependentFieldModalComponent', () => {
   let component: DependentFieldModalComponent;
   let fixture: ComponentFixture<DependentFieldModalComponent>;
+  const modalControllerSpy = jasmine.createSpyObj('ModalController', ['create']);
+  const dependentFieldsServiceSpy = jasmine.createSpyObj('DependentFieldsService', ['getOptionsForDependentField']);
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [DependentFieldModalComponent],
-      imports: [IonicModule.forRoot()],
+      imports: [
+        IonicModule.forRoot(),
+        MatIconModule,
+        MatFormFieldModule,
+        FormsModule,
+        MatIconTestingModule,
+        MatIconModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [
+        {
+          provide: ModalController,
+          useValue: modalControllerSpy,
+        },
+        {
+          provide: DependentFieldsService,
+          useValue: dependentFieldsServiceSpy,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DependentFieldModalComponent);
