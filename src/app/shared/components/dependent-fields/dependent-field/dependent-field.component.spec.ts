fd463f858a26fb145d7229ce0876f4ba84d3d67d
yksolanki9 <yashsolanki1709@gmail.com>
2023-04-26 01:39:29 +0530

    Initialize tests and write tests for onBlur and writevalue methods

--- src/app/shared/components/dependent-fields/dependent-field/dependent-field.component.spec.ts
+++ src/app/shared/components/dependent-fields/dependent-field/dependent-field.component.spec.ts
@@ -1,24 +1,75 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { ModalController } from '@ionic/angular';
+import { ModalPropertiesService } from 'src/app/core/services/modal-properties.service';
 import { DependentFieldComponent } from './dependent-field.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { DependentFieldModalComponent } from './dependent-field-modal/dependent-field-modal.component';
+import { FormsModule } from '@angular/forms';
 
-xdescribe('DependentFieldComponent', () => {
+fdescribe('DependentFieldComponent', () => {
   let component: DependentFieldComponent;
   let fixture: ComponentFixture<DependentFieldComponent>;
+  let componentElement: DebugElement;
+  let modalController: jasmine.SpyObj<ModalController>;
+  let modalProperties: jasmine.SpyObj<ModalPropertiesService>;
+
+  const modalControllerSpy = jasmine.createSpyObj('ModalController', ['create']);
+  const modalPropertiesSpy = jasmine.createSpyObj('ModalPropertiesService', ['getModalDefaultProperties']);
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [DependentFieldComponent],
-      imports: [IonicModule.forRoot()],
-    }).compileComponents();
+      declarations: [DependentFieldComponent, DependentFieldModalComponent],
+      imports: [IonicModule.forRoot(), MatIconModule, MatIconTestingModule, FormsModule],
+      providers: [
+        {
+          provide: ModalController,
+          useValue: modalControllerSpy,
+        },
+        {
+          provide: ModalPropertiesService,
+          useValue: modalPropertiesSpy,
+        },
+      ],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(DependentFieldComponent);
+        component = fixture.componentInstance;
+        componentElement = fixture.debugElement;
 
-    fixture = TestBed.createComponent(DependentFieldComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+        modalController = TestBed.inject(ModalController) as jasmine.SpyObj<ModalController>;
+        modalProperties = TestBed.inject(ModalPropertiesService) as jasmine.SpyObj<ModalPropertiesService>;
+
+        fixture.detectChanges();
+      });
   }));
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  xit('openModal', () => {});
+
+  it('onBlur(): component should be touched', () => {
+    spyOn(component, 'onTouchedCallback');
+    const inputElement = componentElement.query(By.css('.dependent-field__input'));
+    inputElement.nativeElement.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+    expect(component.onTouchedCallback).toHaveBeenCalledTimes(1);
+    expect(inputElement.nativeElement.classList.contains('ng-touched')).toBe(true);
+  });
+
+  it('writeValue(): should write the formControl value to select field', () => {
+    const dependentFieldValue = 'Dep Field 1';
+    component.writeValue(dependentFieldValue);
+    expect(component.displayValue).toEqual(dependentFieldValue);
+  });
+
+  xit('registerOnChange', () => {});
+
+  xit('registerOnTouched', () => {});
 });
