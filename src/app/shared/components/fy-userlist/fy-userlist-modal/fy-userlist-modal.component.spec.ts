2fbe0a3e3ebb2983dd37f098b920740f5ef74983
Manjiri Chavande <manjiri.c@fyle.in>
2023-04-25 14:40:20 +0530

    code coverage 60%

--- src/app/shared/components/fy-userlist/fy-userlist-modal/fy-userlist-modal.component.spec.ts
+++ src/app/shared/components/fy-userlist/fy-userlist-modal/fy-userlist-modal.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import { MatChipsModule } from '@angular/material/chips';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { ModalController } from '@ionic/angular';
 import { OrgUserService } from 'src/app/core/services/org-user.service';
@@ -13,6 +13,8 @@ import { customExpensefields } from 'src/app/core/mock-data/expense-field.data';
 import { customPropertiesData } from 'src/app/core/mock-data/custom-property.data';
 import { employeesParamsRes, employeesRes } from 'src/app/core/test-data/org-user.service.spec.data';
 import { of } from 'rxjs';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { getElementBySelector } from 'src/app/core/dom-helpers';
 
 const params = {
   order: 'us_full_name.asc,us_email.asc,ou_id',
@@ -37,7 +39,14 @@ fdescribe('FyUserlistModalComponent', () => {
 
     TestBed.configureTestingModule({
       declarations: [FyUserlistModalComponent],
-      imports: [IonicModule.forRoot(), MatIconTestingModule, FormsModule, MatChipsModule, MatIconModule],
+      imports: [
+        IonicModule.forRoot(),
+        MatIconTestingModule,
+        FormsModule,
+        MatChipsModule,
+        MatIconModule,
+        MatCheckboxModule,
+      ],
       providers: [
         { provide: ModalController, useValue: modalControllerSpy },
         { provide: ChangeDetectorRef, useValue: cdrSpy },
@@ -54,6 +63,7 @@ fdescribe('FyUserlistModalComponent', () => {
     orgUserService.getEmployeesBySearch.and.returnValue(of(employeesParamsRes.data));
     fixture = TestBed.createComponent(FyUserlistModalComponent);
     component = fixture.componentInstance;
+    component.value = 'test value';
     component.currentSelections = [
       'ajain+12+12+1@fyle.in',
       'ajain+12121212@fyle.in',
@@ -69,9 +79,46 @@ fdescribe('FyUserlistModalComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  xit('getSelectedItemDict', () => {});
-  xit('getSeparatorKeysCodes', () => {});
-  xit('addChip', () => {});
+  it('getSeparatorKeysCodes: should get the number of separator keycodes', () => {
+    const keyCodeData = [13, 188];
+    const separatorKeysCodes = component.getSeparatorKeysCodes();
+    expect(separatorKeysCodes).toEqual(keyCodeData);
+  });
+
+  it('getSelectedItemDict(): shouls retutn a selected item dictonary', () => {
+    const selectedItem = {
+      'ajain+12121212@fyle.in': true,
+      'aaaaaaa@aaaabbbb.com': true,
+      'aaaaasdjskjd@sdsd.com': true,
+      'ajain+12+12+1@fyle.in': true,
+      'kawaljeet.ravi22@gmail.com': true,
+      'abcdefg@somemail.com': true,
+    };
+
+    const selectedItemDict = component.getSelectedItemDict();
+    expect(selectedItemDict).toEqual(selectedItem);
+  });
+
+  it('clearValue(): should clear value ', () => {
+    const input = getElementBySelector(fixture, '.selection-modal--form-input') as HTMLInputElement;
+
+    component.clearValue();
+    expect(component.value).toEqual('');
+    expect(input.value).toEqual('');
+  });
+
+  it('addChip(): should add material chip', () => {
+    const inputElement = getElementBySelector(fixture, '.selection-modal--form-input') as HTMLInputElement;
+    const chipInput = jasmine.createSpyObj('chipInput', ['clear']);
+    const event: MatChipInputEvent = {
+      value: 'label',
+      chipInput,
+      input: inputElement,
+    };
+    component.addChip(event);
+    expect(chipInput.clear).toHaveBeenCalledTimes(1);
+  });
+
   xit('removeChip', () => {});
   xit('clearValue', () => {});
   xit('getDefaultUsersList', () => {});
