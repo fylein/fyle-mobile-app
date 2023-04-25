9f4ba06ab8b07ea0185734c2e2cba92090bccb6d
jayfyle <115472256+jayfyle@users.noreply.github.com>
2023-04-25 14:17:12 +0530

    test: Code Coverage: Receipt Preview Component (#1953)
    * initial tests
    
    * coveraga at 95
    
    * resolved commments
    
    * coverage at 98
    
    * resolved comments -2
    
    * resolve comments -3
    
    * reverting changes
    
    * revert changes -2
    
    * removed fdescribe
    
    ---------
    
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>

--- src/app/shared/components/fy-multiselect/fy-multiselect-modal/fy-multiselect-modal.component.spec.ts
+++ src/app/shared/components/fy-multiselect/fy-multiselect-modal/fy-multiselect-modal.component.spec.ts
@@ -7,18 +7,16 @@ xdescribe('FyMultiselectModalComponent', () => {
   let component: FyMultiselectModalComponent;
   let fixture: ComponentFixture<FyMultiselectModalComponent>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [FyMultiselectModalComponent],
-        imports: [IonicModule.forRoot()],
-      }).compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [FyMultiselectModalComponent],
+      imports: [IonicModule.forRoot()],
+    }).compileComponents();
 
-      fixture = TestBed.createComponent(FyMultiselectModalComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
-  );
+    fixture = TestBed.createComponent(FyMultiselectModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
