8849f8835dcf647ff1948d025b4ed5064a1f41b0
Manjiri Chavande <manjiri.c@fyle.in>
2023-04-25 19:33:45 +0530

    added mock data

---
+++ src/app/core/mock-data/employee.data.ts
@@ -0,0 +1,12 @@
+import { Employee } from '../models/spender/employee.model';
+
+export const selectedOptionRes: Partial<Employee> = {
+  ou_id: 'oubQzXeZbwbS',
+  ou_org_id: 'orNVthTo2Zyo',
+  ou_roles: '["FYLER","APPROVER","HOD","HOP","AUDITOR","FINANCE"]',
+  ou_status: '"ACTIVE"',
+  us_email: 'ajain+12+12+1@fyle.in',
+  us_full_name: 'AA',
+  us_id: 'usTdvbcxOqjs',
+  is_selected: false,
+};
