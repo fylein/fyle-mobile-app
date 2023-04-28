e9e88622bd13bea566ae1ac6e91b75f47d7ac09c
yksolanki9 <yashsolanki1709@gmail.com>
2023-04-28 15:33:53 +0530

    Initial test setup

--- src/app/shared/components/dependent-fields/dependent-fields.component.spec.ts
+++ src/app/shared/components/dependent-fields/dependent-fields.component.spec.ts
@@ -1,18 +1,28 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { DependentFieldsService } from 'src/app/core/services/dependent-fields.service';
 import { DependentFieldsComponent } from './dependent-fields.component';
+import { DependentFieldComponent } from './dependent-field/dependent-field.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
-xdescribe('DependentFieldsComponent', () => {
+fdescribe('DependentFieldsComponent', () => {
   let component: DependentFieldsComponent;
   let fixture: ComponentFixture<DependentFieldsComponent>;
+  let dependentFieldsService: jasmine.SpyObj<DependentFieldsService>;
+  let formBuilder: jasmine.SpyObj<FormBuilder>;
 
   beforeEach(waitForAsync(() => {
+    const dependentFieldsServiceSpy = jasmine.createSpyObj('DependentFieldsService', ['getOptionsForDependentField']);
     TestBed.configureTestingModule({
-      declarations: [DependentFieldsComponent],
-      imports: [IonicModule.forRoot()],
+      declarations: [DependentFieldsComponent, DependentFieldComponent],
+      imports: [IonicModule.forRoot(), ReactiveFormsModule],
+      providers: [
+        {
+          provide: DependentFieldsService,
+          useValue: dependentFieldsServiceSpy,
+        },
+      ],
     }).compileComponents();
-
     fixture = TestBed.createComponent(DependentFieldsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,4 +31,12 @@ xdescribe('DependentFieldsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  xit('addDependentFieldWithValue', () => {});
+
+  xit('getDependentField', () => {});
+
+  xit('addDependentField', () => {});
+
+  xit('removeAllDependentFields', () => {});
 });
