eb9a7bdecf1eda75b361e6cf9dfed0f78f952caf
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-05-05 16:46:51 +0530

    initial test

--- src/app/core/services/camera-preview.service.spec.ts
+++ src/app/core/services/camera-preview.service.spec.ts
@@ -1,16 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CameraPreviewService } from './camera-preview.service';
+import { CameraPreview, CameraPreviewPictureOptions } from '@capacitor-community/camera-preview';
+import { CameraPreviewPlugin } from '@capacitor-community/camera-preview';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('CameraPreviewService', () => {
-  let service: CameraPreviewService;
+fdescribe('CameraPreviewService', () => {
+  let cameraPreviewService: CameraPreviewService;
+
+  class CameraPreviewStub {
+    capture() {
+      return Promise.resolve({});
+    }
+  }
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CameraPreviewService);
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: CameraPreview,
+          useClass: CameraPreviewStub,
+        },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    });
+    cameraPreviewService = TestBed.inject(CameraPreviewService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(cameraPreviewService).toBeTruthy();
+  });
+
+  it('capture(): should capture in the camera', async () => {
+    const cameraPreviewPictureOptions: CameraPreviewPictureOptions = {
+      quality: 70,
+    };
+
+    await cameraPreviewService.capture(cameraPreviewPictureOptions);
   });
 });
