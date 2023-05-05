5f7b3ac5dcd72422b62c567ff35b66ffc759ad0a
OmkarJ13 <oajoshi13@gmail.com>
2023-05-05 11:12:59 +0530

    Revert "Merge branch 'split-visibility-my-expenses' into split-evenly"
    This reverts commit 12d7f69394aedeb8324e9b235848b345bcf0d8ec, reversing
    changes made to 65c2acb8e1167f7b278c3b60aea4d08212c5af90.

--- src/app/shared/icon/icon.module.ts
+++ src/app/shared/icon/icon.module.ts
@@ -171,7 +171,6 @@ export class IconModule {
     'warning.svg',
     'fy-merge.svg',
     'split-evenly.svg',
-    'split-new.svg',
   ];
 
   constructor(private domSanitizer: DomSanitizer, private matIconRegistry: MatIconRegistry) {
