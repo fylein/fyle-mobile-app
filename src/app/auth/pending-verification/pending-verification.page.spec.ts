b74319c1d109887fa5ee0b7163c8ebf1d740671e
suyashpatil78 <127177049+suyashpatil78@users.noreply.github.com>
2023-05-08 19:48:20 +0530

    Revert "removed focus from describe"
    This reverts commit 708e5278bc632e3123fb5f2e045c1e3869c331d5.

--- src/app/auth/pending-verification/pending-verification.page.spec.ts
+++ src/app/auth/pending-verification/pending-verification.page.spec.ts
@@ -10,7 +10,7 @@ import { authResData1 } from 'src/app/core/mock-data/auth-reponse.data';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
-describe('PendingVerificationPage', () => {
+fdescribe('PendingVerificationPage', () => {
   let component: PendingVerificationPage;
   let fixture: ComponentFixture<PendingVerificationPage>;
   let router: jasmine.SpyObj<Router>;
