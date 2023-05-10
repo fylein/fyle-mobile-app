996b6c7823b7db89071822c6c770ed513d900c20
sowmya <sowmya@sowmyas-MacBook-Air.local>
2023-05-10 18:39:37 +0530

    secure flag

--- android/app/src/main/java/com/ionicframework/fyle595781/MainActivity.java
+++ android/app/src/main/java/com/ionicframework/fyle595781/MainActivity.java
@@ -3,4 +3,10 @@ package com.ionicframework.fyle595781;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the Secure flag for this Window
+        getWindow().setFlags(LayoutParams.FLAG_SECURE, LayoutParams.FLAG_SECURE);
+    }
 }
