044c47dc4566a9e323957158d62edb5dae0a76cc
Manjiri Chavande <82377810+CODEX108@users.noreply.github.com>
2023-05-18 19:25:36 +0530

    test: code coverage 100% RequestInvitationPage  (#2023)
    * code coverage 100%
    
    * merge master into FYLE-85zt04z7w
    
    * rearranged the order
    
    * fixed typo
    
    * Fix error handling in sendRequestInvitation method
    
    * Fix tests
    
    ---------
    
    Co-authored-by: yksolanki9 <yashsolanki1709@gmail.com>

--- src/app/auth/request-invitation/request-invitation.page.spec.ts
+++ src/app/auth/request-invitation/request-invitation.page.spec.ts
@@ -1,26 +1,117 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { ActivatedRoute } from '@angular/router';
+import { LoaderService } from 'src/app/core/services/loader.service';
+import { InvitationRequestsService } from 'src/app/core/services/invitation-requests.service';
 import { RequestInvitationPage } from './request-invitation.page';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { of, throwError } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-xdescribe('RequestInvitationPage', () => {
+describe('RequestInvitationPage', () => {
   let component: RequestInvitationPage;
   let fixture: ComponentFixture<RequestInvitationPage>;
+  let fb: FormBuilder;
+  let activeroutemock: ActivatedRoute;
+  let loaderService: jasmine.SpyObj<LoaderService>;
+  let invitationRequestsService: jasmine.SpyObj<InvitationRequestsService>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [RequestInvitationPage],
-        imports: [IonicModule.forRoot()],
-      }).compileComponents();
+  beforeEach(waitForAsync(() => {
+    const loaderServiceSpy = jasmine.createSpyObj('LoaderService', ['showLoader', 'hideLoader']);
+    const invitationRequestsServiceSpy = jasmine.createSpyObj('InvitationRequestsService', ['upsertRouter']);
 
-      fixture = TestBed.createComponent(RequestInvitationPage);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
-  );
+    TestBed.configureTestingModule({
+      declarations: [RequestInvitationPage],
+      imports: [IonicModule.forRoot(), RouterModule.forRoot([]), FormsModule, ReactiveFormsModule],
+      providers: [
+        FormBuilder,
+        {
+          provide: LoaderService,
+          useValue: loaderServiceSpy,
+        },
+        {
+          provide: InvitationRequestsService,
+          useValue: invitationRequestsServiceSpy,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: {
+                email: '',
+              },
+            },
+          },
+        },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+    fixture = TestBed.createComponent(RequestInvitationPage);
+
+    fb = TestBed.inject(FormBuilder);
+    activeroutemock = TestBed.inject(ActivatedRoute);
+    loaderService = TestBed.inject(LoaderService) as jasmine.SpyObj<LoaderService>;
+    invitationRequestsService = TestBed.inject(InvitationRequestsService) as jasmine.SpyObj<InvitationRequestsService>;
+
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('sendRequestInvitation', () => {
+    it('should call the upsertRouter method with the correct email', fakeAsync(() => {
+      invitationRequestsService.upsertRouter.and.returnValue(of(null));
+      loaderService.showLoader.and.returnValue(Promise.resolve());
+      loaderService.hideLoader.and.returnValue(Promise.resolve());
+      const ipEmail = 'ajain1234@fyle.in';
+      component.fg.controls.email.setValue(ipEmail);
+
+      component.sendRequestInvitation();
+      tick(500);
+      expect(loaderService.showLoader).toHaveBeenCalledOnceWith('Sending request to join organization...');
+      expect(invitationRequestsService.upsertRouter).toHaveBeenCalledOnceWith(ipEmail);
+      expect(loaderService.hideLoader).toHaveBeenCalledTimes(1);
+      expect(component.currentPageState).toBe(component.RequestInvitationStates.success);
+    }));
+
+    it('should set current page state to alreadySent if error status is 400', fakeAsync(() => {
+      const error = { status: 400 };
+      activeroutemock.snapshot.params = {
+        email: '',
+      };
+      component.fg.controls.email.setValue('');
+      invitationRequestsService.upsertRouter.and.returnValue(throwError(() => error));
+      loaderService.showLoader.and.returnValue(Promise.resolve());
+      loaderService.hideLoader.and.returnValue(Promise.resolve());
+
+      component.sendRequestInvitation();
+      tick(500);
+      expect(loaderService.showLoader).toHaveBeenCalledOnceWith('Sending request to join organization...');
+      expect(invitationRequestsService.upsertRouter).toHaveBeenCalledOnceWith('');
+      expect(loaderService.hideLoader).toHaveBeenCalledTimes(1);
+      expect(component.currentPageState).toBe(component.RequestInvitationStates.alreadySent);
+    }));
+
+    it('should set current page state to failure if error status is not 400', fakeAsync(() => {
+      const error = new Error('Some error');
+      activeroutemock.snapshot.params = {
+        email: '',
+      };
+      component.fg.controls.email.setValue('');
+      invitationRequestsService.upsertRouter.and.returnValue(throwError(() => error));
+      loaderService.showLoader.and.returnValue(Promise.resolve());
+      loaderService.hideLoader.and.returnValue(Promise.resolve());
+
+      component.sendRequestInvitation();
+      tick(500);
+      expect(loaderService.showLoader).toHaveBeenCalledOnceWith('Sending request to join organization...');
+      expect(invitationRequestsService.upsertRouter).toHaveBeenCalledOnceWith('');
+      expect(loaderService.hideLoader).toHaveBeenCalledTimes(1);
+      expect(component.currentPageState).toBe(component.RequestInvitationStates.failure);
+    }));
   });
 });
