68f050bb575fe7571c0ab2cf50778a02a4ea0b78
yksolanki9 <yashsolanki1709@gmail.com>
2023-05-18 16:46:21 +0530

    Add unit tests for info-card component

--- src/app/fyle/my-profile/info-card/info-card.component.spec.ts
+++ src/app/fyle/my-profile/info-card/info-card.component.spec.ts
@@ -1,25 +1,56 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { ClipboardService } from 'src/app/core/services/clipboard.service';
 import { InfoCardComponent } from './info-card.component';
+import { By } from '@angular/platform-browser';
 
-//TODO: Add tests for this component
-xdescribe('InfoCardComponent', () => {
+describe('InfoCardComponent', () => {
   let component: InfoCardComponent;
   let fixture: ComponentFixture<InfoCardComponent>;
+  let clipboardService: jasmine.SpyObj<ClipboardService>;
 
   beforeEach(waitForAsync(() => {
+    const clipboardServiceSpy = jasmine.createSpyObj('ClipboardService', ['writeString']);
     TestBed.configureTestingModule({
       declarations: [InfoCardComponent],
       imports: [IonicModule.forRoot()],
+      providers: [{ provide: ClipboardService, useValue: clipboardServiceSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(InfoCardComponent);
     component = fixture.componentInstance;
+    clipboardService = TestBed.inject(ClipboardService) as jasmine.SpyObj<ClipboardService>;
+
+    component.title = 'Email Receipts';
+    component.content = 'Forward your receipts to Fyle at receipts@fylehq.com.';
+    component.contentToCopy = 'receipts@fylehq.com';
+    component.toastMessageContent = 'Email Copied Successfully';
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('copyToClipboard(): Should copy content to clipboard and emit copiedText event', fakeAsync(() => {
+    spyOn(component, 'copyToClipboard').and.callThrough();
+    clipboardService.writeString.and.resolveTo();
+    spyOn(component.copiedText, 'emit');
+
+    const infoCard = fixture.debugElement.query(By.css('.info-card__card'));
+    infoCard.nativeElement.click();
+    tick(100);
+
+    expect(component.copyToClipboard).toHaveBeenCalledOnceWith(component.contentToCopy);
+    expect(clipboardService.writeString).toHaveBeenCalledOnceWith(component.contentToCopy);
+    expect(component.copiedText.emit).toHaveBeenCalledOnceWith(component.toastMessageContent);
+  }));
+
+  it('should render title and content in the ui', () => {
+    const title = fixture.debugElement.query(By.css('.info-card__card__title'));
+    const content = fixture.debugElement.query(By.css('.info-card__content-container__content'));
+
+    expect(title.nativeElement.textContent).toEqual(component.title);
+    expect(content.nativeElement.textContent).toEqual(component.content);
   });
 });
