2286bd7000a89d048645a145ff034b55f331508f
yksolanki9 <yashsolanki1709@gmail.com>
2023-05-18 16:00:54 +0530

    Add tests for mobile-number-verification service

--- src/app/core/services/mobile-number-verification.service.spec.ts
+++ src/app/core/services/mobile-number-verification.service.spec.ts
@@ -1,17 +1,52 @@
 import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 import { MobileNumberVerificationService } from './mobile-number-verification.service';
+import { ApiService } from './api.service';
 
-//TODO: Write unit tests for this service
-xdescribe('MobileNumberVerificationService', () => {
+describe('MobileNumberVerificationService', () => {
   let service: MobileNumberVerificationService;
+  let apiService: jasmine.SpyObj<ApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const apiServiceSpy = jasmine.createSpyObj('ApiService', ['post']);
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: ApiService,
+          useValue: apiServiceSpy,
+        },
+      ],
+    });
     service = TestBed.inject(MobileNumberVerificationService);
+    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
+
+    apiService.post.and.returnValue(of({}));
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('sendOtp(): should send otp', () => {
+    service.sendOtp().subscribe(() => {
+      expect(apiService.post).toHaveBeenCalledOnceWith('/orgusers/verify_mobile');
+    });
+  });
+
+  it('verifyOtp(): should verify otp', () => {
+    const otp = '123456';
+    service.verifyOtp(otp).subscribe(() => {
+      expect(apiService.post).toHaveBeenCalledOnceWith(
+        '/orgusers/check_mobile_verification_code',
+        otp,
+        jasmine.any(Object)
+      );
+
+      //Check if the 3rd argument is an object with Content-Type header set to desired value
+      const calls = apiService.post.calls.all();
+      const data = calls[0].args[2] as { headers: HttpHeaders };
+      expect(data.headers.get('Content-Type')).toBe('application/json; charset=utf-8');
+    });
   });
 });
