4af159bc5617227dd180ba026ad99989fe725426
Manjiri Chavande <manjiri.c@fyle.in>
2023-05-19 17:29:55 +0530

    code coverage 27%

--- src/app/fyle/view-expense/view-expense.page.spec.ts
+++ src/app/fyle/view-expense/view-expense.page.spec.ts
@@ -1,26 +1,369 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { LoaderService } from 'src/app/core/services/loader.service';
+import { TransactionService } from 'src/app/core/services/transaction.service';
+import { CustomInputsService } from 'src/app/core/services/custom-inputs.service';
+import { StatusService } from 'src/app/core/services/status.service';
+import { ReportService } from 'src/app/core/services/report.service';
+import { FileService } from 'src/app/core/services/file.service';
+import { NetworkService } from '../../core/services/network.service';
+import { PolicyService } from 'src/app/core/services/policy.service';
+import { ModalPropertiesService } from 'src/app/core/services/modal-properties.service';
+import { TrackingService } from '../../core/services/tracking.service';
+import { CorporateCreditCardExpenseService } from 'src/app/core/services/corporate-credit-card-expense.service';
+import { ExpenseFieldsService } from 'src/app/core/services/expense-fields.service';
+import { OrgSettingsService } from 'src/app/core/services/org-settings.service';
+import { CategoriesService } from 'src/app/core/services/categories.service';
+import { DependentFieldsService } from 'src/app/core/services/dependent-fields.service';
 import { ViewExpensePage } from './view-expense.page';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ModalController, PopoverController } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, EventEmitter } from '@angular/core';
+import { of } from 'rxjs';
+import { expenseData1 } from 'src/app/core/mock-data/expense.data';
+import { ViewCommentComponent } from 'src/app/shared/components/comments-history/view-comment/view-comment.component';
+import { ExpenseView } from 'src/app/core/models/expense-view.enum';
+import { getApiResponse } from 'src/app/core/test-data/status.service.spec.data';
+import {
+  ApproverExpensePolicyStatesData,
+  expensePolicyStatesData,
+} from 'src/app/core/mock-data/platform-policy-expense.data';
 
-xdescribe('ViewExpensePage', () => {
+fdescribe('ViewExpensePage', () => {
   let component: ViewExpensePage;
   let fixture: ComponentFixture<ViewExpensePage>;
+  let loaderService: jasmine.SpyObj<LoaderService>;
+  let transactionService: jasmine.SpyObj<TransactionService>;
+  let reportService: jasmine.SpyObj<ReportService>;
+  let customInputsService: jasmine.SpyObj<CustomInputsService>;
+  let statusService: jasmine.SpyObj<StatusService>;
+  let fileService: jasmine.SpyObj<FileService>;
+  let modalController: jasmine.SpyObj<ModalController>;
+  let router: jasmine.SpyObj<Router>;
+  let popoverController: jasmine.SpyObj<PopoverController>;
+  let networkService: jasmine.SpyObj<NetworkService>;
+  let policyService: jasmine.SpyObj<PolicyService>;
+  let modalProperties: jasmine.SpyObj<ModalPropertiesService>;
+  let trackingService: jasmine.SpyObj<TrackingService>;
+  let corporateCreditCardExpenseService: jasmine.SpyObj<CorporateCreditCardExpenseService>;
+  let expenseFieldsService: jasmine.SpyObj<ExpenseFieldsService>;
+  let orgSettingsService: jasmine.SpyObj<OrgSettingsService>;
+  let categoriesService: jasmine.SpyObj<CategoriesService>;
+  let dependentFieldsService: jasmine.SpyObj<DependentFieldsService>;
+  let activateRouteMock: ActivatedRoute;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [ViewExpensePage],
-        imports: [IonicModule.forRoot()],
-      }).compileComponents();
+  beforeEach(waitForAsync(() => {
+    const loaderServiceSpy = jasmine.createSpyObj('LoaderService', ['hideLoader', 'showLoader']);
+    const transactionServiceSpy = jasmine.createSpyObj('TransactionService', ['getEtxn', 'manualUnflag', 'manualFlag']);
+    const reportServiceSpy = jasmine.createSpyObj('ReportService', ['getTeamReport', 'removeTransaction']);
+    const customInputsServiceSpy = jasmine.createSpyObj('CustomInputsService', [
+      'getCustomPropertyDisplayValue',
+      'fillCustomProperties',
+    ]);
+    const statusServiceSpy = jasmine.createSpyObj('StatusService', ['find', 'post']);
+    const fileServiceSpy = jasmine.createSpyObj('FileService', ['findByTransactionId', 'downloadUrl']);
+    const modalControllerSpy = jasmine.createSpyObj('ModalController', ['create']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    const popoverControllerSpy = jasmine.createSpyObj('PopoverController', ['create']);
+    const networkServiceSpy = jasmine.createSpyObj('NetworkService', [
+      'isConnected',
+      'connectivityWatcher',
+      'isOnline',
+    ]);
+    const policyServiceSpy = jasmine.createSpyObj('PolicyService', [
+      'getApproverExpensePolicyViolations',
+      'getSpenderExpensePolicyViolations',
+    ]);
+    const modalPropertiesSpy = jasmine.createSpyObj('ModalPropertiesService', ['getModalDefaultProperties']);
+    const trackingServiceSpy = jasmine.createSpyObj('TrackingService', [
+      'expenseRemovedByApprover',
+      'addComment',
+      'viewComment',
+      'expenseFlagUnflagClicked',
+    ]);
+    const corporateCreditCardExpenseServiceSpy = jasmine.createSpyObj('CorporateCreditCardExpenseService', [
+      'getEccceByGroupId',
+    ]);
+    const expenseFieldsServiceSpy = jasmine.createSpyObj('ExpenseFieldsService', ['getAllMap']);
+    const orgSettingsServiceSpy = jasmine.createSpyObj('OrgSettingsService', ['get']);
+    const categoriesServiceSpy = jasmine.createSpyObj('CategoriesService', [
+      'getSystemCategories',
+      'getSystemCategoriesWithTaxi',
+      'getBreakfastSystemCategories',
+      'getTravelSystemCategories',
+      'getFlightSystemCategories',
+    ]);
+    const dependentFieldsServiceSpy = jasmine.createSpyObj('DependentFieldsService', [
+      'getDependentFieldValuesForBaseField',
+    ]);
 
-      fixture = TestBed.createComponent(ViewExpensePage);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
-  );
+    TestBed.configureTestingModule({
+      declarations: [ViewExpensePage],
+      imports: [IonicModule.forRoot(), FormsModule, MatIconModule, MatIconTestingModule],
+      providers: [
+        {
+          useValue: loaderServiceSpy,
+          provide: LoaderService,
+        },
+        {
+          useValue: transactionServiceSpy,
+          provide: TransactionService,
+        },
+        {
+          useValue: reportServiceSpy,
+          provide: ReportService,
+        },
+        {
+          useValue: customInputsServiceSpy,
+          provide: CustomInputsService,
+        },
+        {
+          useValue: statusServiceSpy,
+          provide: StatusService,
+        },
+        {
+          useValue: fileServiceSpy,
+          provide: FileService,
+        },
+        {
+          useValue: modalControllerSpy,
+          provide: ModalController,
+        },
+        {
+          useValue: routerSpy,
+          provide: Router,
+        },
+        {
+          useValue: popoverControllerSpy,
+          provide: PopoverController,
+        },
+        {
+          useValue: networkServiceSpy,
+          provide: NetworkService,
+        },
+        {
+          useValue: policyServiceSpy,
+          provide: PolicyService,
+        },
+        {
+          useValue: modalPropertiesSpy,
+          provide: ModalPropertiesService,
+        },
+        {
+          useValue: trackingServiceSpy,
+          provide: TrackingService,
+        },
+        {
+          useValue: corporateCreditCardExpenseServiceSpy,
+          provide: CorporateCreditCardExpenseService,
+        },
+        {
+          useValue: expenseFieldsServiceSpy,
+          provide: ExpenseFieldsService,
+        },
+        {
+          useValue: orgSettingsServiceSpy,
+          provide: OrgSettingsService,
+        },
+        {
+          useValue: categoriesServiceSpy,
+          provide: CategoriesService,
+        },
+        {
+          useValue: dependentFieldsServiceSpy,
+          provide: DependentFieldsService,
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: {
+                id: 'tx5fBcPBAxLv',
+                view: ExpenseView.individual,
+                txnIds: ['tx5fBcPBAxLv', 'txCBp2jIK6G3'],
+                activeIndex: 0,
+              },
+            },
+          },
+        },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ViewExpensePage);
+    component = fixture.componentInstance;
+    transactionService = TestBed.inject(TransactionService) as jasmine.SpyObj<TransactionService>;
+    reportService = TestBed.inject(ReportService) as jasmine.SpyObj<ReportService>;
+    customInputsService = TestBed.inject(CustomInputsService) as jasmine.SpyObj<CustomInputsService>;
+    statusService = TestBed.inject(StatusService) as jasmine.SpyObj<StatusService>;
+    fileService = TestBed.inject(FileService) as jasmine.SpyObj<FileService>;
+    modalController = TestBed.inject(ModalController) as jasmine.SpyObj<ModalController>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    popoverController = TestBed.inject(PopoverController) as jasmine.SpyObj<PopoverController>;
+    networkService = TestBed.inject(NetworkService) as jasmine.SpyObj<NetworkService>;
+    policyService = TestBed.inject(PolicyService) as jasmine.SpyObj<PolicyService>;
+    modalProperties = TestBed.inject(ModalPropertiesService) as jasmine.SpyObj<ModalPropertiesService>;
+    trackingService = TestBed.inject(TrackingService) as jasmine.SpyObj<TrackingService>;
+    corporateCreditCardExpenseService = TestBed.inject(
+      CorporateCreditCardExpenseService
+    ) as jasmine.SpyObj<CorporateCreditCardExpenseService>;
+    expenseFieldsService = TestBed.inject(ExpenseFieldsService) as jasmine.SpyObj<ExpenseFieldsService>;
+    orgSettingsService = TestBed.inject(OrgSettingsService) as jasmine.SpyObj<OrgSettingsService>;
+    categoriesService = TestBed.inject(CategoriesService) as jasmine.SpyObj<CategoriesService>;
+    dependentFieldsService = TestBed.inject(DependentFieldsService) as jasmine.SpyObj<DependentFieldsService>;
+    loaderService = TestBed.inject(LoaderService) as jasmine.SpyObj<LoaderService>;
+    activateRouteMock = TestBed.inject(ActivatedRoute);
+
+    fixture.detectChanges();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('ionViewWillLeave(): should execute the on page exit next function', () => {
+    spyOn(component.onPageExit, 'next');
+
+    component.ionViewWillLeave();
+    expect(component.onPageExit.next).toHaveBeenCalledOnceWith(null);
+  });
+
+  it('setupNetworkWatcher(): should setup network watcher', () => {
+    networkService.connectivityWatcher.and.returnValue(new EventEmitter(true));
+    networkService.isOnline.and.returnValue(of(false));
+
+    component.setupNetworkWatcher();
+    expect(networkService.isOnline).toHaveBeenCalledTimes(1);
+    expect(networkService.connectivityWatcher).toHaveBeenCalledTimes(1);
+    expect(router.navigate).toHaveBeenCalledOnceWith(['/', 'enterprise', 'my_dashboard']);
+  });
+
+  describe('isNumber', () => {
+    it('should return true for a number', () => {
+      const result = component.isNumber(42);
+      expect(result).toBe(true);
+    });
+
+    it('should return false for a non-number value', () => {
+      const result = component.isNumber('42');
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('openCommentsModal', () => {
+    it('on opening the comments modal it should add a comment if the data is updated', fakeAsync(() => {
+      component.view = ExpenseView.individual;
+      transactionService.getEtxn.and.returnValue(of(expenseData1));
+      const modalSpy = jasmine.createSpyObj('HTMLIonModalElement', ['present', 'onDidDismiss']);
+      modalController.create.and.returnValue(Promise.resolve(modalSpy));
+      modalSpy.onDidDismiss.and.returnValue(Promise.resolve({ data: { updated: true } } as any));
+      component.openCommentsModal();
+      tick(500);
+      expect(modalController.create).toHaveBeenCalledOnceWith({
+        component: ViewCommentComponent,
+        componentProps: {
+          objectType: 'transactions',
+          objectId: expenseData1.tx_id,
+        },
+        ...modalProperties.getModalDefaultProperties(),
+      });
+      expect(modalSpy.present).toHaveBeenCalledTimes(1);
+      expect(modalSpy.onDidDismiss).toHaveBeenCalledTimes(1);
+      expect(transactionService.getEtxn).toHaveBeenCalledOnceWith(activateRouteMock.snapshot.params.id);
+      expect(trackingService.addComment).toHaveBeenCalledOnceWith({ view: 'Individual' });
+    }));
+
+    it('on opening the comments modal it should show the comments if the data not updated', fakeAsync(() => {
+      component.view = ExpenseView.individual;
+      transactionService.getEtxn.and.returnValue(of(expenseData1));
+      const modalSpy = jasmine.createSpyObj('HTMLIonModalElement', ['present', 'onDidDismiss']);
+      modalController.create.and.returnValue(Promise.resolve(modalSpy));
+      modalSpy.onDidDismiss.and.returnValue(Promise.resolve({ data: { updated: false } } as any));
+      component.openCommentsModal();
+      tick(500);
+      expect(modalController.create).toHaveBeenCalledOnceWith({
+        component: ViewCommentComponent,
+        componentProps: {
+          objectType: 'transactions',
+          objectId: expenseData1.tx_id,
+        },
+        ...modalProperties.getModalDefaultProperties(),
+      });
+      expect(modalSpy.present).toHaveBeenCalledTimes(1);
+      expect(modalSpy.onDidDismiss).toHaveBeenCalledTimes(1);
+      expect(transactionService.getEtxn).toHaveBeenCalledOnceWith(activateRouteMock.snapshot.params.id);
+      expect(trackingService.viewComment).toHaveBeenCalledOnceWith({ view: 'Individual' });
+    }));
+  });
+
+  describe('isPolicyComment', () => {
+    it('should return true if the comment is a policy comment', () => {
+      const estatus = { st_org_user_id: 'POLICY' };
+      const result = component.isPolicyComment(estatus);
+      expect(result).toBeTrue();
+    });
+
+    it('should return false if the comment is not a policy comment', () => {
+      const estatus = { st_org_user_id: 'SYSTEM' };
+      const result = component.isPolicyComment(estatus);
+      expect(result).toBeFalse();
+    });
+  });
+
+  describe('getPolicyDetails():', () => {
+    it('should get policy details for team expenses', () => {
+      component.view = ExpenseView.team;
+      policyService.getApproverExpensePolicyViolations.and.returnValue(
+        of(ApproverExpensePolicyStatesData.data[0].individual_desired_states)
+      );
+      component.getPolicyDetails('txRNWeQRXhso');
+      expect(policyService.getApproverExpensePolicyViolations).toHaveBeenCalledOnceWith('txRNWeQRXhso');
+    });
+
+    it('should get policy details for individual expenses', () => {
+      component.view = ExpenseView.individual;
+      policyService.getSpenderExpensePolicyViolations.and.returnValue(
+        of(expensePolicyStatesData.data[0].individual_desired_states)
+      );
+      component.getPolicyDetails('txVTmNOp5JEa');
+      expect(policyService.getSpenderExpensePolicyViolations).toHaveBeenCalledOnceWith('txVTmNOp5JEa');
+    });
+  });
+
+  describe('goBack', () => {
+    it('should go to view team report if the expense is a team expense', () => {
+      component.reportId = 'rpWDg3QX3';
+      component.view = ExpenseView.team;
+      component.goBack();
+      expect(router.navigate).toHaveBeenCalledOnceWith([
+        '/',
+        'enterprise',
+        'view_team_report',
+        { id: component.reportId, navigate_back: true },
+      ]);
+    });
+
+    it('should go to view report if the expense is an individual expense', () => {
+      component.reportId = 'rpJFg3Da4';
+      component.view = ExpenseView.individual;
+      component.goBack();
+      expect(router.navigate).toHaveBeenCalledOnceWith([
+        '/',
+        'enterprise',
+        'my_view_report',
+        { id: component.reportId, navigate_back: true },
+      ]);
+    });
+  });
+
+  xit('getDisplayValue', () => {});
+  xit('ionViewWillEnter', () => {});
+  xit('getReceiptExtension', () => {});
+  xit('getReceiptDetails', () => {});
+  xit('removeExpenseFromReport', () => {});
+  xit('flagUnflagExpense', () => {});
+  xit('viewAttachments', () => {});
 });
