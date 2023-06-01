44d73602df22792efceac3717de824642588711e
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-06-01 20:17:59 +0530

    fixed types

--- src/app/core/models/txn-custom-properties.model.ts
+++ src/app/core/models/txn-custom-properties.model.ts
@@ -2,10 +2,15 @@ export interface TxnCustomProperties {
   id?: number;
   mandatory?: boolean;
   name: string;
-  options?: { label: string; value: string }[] | string[];
+  options?: CustomInputsOptions[] | string[];
   placeholder?: string;
   prefix?: string;
   type?: string;
   value: any;
   parent_field_id?: number;
+}
+
+export interface CustomInputsOptions {
+  label: string;
+  value: string;
 }
--- src/app/core/services/custom-fields.service.ts
+++ src/app/core/services/custom-fields.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CustomProperty } from '../models/custom-properties.model';
-import { TxnCustomProperties } from '../models/txn-custom-properties.model';
+import { CustomInputsOptions, TxnCustomProperties } from '../models/txn-custom-properties.model';
 import { ExpenseField } from '../models/v1/expense-field.model';
 
 @Injectable({
@@ -36,11 +36,11 @@ export class CustomFieldsService {
     /* Setting the name and mandatory based on the custom input key
      * Reason: Same method is used for expense custom fields and transport/advance request custom fields
      */
-    let customInputName;
+    let customInputName: string;
     if (customInput.hasOwnProperty('field_name')) {
       customInputName = customInput.field_name;
     } else {
-      customInputName = customInput[prefix + 'name'];
+      customInputName = customInput[prefix + 'name'] as string;
     }
 
     let customInputMandatory;
@@ -48,7 +48,7 @@ export class CustomFieldsService {
       customInputMandatory = customInput.is_mandatory;
     } else {
       // eslint-disable-next-line @typescript-eslint/dot-notation
-      customInputMandatory = customInput['mandatory'];
+      customInputMandatory = customInput['mandatory'] as boolean;
     }
 
     let property: TxnCustomProperties = {
@@ -56,23 +56,23 @@ export class CustomFieldsService {
       prefix,
       name: customInputName,
       value: null,
-      placeholder: customInput[prefix + 'placeholder'],
-      type: customInput[prefix + 'type'],
-      mandatory: customInputMandatory,
-      options: customInput[prefix + 'options'],
+      placeholder: customInput[prefix + 'placeholder'] as string,
+      type: customInput[prefix + 'type'] as string,
+      mandatory: customInputMandatory as boolean,
+      options: customInput[prefix + 'options'] as CustomInputsOptions[],
       parent_field_id: customInput.parent_field_id,
     };
 
-    property = this.setDefaultValue(property, customInput[prefix + 'type']);
+    property = this.setDefaultValue(property, customInput[prefix + 'type'] as string);
 
     if (customProperties) {
       for (const customProperty of customProperties) {
         if (customProperty.name === customInputName) {
           if (property.type === 'DATE' && customProperty.value) {
             // TODO: Check if this is required since the value is null by default
-            property.value = new Date(customProperty.value);
+            property.value = new Date(customProperty.value as string);
           } else {
-            property.value = customProperty.value;
+            property.value = customProperty.value as string;
           }
           break;
         }
