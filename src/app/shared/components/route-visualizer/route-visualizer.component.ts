83899c7e2f5fc5289991ac1fad372a45d98d5188
OmkarJ13 <oajoshi13@gmail.com>
2023-06-05 10:58:53 +0530

    Minor

--- src/app/shared/components/route-visualizer/route-visualizer.component.ts
+++ src/app/shared/components/route-visualizer/route-visualizer.component.ts
@@ -42,14 +42,16 @@ export class RouteVisualizerComponent implements OnChanges, OnInit {
     this.showCurrentLocationMap = false;
 
     // Only render the map if there are at least two locations and all locations have a valid latitude and longitude value
-    const hasValidLocations = this.mileageLocations.every((location) => location?.latitude && location?.longitude);
+    const hasValidLocations = this.mileageLocations.every(
+      (location) => location && location.latitude && location.longitude
+    );
 
     if (hasValidLocations && this.mileageLocations.length >= 2) {
       const mileageRoute = this.locationService.getMileageRoute(this.mileageLocations);
       this.renderMap(mileageRoute);
     } else {
       const allLocationsInvalid = this.mileageLocations.every(
-        (location) => !location?.latitude && !location?.longitude
+        (location) => !(location && location.latitude && location.longitude)
       );
       if (allLocationsInvalid) {
         this.showCurrentLocationMap = true;
