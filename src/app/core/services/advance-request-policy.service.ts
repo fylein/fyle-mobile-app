fda457c6f85ec6159c8ea90ed6f744360c8bc5c9
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-06-05 19:04:52 +0530

    fixed types

--- src/app/core/services/advance-request-policy.service.ts
+++ src/app/core/services/advance-request-policy.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { PolicyViolationCheck } from '../models/policy-violation-check.model';
+import { AdvanceRequests } from '../models/advance-requests.model';
 
 @Injectable({
   providedIn: 'root',
@@ -23,7 +24,7 @@ export class AdvanceRequestPolicyService {
       .map((desiredState) => desiredState.description);
   }
 
-  servicePost(url, data, config) {
+  servicePost(url: string, data: AdvanceRequests) {
     return this.httpClient.post(this.ROOT_ENDPOINT + '/policy/advance_requests' + url, data);
   }
 }
