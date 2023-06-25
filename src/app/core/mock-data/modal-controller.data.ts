7a229aea0ece6d3d013b2010c57333dab99aebfb
suyashpatil78 <127177049+suyashpatil78@users.noreply.github.com>
2023-06-25 10:48:54 +0530

    added data in separate data files

--- src/app/core/mock-data/modal-controller.data.ts
+++ src/app/core/mock-data/modal-controller.data.ts
@@ -2,6 +2,12 @@ import { FyFiltersComponent } from 'src/app/shared/components/fy-filters/fy-filt
 import { filterOptions1 } from './filter.data';
 import { selectedFilters1 } from './selected-filters.data';
 import { FilterOptionType } from 'src/app/shared/components/fy-filters/filter-option-type.enum';
+import { CreateNewReportComponent } from 'src/app/shared/components/create-new-report/create-new-report.component';
+import { apiExpenseRes } from './expense.data';
+import { Mode } from '@ionic/core';
+import { fyModalProperties } from './model-properties.data';
+import { AddTxnToReportDialogComponent } from 'src/app/fyle/my-expenses/add-txn-to-report-dialog/add-txn-to-report-dialog.component';
+import { PopupAlertComponent } from 'src/app/shared/components/popup-alert/popup-alert.component';
 
 export const modalControllerParams = {
   component: FyFiltersComponent,
@@ -33,4 +39,49 @@ export const modalControllerParams2 = {
     activeFilterInitialName: 'approvalDate',
   },
   cssClass: 'dialog-popover',
+};
+
+export const newReportModalParams = {
+  component: CreateNewReportComponent,
+  componentProps: {
+    selectedExpensesToReport: apiExpenseRes,
+  },
+  mode: <Mode>'ios',
+  ...fyModalProperties,
+};
+
+export const addExpenseToReportModalParams = {
+  component: AddTxnToReportDialogComponent,
+  componentProps: {
+    txId: '12345',
+  },
+  mode: <Mode>'ios',
+  ...fyModalProperties,
+};
+
+export const popoverControllerParams = {
+  component: PopupAlertComponent,
+  componentProps: {
+    title: '2 Draft Expenses blocking the way',
+    message: '2 expenses are in draft state.',
+    primaryCta: {
+      text: 'Exclude and Continue',
+      action: 'continue',
+    },
+    secondaryCta: {
+      text: 'Cancel',
+      action: 'cancel',
+    },
+  },
+  cssClass: 'pop-up-in-center',
+};
+
+export const openFromComponentConfig = {
+  data: {
+    icon: 'danger',
+    showCloseButton: true,
+    message: 'Please select one or more expenses to be reported',
+  },
+  duration: 3000,
+  panelClass: ['msb-failure-with-report-btn'],
 };
