1a5d50f98d9b079f587aae251fa35c2f1648528a
jayfyle <115472256+jayfyle@users.noreply.github.com>
2023-06-26 16:23:38 +0530

    test: Code Coverage: Add Edit Expense Page #2 (#2126)
    * coverage at 10%
    
    * removed fdescribe
    
    * coverage at 17
    
    * coverage at 18
    
    * coverage at 20
    
    * resolved comments -1
    
    * launch darkly service added
    
    * updated a spec for changed method
    
    * updated specs and resolved comments
    
    * updated spec and moved data to mock file
    
    * resolved comments -3
    
    * removed fdescribe
    
    ---------
    
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>

---
+++ src/app/core/mock-data/action-sheet-options.data.ts
@@ -0,0 +1,22 @@
+export const actionSheetOptionsData = [
+  {
+    text: 'Split Expense By Category',
+    handler: () => {},
+  },
+  {
+    text: 'Split Expense By Project',
+    handler: () => {},
+  },
+  {
+    text: 'Split Expense By Cost Center',
+    handler: () => {},
+  },
+  {
+    text: 'Dismiss as Card Payment',
+    handler: () => {},
+  },
+  {
+    text: 'Remove Card Expense',
+    handler: () => {},
+  },
+];
