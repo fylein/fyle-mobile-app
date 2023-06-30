da8a861a2463520c6927fc434e0d38524ad2b1d1
jayfyle <115472256+jayfyle@users.noreply.github.com>
2023-06-30 17:05:11 +0530

    test: Code Coverage: Add Edit Expense #3 1st Spec File (#2160)
    * added first split file
    
    * test: Code Coverage: Add Edit Expense #3 2nd Spec File (#2161)
    
    * added 2nd split file
    
    * resolved comments
    
    * resolved comments
    
    ---------
    
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
    
    ---------
    
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>

--- src/app/core/mock-data/duplicate-sets.data.ts
+++ src/app/core/mock-data/duplicate-sets.data.ts
@@ -1,5 +1,4 @@
 import { DuplicateSet } from '../models/v2/duplicate-sets.model';
-import { expenseData1 } from './expense.data';
 
 export const duplicateSetData1: DuplicateSet = {
   fields: ['field1', 'field2'],
