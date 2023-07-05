ff437e1eb6101b4532ee9c89a37b71cbace140b8
yksolanki9 <yashsolanki1709@gmail.com>
2023-07-05 14:00:19 +0530

    Fix type issues

--- src/app/core/services/mobile-number-verification.service.ts
+++ src/app/core/services/mobile-number-verification.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { HttpHeaders } from '@angular/common/http';
+import { OtpDetails } from '../models/otp-details.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,11 +10,11 @@ import { HttpHeaders } from '@angular/common/http';
 export class MobileNumberVerificationService {
   constructor(private apiService: ApiService) {}
 
-  sendOtp() {
+  sendOtp(): Observable<OtpDetails> {
     return this.apiService.post('/orgusers/verify_mobile');
   }
 
-  verifyOtp(otp: string) {
+  verifyOtp(otp: string): Observable<{ message: string }> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' });
     return this.apiService.post('/orgusers/check_mobile_verification_code', otp, { headers });
   }
