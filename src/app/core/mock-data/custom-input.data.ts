fb8da3f58a42d2ae2a5b19775f3d7ec22b78d110
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-07-21 11:21:03 +0530

    fixed unit tests

--- src/app/core/mock-data/custom-input.data.ts
+++ src/app/core/mock-data/custom-input.data.ts
@@ -54,3 +54,58 @@ export const customInputData1: Partial<CustomInput>[] = [
     value: null,
   },
 ];
+
+export const customInputData2: Partial<CustomInput>[] = [
+  {
+    id: 218266,
+    options: [],
+    placeholder: 'Enter Date',
+    type: 'DATE',
+    value: null,
+    mandatory: undefined,
+    name: undefined,
+    prefix: undefined,
+  },
+  {
+    id: 218266,
+    options: [],
+    placeholder: 'Enter Date',
+    type: 'DATE',
+    value: null,
+    mandatory: undefined,
+    name: undefined,
+    prefix: undefined,
+  },
+  {
+    id: 211326,
+    options: [],
+    placeholder: 'helo date',
+    type: 'DATE',
+    value: null,
+    mandatory: undefined,
+    name: undefined,
+    prefix: undefined,
+  },
+  {
+    id: 218265,
+    options: [],
+    placeholder: 'Boolean value',
+    type: 'BOOLEAN',
+    value: null,
+    mandatory: undefined,
+    name: undefined,
+    prefix: undefined,
+  },
+  {
+    name: 'CUSTOM FIELD',
+    value: 'qwwq',
+  },
+  {
+    name: 'Cost Code',
+    value: 'Wow this Works',
+  },
+  {
+    name: 'Cost Area',
+    value: '-',
+  },
+];
