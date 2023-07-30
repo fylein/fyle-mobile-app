5280678e8d4c5c87ba57bd07ab26e3cbc444e1d4
OmkarJ13 <oajoshi13@gmail.com>
2023-07-30 18:39:36 +0530

    Minor

--- src/app/shared/components/add-card/add-card.component.ts
+++ src/app/shared/components/add-card/add-card.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-card',
   templateUrl: './add-card.component.html',
   styleUrls: ['./add-card.component.scss'],
 })
-export class AddCardComponent {}
+export class AddCardComponent {
+  @Input() showZeroStateMessage: boolean;
+
+  @Output() addCardClick: EventEmitter<Event> = new EventEmitter<Event>();
+}
