cbbfe08c2c6a33d7c477195cac5f9e44ee0d2269
OmkarJ13 <oajoshi13@gmail.com>
2023-07-30 22:46:01 +0530

    Added tests

--- src/app/shared/components/add-card/add-card.component.spec.ts
+++ src/app/shared/components/add-card/add-card.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { AddCardComponent } from './add-card.component';
+import { getElementRef, getTextContent } from 'src/app/core/dom-helpers';
 
 describe('AddCardComponent', () => {
   let component: AddCardComponent;
@@ -20,5 +21,33 @@ describe('AddCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show a zero state message if showZeroStateMessage is true', () => {
+    component.showZeroStateMessage = true;
+    fixture.detectChanges();
+
+    const zeroStateMessageElement = getElementRef(fixture, '[data-testid="zero-state-message"]');
+    const zeroStateMessage = getTextContent(zeroStateMessageElement.nativeElement);
+
+    expect(zeroStateMessageElement).toBeTruthy();
+    expect(zeroStateMessage).toEqual('No card added yet!');
+  });
+
+  it('should not show a zero state message if showZeroStateMessage is false', () => {
+    component.showZeroStateMessage = false;
+    fixture.detectChanges();
+
+    const zeroStateMessageElement = getElementRef(fixture, '[data-testid="zero-state-message"]');
+    expect(zeroStateMessageElement).toBeNull();
+  });
+
+  it('should raise an event addCardClick when the add card button is clicked', () => {
+    spyOn(component.addCardClick, 'emit');
+
+    const addCardButton = getElementRef(fixture, '[data-testid="add-card-button"]');
+    addCardButton.nativeElement.click();
+
+    expect(component.addCardClick.emit).toHaveBeenCalled();
   });
 });
