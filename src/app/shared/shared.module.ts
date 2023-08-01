4bddd9bb22510e3e44353a3dd71718f51c7ffa1e
rupam-seal <rupam.x.seal@gmail.com>
2023-08-01 11:07:04 +0530

    feat: Upgrade Other Packages to Latest

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -53,7 +53,7 @@ import { FormButtonValidationDirective } from './directive/form-button-validatio
 import { FormatDateDirective } from './directive/format-date.directive';
 
 import { FyPreviewAttachmentsComponent } from './components/fy-preview-attachments/fy-preview-attachments.component';
-import { PinchZoomModule } from 'ngx13-pinch-zoom';
+import { PinchZoomModule } from 'ngx-pinch-zoom-16';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { FyZeroStateComponent } from './components/fy-zero-state/fy-zero-state.component';
 import { FyPopupComponent } from './components/fy-popup/fy-popup.component';
