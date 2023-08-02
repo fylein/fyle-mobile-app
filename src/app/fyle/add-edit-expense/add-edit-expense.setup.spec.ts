3392504a04f4b3f5147896908afd0f5d6239fb56
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-08-03 00:10:52 +0530

    removed fdescribe

--- src/app/fyle/add-edit-expense/add-edit-expense.setup.spec.ts
+++ src/app/fyle/add-edit-expense/add-edit-expense.setup.spec.ts
@@ -65,7 +65,7 @@ export function setFormValid(component) {
   });
 }
 
-fdescribe('AddEditExpensePage', () => {
+describe('AddEditExpensePage', () => {
   const getTestBed = () => {
     const accountsServiceSpy = jasmine.createSpyObj('AccountsService', [
       'getEMyAccounts',
