50485d9e7fc94316e27a397aef0965679d3c0287
OmkarJ13 <oajoshi13@gmail.com>
2023-08-02 09:46:23 +0530

    Minor

--- src/app/shared/components/card-number/card-number.component.spec.ts
+++ src/app/shared/components/card-number/card-number.component.spec.ts
@@ -2,8 +2,9 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { CardNumberComponent } from './card-number.component';
+import { getElementBySelector } from 'src/app/core/dom-helpers';
 
-xdescribe('CardNumberComponent', () => {
+describe('CardNumberComponent', () => {
   let component: CardNumberComponent;
   let fixture: ComponentFixture<CardNumberComponent>;
 
@@ -15,10 +16,22 @@ xdescribe('CardNumberComponent', () => {
 
     fixture = TestBed.createComponent(CardNumberComponent);
     component = fixture.componentInstance;
+
+    component.cardNumber = '488888******1234';
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should mask 12 digits of the card number and show dots insteads', () => {
+    const maskedDigits = getElementBySelector(fixture, '[data-testid="masked-digits"]');
+    expect(maskedDigits.childElementCount).toEqual(12);
+  });
+
+  it('should show the last four digits unmasked', () => {
+    const unmaskedDigits = getElementBySelector(fixture, '[data-testid="unmasked-digits"]');
+    expect(unmaskedDigits.textContent.trim()).toBe('1234');
   });
 });
