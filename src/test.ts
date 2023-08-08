afaba5661a8e932e35084e45520636c738e62e42
rupam-seal <rupam.x.seal@gmail.com>
2023-08-08 18:27:47 +0530

    fix: test

--- src/test.ts
+++ src/test.ts
@@ -10,3 +10,7 @@ declare const require: any;
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
   teardown: { destroyAfterEach: false },
 });
+// Then we find all the tests.
+const context = require.context('./', true, /\.spec\.ts$/);
+// And load the modules.
+context.keys().map(context);
