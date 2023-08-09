f2b557f96fa6d9a985c33e1addb014700eb9e92a
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-08-09 13:40:13 +0530

    fixed data conflict

--- src/app/core/mock-data/custom-input.data.ts
+++ src/app/core/mock-data/custom-input.data.ts
@@ -137,3 +137,59 @@ export const customInputsData3: Partial<CustomInput>[] = [
     value: 'Wow this Works',
   },
 ];
+
+export const customInputsData4: Partial<CustomInput>[] = [
+  {
+    id: 200227,
+    name: 'userlist',
+    options: [],
+    value: [],
+  },
+  {
+    id: 210649,
+    name: 'User List',
+    options: [],
+    value: [],
+  },
+  {
+    id: 210281,
+    name: 'test',
+    options: [],
+    value: '',
+  },
+  {
+    id: 212819,
+    name: 'category2',
+    options: [],
+    value: '',
+  },
+  {
+    id: 206206,
+    name: 'pub create hola 1',
+    options: [],
+    value: null,
+  },
+  {
+    id: 211321,
+    name: 'test 112',
+    options: [],
+    value: null,
+  },
+  {
+    id: 206198,
+    name: '2232323',
+    options: [],
+    value: null,
+  },
+  {
+    id: 211326,
+    name: 'select all 2',
+    options: [
+      {
+        label: '2023-02-13T17:00:00.000Z',
+        value: '2023-02-13T17:00:00.000Z',
+      },
+    ],
+    value: '2023-02-13T17:00:00.000Z',
+  },
+];
