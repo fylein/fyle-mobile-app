c19081602820b85633c0bbe2b7a68793f22f901b
OmkarJ13 <oajoshi13@gmail.com>
2023-08-11 14:36:51 +0530

    Fixed lints

--- src/app/core/services/real-time-feed.service.ts
+++ src/app/core/services/real-time-feed.service.ts
@@ -1,14 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, map, throwError } from 'rxjs';
-
-import { PlatformCorporateCard } from '../models/platform/platform-corporate-card.model';
-import { PlatformApiPayload } from '../models/platform/platform-api-payload.model';
-import { EnrollCardPayload } from '../models/platform/enroll-card-payload.model';
-import { SpenderPlatformV1ApiService } from './spender-platform-v1-api.service';
-import { EnrollCardResponse } from '../models/platform/enroll-card-response.model';
 import { RTFCardType } from '../enums/rtf-card-type.enum';
-import { PlatformApiError } from '../models/platform/platform-api-error.model';
 
 @Injectable({
   providedIn: 'root',
