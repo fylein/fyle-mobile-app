63e76b52c1c17a6e357b2c09994261aff012fa50
OmkarJ13 <oajoshi13@gmail.com>
2023-08-11 14:30:03 +0530

    Reverted manage corporate cards page

--- src/app/fyle/manage-corporate-cards/manage-corporate-cards.page.ts
+++ src/app/fyle/manage-corporate-cards/manage-corporate-cards.page.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { ActionSheetButton, ActionSheetController, PopoverController } from '@ionic/angular';
-import { BehaviorSubject, Observable, forkJoin, map, switchMap, tap } from 'rxjs';
+import { ActionSheetButton, ActionSheetController } from '@ionic/angular';
+import { Observable, forkJoin, map } from 'rxjs';
 import { DataFeedSource } from 'src/app/core/enums/data-feed-source.enum';
 import { PlatformCorporateCard } from 'src/app/core/models/platform/platform-corporate-card.model';
 import { CorporateCreditCardExpenseService } from 'src/app/core/services/corporate-credit-card-expense.service';
 import { OrgSettingsService } from 'src/app/core/services/org-settings.service';
-import { AddCorporateCardComponent } from './add-corporate-card/add-corporate-card.component';
-import { OrgUserSettingsService } from 'src/app/core/services/org-user-settings.service';
-import { OverlayResponse } from 'src/app/core/models/overlay-response.modal';
 
 @Component({
   selector: 'app-manage-corporate-cards',
@@ -22,19 +19,11 @@ export class ManageCorporateCardsPage {
 
   isMastercardRTFEnabled$: Observable<boolean>;
 
-  isYodleeEnabled$: Observable<boolean>;
-
-  isLoadingCards: boolean;
-
-  private loadCorporateCards$ = new BehaviorSubject<void>(null);
-
   constructor(
     private router: Router,
     private corporateCreditCardExpenseService: CorporateCreditCardExpenseService,
     private actionSheetController: ActionSheetController,
-    private orgSettingsService: OrgSettingsService,
-    private orgUserSettingsService: OrgUserSettingsService,
-    private popoverController: PopoverController
+    private orgSettingsService: OrgSettingsService
   ) {}
 
   goBack(): void {
@@ -42,17 +31,9 @@ export class ManageCorporateCardsPage {
   }
 
   ionViewWillEnter(): void {
-    this.isLoadingCards = true;
-
-    this.corporateCards$ = this.loadCorporateCards$.pipe(
-      switchMap(() => this.corporateCreditCardExpenseService.getCorporateCards()),
-      tap(() => {
-        this.isLoadingCards = false;
-      })
-    );
+    this.corporateCards$ = this.corporateCreditCardExpenseService.getCorporateCards();
 
     const orgSettings$ = this.orgSettingsService.get();
-    const orgUserSettings$ = this.orgUserSettingsService.get();
 
     this.isVisaRTFEnabled$ = orgSettings$.pipe(
       map((orgSettings) => orgSettings.visa_enrollment_settings.allowed && orgSettings.visa_enrollment_settings.enabled)
@@ -62,15 +43,6 @@ export class ManageCorporateCardsPage {
       map(
         (orgSettings) =>
           orgSettings.mastercard_enrollment_settings.allowed && orgSettings.mastercard_enrollment_settings.enabled
-      )
-    );
-
-    this.isYodleeEnabled$ = forkJoin([orgSettings$, orgUserSettings$]).pipe(
-      map(
-        ([orgSettings, orgUserSettings]) =>
-          orgSettings.bank_data_aggregation_settings.allowed &&
-          orgSettings.bank_data_aggregation_settings.enabled &&
-          orgUserSettings.bank_data_aggregation_settings.enabled
       )
     );
   }
@@ -131,29 +103,5 @@ export class ManageCorporateCardsPage {
 
       await actionSheet.present();
     });
-  }
-
-  openAddCorporateCardPopover(): void {
-    forkJoin([this.isVisaRTFEnabled$, this.isMastercardRTFEnabled$, this.isYodleeEnabled$]).subscribe(
-      async ([isVisaRTFEnabled, isMastercardRTFEnabled, isYodleeEnabled]) => {
-        const addCorporateCardPopover = await this.popoverController.create({
-          component: AddCorporateCardComponent,
-          cssClass: 'fy-dialog-popover',
-          componentProps: {
-            isVisaRTFEnabled,
-            isMastercardRTFEnabled,
-            isYodleeEnabled,
-          },
-        });
-
-        await addCorporateCardPopover.present();
-
-        const { data }: OverlayResponse<{ success: boolean }> = await addCorporateCardPopover.onDidDismiss();
-        if (data.success) {
-          this.isLoadingCards = true;
-          this.loadCorporateCards$.next();
-        }
-      }
-    );
   }
 }
