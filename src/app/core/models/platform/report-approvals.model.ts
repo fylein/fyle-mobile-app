7fc5a4988b44b2342ce0926a878ece72dd585766
Dimple <dimple.kh@fyle.in>
2023-08-14 23:50:25 +0530

    minor

--- src/app/core/models/platform/report-approvals.model.ts
+++ src/app/core/models/platform/report-approvals.model.ts
@@ -1,9 +1,6 @@
+import { User } from '../user.model';
 export interface ReportApprovals {
   approver_user_id: string;
-  approver_user: {
-    id: string;
-    email: string;
-    full_name: string;
-  };
+  approver_user: Pick<User, 'id' | 'full_name' | 'email'>;
   state: string;
 }
