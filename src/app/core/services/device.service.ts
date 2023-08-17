9ad711af7b13486b286b925568b9aa6693cb7e88
rupam-seal <rupam.x.seal@gmail.com>
2023-08-17 11:44:55 +0530

    fix: pr comments

--- src/app/core/services/device.service.ts
+++ src/app/core/services/device.service.ts
@@ -21,7 +21,7 @@ export class DeviceService {
     }).pipe(
       map(({ deviceInfo, deviceId, appInfo }) =>
         Object.assign(deviceInfo, deviceId, {
-          uuid: '',
+          uuid: deviceId.identifier,
           appVersion: appInfo.version,
           liveUpdateAppVersion: environment.LIVE_UPDATE_APP_VERSION,
         }),
