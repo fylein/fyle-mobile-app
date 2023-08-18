e079cec112a91d219b95c3218346033e78b1fefd
Dimple <dimple.kh@fyle.in>
2023-08-18 13:24:03 +0530

    platform api service tests fixed

--- src/app/core/services/approver-platform-api.service.spec.ts
+++ src/app/core/services/approver-platform-api.service.spec.ts
@@ -7,8 +7,16 @@ import { platformReportData } from '../mock-data/platform-report.data';
 import { of } from 'rxjs';
 import { PlatformReport } from '../models/platform/platform-report.model';
 
+const requestObj = {
+  someKey: 'someValue',
+};
+
+const apiResponse = {
+  message: 'SUCCESS',
+};
+
 fdescribe('ApproverPlatformApiService', () => {
-  let approverPlatformApiService: ApproverPlatformApiService;
+  let approverPlatformService: ApproverPlatformApiService;
   let httpTestingController: HttpTestingController;
   let httpClient: jasmine.SpyObj<HttpClient>;
   const rootUrl = 'https://staging.fyle.tech';
@@ -26,47 +34,66 @@ fdescribe('ApproverPlatformApiService', () => {
         },
       ],
     });
-    approverPlatformApiService = TestBed.inject(ApproverPlatformApiService);
+    approverPlatformService = TestBed.inject(ApproverPlatformApiService);
     httpTestingController = TestBed.inject(HttpTestingController);
     httpClient = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
-    approverPlatformApiService.setRoot(rootUrl);
+    approverPlatformService.setRoot(rootUrl);
   });
 
   it('should be created', () => {
-    expect(approverPlatformApiService).toBeTruthy();
+    expect(approverPlatformService).toBeTruthy();
   });
 
   it('setRoot(): should set root url', () => {
-    expect(approverPlatformApiService.ROOT_ENDPOINT).toBe(rootUrl);
+    expect(approverPlatformService.ROOT_ENDPOINT).toBe(rootUrl);
   });
 
   describe('get():', () => {
-    it('should get data from the API without config', (done) => {
-      const url = '/expense_policy_states';
-      approverPlatformApiService.get(url).subscribe((res) => {
-        expect(res).toEqual(ApproverExpensePolicyStatesData);
+    it('should make GET request without params', (done) => {
+      httpClient.get.and.returnValue(of(apiResponse));
+
+      approverPlatformService.get('/reports').subscribe((res) => {
+        expect(res).toEqual(apiResponse);
+        expect(httpClient.get).toHaveBeenCalledOnceWith('https://staging.fyle.tech/platform/v1/approver/reports', {});
         done();
       });
-      const req = httpTestingController.expectOne(`${rootUrl}/platform/v1/approver${url}`);
-      expect(req.request.method).toEqual('GET');
-      req.flush(ApproverExpensePolicyStatesData);
     });
 
-    it('should get data from the API with config', (done) => {
-      const url = '/expense_policy_states';
-      const exp_id = {
-        expense_id: 'eq.txRNWeQRXhso',
-      };
-      approverPlatformApiService.get(url, { params: exp_id }).subscribe((res) => {
-        expect(res).toEqual(ApproverExpensePolicyStatesData);
+    it('should make GET request with params', (done) => {
+      httpClient.get.and.returnValue(of(apiResponse));
+
+      approverPlatformService.get('/reports', { params: requestObj }).subscribe((res) => {
+        expect(res).toEqual(apiResponse);
+        expect(httpClient.get).toHaveBeenCalledOnceWith('https://staging.fyle.tech/platform/v1/approver/reports', {
+          params: requestObj,
+        });
         done();
       });
-      const req = httpTestingController.expectOne(
-        `${rootUrl}/platform/v1/approver${url}?expense_id=${exp_id.expense_id}`
-      );
-      expect(req.request.method).toEqual('GET');
-      expect(req.request.params.get('expense_id')).toEqual(exp_id.expense_id);
-      req.flush(ApproverExpensePolicyStatesData);
+    });
+  });
+
+  describe('post():', () => {
+    it('should make POST request without body', (done) => {
+      httpClient.post.and.returnValue(of(apiResponse));
+
+      approverPlatformService.post('/reports').subscribe((res) => {
+        expect(res).toEqual(apiResponse);
+        expect(httpClient.post).toHaveBeenCalledOnceWith('https://staging.fyle.tech/platform/v1/approver/reports', {});
+        done();
+      });
+    });
+
+    it('should make POST request with body', (done) => {
+      httpClient.post.and.returnValue(of(apiResponse));
+
+      approverPlatformService.post('/reports', requestObj).subscribe((res) => {
+        expect(res).toEqual(apiResponse);
+        expect(httpClient.post).toHaveBeenCalledOnceWith(
+          'https://staging.fyle.tech/platform/v1/approver/reports',
+          requestObj
+        );
+        done();
+      });
     });
   });
 
