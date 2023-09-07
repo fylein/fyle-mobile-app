d05be419b9e62ed5e965bf98826fb187247a7b13
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-09-07 16:58:13 +0530

    fixed linting

--- src/app/fyle/my-profile/my-profile.page.ts
+++ src/app/fyle/my-profile/my-profile.page.ts
@@ -1,34 +1,35 @@
 import { Component, EventEmitter } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
-import { BehaviorSubject, concat, forkJoin, from, noop, Observable } from 'rxjs';
-import { finalize, map, shareReplay, switchMap, take, tap } from 'rxjs/operators';
+import { PopoverController } from '@ionic/angular';
+import { BehaviorSubject, Observable, Subscription, concat, forkJoin, from, noop } from 'rxjs';
+import { finalize, shareReplay, switchMap, take } from 'rxjs/operators';
+import { Currency } from 'src/app/core/models/currency.model';
+import { ExtendedOrgUser } from 'src/app/core/models/extended-org-user.model';
+import { InfoCardData } from 'src/app/core/models/info-card-data.model';
+import { Org } from 'src/app/core/models/org.model';
+import { CommonOrgUserSettings, OrgUserSettings } from 'src/app/core/models/org_user_settings.model';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { OrgUserSettingsService } from 'src/app/core/services/org-user-settings.service';
-import { UserEventService } from 'src/app/core/services/user-event.service';
-import { SecureStorageService } from 'src/app/core/services/secure-storage.service';
-import { StorageService } from 'src/app/core/services/storage.service';
 import { DeviceService } from 'src/app/core/services/device.service';
 import { LoaderService } from 'src/app/core/services/loader.service';
-import { ExtendedOrgUser } from 'src/app/core/models/extended-org-user.model';
-import { globalCacheBusterNotifier } from 'ts-cacheable';
-import { TokenService } from 'src/app/core/services/token.service';
-import { TrackingService } from '../../core/services/tracking.service';
-import { environment } from 'src/environments/environment';
-import { Currency } from 'src/app/core/models/currency.model';
-import { Org } from 'src/app/core/models/org.model';
-import { OrgUserSettings } from 'src/app/core/models/org_user_settings.model';
-import { OrgService } from 'src/app/core/services/org.service';
 import { NetworkService } from 'src/app/core/services/network.service';
 import { OrgSettingsService } from 'src/app/core/services/org-settings.service';
-import { PopoverController } from '@ionic/angular';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { OrgUserSettingsService } from 'src/app/core/services/org-user-settings.service';
+import { OrgService } from 'src/app/core/services/org.service';
+import { SecureStorageService } from 'src/app/core/services/secure-storage.service';
 import { SnackbarPropertiesService } from 'src/app/core/services/snackbar-properties.service';
-import { ToastMessageComponent } from 'src/app/shared/components/toast-message/toast-message.component';
-import { VerifyNumberPopoverComponent } from './verify-number-popover/verify-number-popover.component';
+import { StorageService } from 'src/app/core/services/storage.service';
+import { TokenService } from 'src/app/core/services/token.service';
+import { UserEventService } from 'src/app/core/services/user-event.service';
 import { PopupWithBulletsComponent } from 'src/app/shared/components/popup-with-bullets/popup-with-bullets.component';
-import { CurrencyService } from 'src/app/core/services/currency.service';
+import { ToastMessageComponent } from 'src/app/shared/components/toast-message/toast-message.component';
+import { environment } from 'src/environments/environment';
+import { globalCacheBusterNotifier } from 'ts-cacheable';
+import { TrackingService } from '../../core/services/tracking.service';
 import { UpdateMobileNumberComponent } from './update-mobile-number/update-mobile-number.component';
-import { InfoCardData } from 'src/app/core/models/info-card-data.model';
+import { VerifyNumberPopoverComponent } from './verify-number-popover/verify-number-popover.component';
+import { OrgSettings } from 'src/app/core/models/org-settings.model';
+import { OverlayResponse } from 'src/app/core/models/overlay-response.modal';
 
 type EventData = {
   key: 'instaFyle' | 'defaultCurrency' | 'formAutofill';
@@ -61,7 +62,7 @@ type CopyCardDetails = {
 export class MyProfilePage {
   orgUserSettings: OrgUserSettings;
 
-  orgSettings: any;
+  orgSettings: OrgSettings;
 
   eou$: Observable<ExtendedOrgUser>;
 
@@ -75,7 +76,9 @@ export class MyProfilePage {
 
   loadEou$: BehaviorSubject<null>;
 
-  settingsMap = {
+  settingsMap: {
+    [key: string]: keyof OrgUserSettings;
+  } = {
     instaFyle: 'insta_fyle_settings',
     defaultCurrency: 'currency_settings',
     formAutofill: 'expense_form_autofills',
@@ -101,19 +104,18 @@ export class MyProfilePage {
     private popoverController: PopoverController,
     private matSnackBar: MatSnackBar,
     private snackbarProperties: SnackbarPropertiesService,
-    private currencyService: CurrencyService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
   ) {}
 
-  setupNetworkWatcher() {
+  setupNetworkWatcher(): void {
     const networkWatcherEmitter = new EventEmitter<boolean>();
     this.networkService.connectivityWatcher(networkWatcherEmitter);
     this.isConnected$ = concat(this.networkService.isOnline(), networkWatcherEmitter.asObservable()).pipe(
-      shareReplay(1)
+      shareReplay(1),
     );
   }
 
-  signOut() {
+  signOut(): void {
     try {
       forkJoin({
         device: this.deviceService.getDeviceInfo(),
@@ -124,14 +126,14 @@ export class MyProfilePage {
             this.authService.logout({
               device_id: device.uuid,
               user_id: eou.us.id,
-            })
+            }),
           ),
           finalize(() => {
             this.secureStorageService.clearAll();
             this.storageService.clearAll();
             globalCacheBusterNotifier.next();
             this.userEventService.logout();
-          })
+          }),
         )
         .subscribe(noop);
     } catch (e) {
@@ -141,9 +143,10 @@ export class MyProfilePage {
     }
   }
 
-  toggleSetting(eventData: EventData) {
+  toggleSetting(eventData: EventData): Subscription {
     const settingName = this.settingsMap[eventData.key];
-    this.orgUserSettings[settingName].enabled = eventData.isEnabled;
+    const setting = this.orgUserSettings[settingName] as CommonOrgUserSettings;
+    setting.enabled = eventData.isEnabled;
 
     if (eventData.key === 'defaultCurrency' && eventData.isEnabled && eventData.selectedCurrency) {
       this.orgUserSettings.currency_settings.preferred_currency = eventData.selectedCurrency.shortCode || null;
@@ -157,9 +160,9 @@ export class MyProfilePage {
     return this.orgUserSettingsService.post(this.orgUserSettings).subscribe(noop);
   }
 
-  ionViewWillEnter() {
+  ionViewWillEnter(): void {
     this.setupNetworkWatcher();
-    this.loadEou$ = new BehaviorSubject(null);
+    this.loadEou$ = new BehaviorSubject<null>(null);
     this.eou$ = this.loadEou$.pipe(switchMap(() => from(this.authService.getEou())));
     this.reset();
     from(this.tokenService.getClusterDomain()).subscribe((clusterDomain) => {
@@ -167,7 +170,7 @@ export class MyProfilePage {
     });
     this.ROUTER_API_ENDPOINT = environment.ROUTER_API_ENDPOINT;
 
-    const popover = this.activatedRoute.snapshot.params.openPopover;
+    const popover = this.activatedRoute.snapshot.params.openPopover as string;
     if (popover) {
       this.eou$.pipe(take(1)).subscribe((eou) => {
         if (popover === 'add_mobile_number') {
@@ -179,7 +182,7 @@ export class MyProfilePage {
     }
   }
 
-  reset() {
+  reset(): void {
     const orgUserSettings$ = this.orgUserSettingsService.get().pipe(shareReplay(1));
     this.org$ = this.orgService.getCurrentOrg();
     const orgSettings$ = this.orgSettingsService.get();
@@ -192,9 +195,9 @@ export class MyProfilePage {
           forkJoin({
             orgUserSettings: orgUserSettings$,
             orgSettings: orgSettings$,
-          })
+          }),
         ),
-        finalize(() => from(this.loaderService.hideLoader()))
+        finalize(() => from(this.loaderService.hideLoader())),
       )
       .subscribe(async (res) => {
         this.orgUserSettings = res.orgUserSettings;
@@ -203,7 +206,7 @@ export class MyProfilePage {
       });
   }
 
-  setPreferenceSettings() {
+  setPreferenceSettings(): void {
     const allPreferenceSettings: PreferenceSetting[] = [
       {
         title: 'Extract receipt details',
@@ -258,7 +261,7 @@ export class MyProfilePage {
     this.infoCardsData = allInfoCardsData.filter((infoCardData) => infoCardData.isShown);
   }
 
-  showToastMessage(message: string, type: 'success' | 'failure') {
+  showToastMessage(message: string, type: 'success' | 'failure'): void {
     const panelClass = type === 'success' ? 'msb-success' : 'msb-failure';
     let snackbarIcon: string;
     if (message.toLowerCase().includes('copied')) {
@@ -271,7 +274,7 @@ export class MyProfilePage {
     this.trackingService.showToastMessage({ ToastContent: message });
   }
 
-  async showSuccessPopover() {
+  async showSuccessPopover(): Promise<void> {
     const fyleMobileNumber = '(302) 440-2921';
     const listItems = [
       {
@@ -301,7 +304,7 @@ export class MyProfilePage {
     this.trackingService.mobileNumberVerified();
   }
 
-  async verifyMobileNumber(eou: ExtendedOrgUser) {
+  async verifyMobileNumber(eou: ExtendedOrgUser): Promise<void> {
     const verifyNumberPopoverComponent = await this.popoverController.create({
       component: VerifyNumberPopoverComponent,
       componentProps: {
@@ -311,7 +314,10 @@ export class MyProfilePage {
     });
 
     await verifyNumberPopoverComponent.present();
-    const { data } = await verifyNumberPopoverComponent.onWillDismiss();
+    const { data } = (await verifyNumberPopoverComponent.onWillDismiss()) as OverlayResponse<{
+      action: string;
+      homeCurrency: string;
+    }>;
 
     if (data) {
       if (data.action === 'BACK') {
@@ -329,7 +335,7 @@ export class MyProfilePage {
     this.trackingService.verifyMobileNumber();
   }
 
-  onVerifyCtaClicked(eou: ExtendedOrgUser) {
+  onVerifyCtaClicked(eou: ExtendedOrgUser): void {
     if (eou.ou.mobile_verification_attempts_left !== 0) {
       this.verifyMobileNumber(eou);
     } else {
@@ -337,7 +343,7 @@ export class MyProfilePage {
     }
   }
 
-  async updateMobileNumber(eou: ExtendedOrgUser) {
+  async updateMobileNumber(eou: ExtendedOrgUser): Promise<void> {
     const updateMobileNumberPopover = await this.popoverController.create({
       component: UpdateMobileNumberComponent,
       componentProps: {
@@ -351,7 +357,7 @@ export class MyProfilePage {
     });
 
     await updateMobileNumberPopover.present();
-    const { data } = await updateMobileNumberPopover.onWillDismiss();
+    const { data } = (await updateMobileNumberPopover.onWillDismiss()) as OverlayResponse<{ action: string }>;
 
     if (data) {
       if (data.action === 'SUCCESS') {
