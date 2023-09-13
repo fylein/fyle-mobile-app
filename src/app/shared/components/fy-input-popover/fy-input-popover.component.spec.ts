dfd5695d2bfe2de6dc7594fdba27ed9442c78fcd
suyashpatil78 <suyash.p@fyle.in>
2023-09-14 00:09:59 +0530

    test: remaining coverage of fy-input-popover comp

--- src/app/shared/components/fy-input-popover/fy-input-popover.component.spec.ts
+++ src/app/shared/components/fy-input-popover/fy-input-popover.component.spec.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { IonicModule, PopoverController } from '@ionic/angular';
-import { click, getElementBySelector, getTextContent } from 'src/app/core/dom-helpers';
+import { click, getElementBySelector, getElementRef, getTextContent } from 'src/app/core/dom-helpers';
 import { FyInputPopoverComponent } from './fy-input-popover.component';
 
 describe('FyInputPopoverComponent', () => {
@@ -44,15 +44,28 @@ describe('FyInputPopoverComponent', () => {
     expect(popoverController.dismiss).toHaveBeenCalledTimes(1);
   }));
 
-  it('saveValue(): should save input value', fakeAsync(() => {
-    popoverController.dismiss.and.returnValue(Promise.resolve(true));
-    component.inputValue = 'input';
-    fixture.detectChanges();
+  describe('saveValue():', () => {
+    beforeEach(() => {
+      popoverController.dismiss.and.returnValue(Promise.resolve(true));
+      component.inputValue = 'input';
+    });
 
-    tick();
-    component.saveValue();
-    expect(popoverController.dismiss).toHaveBeenCalledOnceWith({ newValue: component.inputValue });
-  }));
+    it('should save input value', fakeAsync(() => {
+      fixture.detectChanges();
+
+      tick();
+      component.saveValue();
+      expect(popoverController.dismiss).toHaveBeenCalledOnceWith({ newValue: component.inputValue });
+    }));
+
+    it('should not call popoverController.dismiss if error length is greater than 0', () => {
+      component.error = 'Please enter a valid mobile number';
+      fixture.detectChanges();
+
+      component.saveValue();
+      expect(popoverController.dismiss).not.toHaveBeenCalled();
+    });
+  });
 
   it('should display title', () => {
     component.title = 'Title';
@@ -81,5 +94,58 @@ describe('FyInputPopoverComponent', () => {
 
     expect(getTextContent(getElementBySelector(fixture, '.input-popover--input-container__label'))).toEqual('Label  *');
     expect(getTextContent(getElementBySelector(fixture, '.input-popover--input-container__mandatory'))).toEqual('*');
+  });
+
+  it('onFocus(): should set error to null', () => {
+    component.error = 'Please enter valid mobile number';
+    fixture.detectChanges();
+
+    component.onFocus();
+    expect(component.error).toEqual(null);
+  });
+
+  it('ngAfterViewInit(): should focus on input element', fakeAsync(() => {
+    const inputEl = getElementRef(fixture, 'input');
+    const focusSpy = spyOn(inputEl.nativeElement, 'focus');
+    component.ngAfterViewInit();
+
+    tick(400);
+    expect(focusSpy).toHaveBeenCalledTimes(1);
+  }));
+
+  describe('validateInput():', () => {
+    beforeEach(() => {
+      component.isRequired = true;
+    });
+
+    it('should not change error if input value is not empty', () => {
+      component.error = null;
+      component.inputValue = 'value';
+      fixture.detectChanges();
+
+      component.validateInput();
+      expect(component.error).toBeNull();
+    });
+
+    it('should set error if input value is empty and other than tel', () => {
+      component.error = null;
+      component.inputLabel = 'Report Name';
+      component.inputValue = '';
+      fixture.detectChanges();
+
+      component.validateInput();
+      expect(component.error).toEqual('Please enter a Report Name');
+    });
+
+    it('should set error if input value is empty and type is tel but value is not valid', () => {
+      component.error = null;
+      component.inputLabel = 'Mobile Number';
+      component.inputValue = '9534';
+      component.inputType = 'tel';
+      fixture.detectChanges();
+
+      component.validateInput();
+      expect(component.error).toEqual('Please enter a valid mobile number with country code. e.g. +12025559975');
+    });
   });
 });
