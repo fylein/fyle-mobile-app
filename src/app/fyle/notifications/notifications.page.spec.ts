c0642cf37e96704ad0fd00e98ca6dd5ae64c4860
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-09-23 15:08:38 +0530

    setup

--- src/app/fyle/notifications/notifications.page.spec.ts
+++ src/app/fyle/notifications/notifications.page.spec.ts
@@ -1,24 +1,92 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 
 import { NotificationsPage } from './notifications.page';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { OrgUserSettingsService } from 'src/app/core/services/org-user-settings.service';
+import { FormArray, FormBuilder } from '@angular/forms';
+import { OrgSettingsService } from 'src/app/core/services/org-settings.service';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { orgUserSettingsData } from 'src/app/core/mock-data/org-user-settings.data';
+import { of } from 'rxjs';
+import { apiEouRes } from 'src/app/core/mock-data/extended-org-user.data';
+import { orgSettingsData } from 'src/app/core/test-data/accounts.service.spec.data';
+import { notificationEventsData } from 'src/app/core/mock-data/notification-events.data';
 
-xdescribe('NotificationsPage', () => {
+fdescribe('NotificationsPage', () => {
   let component: NotificationsPage;
   let fixture: ComponentFixture<NotificationsPage>;
+  let authService: jasmine.SpyObj<AuthService>;
+  let orgUserSettingsService: jasmine.SpyObj<OrgUserSettingsService>;
+  let fb: FormBuilder;
+  let orgSettingsService: jasmine.SpyObj<OrgSettingsService>;
+  let router: jasmine.SpyObj<Router>;
+  let navController: jasmine.SpyObj<NavController>;
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [NotificationsPage],
-        imports: [IonicModule.forRoot()],
-      }).compileComponents();
+  beforeEach(waitForAsync(() => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['getEou']);
+    const orgUserSettingsServiceSpy = jasmine.createSpyObj('OrgUserSettingsService', [
+      'post',
+      'clearOrgUserSettings',
+      'get',
+      'getNotificationEvents',
+    ]);
+    const orgSettingsServiceSpy = jasmine.createSpyObj('OrgSettingsService', ['get']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    const navControllerSpy = jasmine.createSpyObj('NavController', ['back']);
 
-      fixture = TestBed.createComponent(NotificationsPage);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    })
-  );
+    TestBed.configureTestingModule({
+      declarations: [NotificationsPage],
+      imports: [RouterTestingModule],
+      providers: [
+        FormBuilder,
+        {
+          provide: AuthService,
+          useValue: authServiceSpy,
+        },
+        {
+          provide: OrgUserSettingsService,
+          useValue: orgUserSettingsServiceSpy,
+        },
+        {
+          provide: OrgSettingsService,
+          useValue: orgSettingsServiceSpy,
+        },
+        {
+          provide: Router,
+          useValue: routerSpy,
+        },
+        {
+          provide: NavController,
+          useValie: navControllerSpy,
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NotificationsPage);
+    component = fixture.componentInstance;
+
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    orgUserSettingsService = TestBed.inject(OrgUserSettingsService) as jasmine.SpyObj<OrgUserSettingsService>;
+    orgSettingsService = TestBed.inject(OrgSettingsService) as jasmine.SpyObj<OrgSettingsService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    navController = TestBed.inject(NavController) as jasmine.SpyObj<NavController>;
+    fb = TestBed.inject(FormBuilder);
+
+    component.notificationForm = fb.group({
+      notifyOption: [],
+      pushEvents: new FormArray([]),
+      emailEvents: new FormArray([]),
+    });
+
+    orgUserSettingsService.get.and.returnValue(of(orgUserSettingsData));
+    authService.getEou.and.resolveTo(apiEouRes);
+    orgSettingsService.get.and.returnValue(of(orgSettingsData));
+    orgUserSettingsService.getNotificationEvents.and.returnValue(of(notificationEventsData));
+
+    fixture.detectChanges();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
