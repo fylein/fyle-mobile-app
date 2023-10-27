d054aae41da7027740bf0995ab82f77187ec81af
OmkarJ13 <oajoshi13@gmail.com>
2023-10-27 11:12:07 +0530

    Minor

--- src/app/core/models/platform/report-approvals.model.ts
+++ src/app/core/models/platform/report-approvals.model.ts
@@ -1,8 +1,13 @@
-import { ApprovalState } from '../approval-state.enum';
 import { User } from './v1/user.model';
 
 export interface ReportApprovals {
   approver_user_id: string;
   approver_user: User;
   state: ApprovalState;
+}
+
+export enum ApprovalState {
+  APPROVAL_PENDING = 'APPROVAL_PENDING',
+  APPROVAL_DONE = 'APPROVAL_DONE',
+  APPROVAL_DISABLED = 'APPROVAL_DISABLED',
 }
