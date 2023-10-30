78e4ff94f485d7aff1dcb1e79c4838cf2be91864
OmkarJ13 <oajoshi13@gmail.com>
2023-10-30 12:26:50 +0530

    Minor

--- src/app/core/models/platform/v1/level.model.ts
+++ src/app/core/models/platform/v1/level.model.ts
@@ -4,8 +4,8 @@ export interface Level {
   created_at: string;
   updated_at: string;
   name: string;
-  band: string;
-  code: string;
-  description: string;
+  band: string | null;
+  code: string | null;
+  description: string | null;
   is_enabled: boolean;
 }
