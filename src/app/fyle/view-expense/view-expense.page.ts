ef35304e2d822d404d7cb36b4645563fd870ea59
OmkarJ13 <oajoshi13@gmail.com>
2023-11-08 17:16:18 +0530

    Minor

--- src/app/fyle/view-expense/view-expense.page.ts
+++ src/app/fyle/view-expense/view-expense.page.ts
@@ -232,15 +232,15 @@ export class ViewExpensePage {
     }
   }
 
-  setPaymentModeandIcon(etxn: PlatformExpense): void {
-    if (etxn.source_account.type === AccountType.PERSONAL_ADVANCE_ACCOUNT) {
+  setPaymentModeandIcon(expense: PlatformExpense): void {
+    if (expense.source_account.type === AccountType.PERSONAL_ADVANCE_ACCOUNT) {
       this.paymentMode = 'Advance';
       this.paymentModeIcon = 'fy-non-reimbursable';
-    } else if (etxn.source_account.type === AccountType.PERSONAL_CORPORATE_CREDIT_CARD_ACCOUNT) {
+    } else if (expense.source_account.type === AccountType.PERSONAL_CORPORATE_CREDIT_CARD_ACCOUNT) {
       this.paymentMode = 'Corporate Card';
       this.paymentModeIcon = 'fy-unmatched';
       this.isCCCTransaction = true;
-    } else if (!etxn.is_reimbursable) {
+    } else if (!expense.is_reimbursable) {
       this.paymentMode = 'Paid by Company';
       this.paymentModeIcon = 'fy-non-reimbursable';
     } else {
