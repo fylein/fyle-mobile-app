283327d3a9973b2193b12be2d98132a169a55e6a
Arjun <arjunmuraliknr@gmail.com>
2023-11-09 10:25:38 +0530

    fix mock data

--- src/app/core/mock-data/platform/v1/expense.data.ts
+++ src/app/core/mock-data/platform/v1/expense.data.ts
@@ -351,4 +351,8 @@ export const expenseData: Expense = {
   },
   user_id: 'us29O6z3jnd3',
   verifier_comments: [],
+  isDraft: false,
+  isPolicyViolated: false,
+  isCriticalPolicyViolated: false,
+  vendorDetails: 'test',
 };
