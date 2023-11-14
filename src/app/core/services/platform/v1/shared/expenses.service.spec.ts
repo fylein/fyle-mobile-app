ce3cf35336fffdb21b45e11129f2de664318b7b8
Arjun <arjunmuraliknr@gmail.com>
2023-11-14 18:42:02 +0530

    initial

--- src/app/core/services/platform/v1/shared/expenses.service.spec.ts
+++ src/app/core/services/platform/v1/shared/expenses.service.spec.ts
@@ -3,6 +3,7 @@ import { ExpensesService } from './expenses.service';
 import {
   criticalPolicyViolatedExpense,
   expenseData,
+  expenseResponseData2,
   mileageExpenseWithDistance,
   mileageExpenseWithoutDistance,
   perDiemExpenseWithMultipleNumDays,
@@ -10,8 +11,10 @@ import {
 } from 'src/app/core/mock-data/platform/v1/expense.data';
 import { Expense } from 'src/app/core/models/platform/v1/expense.model';
 import { ExpenseState } from 'src/app/core/models/expense-state.enum';
+import { currencySummaryData } from 'src/app/core/mock-data/currency-summary.data';
+import { AccountType } from 'src/app/core/models/platform/v1/account.model';
 
-describe('ExpensesService', () => {
+fdescribe('ExpensesService', () => {
   let service: ExpensesService;
 
   beforeEach(() => {
@@ -71,6 +74,233 @@ describe('ExpensesService', () => {
 
     it('should retuen vendor details for per diem expense with multiple days', () => {
       expect(service.getVendorDetails(perDiemExpenseWithMultipleNumDays)).toEqual('3 Days');
+    });
+  });
+
+  it('getPaymentModeWiseSummary(): should return the payment mode wise summary', () => {
+    // @ts-ignore
+    spyOn(service, 'getPaymentModeForExpense').and.returnValue({
+      name: 'Reimbursable',
+      key: 'reimbursable',
+    });
+
+    const paymentModes = [
+      {
+        name: 'Reimbursable',
+        key: 'reimbursable',
+      },
+      {
+        name: 'Non-Reimbursable',
+        key: 'nonReimbursable',
+      },
+      {
+        name: 'Advance',
+        key: 'advance',
+      },
+      {
+        name: 'CCC',
+        key: 'ccc',
+      },
+    ];
+
+    const summary = {
+      reimbursable: {
+        name: 'Reimbursable',
+        key: 'reimbursable',
+        amount: 207000.78,
+        count: 2,
+      },
+    };
+
+    expect(service.getPaymentModeWiseSummary(expenseResponseData2)).toEqual(summary);
+    // @ts-ignore
+    expect(service.getPaymentModeForExpense).toHaveBeenCalledWith(expenseResponseData2[0], paymentModes);
+    // @ts-ignore
+    expect(service.getPaymentModeForExpense).toHaveBeenCalledWith(expenseResponseData2[1], paymentModes);
+    // @ts-ignore
+    // expect(service.getPaymentModeForExpense).toHaveBeenCalledWith(expenseList4[2], paymentModes);
+    // @ts-ignore
+    expect(service.getPaymentModeForExpense).toHaveBeenCalledTimes(2);
+  });
+
+  it('getCurrenyWiseSummary(): should return the currency wise summary', () => {
+    // @ts-ignore
+    spyOn(service, 'addExpenseToCurrencyMap').and.callThrough();
+
+    const currencyMap = {
+      INR: { name: 'INR', currency: 'INR', amount: 89, origAmount: 89, count: 1 },
+      CLF: { name: 'CLF', currency: 'CLF', amount: 33611, origAmount: 12, count: 1 },
+      EUR: { name: 'EUR', currency: 'EUR', amount: 15775.76, origAmount: 178, count: 1 },
+    };
+
+    expect(service.getCurrenyWiseSummary(expenseResponseData2)).toEqual(currencySummaryData);
+    // @ts-ignore
+    expect(service.addExpenseToCurrencyMap).toHaveBeenCalledWith(currencyMap, 'INR', 89);
+    // @ts-ignore
+    expect(service.addExpenseToCurrencyMap).toHaveBeenCalledWith(currencyMap, 'CLF', 33611, 12);
+    // @ts-ignore
+    expect(service.addExpenseToCurrencyMap).toHaveBeenCalledWith(currencyMap, 'EUR', 15775.76, 178);
+    // @ts-ignore
+    expect(service.addExpenseToCurrencyMap).toHaveBeenCalledTimes(3);
+  });
+
+  it('getPaymentModeForExpense(): should return payment mode for expense', () => {
+    spyOn(service, 'isExpenseInPaymentMode').and.returnValue(true);
+    const paymentModeList = [
+      {
+        name: 'Reimbursable',
+        key: 'reimbursable',
+      },
+      {
+        name: 'Non-Reimbursable',
+        key: 'nonReimbursable',
+      },
+      {
+        name: 'Advance',
+        key: 'advance',
+      },
+      {
+        name: 'CCC',
+        key: 'ccc',
+      },
+    ];
+    const expensePaymentMode = { name: 'Reimbursable', key: 'reimbursable' };
+    // @ts-ignore
+    expect(service.getPaymentModeForExpense(expenseData, paymentModeList)).toEqual(expensePaymentMode);
+    expect(service.isExpenseInPaymentMode).toHaveBeenCalledOnceWith(
+      !expenseData.is_reimbursable,
+      expenseData.source_account.type,
+      expensePaymentMode.key
+    );
+  });
+
+  describe('addExpenseToCurrencyMap():', () => {
+    it('should add a new currency to the map when the currencyMap does not exist', () => {
+      const currencyMap = {};
+      const expenseCurrency = 'USD';
+      const expenseAmount = 10;
+      // @ts-ignore
+      service.addExpenseToCurrencyMap(currencyMap, expenseCurrency, expenseAmount);
+
+      expect(currencyMap).toEqual({
+        USD: {
+          name: 'USD',
+          currency: 'USD',
+          amount: 10,
+          origAmount: 10,
+          count: 1,
+        },
+      });
+    });
+
+    it('should add a new currency to the map with the orig currency', () => {
+      const currencyMap = {};
+      const expenseCurrency = 'USD';
+      const expenseAmount = 10;
+      const expenseForeignAmount = 200;
+      // @ts-ignore
+      service.addExpenseToCurrencyMap(currencyMap, expenseCurrency, expenseAmount, expenseForeignAmount);
+
+      expect(currencyMap).toEqual({
+        USD: {
+          name: 'USD',
+          currency: 'USD',
+          amount: 10,
+          origAmount: 200,
+          count: 1,
+        },
+      });
+    });
+
+    it('should add the transaction amount to an existing currency in the map', () => {
+      const currencyMap = {
+        USD: {
+          name: 'USD',
+          currency: 'USD',
+          amount: 10,
+          origAmount: 10,
+          count: 1,
+        },
+      };
+      const expenseCurrency = 'USD';
+      const expenseAmount = 20;
+      // @ts-ignore
+      service.addExpenseToCurrencyMap(currencyMap, expenseCurrency, expenseAmount);
+
+      expect(currencyMap).toEqual({
+        USD: {
+          name: 'USD',
+          currency: 'USD',
+          amount: 30,
+          origAmount: 30,
+          count: 2,
+        },
+      });
+    });
+
+    it('should add the transaction amount and original amount to an existing currency in the map', () => {
+      const currencyMap = {
+        USD: {
+          name: 'USD',
+          currency: 'USD',
+          amount: 10,
+          origAmount: 10,
+          count: 1,
+        },
+      };
+      const expenseCurrency = 'USD';
+      const expenseAmount = 20;
+      const expenseForeignAmount = 30;
+      // @ts-ignore
+      service.addExpenseToCurrencyMap(currencyMap, expenseCurrency, expenseAmount, expenseForeignAmount);
+
+      expect(currencyMap).toEqual({
+        USD: {
+          name: 'USD',
+          currency: 'USD',
+          amount: 30,
+          origAmount: 40,
+          count: 2,
+        },
+      });
+    });
+  });
+
+  describe('isExpenseInPaymentMode():', () => {
+    it('should return isExpenseInPaymentMode with reimbursable payment mode', () => {
+      const expenseSkipReimbursement = false;
+      const expensePaymentMode = 'reimbursable';
+      const expenseSourceAccountType = AccountType.PERSONAL_CASH_ACCOUNT;
+      expect(
+        service.isExpenseInPaymentMode(expenseSkipReimbursement, expenseSourceAccountType, expensePaymentMode)
+      ).toBeTrue();
+    });
+
+    it('should return isExpenseInPaymentMode with non-reimbursable payment mode', () => {
+      const expenseSkipReimbursement = true;
+      const expensePaymentMode = 'nonReimbursable';
+      const expenseSourceAccountType = AccountType.PERSONAL_CASH_ACCOUNT;
+      expect(
+        service.isExpenseInPaymentMode(expenseSkipReimbursement, expenseSourceAccountType, expensePaymentMode)
+      ).toBeTrue();
+    });
+
+    it('should return isExpenseInPaymentMode with advance payment mode', () => {
+      const expenseSkipReimbursement = false;
+      const expensePaymentMode = 'advance';
+      const expenseSourceAccountType = AccountType.PERSONAL_ADVANCE_ACCOUNT;
+      expect(
+        service.isExpenseInPaymentMode(expenseSkipReimbursement, expenseSourceAccountType, expensePaymentMode)
+      ).toBeTrue();
+    });
+
+    it('should return isExpenseInPaymentMode with advance payment mode', () => {
+      const expenseSkipReimbursement = false;
+      const expensePaymentMode = 'ccc';
+      const expenseSourceAccountType = AccountType.PERSONAL_CORPORATE_CREDIT_CARD_ACCOUNT;
+      expect(
+        service.isExpenseInPaymentMode(expenseSkipReimbursement, expenseSourceAccountType, expensePaymentMode)
+      ).toBeTrue();
     });
   });
 });
