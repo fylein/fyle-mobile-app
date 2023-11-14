f5f46c7b8428b93a29ed3bc723b656e100fdf3ff
Arjun <arjunmuraliknr@gmail.com>
2023-11-14 13:34:30 +0530

    remove fdescribe

--- src/app/shared/components/expenses-card-v2/expenses-card.component.spec.ts
+++ src/app/shared/components/expenses-card-v2/expenses-card.component.spec.ts
@@ -39,7 +39,7 @@ import { AccountType } from 'src/app/core/models/platform/v1/account.model';
 import { ExpenseService as SharedExpenseService } from 'src/app/core/services/platform/v1/shared/expense.service';
 import { PopupAlertComponent } from '../popup-alert/popup-alert.component';
 
-fdescribe('ExpensesCardComponent', () => {
+describe('ExpensesCardComponent', () => {
   let component: ExpensesCardComponent;
   let fixture: ComponentFixture<ExpensesCardComponent>;
   let transactionService: jasmine.SpyObj<TransactionService>;
