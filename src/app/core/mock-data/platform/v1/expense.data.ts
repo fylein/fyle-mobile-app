76cc527026b4b9fb59279bab77862705edbbe8f9
Arjun <arjunmuraliknr@gmail.com>
2023-11-19 12:00:47 +0530

    test: Copy tests for shared expenses service from transaction service (#2564)
    * initial
    
    * fixes
    
    * feat: Remove unused code from transaction and report service (#2565)
    
    * Remove unused service methods from transaction service
    
    * remove getReportETxnc from report service
    
    * feat: My view report page to use the platform API for report expenses. (#2573)
    
    * my-view-report page get report expenses to use platform API
    
    * rename transactionsCacheBuster$ to expensesCacheBuster$

--- src/app/core/mock-data/platform/v1/expense.data.ts
+++ src/app/core/mock-data/platform/v1/expense.data.ts
@@ -1,3 +1,4 @@
+import { cloneDeep } from 'lodash';
 import { ExpenseState } from 'src/app/core/models/expense-state.enum';
 import { PlatformCategory } from 'src/app/core/models/platform/platform-category.model';
 import { MileageUnitEnum } from 'src/app/core/models/platform/platform-mileage-rates.model';
@@ -358,6 +359,30 @@ export const expenseData: Expense = {
 export const expenseResponseData = [expenseData];
 
 export const expenseResponseData2 = [expenseData, expenseData];
+
+export const expenseResponseData3: Expense[] = [
+  {
+    ...cloneDeep(expenseData),
+    amount: 89,
+    currency: 'INR',
+    foreign_currency: null,
+    foreign_amount: null,
+  },
+  {
+    ...cloneDeep(expenseData),
+    amount: 33611,
+    foreign_amount: 12,
+    currency: 'INR',
+    foreign_currency: 'CLF',
+  },
+  {
+    ...cloneDeep(expenseData),
+    amount: 15775.76,
+    foreign_amount: 178,
+    currency: 'INR',
+    foreign_currency: 'EUR',
+  },
+];
 
 export const criticalPolicyViolatedExpense: Expense = {
   ...expenseData,
