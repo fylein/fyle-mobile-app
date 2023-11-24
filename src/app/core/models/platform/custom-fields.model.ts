0990607d9f912664db7e787448368578e0fe6369
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-11-24 13:44:26 +0530

    resovled comments

--- src/app/core/models/platform/custom-fields.model.ts
+++ src/app/core/models/platform/custom-fields.model.ts
@@ -1,6 +1,8 @@
+import { CustomFieldTypes } from '../../enums/platform/v1/custom-fields-type.enum';
+
 export interface CustomFields {
   name: string;
   value: string | string[] | boolean | number | Record<string, string | string[] | boolean>;
-  type?: string;
-  is_enabled?: boolean;
+  type: CustomFieldTypes;
+  is_enabled: boolean;
 }
