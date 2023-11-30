e1abb9a1222c49a4da9bd783921e4c0983289121
jayfyle <115472256+jayfyle@users.noreply.github.com>
2023-11-30 20:31:30 +0530

    test: Move My Expenses to Platform: Fixed All Tests (#2601)
    * fixed all tests
    
    * removed optional chaining
    
    * resolved comments
    
    * fixed broken test
    
    * fixing failing test -1
    
    * fixing test -3
    
    * added data
    
    * feat: Move My Expenses to Platform: Added Public My Expenses And Renamed Platform to V2 (#2602)
    
    * added public my-expenses page
    
    * removed unrequired files
    
    * updated imports
    
    * feat: Move My Expenses to Platform: Fixed Card Filter Param (#2603)
    
    * fixed card params
    
    * feat: Move My Expenses To Platform: Added Route Guard for redirection And Updated Model (#2607)
    
    * added guard and updated model
    
    * added test and resolved comments
    
    ---------
    
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
    
    ---------
    
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
    
    ---------
    
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
    
    ---------
    
    Co-authored-by: Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>

--- src/app/core/mock-data/get-expenses-query-params-with-filters.data.ts
+++ src/app/core/mock-data/get-expenses-query-params-with-filters.data.ts
@@ -1,4 +1,14 @@
+import { GetExpensesQueryParamsWithFilters } from '../models/get-expenses-query-params-with-filters.model';
 import { GetExpenseQueryParam } from '../models/platform/v1/get-expenses-query.model';
+
+export const expectedCurrentParams: Partial<GetExpensesQueryParamsWithFilters> = {
+  sortDir: 'asc',
+  queryParams: {
+    corporate_credit_card_account_number: 'in.(789)',
+    and: '(tx_txn_dt.gte.March,tx_txn_dt.lt.April)',
+    or: ['(tx_is_split_expense.eq.true)'],
+  },
+};
 
 export const expectedCurrentParamsWoFilterState: Partial<GetExpenseQueryParam> = {
   sortDir: 'asc',
