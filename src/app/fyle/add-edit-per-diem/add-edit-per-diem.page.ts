00cb6bef6d8bd6a238928763f9c6f8530fe56348
OmkarJ13 <oajoshi13@gmail.com>
2023-12-01 17:28:31 +0530

    Tests

--- src/app/fyle/add-edit-per-diem/add-edit-per-diem.page.ts
+++ src/app/fyle/add-edit-per-diem/add-edit-per-diem.page.ts
@@ -553,7 +553,7 @@ export class AddEditPerDiemPage implements OnInit {
           } else if (
             defaultValueColumn === 'billable' &&
             this.fg.controls.project.value &&
-            (control.value === null || control.value === undefined) &&
+            (control.value === undefined || control.value === null) &&
             !control.touched
           ) {
             control.patchValue(defaultValues[defaultValueColumn]);
