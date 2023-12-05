1efaa200d26eed0cfd72a90ad98daca8f990a042
Jay Budhadev <jaybudhadev@Jays-MacBook-Air.local>
2023-12-05 14:35:43 +0530

    reverted karma file

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,9 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['parallel', 'jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      // require('karma-parallel'),
+      require('karma-parallel'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
