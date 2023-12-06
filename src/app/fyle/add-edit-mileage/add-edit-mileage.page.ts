33b2e22887667c50202bf891d451d5787860de18
Suyash Patil <127177049+suyashpatil78@users.noreply.github.com>
2023-12-06 09:50:42 +0530

    fix: typo in policy violation default comment (#2636)
    * fix: typo in policy violation default comment
    
    * Revert "fix: typo in policy violation default comment"
    
    This reverts commit a035577997fe7ba26bad0e223ef8463d521d522c.
    
    * minor

--- src/app/fyle/add-edit-mileage/add-edit-mileage.page.ts
+++ src/app/fyle/add-edit-mileage/add-edit-mileage.page.ts
@@ -2417,7 +2417,7 @@ export class AddEditMileagePage implements OnInit {
       switchMap((continueWithTransaction: { comment: string }) => {
         if (continueWithTransaction) {
           if (continueWithTransaction.comment === '' || continueWithTransaction.comment === null) {
-            continueWithTransaction.comment = 'No policy violation explaination provided';
+            continueWithTransaction.comment = 'No policy violation explanation provided';
           }
           return from(this.loaderService.showLoader()).pipe(
             switchMap(() => of({ etxn: err.etxn, comment: continueWithTransaction.comment }))
