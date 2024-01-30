b7e1db4a45b24be4580bf6284f90f2ee9e1b148e
Aastha Bist <aastha.b@fyle.in>
2024-01-30 10:12:52 +0530

    Update virtual-cards.service.ts

--- src/app/core/services/virtual-cards.service.ts
+++ src/app/core/services/virtual-cards.service.ts
@@ -32,24 +32,7 @@ export class VirtualCardsService {
       .post<Record<string, CardDetailsResponse>>('/virtual_cards/show_card_details', params)
       .pipe(map((response) => response.data));
   }
-  // getCardDetailsInBatches(virtualCardIds: string[]): any {
-  //   const batchSize = 10;
 
-  //   // Create an observable from the array of virtual card IDs
-  //   const virtualCardIds$ = from(virtualCardIds);
-
-  //   // Buffer the virtual card IDs into batches of size 'batchSize'
-  //   const batches$ = virtualCardIds$.pipe(bufferCount(batchSize));
-
-  //   // Use mergeMap to parallelize API calls within a batch
-  //   return batches$.pipe(
-  //     concatMap((batch) => zip(...batch.map((virtualCardId) => this.getCurrentAmountById(virtualCardId)))
-  //     ),
-  //     map(res => {
-  //       console.log(res);
-  //     })
-  //   );
-  // }
   getCardDetailsInBatches(virtualCardIds: string[]): any {
     const batchSize = 5;
 
@@ -76,48 +59,6 @@ export class VirtualCardsService {
       ),
       map(() => virtualCardMap)
     );
-  }
-  // getCardDetailsInBatches(virtualCardIds: string[]): Observable<any> {
-  //   const batchSize = 10;
-
-  //   // Create an observable from the array of virtual card IDs
-  //   const virtualCardIds$ = from(virtualCardIds);
-
-  //   // Buffer the virtual card IDs into batches of size 'batchSize'
-  //   const batches$ = virtualCardIds$.pipe(bufferCount(batchSize));
-
-  //   // Use concatMap to sequentially process each batch
-  //   return batches$.pipe(
-  //     concatMap((batch) =>
-  //       // Use mergeMap to parallelize API calls within a batch
-  //       from(batch).pipe(
-  //         mergeMap((virtualCardId) =>
-  //           // Make the API call for each virtual card ID
-  //           this.getCardDetailsById(virtualCardId)
-  //         ),
-  //         // Collect the results into an array
-  //         toArray(),
-  //         // Combine the virtual card IDs with their corresponding results
-  //         switchMap((cardDetailsArray) => {
-  //           const result: any = {};
-  //           batch.forEach((virtualCardId, index) => {
-  //             result[virtualCardId] = cardDetailsArray[index];
-  //           });
-  //           return result;
-  //         })
-  //       )
-  //     )
-  //   );
-  // }
-  getCardDetailsFiltered(offset: number, limit: number, virtualCardIds: string[]): Observable<any> {
-    const filteredVirtualCardIds = virtualCardIds.slice(offset, limit);
-    return forkJoin(filteredVirtualCardIds.map((id) => this.getCardDetailsById(id)));
-  }
-
-  getVirtualCardsCount(): Observable<number> {
-    return this.spenderPlatformV1ApiService
-      .post<PlatformApiResponse<Record<string, string>>>('/virtual_cards')
-      .pipe(map((response) => response.count));
   }
 
   getCurrentAmountById(virtualCardId: string): Observable<number> {
