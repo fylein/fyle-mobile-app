f813eae1ae3669b33ced78466aebfdf7a6d6bff9
Aastha Bist <aastha.b@fyle.in>
2024-01-31 15:28:59 +0530

    Update virtual-cards.service.ts

--- src/app/core/services/virtual-cards.service.ts
+++ src/app/core/services/virtual-cards.service.ts
@@ -9,25 +9,25 @@ import { PlatformApiResponse } from '../models/platform/platform-api-response.mo
 export class VirtualCardsService {
   constructor(private spenderPlatformV1ApiService: SpenderPlatformV1ApiService) {}
 
-  getCardDetailsById(virtualCardId: string): Observable<Record<string, string>> {
+  getCardDetailsById(virtualCardId: string): Observable<CardDetailsResponse> {
     const params = {
       data: {
         id: virtualCardId,
       },
     };
     return this.spenderPlatformV1ApiService
-      .post<PlatformApiResponse<Record<string, string>>>('/virtual_cards/show_card_details', params)
-      .pipe(map((response) => response.data[0]));
+      .post<Record<string, CardDetailsResponse>>('/virtual_cards/show_card_details', params)
+      .pipe(map((response) => response.data));
   }
 
-  getCurrentAmountById(virtualCardId: string): Observable<Record<string, string>> {
+  getCurrentAmountById(virtualCardId: string): Observable<number> {
     const params = {
       data: {
         id: virtualCardId,
       },
     };
     return this.spenderPlatformV1ApiService
-      .post<PlatformApiResponse<Record<string, string>>>('/virtual_cards/get_current_amount', params)
-      .pipe(map((response) => response.data[0]));
+      .post<Record<string, Record<string, number>>>('/virtual_cards/get_current_amount', params)
+      .pipe(map((response) => response.data.current_amount));
   }
 }
