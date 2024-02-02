1e6e254b6e555e330ffc37b72cb54fd6a4d990e7
Aastha Bist <aastha.b@fyle.in>
2024-02-02 19:26:34 +0530

    Update virtual-card-details-response.data.ts

--- src/app/core/mock-data/virtual-card-details-response.data.ts
+++ src/app/core/mock-data/virtual-card-details-response.data.ts
@@ -5,7 +5,7 @@ export const virtualCardDetailsResponse: Record<string, CardDetailsResponse> = {
   data: {
     full_card_number: '123412341234123',
     cvv: '123',
-    expiry_date: new Date('2029-01-01T00:00:00+00:00'),
+    expiry_date: '2029-01-01T00:00:00+00:00',
   },
 };
 
