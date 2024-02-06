64d3927f0eb485888b6afa12df35b35e1b6405bc
aastha <aastha.b@fyle.in>
2024-02-06 08:54:22 +0530

    Minor

--- src/app/core/services/virtual-cards.service.ts
+++ src/app/core/services/virtual-cards.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { SpenderPlatformV1ApiService } from './spender-platform-v1-api.service';
 import { Observable, map } from 'rxjs';
 import { VirtualCardsRequest } from '../models/virtual-cards-request.model';
-import { CardDetailsAmountResponse, CardDetailsResponse } from '../models/card-details-response.model';
+import { CardDetailsResponse } from '../models/card-details-response.model';
+import { CardDetailsAmountResponse } from '../models/card-details-amount-response';
 
 @Injectable({
   providedIn: 'root',
@@ -13,20 +14,20 @@ export class VirtualCardsService {
   getCardDetailsById(virtualCardRequestPayload: VirtualCardsRequest): Observable<CardDetailsResponse> {
     return this.spenderPlatformV1ApiService
       .post<{
-        data: CardDetailsResponse
+        data: CardDetailsResponse;
       }>('/virtual_cards/show_card_details', {
         data: virtualCardRequestPayload,
       })
       .pipe(map((response) => response.data));
   }
 
-  getCurrentAmountById(virtualCardRequestPayload: VirtualCardsRequest): Observable<number> {
+  getCurrentAmountById(virtualCardRequestPayload: VirtualCardsRequest): Observable<CardDetailsAmountResponse> {
     return this.spenderPlatformV1ApiService
       .post<{
-        data: CardDetailsAmountResponse
+        data: CardDetailsAmountResponse;
       }>('/virtual_cards/get_current_amount', {
         data: virtualCardRequestPayload,
       })
-      .pipe(map((response) => response.data.current_amount));
+      .pipe(map((response) => response.data));
   }
 }
