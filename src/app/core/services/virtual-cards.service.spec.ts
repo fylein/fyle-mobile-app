ff9351ddb81a566a1107c5fe18f0d51f00f90598
Aastha Bist <aastha.b@fyle.in>
2024-02-15 00:10:46 +0530

    feat: Add tests for Virtual Cards service (#2696)

--- src/app/core/services/virtual-cards.service.spec.ts
+++ src/app/core/services/virtual-cards.service.spec.ts
@@ -2,8 +2,15 @@ import { TestBed } from '@angular/core/testing';
 
 import { VirtualCardsService } from './virtual-cards.service';
 import { SpenderPlatformV1ApiService } from './spender-platform-v1-api.service';
+import {
+  virtualCardCurrentAmountResponse,
+  virtualCardDetailsResponse,
+} from '../mock-data/virtual-card-details-response.data';
+import { of } from 'rxjs';
+import { VirtualCardsRequest } from '../models/virtual-cards-request.model';
+import { CardDetailsResponse } from '../models/card-details-response.model';
 
-xdescribe('VirtualCardsService', () => {
+describe('VirtualCardsService', () => {
   let virtualCardsService: VirtualCardsService;
   let spenderPlatformV1ApiService: jasmine.SpyObj<SpenderPlatformV1ApiService>;
 
@@ -26,5 +33,43 @@ xdescribe('VirtualCardsService', () => {
 
   it('should be created', () => {
     expect(virtualCardsService).toBeTruthy();
+  });
+
+  it('getCardDetailsById(): should get virtual card details', (done) => {
+    const params = {
+      data: {
+        id: 'vc1234',
+      },
+    };
+    const virtualCardsRequest: VirtualCardsRequest = {
+      id: 'vc1234',
+    };
+    const expectedResponse: { data: CardDetailsResponse } = virtualCardDetailsResponse;
+    expectedResponse.data.expiry_date = new Date(expectedResponse.data.expiry_date);
+    spenderPlatformV1ApiService.post.and.returnValue(of(virtualCardDetailsResponse));
+
+    virtualCardsService.getCardDetailsById(virtualCardsRequest).subscribe((res) => {
+      expect(res).toEqual(expectedResponse.data);
+      expect(spenderPlatformV1ApiService.post).toHaveBeenCalledOnceWith('/virtual_cards/show_card_details', params);
+      done();
+    });
+  });
+
+  it('getCurrentAmountById(): should get virtual card current Amount', (done) => {
+    const params = {
+      data: {
+        id: 'vc1234',
+      },
+    };
+    const virtualCardsRequest: VirtualCardsRequest = {
+      id: 'vc1234',
+    };
+    spenderPlatformV1ApiService.post.and.returnValue(of(virtualCardCurrentAmountResponse));
+
+    virtualCardsService.getCurrentAmountById(virtualCardsRequest).subscribe((res) => {
+      expect(res).toEqual(virtualCardCurrentAmountResponse.data);
+      expect(spenderPlatformV1ApiService.post).toHaveBeenCalledOnceWith('/virtual_cards/get_current_amount', params);
+      done();
+    });
   });
 });
