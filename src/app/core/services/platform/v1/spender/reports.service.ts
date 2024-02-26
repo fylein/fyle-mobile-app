4196598fc38121fb952ccbd0aef081f265f780c4
Harshal patil <harshalmukundapatil@gmail.com>
2024-02-26 17:42:33 +0530

    Refactor report service and update reports query params model

--- src/app/core/services/platform/v1/spender/reports.service.ts
+++ src/app/core/services/platform/v1/spender/reports.service.ts
@@ -4,43 +4,49 @@ import { catchError, concatMap, map, mergeMap, reduce, switchMap, tap } from 'rx
 import { PlatformReport } from 'src/app/core/models/platform/platform-report.model';
 import { SpenderPlatformV1ApiService } from '../../../spender-platform-v1-api.service';
 import { PlatformApiResponse } from 'src/app/core/models/platform/platform-api-response.model';
-import { ReportPlatformParams } from 'src/app/core/models/platform/v1/reports-query-params.model';
+import { ReportsQueryParams } from 'src/app/core/models/platform/v1/reports-query-params.model';
 import { PAGINATION_SIZE } from 'src/app/constants';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PlatformReportService {
+export class ReportsService {
   constructor(
     @Inject(PAGINATION_SIZE) private paginationSize: number,
     private spenderPlatformV1ApiService: SpenderPlatformV1ApiService
   ) {}
 
-  getAllReportsByParams(queryParams: ReportPlatformParams): Observable<PlatformReport[]> {
+  getAllReportsByParams(queryParams: ReportsQueryParams): Observable<PlatformReport[]> {
     return this.getReportsCount(queryParams).pipe(
       switchMap((count) => {
         count = count > this.paginationSize ? count / this.paginationSize : 1;
         return range(0, count);
       }),
-      concatMap((page) => this.getReportsByParams(queryParams, this.paginationSize * page, this.paginationSize)),
+      concatMap((page) => {
+        let params = {
+          state: queryParams.state,
+          offset: this.paginationSize * page,
+          limit: this.paginationSize,
+        };
+        return this.getReportsByParams(params);
+      }),
       reduce((acc, curr) => acc.concat(curr.data), [] as PlatformReport[])
     );
   }
 
-  getReportsCount(queryParams: ReportPlatformParams): Observable<number> {
-    return this.getReportsByParams(queryParams).pipe(map((res) => res.count));
+  getReportsCount(queryParams: ReportsQueryParams): Observable<number> {
+    let params = {
+      state: queryParams.state,
+      limit: 1,
+      offset: 0,
+    };
+    return this.getReportsByParams(params).pipe(map((res) => res.count));
   }
 
-  getReportsByParams(
-    queryParams: ReportPlatformParams = {},
-    offset: number = 0,
-    limit: number = 1
-  ): Observable<PlatformApiResponse<PlatformReport>> {
+  getReportsByParams(queryParams: ReportsQueryParams = {}): Observable<PlatformApiResponse<PlatformReport>> {
     const config = {
       params: {
         ...queryParams,
-        offset,
-        limit,
       },
     };
     return this.spenderPlatformV1ApiService.get<PlatformApiResponse<PlatformReport>>('/reports', config);
