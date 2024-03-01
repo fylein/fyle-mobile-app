7bbe8cdb34b37149bddac85284726eedb7a5a80d
suyashpatil78 <suyash.p@fyle.in>
2024-03-01 18:22:16 +0530

    minor

--- src/app/core/services/platform/v1/spender/employees.service.ts
+++ src/app/core/services/platform/v1/spender/employees.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { SpenderService } from './spender.service';
 import { Observable } from 'rxjs';
 import { CommuteDetails } from 'src/app/core/models/platform/v1/commute-details.model';
+import { CommuteDetailsResponse } from 'src/app/core/models/platform/commute-details-response.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ import { CommuteDetails } from 'src/app/core/models/platform/v1/commute-details.
 export class EmployeesService {
   constructor(private spenderService: SpenderService) {}
 
-  postCommuteDetails(commuteDetails: CommuteDetails): Observable<any> {
+  postCommuteDetails(commuteDetails: CommuteDetails): Observable<CommuteDetailsResponse> {
     return this.spenderService.post('/employees/commute_details', {
       data: {
         commute_details: commuteDetails,
