9f7031a60b6912da6933a95721655dae533cd6de
suyashpatil78 <suyash.p@fyle.in>
2024-04-17 16:32:09 +0530

    feat: added script to run test without sharding browsers

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,21 @@
 module.exports = function (config) {
+  var isParallelDisabled = process && process.env && process.env.npm_lifecycle_event === 'test:no-parallel';
+
+  if (isParallelDisabled) {
+
+  }
   config.set({
     basePath: '',
-    frameworks: ['parallel', 'jasmine', '@angular-devkit/build-angular'],
-    plugins: [
+    frameworks: isParallelDisabled ? ['jasmine', '@angular-devkit/build-angular'] : ['parallel', 'jasmine', '@angular-devkit/build-angular'],
+    plugins: isParallelDisabled ? [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'), // NEWLY ADDED
+      // ORIGINALLY HERE NOW REMOVED require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ] :
+    [
       require('karma-parallel'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
