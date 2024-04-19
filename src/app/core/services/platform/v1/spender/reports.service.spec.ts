e1a2cfe675491467e5e06fbb7c51c854266cc5e7
Aastha Bist <aastha.b@fyle.in>
2024-04-19 12:34:42 +0530

    Update reports.service.spec.ts

--- src/app/core/services/platform/v1/spender/reports.service.spec.ts
+++ src/app/core/services/platform/v1/spender/reports.service.spec.ts
@@ -14,7 +14,7 @@ import {
 import { ReportsQueryParams } from 'src/app/core/models/platform/v1/reports-query-params.model';
 
 describe('SpenderReportsService', () => {
-  let reportsService: SpenderReportsService;
+  let spenderReportsService: SpenderReportsService;
   const spenderPlatformV1ApiServiceMock = jasmine.createSpyObj('SpenderPlatformV1ApiService', ['get']);
 
   beforeEach(() => {
@@ -25,16 +25,16 @@ describe('SpenderReportsService', () => {
         { provide: SpenderPlatformV1ApiService, useValue: spenderPlatformV1ApiServiceMock },
       ],
     });
-    reportsService = TestBed.inject(SpenderReportsService);
+    spenderReportsService = TestBed.inject(SpenderReportsService);
   });
 
   it('should be created', () => {
-    expect(reportsService).toBeTruthy();
+    expect(spenderReportsService).toBeTruthy();
   });
 
   it('getReportsCount(): should get a count of reports', (done) => {
     // Mock the response of getReportsByParams
-    spyOn(reportsService, 'getReportsByParams').and.returnValue(of(platformReportCountData));
+    spyOn(spenderReportsService, 'getReportsByParams').and.returnValue(of(platformReportCountData));
 
     const expectedParams: ReportsQueryParams = {
       ...mockQueryParams,
@@ -42,17 +42,17 @@ describe('SpenderReportsService', () => {
       offset: 0,
     };
 
-    reportsService.getReportsCount(mockQueryParams).subscribe((res) => {
+    spenderReportsService.getReportsCount(mockQueryParams).subscribe((res) => {
       // Verify
       expect(res).toEqual(4); // Check if the count is as expected
-      expect(reportsService.getReportsByParams).toHaveBeenCalledWith(expectedParams); // Check if the method is called with the expected params
+      expect(spenderReportsService.getReportsByParams).toHaveBeenCalledWith(expectedParams); // Check if the method is called with the expected params
       done(); // Call 'done' to indicate the end of the asynchronous test
     });
   });
 
   it('getAllReportsByParams(): should get all reports multiple pages', (done) => {
-    const getReportsByParams = spyOn(reportsService, 'getReportsByParams');
-    spyOn(reportsService, 'getReportsCount').and.returnValue(of(4));
+    const getReportsByParams = spyOn(spenderReportsService, 'getReportsByParams');
+    spyOn(spenderReportsService, 'getReportsCount').and.returnValue(of(4));
 
     const expectedParams1: ReportsQueryParams = {
       ...mockQueryParams,
@@ -68,9 +68,9 @@ describe('SpenderReportsService', () => {
     getReportsByParams.withArgs(expectedParams1).and.returnValue(of(allReportsPaginated1));
     getReportsByParams.withArgs(expectedParams2).and.returnValue(of(allReportsPaginated2));
 
-    reportsService.getAllReportsByParams(mockQueryParams).subscribe((res) => {
+    spenderReportsService.getAllReportsByParams(mockQueryParams).subscribe((res) => {
       expect(res).toEqual(expectedReportsPaginated);
-      expect(reportsService.getReportsCount).toHaveBeenCalledOnceWith({
+      expect(spenderReportsService.getReportsCount).toHaveBeenCalledOnceWith({
         state: 'in.(DRAFT,APPROVER_PENDING,APPROVER_INQUIRY)',
       });
       expect(getReportsByParams).toHaveBeenCalledWith(expectedParams1);
@@ -81,8 +81,8 @@ describe('SpenderReportsService', () => {
   });
 
   it('getAllReportsByParams(): should get all reports single page', (done) => {
-    const getReportsByParams = spyOn(reportsService, 'getReportsByParams');
-    spyOn(reportsService, 'getReportsCount').and.returnValue(of(1));
+    const getReportsByParams = spyOn(spenderReportsService, 'getReportsByParams');
+    spyOn(spenderReportsService, 'getReportsCount').and.returnValue(of(1));
 
     const expectedParams: ReportsQueryParams = {
       ...mockQueryParams,
@@ -92,9 +92,9 @@ describe('SpenderReportsService', () => {
 
     getReportsByParams.withArgs(expectedParams).and.returnValue(of(allReportsPaginated1));
 
-    reportsService.getAllReportsByParams(mockQueryParams).subscribe((res) => {
+    spenderReportsService.getAllReportsByParams(mockQueryParams).subscribe((res) => {
       expect(res).toEqual(expectedReportsSinglePage);
-      expect(reportsService.getReportsCount).toHaveBeenCalledOnceWith(mockQueryParams);
+      expect(spenderReportsService.getReportsCount).toHaveBeenCalledOnceWith(mockQueryParams);
       expect(getReportsByParams).toHaveBeenCalledOnceWith(expectedParams);
       done();
     });
@@ -112,7 +112,7 @@ describe('SpenderReportsService', () => {
       },
     };
     spenderPlatformV1ApiServiceMock.get.and.returnValue(of(allReportsPaginated1));
-    reportsService.getReportsByParams(queryParams).subscribe((response) => {
+    spenderReportsService.getReportsByParams(queryParams).subscribe((response) => {
       expect(response).toEqual(allReportsPaginated1);
       expect(spenderPlatformV1ApiServiceMock.get).toHaveBeenCalledOnceWith('/reports', expectedConfig);
       done();
