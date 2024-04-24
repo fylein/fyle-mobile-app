214f17855e4528da13119dc393a3620b994c173e
Dimple <dimple.kh@fyle.in>
2024-04-24 18:31:55 +0530

    test fix

--- src/app/core/mock-data/corporate-card-trackers.data.ts
+++ src/app/core/mock-data/corporate-card-trackers.data.ts
@@ -9,14 +9,12 @@ import {
 export const cardEnrolledProperties1: CardEnrolledProperties = {
   Source: '/enterprise/manage_corporate_cards',
   'Card Network': CardNetworkType.VISA,
-  'Existing Card': '',
   'Card ID': 'bacc15bbrRGWzf',
 };
 
 export const cardEnrolledProperties2: CardEnrolledProperties = {
   Source: '/enterprise/manage_corporate_cards',
   'Card Network': CardNetworkType.VISA,
-  'Existing Card': '5555',
   'Card ID': 'bacc15bbrRGWzf',
 };
 
@@ -28,33 +26,27 @@ export const cardUnenrolledProperties: CardUnenrolledProperties = {
 export const cardEnrollmentErrorsProperties1: CardEnrollmentErrorsProperties = {
   'Card Network': CardNetworkType.VISA,
   Source: '/enterprise/manage_corporate_cards',
-  'Existing Card': '',
   'Error Message': 'This card already exists in the system',
-  'Card Number': '4555 **** **** 5555',
 };
 
 export const cardEnrollmentErrorsProperties2: CardEnrollmentErrorsProperties = {
   'Card Network': CardNetworkType.VISA,
   Source: '/enterprise/manage_corporate_cards',
-  'Existing Card': '',
   'Error Message': 'Something went wrong. Please try after some time.',
 };
 
 export const cardEnrollmentErrorsProperties3: CardEnrollmentErrorsProperties = {
   'Card Network': CardNetworkType.VISA,
   Source: '/enterprise/manage_corporate_cards',
-  'Existing Card': '',
   'Error Message': 'Invalid card number',
 };
 
 export const cardEnrollmentErrorsProperties4: CardEnrollmentErrorsProperties = {
   'Card Network': CardNetworkType.VISA,
   Source: '/enterprise/manage_corporate_cards',
-  'Existing Card': '',
   'Error Message': 'Invalid card network',
 };
 
 export const enrollingNonRTFCardProperties: EnrollingNonRTFCardProperties = {
-  'Existing Card': '',
   Source: '/enterprise/manage_corporate_cards',
 };
