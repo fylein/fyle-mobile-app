6eb0a362b2d98b16614d324b28d9ddab1d517456
suyashpatil78 <suyash.p@fyle.in>
2024-04-28 10:36:37 +0530

    feat: adding eslint rule for converting toBe(true) to toBeTrue() etc

--- eslint-custom-rules/index.js
+++ eslint-custom-rules/index.js
@@ -2,5 +2,6 @@ module.exports = {
     rules: {
       'prefer-deep-freeze': require('./rules/eslint-plugin-prefer-deep-freeze'),
       'space-before-it-blocks': require('./rules/eslint-plugin-space-before-it-blocks'),
+      'prefer-jasmine-matchers': require('./rules/eslint-plugin-prefer-jasmine-matchers'),
     },
 };
