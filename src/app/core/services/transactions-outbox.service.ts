bfbc1c84d163f6f3c42d405b88f270aa855dcc3f
Aastha Bist <aastha.b@fyle.in>
2024-04-30 18:29:21 +0530

    fix: revert eject expenses changes (#2932)
    * Revert "fix: remove /txns from create report flow (#2906)"
    
    This reverts commit 11064facce97b25851f2b286e9a5c336b1198a65.
    
    * Revert "feat: Add eject and add expenses from platform (#2875)"
    
    This reverts commit e7c6d257db5421307c50495c1a6226a327c32b17.
    
    * Update reports.service.ts
    
    * Update report.service.ts
    
    * Update reports.service.ts

--- src/app/core/services/transactions-outbox.service.ts
+++ src/app/core/services/transactions-outbox.service.ts
@@ -9,6 +9,7 @@ import { TransactionService } from './transaction.service';
 import { FileService } from './file.service';
 import { StatusService } from './status.service';
 import { cloneDeep, indexOf } from 'lodash';
+import { ReportService } from './report.service';
 import { ParsedReceipt } from '../models/parsed_receipt.model';
 import { TrackingService } from './tracking.service';
 import { Expense } from '../models/expense.model';
@@ -18,7 +19,6 @@ import { Transaction } from '../models/v1/transaction.model';
 import { FileObject } from '../models/file-obj.model';
 import { OutboxQueue } from '../models/outbox-queue.model';
 import { ExpensesService } from './platform/v1/spender/expenses.service';
-import { SpenderReportsService } from './platform/v1/spender/reports.service';
 
 @Injectable({
   providedIn: 'root',
@@ -46,7 +46,7 @@ export class TransactionsOutboxService {
     private fileService: FileService,
     private statusService: StatusService,
     private httpClient: HttpClient,
-    private spenderReportsService: SpenderReportsService,
+    private reportService: ReportService,
     private trackingService: TrackingService,
     private currencyService: CurrencyService,
     private orgUserSettingsService: OrgUserSettingsService,
@@ -400,8 +400,8 @@ export class TransactionsOutboxService {
               that.removeEntry(entry);
               if (reportId) {
                 const txnIds = [resp.id];
-                that.spenderReportsService
-                  .addExpenses(reportId, txnIds)
+                that.reportService
+                  .addTransactions(reportId, txnIds)
                   .toPromise()
                   .then(() => {
                     this.trackingService.addToExistingReportAddEditExpense();
