81c065b0423ffaba794829cc9265a5fbbac9c2c0
Chethan <chethan7@live.com>
2024-04-30 14:29:46 +0530

    minor

--- src/app/core/services/platform/v1/approver/file.service.spec.ts
+++ src/app/core/services/platform/v1/approver/file.service.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ApproverFileService } from './file.service';
+import { ApproverPlatformApiService } from '../../../approver-platform-api.service';
 
 describe('ApproverFileService', () => {
   let service: ApproverFileService;
+  let approverPlatformApiService: jasmine.SpyObj<ApproverPlatformApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const approverPlatformApiServiceSpy = jasmine.createSpyObj('ApproverPlatformApiService', ['get', 'post']);
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: ApproverPlatformApiService,
+          useValue: approverPlatformApiServiceSpy,
+        },
+      ],
+    });
     service = TestBed.inject(ApproverFileService);
+    approverPlatformApiService = TestBed.inject(
+      ApproverPlatformApiService
+    ) as jasmine.SpyObj<ApproverPlatformApiService>;
   });
 
   it('should be created', () => {
