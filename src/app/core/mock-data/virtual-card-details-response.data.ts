6dc26bfb0a7fe1e25a26ef1682b1328878b657db
suyashpatil78 <suyash.p@fyle.in>
2024-04-30 17:23:55 +0530

    feat: added deepFreeze to not pollute global mock data - Part 13

--- src/app/core/mock-data/virtual-card-details-response.data.ts
+++ src/app/core/mock-data/virtual-card-details-response.data.ts
@@ -1,17 +1,19 @@
+import deepFreeze from 'deep-freeze-strict';
+
 import { CardDetailsAmountResponse } from '../models/card-details-amount-response';
 import { CardDetailsResponse } from '../models/card-details-response.model';
 
-export const virtualCardDetailsResponse: { data: CardDetailsResponse } = {
+export const virtualCardDetailsResponse: { data: CardDetailsResponse } = deepFreeze({
   data: {
     full_card_number: '123412341234123',
     cvv: '123',
     // @ts-ignore
     expiry_date: '2029-01-01T00:00:00+00:00',
   },
-};
+});
 
-export const virtualCardCurrentAmountResponse: { data: CardDetailsAmountResponse } = {
+export const virtualCardCurrentAmountResponse: { data: CardDetailsAmountResponse } = deepFreeze({
   data: {
     current_amount: 1000,
   },
-};
+});
