f5e0051fed45cc98c35100d8f8613ce01ea8042c
suyashpatil78 <suyash.p@fyle.in>
2024-04-30 14:50:41 +0530

    feat: added deepFreeze to not pollute global mock data - Part 9

--- src/app/core/mock-data/popover-params.data.ts
+++ src/app/core/mock-data/popover-params.data.ts
@@ -1,13 +1,15 @@
-export const getMarkDismissModalParamsData1 = {
+import deepFreeze from 'deep-freeze-strict';
+
+export const getMarkDismissModalParamsData1 = deepFreeze({
   header: 'Dismiss this expense?',
   body: "This corporate card expense will be dismissed and you won't be able to edit it.\nDo you wish to proceed?",
   ctaText: 'Yes',
   ctaLoadingText: 'Dismissing',
-};
+});
 
-export const getMarkDismissModalParamsData2 = {
+export const getMarkDismissModalParamsData2 = deepFreeze({
   header: 'Mark Expense as Personal',
   body: "This corporate card expense will be marked as personal and you won't be able to edit it.\nDo you wish to proceed?",
   ctaText: 'Yes',
   ctaLoadingText: 'Marking',
-};
+});
