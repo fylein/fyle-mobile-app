a4083f67c76f28de34d41748aacab2cd21d87134
Arjun <arjunmuraliknr@gmail.com>
2024-05-07 21:40:08 +0530

    Fix for tap jackig

--- android/app/src/main/java/com/ionicframework/fyle595781/MainActivity.java
+++ android/app/src/main/java/com/ionicframework/fyle595781/MainActivity.java
@@ -1,6 +1,17 @@
 package com.ionicframework.fyle595781;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends BridgeActivity {
+    
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+      
+    // Prevent tapjacking attacks (vapt)
+    View rootView = getWindow().getDecorView().getRootView();
+    rootView.setFilterTouchesWhenObscured(true);
+  }
 }
