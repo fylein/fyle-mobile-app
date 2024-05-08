3216ffcfb914e8ded84ec521ffe91416bdb1cd92
Arjun <arjunmuraliknr@gmail.com>
2024-05-08 22:32:18 +0530

    fix root detection vapt: Close the app if the device is rooted (#2959)

--- android/app/src/main/java/com/ionicframework/fyle595781/MainActivity.java
+++ android/app/src/main/java/com/ionicframework/fyle595781/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ionicframework.fyle595781;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
 import android.view.View;
+import com.ionicframework.fyle595781.utils.RootCheck;
+import android.widget.Toast;
 
 public class MainActivity extends BridgeActivity {
 
@@ -13,5 +16,15 @@ public class MainActivity extends BridgeActivity {
     // Prevent tapjacking attacks (vapt)
     View rootView = getWindow().getDecorView().getRootView();
     rootView.setFilterTouchesWhenObscured(true);
+
+    // Prevent rooted devices (vapt)
+    RootCheck rootCheck = new RootCheck(this);
+    Boolean isDeviceRooted = rootCheck.rootBeerCheck();
+
+    if (isDeviceRooted) {
+      // Show a Toast message and close the app
+      Toast.makeText(this, "This application can't run on Rooted android phone", Toast.LENGTH_LONG).show();
+      finish();
+    }
   }
 }
