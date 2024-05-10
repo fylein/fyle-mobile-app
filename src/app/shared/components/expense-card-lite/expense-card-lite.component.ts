f267dc09ea664dc99152ae978b50c9f08742f7f7
Chethan <chethan7@live.com>
2024-05-10 11:27:26 +0530

    minor

--- src/app/shared/components/expense-card-lite/expense-card-lite.component.ts
+++ src/app/shared/components/expense-card-lite/expense-card-lite.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FileObject } from 'src/app/core/models/file-obj.model';
-import { FileService } from 'src/app/core/services/file.service';
+import { Expense } from 'src/app/core/models/platform/v1/expense.model';
+import { ExpensesService } from 'src/app/core/services/platform/v1/spender/expenses.service';
 
 @Component({
   selector: 'app-expense-card-lite',
@@ -12,15 +12,15 @@ export class ExpenseCardLiteComponent implements OnInit {
 
   isReceiptPresent: boolean;
 
-  constructor(private fileService: FileService) {}
+  constructor(private expensesService: ExpensesService) {}
 
   ngOnInit(): void {
     this.getReceipt();
   }
 
   getReceipt() {
-    this.fileService.findByTransactionId(this.expense.id).subscribe((files: FileObject[]) => {
-      this.isReceiptPresent = files.length > 0;
+    this.expensesService.getExpenseById(this.expense.id).subscribe((expense: Expense) => {
+      this.isReceiptPresent = expense.file_ids?.length > 0;
     });
   }
 }
