d4477e0b96d690d6229da131ade964bb510a2d5d
aastha <aastha.b@fyle.in>
2024-05-10 09:03:24 +0530

    Minor

--- src/app/fyle/my-view-report/my-view-report.page.ts
+++ src/app/fyle/my-view-report/my-view-report.page.ts
@@ -267,7 +267,7 @@ export class MyViewReportPage {
       .pipe(
         map(
           (orgSetting) =>
-            orgSetting?.corporate_credit_card_settings?.enabled && orgSetting?.pending_cct_expense_restriction?.enabled
+            orgSetting?.corporate_credit_card_settings?.enabled && orgSetting.pending_cct_expense_restriction?.enabled
         ),
         switchMap((filterPendingTxn: boolean) => {
           if (filterPendingTxn) {
