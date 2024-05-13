d56139eb7b475c4d882c8ede7c339264531f8928
Chethan <chethan7@live.com>
2024-05-13 22:29:09 +0530

    minor

--- src/app/core/models/matched-expense.model.ts
+++ src/app/core/models/matched-expense.model.ts
@@ -1,15 +1,17 @@
+import { ExpenseState } from './expense-state.enum';
+
 export interface MatchedExpense {
   id: string;
   currency: string;
   amount: number;
-  spent_at: string;
-  txn_dt: string;
+  spent_at: Date;
+  txn_dt: Date;
   merchant: string;
   vendor: string;
   foreign_currency: string;
   foreign_amount: number;
   purpose: string;
-  state: string;
+  state: ExpenseState;
   seq_num: string;
   no_of_files: number;
   category_display_name: string;
