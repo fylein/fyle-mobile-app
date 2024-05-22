35d8572c6afc9d347e293500692f147830548b87
aastha <aastha.b@fyle.in>
2024-05-22 08:37:39 +0530

    Minor

--- src/app/core/models/extended_status.model.ts
+++ src/app/core/models/extended_status.model.ts
@@ -10,7 +10,7 @@ export interface ExtendedStatus {
   st_created_at: Date;
   st_diff?: any;
   st_id: string;
-  st_org_user_id: string;
+  st_org_user_id?: string;
   st_report_id?: string;
   st_state?: string;
   st_transaction_id?: string;
