3efa81607693d826a78dc16d53a8ce83c43a8454
Chethan <chethan7@live.com>
2024-05-23 18:50:24 +0530

    minor

--- src/app/core/mock-data/platform/v1/attach-receipt-payload.data.ts
+++ src/app/core/mock-data/platform/v1/attach-receipt-payload.data.ts
@@ -1,15 +1,17 @@
-export const attachReceiptPayload1 = {
+import deepFreeze from 'deep-freeze-strict';
+
+export const attachReceiptPayload1 = deepFreeze({
   data: {
     id: 'txcSFe6efB6R',
     file_id: 'fi1w2IE6JeqS',
   },
-};
+});
 
-export const attachReceiptsPayload1 = {
+export const attachReceiptsPayload1 = deepFreeze({
   data: [
     {
       id: 'txcSFe6efB6R',
       file_ids: ['fi1w2IE6JeqS', 'fiQ8ejWk25rF'],
     },
   ],
-};
+});
