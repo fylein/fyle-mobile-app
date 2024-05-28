a7921680488153d5a2c3d213162a718282961a9f
aastha <aastha.b@fyle.in>
2024-05-28 18:37:00 +0530

    fix: fix test for permission

--- src/app/core/services/permissions.service.ts
+++ src/app/core/services/permissions.service.ts
@@ -1613,9 +1613,11 @@ export class PermissionsService {
         if (this.allowedAccess(resource, orgSettings)) {
           for (const currentRole of filteredRoles) {
             const role = currentRole.toLowerCase();
+            console.log(actions, allowedActions, role, resource);
             this.setAllowedActions(actions, allowedActions, role, resource);
           }
         }
+        console.log(allowedActions);
         return allowedActions;
       }),
       switchMap((currentAllowedActions) => {
@@ -1641,6 +1643,7 @@ export class PermissionsService {
     for (const action of actions) {
       if (!allowedActions.hasOwnProperty(action) || !allowedActions[action]) {
         allowedActions[action] = this.roleActionMap[role][resource][action];
+        console.log(role, resource, action, this.roleActionMap[role][resource][action]);
         if (allowedActions[action]) {
           allowedActions.allowedRouteAccess = true;
         }
