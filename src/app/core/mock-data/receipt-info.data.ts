ee7831c37f4637f4e7c7399ffa508a53abbc3190
Chethan-Fyle <93918979+Chethan-Fyle@users.noreply.github.com>
2024-05-29 11:23:09 +0530

    feat: delete attachment using platform api (#3029)

--- src/app/core/mock-data/receipt-info.data.ts
+++ src/app/core/mock-data/receipt-info.data.ts
@@ -2,6 +2,8 @@ import deepFreeze from 'deep-freeze-strict';
 import { ReceiptInfo } from '../models/receipt-info.model';
 
 export const receiptInfoData1: ReceiptInfo = deepFreeze({
+  id: '1',
+  name: 'invoice.pdf',
   type: 'pdf',
   url: 'https://sampledownloadurl.com',
   thumbnail: 'img/fy-pdf.svg',
@@ -9,6 +11,8 @@ export const receiptInfoData1: ReceiptInfo = deepFreeze({
 
 export const receiptInfoData2: ReceiptInfo[] = deepFreeze([
   {
+    id: '1',
+    name: 'invoice.pdf',
     type: 'pdf',
     url: 'https://sampledownloadurl.com',
     thumbnail: 'img/fy-pdf.svg',
