049e711417e2b1ad0f2d60186b697dea186754f5
suyashpatil78 <suyash.p@fyle.in>
2024-06-14 22:34:28 +0530

    feat: new eslint rule for restricting more than 1 interface

--- eslint-custom-rules/index.js
+++ eslint-custom-rules/index.js
@@ -3,6 +3,7 @@ module.exports = {
       'prefer-deep-freeze': require('./rules/eslint-plugin-prefer-deep-freeze'),
       'space-before-it-blocks': require('./rules/eslint-plugin-space-before-it-blocks'),
       'prefer-jasmine-matchers': require('./rules/eslint-plugin-prefer-jasmine-matchers'),
-      'prefer-resolve-to-reject-with': require('./rules/eslint-plugin-prefer-resolve-to-reject-with')
+      'prefer-resolve-to-reject-with': require('./rules/eslint-plugin-prefer-resolve-to-reject-with'),
+      'one-interface-per-file': require('./rules/eslint-plugin-one-interface-per-file'),
     },
 };
