32bc00dbd40d9a8de9fb7291dcdf1632e54f852f
Aastha Bist <aastha.b@fyle.in>
2024-08-02 19:22:43 +0530

    feat: fix projects search using display_name (#3159)

--- src/app/core/services/projects.service.ts
+++ src/app/core/services/projects.service.ts
@@ -83,8 +83,7 @@ export class ProjectsService {
 
   addNameSearchFilter(searchNameText: string, params: PlatformProjectParams): void {
     if (typeof searchNameText !== 'undefined' && searchNameText) {
-      params.or = params.or ? `${params.or.slice(0, -1)},` : '(';
-      params.or += `name.ilike."%${searchNameText}%",sub_project.ilike."%${searchNameText}%")`;
+      params.display_name = `ilike."%${searchNameText}%"`;
     }
   }
 
