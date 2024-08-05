1a76690806cffadbaa8906c57cd12194d503384f
Aiyush <aiyush.dhar@fyle.in>
2024-08-05 19:55:19 +0530

    Added sentry to appflow command (#3163)

--- build_appflow.sh
+++ build_appflow.sh
@@ -1,3 +1,4 @@
 VERSION=`sentry-cli releases propose-version`
 ionic capacitor sync --prod --source-map
+npm run sentry:sourcemaps
 sentry-cli releases --org fyle-technologies-private-limi -p "$SENTRY_PROJECT_NAME" files "$VERSION" upload-sourcemaps www
