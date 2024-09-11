63269bdca69f7fca1ac9b3da6f3ae3b603c7d1cf
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2024-09-11 14:16:51 +0530

    fix: Replace the scanning receipt loader

--- src/app/core/services/loader.service.ts
+++ src/app/core/services/loader.service.ts
@@ -8,11 +8,22 @@ import { noop } from 'rxjs';
 export class LoaderService {
   constructor(private loadingController: LoadingController) {}
 
-  async showLoader(message = 'Please wait...', duration = 1000) {
+  async showLoader(message = 'Please wait...', duration = 1000, customLoaderUrl?: string) {
     const loading = await this.loadingController.create({
       message,
       duration,
+      spinner: customLoaderUrl ? null : 'crescent',
+      cssClass: customLoaderUrl ? 'custom-loader' : '',
     });
+
+    if (customLoaderUrl) {
+      loading.message = `
+        <div class="custom-loading">
+          <img src="${customLoaderUrl}" class="custom-loader-gif"/>
+          <span>${message}</span>
+        </div>
+      `;
+    }
     return await loading.present();
   }
 
