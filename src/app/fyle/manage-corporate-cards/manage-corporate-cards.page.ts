e8ffda31bb28862fc74894b2533f6272d3aecfb9
aastha <aastha.b@fyle.in>
2024-10-23 13:54:32 +0530

    Minor

--- src/app/fyle/manage-corporate-cards/manage-corporate-cards.page.ts
+++ src/app/fyle/manage-corporate-cards/manage-corporate-cards.page.ts
@@ -346,6 +346,7 @@ export class ManageCorporateCardsPage {
   }
 
   private checkCardsAvailabilityAndSetupSegment(corporateCards: PlatformCorporateCard[]): void {
+    // segment will only be made visible if there are virtual cards present
     this.showSegment = corporateCards.filter((corporateCard) => corporateCard.virtual_card_id).length > 0;
   }
 
