c0c2f72caa7755cbd7543305f0db380eec69da00
Aniruddha Shriwant <aniruddhashriwant@gmail.com>
2024-11-19 08:53:08 +0530

    Revert "adding logs for debugging"
    This reverts commit 3383438baef32549e9c58ef87544b7cf932b7e33.

--- build_appflow.sh
+++ build_appflow.sh
@@ -1,5 +1,3 @@
 ionic capacitor sync --prod --source-map
 npm run sentry:sourcemaps
-echo "Sentry project name: $SENTRY_PROJECT_NAME"
-echo "Live update app version: $LIVE_UPDATE_APP_VERSION"
 sentry-cli releases --org fyle-technologies-private-limi -p "$SENTRY_PROJECT_NAME" files "$LIVE_UPDATE_APP_VERSION" upload-sourcemaps www
