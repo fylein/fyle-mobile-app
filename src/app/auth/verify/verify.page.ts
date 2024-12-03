1b860743bd590132fb5ebf9fb1e04a6f631578a9
Aastha Bist <aastha.b@fyle.in>
2024-12-03 22:47:11 +0530

    fix: Revert "feat: Remove error state from verify page (#3284)" (#3291)

--- src/app/auth/verify/verify.page.ts
+++ src/app/auth/verify/verify.page.ts
@@ -4,6 +4,7 @@ import { RouterAuthService } from 'src/app/core/services/router-auth.service';
 import { switchMap, tap } from 'rxjs/operators';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { TrackingService } from '../../core/services/tracking.service';
+import { VerifyPageState } from './verify.enum';
 
 @Component({
   selector: 'app-verify',
@@ -11,6 +12,8 @@ import { TrackingService } from '../../core/services/tracking.service';
   styleUrls: ['./verify.page.scss'],
 })
 export class VerifyPage implements OnInit {
+  currentPageState: VerifyPageState = VerifyPageState.verifying;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private routerAuthService: RouterAuthService,
@@ -18,6 +21,10 @@ export class VerifyPage implements OnInit {
     private router: Router,
     private trackingService: TrackingService
   ) {}
+
+  get PageStates(): typeof VerifyPageState {
+    return VerifyPageState;
+  }
 
   ngOnInit(): void {
     const verificationCode = this.activatedRoute.snapshot.params.verification_code as string;
@@ -32,18 +39,21 @@ export class VerifyPage implements OnInit {
       )
       .subscribe({
         next: () => this.router.navigate(['/', 'auth', 'switch_org', { invite_link: true }]),
-        error: (err: { status: number }) => this.handleError(err),
+        error: (err) => this.handleError(err),
       });
   }
 
-  handleError(err: { status: number }): void {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  handleError(err: any): void {
     const orgId = this.activatedRoute.snapshot.params.org_id as string;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (err.status === 422) {
       this.router.navigate(['/', 'auth', 'disabled']);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     } else if (err.status === 440) {
       this.router.navigate(['/', 'auth', 'pending_verification', { hasTokenExpired: true, orgId }]);
     } else {
-      this.router.navigate(['/', 'auth', 'pending_verification', { orgId }]);
+      this.currentPageState = VerifyPageState.error;
     }
   }
 }
