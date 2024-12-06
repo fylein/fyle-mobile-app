a0f43ef35d24ffa310d27124e15eddd6a3d349a7
aastha <aastha.b@fyle.in>
2024-12-06 14:29:13 +0530

    Minor

--- src/app/auth/pending-verification/pending-verification.page.ts
+++ src/app/auth/pending-verification/pending-verification.page.ts
@@ -3,49 +3,67 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
 import { RouterAuthService } from 'src/app/core/services/router-auth.service';
 import { PageState } from 'src/app/core/models/page-state.enum';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SnackbarPropertiesService } from 'src/app/core/services/snackbar-properties.service';
+import { ToastMessageComponent } from 'src/app/shared/components/toast-message/toast-message.component';
 
 @Component({
   selector: 'app-pending-verification',
   templateUrl: './pending-verification.page.html',
+  styleUrls: ['./pending-verification.page.scss'],
 })
 export class PendingVerificationPage implements OnInit {
   currentPageState: PageState;
 
   isLoading = false;
 
-  hasTokenExpired = false;
+  isInvitationLinkSent = false;
+
+  fg: FormGroup;
 
   constructor(
+    private formBuilder: FormBuilder,
     private routerAuthService: RouterAuthService,
     private router: Router,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private matSnackBar: MatSnackBar,
+    private snackbarProperties: SnackbarPropertiesService
   ) {}
 
-  ngOnInit() {}
-
-  ionViewWillEnter() {
-    this.hasTokenExpired = this.activatedRoute.snapshot.params.hasTokenExpired || false;
-    this.currentPageState = PageState.notSent;
+  ngOnInit(): void {
+    this.fg = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.pattern('\\S+@\\S+\\.\\S{2,}')])],
+    });
   }
 
-  resendVerificationLink(email: string) {
+  resendVerificationLink(email: string): void {
     this.isLoading = true;
-    const orgId = this.activatedRoute.snapshot.params.orgId;
+    const orgId = this.activatedRoute.snapshot.params.orgId as string;
 
     this.routerAuthService
       .resendVerificationLink(email, orgId)
       .pipe(tap(() => (this.isLoading = false)))
       .subscribe({
-        next: () => (this.currentPageState = PageState.success),
-        error: (err) => this.handleError(err),
+        next: () => {
+          this.isInvitationLinkSent = true;
+        },
+        error: (err: { status: number }) => this.handleError(err),
       });
   }
 
-  handleError(err: any) {
+  handleError(err: { status: number }): void {
     if (err.status === 422) {
       this.router.navigate(['/', 'auth', 'disabled']);
     } else {
-      this.currentPageState = PageState.failure;
+      const toastMessageData = {
+        message: 'Something went wrong. Please try after some time.',
+      };
+
+      this.matSnackBar.openFromComponent(ToastMessageComponent, {
+        ...this.snackbarProperties.setSnackbarProperties('failure', toastMessageData),
+        panelClass: ['msb-failure'],
+      });
     }
   }
 }
