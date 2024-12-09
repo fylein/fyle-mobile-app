ad3f4bf88fe626a6d3cd6cb098a86c58e393f40a
aastha <aastha.b@fyle.in>
2024-12-09 06:02:00 +0530

    feat: Redesign reset password page

--- src/app/auth/new-password/new-password.page.ts
+++ src/app/auth/new-password/new-password.page.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { finalize, map, switchMap, tap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { finalize, switchMap, tap } from 'rxjs/operators';
 import { from, Observable } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoaderService } from 'src/app/core/services/loader.service';
 import { RouterAuthService } from 'src/app/core/services/router-auth.service';
 import { AuthService } from 'src/app/core/services/auth.service';
-import { PopoverController } from '@ionic/angular';
-import { PopupComponent } from './popup/popup.component';
 import { TrackingService } from '../../core/services/tracking.service';
 import { DeviceService } from '../../core/services/device.service';
 import { LoginInfoService } from '../../core/services/login-info.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SnackbarPropertiesService } from 'src/app/core/services/snackbar-properties.service';
+import { ToastMessageComponent } from 'src/app/shared/components/toast-message/toast-message.component';
 
 @Component({
   selector: 'app-new-password',
@@ -30,7 +31,13 @@ export class NewPasswordPage implements OnInit {
 
   lowercaseValidationDisplay$: Observable<boolean>;
 
+  isPasswordValid = false;
+
   hide = false;
+
+  hideConfirmPassword = false;
+
+  showPasswordTooltip = false;
 
   constructor(
     private fb: FormBuilder,
@@ -38,54 +45,27 @@ export class NewPasswordPage implements OnInit {
     private loaderService: LoaderService,
     private routerAuthService: RouterAuthService,
     private authService: AuthService,
-    private popoverController: PopoverController,
     private trackingService: TrackingService,
     private deviceService: DeviceService,
-    private loginInfoService: LoginInfoService
+    private loginInfoService: LoginInfoService,
+    private router: Router,
+    private matSnackBar: MatSnackBar,
+    private snackbarPropertiesService: SnackbarPropertiesService
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.fg = this.fb.group({
-      password: [
-        '',
-        Validators.compose([
-          Validators.required,
-          Validators.minLength(12),
-          Validators.maxLength(32),
-          Validators.pattern(/[A-Z]/),
-          Validators.pattern(/[a-z]/),
-          Validators.pattern(/[0-9]/),
-          Validators.pattern(/[!@#$%^&*()+\-:;<=>{}|~?]/),
-        ]),
-      ],
+      password: ['', [Validators.required, this.customPasswordValidator]],
+      confirmPassword: ['', [Validators.required, this.validatePasswordEquality]],
     });
-
-    this.lengthValidationDisplay$ = this.fg.controls.password.valueChanges.pipe(
-      map((password) => password && password.length >= 12 && password.length <= 32)
-    );
-
-    this.uppercaseValidationDisplay$ = this.fg.controls.password.valueChanges.pipe(
-      map((password) => /[A-Z]/.test(password))
-    );
-
-    this.numberValidationDisplay$ = this.fg.controls.password.valueChanges.pipe(
-      map((password) => /[0-9]/.test(password))
-    );
-    this.specialCharValidationDisplay$ = this.fg.controls.password.valueChanges.pipe(
-      map((password) => /[!@#$%^&*()+\-:;<=>{}|~?]/.test(password))
-    );
-
-    this.lowercaseValidationDisplay$ = this.fg.controls.password.valueChanges.pipe(
-      map((password) => /[a-z]/.test(password))
-    );
   }
 
-  changePassword() {
-    const refreshToken = this.activatedRoute.snapshot.params.refreshToken;
+  changePassword(): void {
+    const refreshToken = this.activatedRoute.snapshot.params.refreshToken as string;
 
     from(this.loaderService.showLoader())
       .pipe(
-        switchMap(() => this.routerAuthService.resetPassword(refreshToken, this.fg.controls.password.value)),
+        switchMap(() => this.routerAuthService.resetPassword(refreshToken, this.fg.controls.password.value as string)),
         switchMap(() => this.authService.refreshEou()),
         tap(async (eou) => {
           const email = eou.us.email;
@@ -96,36 +76,57 @@ export class NewPasswordPage implements OnInit {
         finalize(() => from(this.loaderService.hideLoader()))
       )
       .subscribe(
-        async () => {
-          const popup = await this.popoverController.create({
-            component: PopupComponent,
-            componentProps: {
-              header: 'Password changed successfully',
-              route: ['/', 'auth', 'switch_org'],
-            },
-            cssClass: 'dialog-popover',
-          });
+        () => {
+          const toastMessageData = {
+            message: 'Password changed successfully',
+          };
 
-          await popup.present();
+          this.matSnackBar.openFromComponent(ToastMessageComponent, {
+            ...this.snackbarPropertiesService.setSnackbarProperties('success', toastMessageData),
+            panelClass: ['msb-success'],
+          });
+          this.router.navigate(['/', 'auth', 'switch_org']);
         },
-        async () => {
-          const popup = await this.popoverController.create({
-            component: PopupComponent,
-            componentProps: {
-              header: 'Setting new password failed. Please try again later.',
-              route: ['/', 'auth', 'sign_in'],
-            },
-            cssClass: 'dialog-popover',
-          });
+        () => {
+          const toastMessageData = {
+            message: 'Something went wrong. Please try after some time.',
+          };
 
-          await popup.present();
+          this.matSnackBar.openFromComponent(ToastMessageComponent, {
+            ...this.snackbarPropertiesService.setSnackbarProperties('failure', toastMessageData),
+            panelClass: ['msb-failure'],
+          });
+          this.router.navigate(['/', 'auth', 'sign_in']);
         }
       );
   }
 
-  async trackLoginInfo() {
+  async trackLoginInfo(): Promise<void> {
     const deviceInfo = await this.deviceService.getDeviceInfo().toPromise();
     this.trackingService.eventTrack('Added Login Info', { label: deviceInfo.appVersion });
     await this.loginInfoService.addLoginInfo(deviceInfo.appVersion, new Date());
   }
+
+  onPasswordValid(isValid: boolean): void {
+    this.isPasswordValid = isValid;
+  }
+
+  redirectToSignIn(): void {
+    this.router.navigate(['/', 'auth', 'sign_in']);
+  }
+
+  setPasswordTooltip(value: boolean): void {
+    this.showPasswordTooltip = value;
+  }
+
+  customPasswordValidator = (): ValidationErrors => (this.isPasswordValid ? null : { invalidPassword: true });
+
+  validatePasswordEquality = (): ValidationErrors => {
+    if (!this.fg) {
+      return null;
+    }
+    const password = this.fg.controls.password.value as string;
+    const confirmPassword = this.fg.controls.confirmPassword.value as string;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  };
 }
