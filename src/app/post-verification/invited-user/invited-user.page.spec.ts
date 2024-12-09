9d7ee7b84405ca9ee001d7e88d0125aa9d90d7f5
aastha <aastha.b@fyle.in>
2024-12-09 15:09:21 +0530

    Minor

--- src/app/post-verification/invited-user/invited-user.page.spec.ts
+++ src/app/post-verification/invited-user/invited-user.page.spec.ts
@@ -95,13 +95,13 @@ describe('InvitedUserPage', () => {
     expect(component).toBeTruthy();
   });
 
-  it('ngOnInit(): should set the fullname value from eou$ and setup network watcher', fakeAsync(() => {
+  it('ionViewWillEnter(): should set the fullname value from eou$ and setup network watcher', fakeAsync(() => {
     networkService.isOnline.and.returnValue(of(true));
     const eventEmitterMock = new EventEmitter<boolean>();
     networkService.connectivityWatcher.and.returnValue(eventEmitterMock);
     component.eou$ = of(currentEouRes);
     component.fg.controls.fullName.setValue('Abhishek Jain');
-    component.ngOnInit();
+    component.ionViewWillEnter();
     tick(500);
     component.isConnected$.pipe(take(1)).subscribe((connectionStatus) => {
       expect(connectionStatus).toBeTrue();
