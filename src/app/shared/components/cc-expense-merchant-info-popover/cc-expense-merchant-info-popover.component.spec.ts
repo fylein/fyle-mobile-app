54d686d2e30c95bf273624c5993dcc916af6353f
Devendra Singh Rana <132900359+devendrafyle@users.noreply.github.com>
2024-12-19 12:39:30 +0530

    test: Added unit tests for the cc merchant info (#3372)

--- src/app/shared/components/cc-expense-merchant-info-popover/cc-expense-merchant-info-popover.component.spec.ts
+++ src/app/shared/components/cc-expense-merchant-info-popover/cc-expense-merchant-info-popover.component.spec.ts
@@ -1,24 +1,58 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, PopoverController } from '@ionic/angular';
 
-import { CcExpenseMerchantInfoPopoverComponent } from './cc-expense-merchant-info-popover.component';
+import { CCExpenseMerchantInfoPopoverComponent } from './cc-expense-merchant-info-popover.component';
+import { getElementBySelector } from 'src/app/core/dom-helpers';
 
-describe('CcExpenseMerchantInfoComponent', () => {
-  let component: CcExpenseMerchantInfoPopoverComponent;
-  let fixture: ComponentFixture<CcExpenseMerchantInfoPopoverComponent>;
+describe('CCExpenseMerchantInfoComponent', () => {
+  let component: CCExpenseMerchantInfoPopoverComponent;
+  let popoverController: jasmine.SpyObj<PopoverController>;
+  let fixture: ComponentFixture<CCExpenseMerchantInfoPopoverComponent>;
 
   beforeEach(waitForAsync(() => {
+    const popoverControllerSpy = jasmine.createSpyObj('PopoverController', ['dismiss']);
+
     TestBed.configureTestingModule({
-      declarations: [CcExpenseMerchantInfoPopoverComponent],
+      declarations: [CCExpenseMerchantInfoPopoverComponent],
       imports: [IonicModule.forRoot()],
+      providers: [
+        {
+          provide: PopoverController,
+          useValue: popoverControllerSpy,
+        },
+      ],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(CcExpenseMerchantInfoPopoverComponent);
+    fixture = TestBed.createComponent(CCExpenseMerchantInfoPopoverComponent);
+    popoverController = TestBed.inject(PopoverController) as jasmine.SpyObj<PopoverController>;
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close the popover when clicked on close button', () => {
+    const closeBtn = getElementBySelector(fixture, '[data-testid="close-btn"') as HTMLButtonElement;
+    closeBtn.click();
+
+    fixture.detectChanges();
+
+    expect(popoverController.dismiss).toHaveBeenCalled();
+  });
+
+  describe('template', () => {
+    it('should display the correct title', () => {
+      fixture.detectChanges();
+      const title = getElementBySelector(fixture, '[data-testid="title"');
+      expect(title.textContent).toEqual('Merchant');
+    });
+
+    it('should display the correct content', () => {
+      fixture.detectChanges();
+      const content = getElementBySelector(fixture, '[data-testid="content"');
+      expect(content.textContent).toEqual('This merchant name comes from the transaction.');
+    });
   });
 });
