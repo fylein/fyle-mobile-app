d4fd372048f2e2e54e0e8cfa26339d78f5498b0e
Aastha Bist <aastha.b@fyle.in>
2024-12-19 10:57:27 +0530

    feat: Add service for spender onboarding (#3378)

---
+++ src/app/core/services/spender-onboarding.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { SpenderPlatformV1ApiService } from './spender-platform-v1-api.service';
+import { PlatformApiResponse } from '../models/platform/platform-api-response.model';
+import { OnboardingWelcomeStepStatus } from '../models/onboarding-welcome-step-status.model';
+import { OnboardingStepStatus } from '../models/onboarding-step-status.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OnboardingService {
+  constructor(private spenderPlatformV1ApiService: SpenderPlatformV1ApiService) {}
+
+  getOnboardingStatus(): Observable<OnboardingStepStatus> {
+    return this.spenderPlatformV1ApiService
+      .get<PlatformApiResponse<OnboardingStepStatus>>('/spender/onboarding')
+      .pipe(map((res) => res.data));
+  }
+
+  processConnectCardsStep(data: OnboardingStepStatus): Observable<OnboardingStepStatus> {
+    return this.spenderPlatformV1ApiService
+      .post<PlatformApiResponse<OnboardingStepStatus>>('/spender/onboarding/process_step_connect_cards', {
+        data,
+      })
+      .pipe(map((res) => res.data));
+  }
+
+  processSmsOptInStep(data: OnboardingStepStatus): Observable<OnboardingStepStatus> {
+    return this.spenderPlatformV1ApiService
+      .post<PlatformApiResponse<OnboardingStepStatus>>('/spender/onboarding/process_step_sms_opt_in', { data })
+      .pipe(map((res) => res.data));
+  }
+
+  processWelcomeModalStep(data: OnboardingWelcomeStepStatus): Observable<OnboardingWelcomeStepStatus> {
+    return this.spenderPlatformV1ApiService
+      .post<PlatformApiResponse<OnboardingWelcomeStepStatus>>('/spender/onboarding/process_step_show_welcome_modal', {
+        data,
+      })
+      .pipe(map((res) => res.data));
+  }
+
+  markWelcomeModalStepAsComplete(): Observable<OnboardingWelcomeStepStatus> {
+    const data: OnboardingWelcomeStepStatus = {
+      is_complete: true,
+    };
+    return this.processWelcomeModalStep(data);
+  }
+
+  markConnectCardsStepAsComplete(): Observable<OnboardingStepStatus> {
+    const data: OnboardingStepStatus = {
+      is_configured: true,
+      is_skipped: false,
+    };
+    return this.processConnectCardsStep(data);
+  }
+
+  skipConnectCardsStep(): Observable<OnboardingStepStatus> {
+    const data: OnboardingStepStatus = {
+      is_configured: false,
+      is_skipped: true,
+    };
+    return this.processConnectCardsStep(data);
+  }
+
+  markSmsOptInStepAsComplete(): Observable<OnboardingStepStatus> {
+    const data: OnboardingStepStatus = {
+      is_configured: true,
+      is_skipped: false,
+    };
+    return this.processSmsOptInStep(data);
+  }
+
+  skipSmsOptInStep(): Observable<OnboardingStepStatus> {
+    const data: OnboardingStepStatus = {
+      is_configured: false,
+      is_skipped: true,
+    };
+    return this.processSmsOptInStep(data);
+  }
+}
