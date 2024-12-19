ea69820639c5e80c1bf048da656c3da967187889
aastha <aastha.b@fyle.in>
2024-12-20 02:03:24 +0530

    Minor

--- src/app/auth/sign-in/sign-in.page.ts
+++ src/app/auth/sign-in/sign-in.page.ts
@@ -257,7 +257,6 @@ export class SignInPage implements OnInit {
           this.fg.reset();
           this.router.navigate(['/', 'auth', 'switch_org', { choose: true }]);
         },
-        error: (err: HttpErrorResponse) => this.handleError(err),
       });
   }
 
