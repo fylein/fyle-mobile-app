ef785acf992f965d5545af6798496a163032f994
Aastha Bist <aastha.b@fyle.in>
2025-01-20 07:48:45 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,21 +11,21 @@ const routes: Routes = [
   },
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    loadChildren: (): void => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: 'post_verification',
-    loadChildren: () => import('./post-verification/post-verification.module').then((m) => m.PostVerificationModule),
+    loadChildren: (): void => import('./post-verification/post-verification.module').then((m) => m.PostVerificationModule),
     canActivate: [AuthGuard],
   },
   {
     path: 'enterprise',
-    loadChildren: () => import('./fyle/fyle.module').then((m) => m.FyleModule),
+    loadChildren: (): void => import('./fyle/fyle.module').then((m) => m.FyleModule),
     canActivate: [AuthGuard, VerifiedOrgAuthGuard],
   },
   {
     path: 'deep_link_redirection',
-    loadChildren: () =>
+    loadChildren: (): void =>
       import('./deep-link-redirection/deep-link-redirection.module').then((m) => m.DeepLinkRedirectionPageModule),
   },
 ];
