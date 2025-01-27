86350be2d2a8ab7a39f6910c6dbe02e4dddd080e
Aastha Bist <aastha.b@fyle.in>
2025-01-27 21:48:27 +0530

    feat: Spender onboarding changes [full feature branch - already approved changes] (#3444)

---
+++ src/app/fyle/spender-onboarding/spender-onboarding.page.ts
@@ -0,0 +1,163 @@
+import { Component } from '@angular/core';
+import { finalize, forkJoin, from, map, switchMap, tap } from 'rxjs';
+import { ExtendedOrgUser } from 'src/app/core/models/extended-org-user.model';
+import { LoaderService } from 'src/app/core/services/loader.service';
+import { OrgUserService } from 'src/app/core/services/org-user.service';
+import { OnboardingStep } from './models/onboarding-step.enum';
+import { SpenderOnboardingService } from 'src/app/core/services/spender-onboarding.service';
+import { OrgSettingsService } from 'src/app/core/services/org-settings.service';
+import { Router } from '@angular/router';
+import { CorporateCreditCardExpenseService } from 'src/app/core/services/corporate-credit-card-expense.service';
+import { OrgSettings } from 'src/app/core/models/org-settings.model';
+import { TrackingService } from 'src/app/core/services/tracking.service';
+
+@Component({
+  selector: 'app-spender-onboarding',
+  templateUrl: './spender-onboarding.page.html',
+  styleUrls: ['./spender-onboarding.page.scss'],
+})
+export class SpenderOnboardingPage {
+  isLoading = true;
+
+  userFullName: string;
+
+  currentStep: OnboardingStep = OnboardingStep.CONNECT_CARD;
+
+  onlyOptInEnabled = false;
+
+  onboardingStep: typeof OnboardingStep = OnboardingStep;
+
+  eou: ExtendedOrgUser;
+
+  orgSettings: OrgSettings;
+
+  onboardingComplete = false;
+
+  onboardingInProgress = true;
+
+  redirectionCount = 3;
+
+  constructor(
+    private loaderService: LoaderService,
+    private orgUserService: OrgUserService,
+    private spenderOnboardingService: SpenderOnboardingService,
+    private orgSettingsService: OrgSettingsService,
+    private corporateCreditCardExpenseService: CorporateCreditCardExpenseService,
+    private router: Router,
+    private trackingService: TrackingService
+  ) {}
+
+  ionViewWillEnter(): void {
+    this.isLoading = true;
+    from(this.loaderService.showLoader())
+      .pipe(
+        switchMap(() =>
+          forkJoin([
+            this.orgUserService.getCurrent(),
+            this.orgSettingsService.get(),
+            this.spenderOnboardingService.getOnboardingStatus(),
+            this.corporateCreditCardExpenseService.getCorporateCards(),
+          ])
+        ),
+        map(([eou, orgSettings, onboardingStatus, corporateCards]) => {
+          this.eou = eou;
+          this.userFullName = eou.us.full_name;
+          this.orgSettings = orgSettings;
+          const isRtfEnabled =
+            orgSettings.visa_enrollment_settings.enabled || orgSettings.mastercard_enrollment_settings.enabled;
+          const isAmexFeedEnabled = orgSettings.amex_feed_enrollment_settings.enabled;
+          const rtfCards = corporateCards.filter((card) => card.is_visa_enrolled || card.is_mastercard_enrolled);
+          if (isAmexFeedEnabled && !isRtfEnabled) {
+            this.currentStep = OnboardingStep.OPT_IN;
+            this.onlyOptInEnabled = true;
+          } else if (isRtfEnabled) {
+            // If Connect Card was skipped earlier or Cards are already enrolled, then go to OPT_IN step
+            if (
+              onboardingStatus.step_connect_cards_is_skipped ||
+              onboardingStatus.step_connect_cards_is_configured ||
+              rtfCards.length > 0
+            ) {
+              this.currentStep = OnboardingStep.OPT_IN;
+              if (onboardingStatus.step_connect_cards_is_configured) {
+                this.onlyOptInEnabled = true;
+              }
+            } else {
+              this.currentStep = OnboardingStep.CONNECT_CARD;
+            }
+          }
+        }),
+        finalize(() => {
+          this.isLoading = false;
+          return from(this.loaderService.hideLoader());
+        })
+      )
+      .subscribe();
+  }
+
+  goBackToConnectCard(): void {
+    this.currentStep = OnboardingStep.CONNECT_CARD;
+  }
+
+  skipOnboardingStep(): void {
+    if (this.currentStep === OnboardingStep.CONNECT_CARD) {
+      this.spenderOnboardingService
+        .skipConnectCardsStep()
+        .pipe(
+          map(() => {
+            this.trackingService.eventTrack('Connect Cards Onboarding Step - Skipped');
+            this.currentStep = OnboardingStep.OPT_IN;
+          })
+        )
+        .subscribe();
+    } else if (this.currentStep === OnboardingStep.OPT_IN) {
+      this.onboardingInProgress = false;
+      this.spenderOnboardingService
+        .skipSmsOptInStep()
+        .pipe(
+          switchMap(() => this.spenderOnboardingService.markWelcomeModalStepAsComplete()),
+          map(() => {
+            this.trackingService.eventTrack('Sms Opt In Onboarding Step - Skipped');
+            this.spenderOnboardingService.setOnboardingStatusEvent();
+            this.router.navigate(['/', 'enterprise', 'my_dashboard']);
+          })
+        )
+        .subscribe();
+    }
+  }
+
+  markStepAsComplete(): void {
+    if (this.currentStep === OnboardingStep.CONNECT_CARD) {
+      this.spenderOnboardingService
+        .markConnectCardsStepAsComplete()
+        .pipe(
+          tap(() => {
+            this.currentStep = OnboardingStep.OPT_IN;
+          })
+        )
+        .subscribe();
+    } else if (this.currentStep === OnboardingStep.OPT_IN) {
+      this.onboardingInProgress = false;
+      this.spenderOnboardingService
+        .markSmsOptInStepAsComplete()
+        .pipe(
+          switchMap(() => this.spenderOnboardingService.markWelcomeModalStepAsComplete()),
+          map(() => {
+            this.spenderOnboardingService.setOnboardingStatusEvent();
+            this.onboardingComplete = true;
+            this.startCountdown();
+          })
+        )
+        .subscribe();
+    }
+  }
+
+  startCountdown(): void {
+    const interval = setInterval(() => {
+      this.redirectionCount--;
+      if (this.redirectionCount === 0) {
+        clearInterval(interval);
+        this.router.navigate(['/', 'enterprise', 'my_dashboard']);
+      }
+    }, 1000);
+  }
+}
--- src/app/post-verification/invited-user/invited-user.page.ts
+++ src/app/post-verification/invited-user/invited-user.page.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, OnInit } from '@angular/core';
 import { Observable, noop, concat, from } from 'rxjs';
 import { NetworkService } from 'src/app/core/services/network.service';
 import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
-import { switchMap, finalize, tap } from 'rxjs/operators';
+import { switchMap, finalize, tap, map } from 'rxjs/operators';
 import { OrgUserService } from 'src/app/core/services/org-user.service';
 import { LoaderService } from 'src/app/core/services/loader.service';
 import { AuthService } from 'src/app/core/services/auth.service';
@@ -12,6 +12,8 @@ import { TrackingService } from '../../core/services/tracking.service';
 import { ToastMessageComponent } from 'src/app/shared/components/toast-message/toast-message.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SnackbarPropertiesService } from 'src/app/core/services/snackbar-properties.service';
+import { OrgSettingsService } from 'src/app/core/services/org-settings.service';
+import { SpenderOnboardingService } from 'src/app/core/services/spender-onboarding.service';
 
 @Component({
   selector: 'app-invited-user',
@@ -58,7 +60,9 @@ export class InvitedUserPage implements OnInit {
     private router: Router,
     private trackingService: TrackingService,
     private matSnackBar: MatSnackBar,
-    private snackbarProperties: SnackbarPropertiesService
+    private snackbarProperties: SnackbarPropertiesService,
+    private orgSettingsService: OrgSettingsService,
+    private spenderOnboardingService: SpenderOnboardingService
   ) {}
 
   ngOnInit(): void {
@@ -90,6 +94,21 @@ export class InvitedUserPage implements OnInit {
     this.showPasswordTooltip = value;
   }
 
+  navigateToDashboard(): void {
+    this.spenderOnboardingService
+      .checkForRedirectionToOnboarding()
+      .pipe(
+        map((shouldProceedToOnboarding) => {
+          if (shouldProceedToOnboarding) {
+            this.router.navigate(['/', 'enterprise', 'spender_onboarding']);
+          } else {
+            this.router.navigate(['/', 'enterprise', 'my_dashboard']);
+          }
+        })
+      )
+      .subscribe();
+  }
+
   async saveData(): Promise<void> {
     this.isLoading = true;
     this.fg.markAllAsTouched();
@@ -113,8 +132,7 @@ export class InvitedUserPage implements OnInit {
           })
         )
         .subscribe(() => {
-          this.router.navigate(['/', 'enterprise', 'my_dashboard']);
-          // return $state.go('enterprise.my_dashboard');
+          this.navigateToDashboard();
         });
     } else {
       const message = `Please enter a valid ${!this.fg.controls.fullName.valid ? 'name' : 'password'}`;
