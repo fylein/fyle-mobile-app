246cf3f8e6ed58414248fc3742d5ec098b88867c
aastha <aastha.b@fyle.in>
2025-01-28 22:44:32 +0530

    Minor

--- src/app/shared/components/sidemenu/sidemenu.component.ts
+++ src/app/shared/components/sidemenu/sidemenu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import * as Sentry from '@sentry/angular';
-import { Observable, from, forkJoin, concat, combineLatest, EMPTY } from 'rxjs';
-import { catchError, shareReplay } from 'rxjs/operators';
+import { Observable, from, forkJoin, concat, combineLatest } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 import { DeviceService } from 'src/app/core/services/device.service';
 import { RouterAuthService } from 'src/app/core/services/router-auth.service';
 import { OrgUserService } from 'src/app/core/services/org-user.service';
