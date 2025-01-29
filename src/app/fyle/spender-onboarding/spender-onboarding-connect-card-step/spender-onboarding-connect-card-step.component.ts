8ea74fb6f4e28657624f95004abc66d785f3e2ac
aastha <aastha.b@fyle.in>
2025-01-29 05:56:10 +0530

    Minor

--- src/app/fyle/spender-onboarding/spender-onboarding-connect-card-step/spender-onboarding-connect-card-step.component.ts
+++ src/app/fyle/spender-onboarding/spender-onboarding-connect-card-step/spender-onboarding-connect-card-step.component.ts
@@ -132,7 +132,7 @@ export class SpenderOnboardingConnectCardStepComponent implements OnInit, OnChan
     } else if (this.cardsList.failedCards.length > 1) {
       const allButLast = this.cardsList.failedCards.slice(0, -1).join(', ');
       const lastCard = this.cardsList.failedCards[this.cardsList.failedCards.length - 1];
-      return `We ran into an issue while processing your request for the cards <span class='text-bold'>${allButLast}</span> and  <span class='text-bold'>${lastCard}</span>.<br><br>You can cancel and retry connecting the failed card or proceed to the next step.`;
+      return `We ran into an issue while processing your request for the cards <span class='text-bold'>${allButLast}</span> and <span class='text-bold'>${lastCard}</span>.<br><br>You can cancel and retry connecting the failed card or proceed to the next step.`;
     } else {
       return `We ran into an issue while processing your request for the card <span class='text-bold'>${this.cardsList.failedCards[0]}</span>.<br><br> You can cancel and retry connecting the failed card or proceed to the next step.`;
     }
