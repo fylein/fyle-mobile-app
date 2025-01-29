1f4003a35197a157e665092b684219af01d537e6
aastha <aastha.b@fyle.in>
2025-01-29 05:44:03 +0530

    Minor

--- src/app/fyle/spender-onboarding/spender-onboarding-connect-card-step/spender-onboarding-connect-card-step.component.spec.ts
+++ src/app/fyle/spender-onboarding/spender-onboarding-connect-card-step/spender-onboarding-connect-card-step.component.spec.ts
@@ -257,7 +257,7 @@ describe('SpenderOnboardingConnectCardStepComponent', () => {
       fixture.detectChanges();
       const message = component.generateMessage();
       expect(message).toBe(
-        `We ran into an issue while processing your request for the cards  **** 1234, **** 5678 and **** 9012.<br><br>You can cancel and retry connecting the failed card or proceed to the next step.`
+        `We ran into an issue while processing your request for the cards <span class='text-bold'>**** 1234, **** 5678</span> and <span class='text-bold'>**** 9012</span>.<br><br>You can cancel and retry connecting the failed card or proceed to the next step.`
       );
     });
 
@@ -269,7 +269,7 @@ describe('SpenderOnboardingConnectCardStepComponent', () => {
       const message = component.generateMessage();
 
       expect(message).toBe(
-        `We ran into an issue while processing your request for the card **** 1234.<br><br> You can cancel and retry connecting the failed card or proceed to the next step.`
+        `We ran into an issue while processing your request for the card <span class='text-bold'>**** 1234</span>.<br><br> You can cancel and retry connecting the failed card or proceed to the next step.`
       );
     });
   });
