f60c39f5f1f672d4193cd9399f6e932cda6995b7
Arjun <arjunmuraliknr@gmail.com>
2025-02-04 13:21:56 +0530

    Revert "fix: Decimal value removal for $0 (#3461)"
    This reverts commit 4629d2598205ef2ff27d910fae850ec3606e4450.

--- src/app/shared/pipes/exact-currency.pipe.ts
+++ src/app/shared/pipes/exact-currency.pipe.ts
@@ -13,8 +13,7 @@ export class ExactCurrencyPipe implements PipeTransform {
     const sign = value < 0 ? '-' : '';
     const amount = Math.abs(value) || 0;
     const symbolType = skipSymbol ? '' : 'symbol';
-    const digitsInfo = amount === 0 ? '1.0-0' : fraction && `1.${fraction}-${fraction}`;
-
+    const digitsInfo = fraction && `1.${fraction}-${fraction}`;
     // Format the exact amount
     const formattedValue = this.fyCurrencyPipe.transform(amount, currencyCode, symbolType, digitsInfo);
     return sign + formattedValue;
