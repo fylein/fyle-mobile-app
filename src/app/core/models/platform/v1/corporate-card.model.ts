d38bbc589412756e6856fee4a086dc2c02f2fab9
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2025-02-16 14:02:01 +0530

    remove cardholder_name

--- src/app/core/models/platform/v1/corporate-card.model.ts
+++ src/app/core/models/platform/v1/corporate-card.model.ts
@@ -2,7 +2,6 @@ export interface CorporateCard {
   id: string;
   bank_name: string;
   card_number: string;
-  cardholder_name: string;
   masked_number: string;
   user_full_name: string;
   user_email: string;
