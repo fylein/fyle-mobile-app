a984e3a11ddaf10779c69be0ba39e88c5c849050
Vaibhav Singh <vaibhav.singh@fyle.in>
2025-02-18 12:50:44 +0530

    feat: Migrate /v2/corporate_card_transactions to platform (#3509)
    * feat: Migrate /v2/corporate_card_transactions to platform
    
    * fix: unit tests for migration of /v2/corporate_card_transactions (#3510)

--- src/app/fyle/merge-expense/generic-fields-form/generic-fields-form.component.ts
+++ src/app/fyle/merge-expense/generic-fields-form/generic-fields-form.component.ts
@@ -1,23 +1,13 @@
 import { Component, EventEmitter, Injector, Input, OnDestroy, OnInit, Output, TemplateRef } from '@angular/core';
 import { Subscription, noop } from 'rxjs';
 import { filter } from 'rxjs/operators';
-import { CorporateCardExpense } from 'src/app/core/models/v2/corporate-card-expense.model';
+import { corporateCardTransaction } from 'src/app/core/models/platform/v1/cc-transaction.model';
 import { FormGroup, ControlValueAccessor, NG_VALUE_ACCESSOR, FormBuilder, Validators } from '@angular/forms';
 import { FileObject } from 'src/app/core/models/file-obj.model';
 import { CustomProperty } from 'src/app/core/models/custom-properties.model';
 import { AllowedPaymentModes } from 'src/app/core/models/allowed-payment-modes.enum';
-
-type Option = Partial<{
-  label: string;
-  value: string;
-}>;
-
-type OptionsData = Partial<{
-  options: Option[];
-  areSameValues: boolean;
-  name: string;
-  value: string;
-}>;
+import { MergeExpensesOption } from 'src/app/core/models/merge-expenses-option.model';
+import { MergeExpensesOptionsData } from 'src/app/core/models/merge-expenses-options-data.model';
 
 @Component({
   selector: 'app-generic-fields-form',
@@ -26,35 +16,35 @@ type OptionsData = Partial<{
   providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: GenericFieldsFormComponent, multi: true }],
 })
 export class GenericFieldsFormComponent implements OnInit, ControlValueAccessor, OnDestroy {
-  @Input() amountOptionsData: OptionsData;
+  @Input() amountOptionsData: MergeExpensesOptionsData<string>;
 
-  @Input() receiptOptions: Option[];
+  @Input() receiptOptions: MergeExpensesOption<string>[];
 
-  @Input() dateOfSpendOptionsData: OptionsData;
+  @Input() dateOfSpendOptionsData: MergeExpensesOptionsData<string>;
 
-  @Input() paymentModeOptionsData: OptionsData;
+  @Input() paymentModeOptionsData: MergeExpensesOptionsData<string>;
 
   @Input() attachments: FileObject[];
 
-  @Input() projectOptionsData: OptionsData;
+  @Input() projectOptionsData: MergeExpensesOptionsData<string>;
 
-  @Input() billableOptionsData: OptionsData;
+  @Input() billableOptionsData: MergeExpensesOptionsData<string>;
 
-  @Input() categoryOptionsData: OptionsData;
+  @Input() categoryOptionsData: MergeExpensesOptionsData<string>;
 
-  @Input() vendorOptionsData: OptionsData;
+  @Input() vendorOptionsData: MergeExpensesOptionsData<string>;
 
-  @Input() taxGroupOptionsData: OptionsData;
+  @Input() taxGroupOptionsData: MergeExpensesOptionsData<string>;
 
-  @Input() taxAmountOptionsData: OptionsData;
+  @Input() taxAmountOptionsData: MergeExpensesOptionsData<string>;
 
-  @Input() constCenterOptionsData: OptionsData;
+  @Input() constCenterOptionsData: MergeExpensesOptionsData<string>;
 
-  @Input() purposeOptionsData: OptionsData;
+  @Input() purposeOptionsData: MergeExpensesOptionsData<string>;
 
   @Input() categoryDependentTemplate: TemplateRef<string[]>;
 
-  @Input() CCCTxns: CorporateCardExpense[];
+  @Input() CCCTxns: corporateCardTransaction[];
 
   @Input() disableFormElements: boolean;
 
