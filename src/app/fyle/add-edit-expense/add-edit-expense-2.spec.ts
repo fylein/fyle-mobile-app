907038ae7d88d16802853112a95ea1369395e6f2
Arjun <arjunmuraliknr@gmail.com>
2025-02-19 14:04:11 +0530

    test

--- src/app/fyle/add-edit-expense/add-edit-expense-2.spec.ts
+++ src/app/fyle/add-edit-expense/add-edit-expense-2.spec.ts
@@ -573,6 +573,25 @@ export function TestCases2(getTestBed) {
           done();
         });
       });
+
+      it('should redirect back if the expense is not returned from the API', (done) => {
+        expensesService.getExpenseById.and.returnValue(throwError(() => new Error('expense not found')));
+        spyOn(component, 'goBack');
+
+        component.getEditExpenseObservable().subscribe({
+          next: () => fail('Expected an error, but got a successful response'),
+          error: () => {}, // No action needed, as we expect an error
+          complete: () => {
+            expect(loaderService.hideLoader).toHaveBeenCalledTimes(1);
+            expect(loaderService.showLoader).toHaveBeenCalledOnceWith(
+              'This expense no longer exists. Redirecting to expenses list',
+              1000
+            );
+            expect(component.goBack).toHaveBeenCalledTimes(1);
+            done();
+          },
+        });
+      });
     });
 
     it('goToPrev(): should go to the previous txn', () => {
