7b9ee3e3bc5ae1c145ee78065e48e415cdc0eb68
Rishabh Sinha <rishabhsinha@Rishabhs-MacBook-Air.local>
2025-02-24 11:22:25 +0530

    Revert "fix unit testt"
    This reverts commit 88f868350c8af6fdfb6eee92f79837a2d6195ae2.

--- src/app/fyle/help/help.page.spec.ts
+++ src/app/fyle/help/help.page.spec.ts
@@ -24,29 +24,54 @@ describe('HelpPage', () => {
   let browserHandlerService: jasmine.SpyObj<BrowserHandlerService>;
 
   beforeEach(waitForAsync(() => {
-    modalController = jasmine.createSpyObj('ModalController', ['create']);
-    orgUserService = jasmine.createSpyObj('OrgUserService', ['getEmployeesByParams']);
-    loaderService = jasmine.createSpyObj('LoaderService', ['showLoader', 'hideLoader']);
-    trackingService = jasmine.createSpyObj('TrackingService', ['viewHelpCard']);
-    authService = jasmine.createSpyObj('AuthService', ['getEou']);
-    browserHandlerService = jasmine.createSpyObj('BrowserHandlerService', ['openLinkWithToolbarColor']);
+    const modalControllerSpy = jasmine.createSpyObj('ModalController', ['create']);
+    const orgUserServiceSpy = jasmine.createSpyObj('OrgUserService', ['getEmployeesByParams']);
+    const loaderServiceSpy = jasmine.createSpyObj('LoaderService', ['showLoader', 'hideLoader']);
+    const trackingServiceSpy = jasmine.createSpyObj('TrackingService', ['viewHelpCard']);
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['getEou']);
+    const browserHandlerServiceSpy = jasmine.createSpyObj('BrowserHandlerService', ['openLinkWithToolbarColor']);
 
     TestBed.configureTestingModule({
       declarations: [HelpPage],
       imports: [IonicModule.forRoot()],
       providers: [
-        { provide: ModalController, useValue: modalController },
-        { provide: OrgUserService, useValue: orgUserService },
-        { provide: LoaderService, useValue: loaderService },
-        { provide: TrackingService, useValue: trackingService },
-        { provide: AuthService, useValue: authService },
-        { provide: BrowserHandlerService, useValue: browserHandlerService },
+        {
+          provide: ModalController,
+          useValue: modalControllerSpy,
+        },
+        {
+          provide: OrgUserService,
+          useValue: orgUserServiceSpy,
+        },
+        {
+          provide: LoaderService,
+          useValue: loaderServiceSpy,
+        },
+        {
+          provide: TrackingService,
+          useValue: trackingServiceSpy,
+        },
+        {
+          provide: AuthService,
+          useValue: authServiceSpy,
+        },
+        {
+          provide: BrowserHandlerService,
+          useValue: browserHandlerServiceSpy,
+        },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
-
     fixture = TestBed.createComponent(HelpPage);
     component = fixture.componentInstance;
+    modalController = TestBed.inject(ModalController) as jasmine.SpyObj<ModalController>;
+    orgUserService = TestBed.inject(OrgUserService) as jasmine.SpyObj<OrgUserService>;
+    loaderService = TestBed.inject(LoaderService) as jasmine.SpyObj<LoaderService>;
+    trackingService = TestBed.inject(TrackingService) as jasmine.SpyObj<TrackingService>;
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    browserHandlerService = TestBed.inject(BrowserHandlerService) as jasmine.SpyObj<BrowserHandlerService>;
+
+    component.orgAdmins = employeesRes;
     fixture.detectChanges();
   }));
 
@@ -75,10 +100,10 @@ describe('HelpPage', () => {
     component.openContactSupportDialog();
     tick(500);
     expect(component.contactSupportLoading).toBeTrue();
-    expect(loaderService.showLoader).toHaveBeenCalledWith('Please wait', 10000);
-    expect(authService.getEou).toHaveBeenCalled();
-    expect(orgUserService.getEmployeesByParams).toHaveBeenCalledWith(params);
-    expect(loaderService.hideLoader).toHaveBeenCalled();
+    expect(loaderService.showLoader).toHaveBeenCalledOnceWith('Please wait', 10000);
+    expect(authService.getEou).toHaveBeenCalledTimes(1);
+    expect(orgUserService.getEmployeesByParams).toHaveBeenCalledOnceWith(params);
+    expect(loaderService.hideLoader).toHaveBeenCalledTimes(1);
     expect(component.orgAdmins).toEqual(employeesRes.data);
     expect(component.presentSupportModal).toHaveBeenCalledWith(dialogType);
   }));
@@ -86,43 +111,43 @@ describe('HelpPage', () => {
   describe('presentSupportModal():', () => {
     it('should open present support modal when the dialog type is contact_support and when data is present', fakeAsync(() => {
       const dialogType = 'contact_support';
-      component.orgAdmins = employeesRes.data;
+      component.orgAdmins = employeesRes;
       const modalControllerSpy = jasmine.createSpyObj('Modal', ['present', 'onDidDismiss']);
       modalController.create.and.returnValue(modalControllerSpy);
       modalControllerSpy.onDidDismiss.and.resolveTo({ data: { dismissed: true } } as any);
       component.presentSupportModal(dialogType);
       tick(500);
-      expect(trackingService.viewHelpCard).toHaveBeenCalled();
-      expect(modalController.create).toHaveBeenCalledWith({
+      expect(trackingService.viewHelpCard).toHaveBeenCalledTimes(1);
+      expect(modalController.create).toHaveBeenCalledOnceWith({
         component: SupportDialogPage,
         componentProps: {
           type: dialogType,
           adminEous: employeesRes,
         },
       });
-      expect(modalControllerSpy.present).toHaveBeenCalled();
-      expect(modalControllerSpy.onDidDismiss).toHaveBeenCalled();
+      expect(modalControllerSpy.present).toHaveBeenCalledTimes(1);
+      expect(modalControllerSpy.onDidDismiss).toHaveBeenCalledTimes(1);
       expect(component.contactSupportLoading).toBeFalse();
     }));
 
     it('should open present support modal when the dialog type is not contact_support and when no data is present', fakeAsync(() => {
       const dialogType = 'capture_email';
-      component.orgAdmins = employeesRes.data;
+      component.orgAdmins = employeesRes;
       const modalControllerSpy = jasmine.createSpyObj('Modal', ['present', 'onDidDismiss']);
       modalController.create.and.returnValue(modalControllerSpy);
       modalControllerSpy.onDidDismiss.and.resolveTo({ data: null });
       component.presentSupportModal(dialogType);
       tick(500);
-      expect(trackingService.viewHelpCard).toHaveBeenCalled();
-      expect(modalController.create).toHaveBeenCalledWith({
+      expect(trackingService.viewHelpCard).toHaveBeenCalledTimes(1);
+      expect(modalController.create).toHaveBeenCalledOnceWith({
         component: SupportDialogPage,
         componentProps: {
           type: dialogType,
           adminEous: employeesRes,
         },
       });
-      expect(modalControllerSpy.present).toHaveBeenCalled();
-      expect(modalControllerSpy.onDidDismiss).toHaveBeenCalled();
+      expect(modalControllerSpy.present).toHaveBeenCalledTimes(1);
+      expect(modalControllerSpy.onDidDismiss).toHaveBeenCalledTimes(1);
       expect(component.contactSupportLoading).toBeFalse();
     }));
   });
@@ -132,6 +157,6 @@ describe('HelpPage', () => {
     const toolbarColor = '#280a31';
     component.openHelpLink();
     tick(500);
-    expect(browserHandlerService.openLinkWithToolbarColor).toHaveBeenCalledWith(toolbarColor, url);
+    expect(browserHandlerService.openLinkWithToolbarColor).toHaveBeenCalledOnceWith(toolbarColor, url);
   }));
 });
