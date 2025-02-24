ac81087d947c8b6bf33c8fca5f22f79037366baa
Rishabh Sinha <rishabhsinha@Rishabhs-MacBook-Air.local>
2025-02-24 11:24:12 +0530

    Revert "fix css and eslint"
    This reverts commit b1176aef21df18755825fd246c1894832b257f71.

--- src/app/core/models/org-admin.model.ts
+++
@@ -1,4 +0,0 @@
-export interface OrgAdmin {
-  us_full_name?: string;
-  us_email?: string;
-}
--- src/app/fyle/help/help.page.ts
+++ src/app/fyle/help/help.page.ts
@@ -8,7 +8,6 @@ import { from } from 'rxjs';
 import { TrackingService } from '../../core/services/tracking.service';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { BrowserHandlerService } from 'src/app/core/services/browser-handler.service';
-import { OrgAdmin } from 'src/app/core/models/org-admin.model';
 
 @Component({
   selector: 'app-help',
@@ -16,7 +15,7 @@ import { OrgAdmin } from 'src/app/core/models/org-admin.model';
   styleUrls: ['./help.page.scss'],
 })
 export class HelpPage implements OnInit {
-  orgAdmins: OrgAdmin[] = [];
+  orgAdmins: unknown;
 
   contactSupportLoading = false;
 
