366d13418adee8fc60dfdc77c0c4fb69ecf22175
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2025-02-25 23:52:03 +0530

    pr review fixes

--- src/app/core/services/extend-query-params.service.ts
+++ src/app/core/services/extend-query-params.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ExtendQueryParamsService {
+  /**
+   * Extends query parameters with a formatted search query based on user input
+   *
+   * @param queryParams - The original query parameters object to extend (defaults to empty object)
+   * @param simpleSearchText - The search text provided by the user
+   * @returns A new object containing the original query parameters plus the formatted search query
+   */
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   extendQueryParamsForTextSearch(queryParams = {}, simpleSearchText: string) {
     if (simpleSearchText === undefined || simpleSearchText.length < 1) {
@@ -15,9 +22,8 @@ export class ExtendQueryParamsService {
     const arrayWithoutLastElement = textArray.slice(0, -1);
 
     const searchQuery = arrayWithoutLastElement
-      .reduce((curr, agg) => agg + ' & ' + curr, '')
-      .concat(lastElement)
-      .concat(':*');
+      .reduce((curr, agg) => `${agg} & ${curr}`, '')
+      .concat(`${lastElement}:*`);
 
     return Object.assign({}, queryParams, { q: searchQuery });
   }
