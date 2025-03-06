bdebb67321ea21c0cacc785b02c75f7226345ff6
Amit Pile <amitpile@Amits-MacBook-Air.local>
2025-03-06 10:13:05 +0530

    new service test cases

--- src/app/core/services/footer.service.spec.ts
+++ src/app/core/services/footer.service.spec.ts
@@ -13,4 +13,19 @@ describe('FooterService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have initial footerCurrentStateIndex$ value as 0', (done) => {
+    service.footerCurrentStateIndex$.subscribe((index) => {
+      expect(index).toBe(0);
+      done();
+    });
+  });
+
+  it('should update footerCurrentStateIndex$ value', (done) => {
+    service.updateCurrentStateIndex(1);
+    service.footerCurrentStateIndex$.subscribe((index) => {
+      expect(index).toBe(1);
+      done();
+    });
+  });
 });
