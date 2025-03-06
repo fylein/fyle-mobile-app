d58d5d42f02533ddf45250fb37ad4297cda78441
Amit Pile <amitpile@Amits-MacBook-Air.local>
2025-03-06 10:28:25 +0530

    only footer changes

--- src/app/fyle/dashboard/dashboard.page.ts
+++ src/app/fyle/dashboard/dashboard.page.ts
@@ -59,8 +59,6 @@ export class DashboardPage {
 
   currentStateIndex = 0;
 
-  currentActiveState = FooterState.HOME;
-
   actionSheetButtons: ActionSheetButton[] = [];
 
   taskCount = 0;
@@ -189,10 +187,8 @@ export class DashboardPage {
     const currentState =
       this.activatedRoute.snapshot.queryParams.state === 'tasks' ? DashboardState.tasks : DashboardState.home;
     if (currentState === DashboardState.tasks) {
-      this.currentActiveState = FooterState.TASKS;
       this.currentStateIndex = 1;
     } else {
-      this.currentActiveState = FooterState.HOME;
       this.currentStateIndex = 0;
     }
 
@@ -283,7 +279,6 @@ export class DashboardPage {
 
   onTaskClicked(): void {
     this.currentStateIndex = 1;
-    this.currentActiveState = FooterState.TASKS;
     const queryParams: Params = { state: 'tasks' };
     this.router.navigate([], {
       relativeTo: this.activatedRoute,
@@ -310,33 +305,8 @@ export class DashboardPage {
     ]);
   }
 
-  onExpensesClicked(): void {
-    this.currentActiveState = FooterState.EXPENSES;
-    this.router.navigate([
-      '/',
-      'enterprise',
-      'my_expenses',
-      {
-        navigate_back: true,
-      },
-    ]);
-  }
-
-  onReportsClicked(): void {
-    this.currentActiveState = FooterState.REPORTS;
-    this.router.navigate([
-      '/',
-      'enterprise',
-      'my_reports',
-      {
-        navigate_back: true,
-      },
-    ]);
-  }
-
   onHomeClicked(): void {
     this.currentStateIndex = 0;
-    this.currentActiveState = FooterState.HOME;
     const queryParams: Params = { state: 'home' };
     this.router.navigate([], {
       relativeTo: this.activatedRoute,
