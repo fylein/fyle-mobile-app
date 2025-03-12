718e8e81dd2d185fef3aa078d4f7d5bba1b64735
Arjun M <arjunm@Arjuns-Air.lan>
2025-03-12 03:16:08 -0700

    Revert "feat: Capacitor v5 upgrade, ionic v7 upgrade (#3567)"
    This reverts commit 0267a26f22dd5a212de98969a3e6f6e1ecaa5f4c.

--- src/app/core/mock-data/extended-device-info.data.ts
+++ src/app/core/mock-data/extended-device-info.data.ts
@@ -3,7 +3,7 @@ import deepFreeze from 'deep-freeze-strict';
 import { ExtendedDeviceInfo } from '../models/extended-device-info.model';
 
 export const extendedDeviceInfoMockData: ExtendedDeviceInfo = deepFreeze({
-  identifier: 'mockidentifier123',
+  uuid: 'mockuuid123',
   appVersion: '5.50.0',
   liveUpdateAppVersion: '5.50.0',
   model: 'iPhone 11 Pro Max',
@@ -16,7 +16,7 @@ export const extendedDeviceInfoMockData: ExtendedDeviceInfo = deepFreeze({
 });
 
 export const extendedDeviceInfoMockData2: ExtendedDeviceInfo = deepFreeze({
-  identifier: 'mockidentifier124',
+  uuid: 'mockuuid124',
   appVersion: '5.52.0',
   liveUpdateAppVersion: '5.52.0',
   model: 'Samsung Galaxy A13',
@@ -29,7 +29,7 @@ export const extendedDeviceInfoMockData2: ExtendedDeviceInfo = deepFreeze({
 });
 
 export const extendedDeviceInfoMockDataWoApp: ExtendedDeviceInfo = deepFreeze({
-  identifier: 'mockidentifier124',
+  uuid: 'mockuuid124',
   appVersion: null,
   liveUpdateAppVersion: '5.52.0',
   model: 'Samsung Galaxy A13',
