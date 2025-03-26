1175aaaca25bd87ac8e6227bd9e544dd1c06c26e
aastha <aastha.b@fyle.in>
2025-03-26 09:26:31 +0530

    Minor

--- src/app/fyle/my-view-report/my-view-report.page.ts
+++ src/app/fyle/my-view-report/my-view-report.page.ts
@@ -18,7 +18,7 @@ import { FyViewReportInfoComponent } from 'src/app/shared/components/fy-view-rep
 import * as dayjs from 'dayjs';
 import { StatusService } from 'src/app/core/services/status.service';
 import { ExtendedStatus } from 'src/app/core/models/extended_status.model';
-import { cloneDeep, isNumber } from 'lodash';
+import { cloneDeep } from 'lodash';
 import { Expense } from 'src/app/core/models/platform/v1/expense.model';
 import { ExpenseView } from 'src/app/core/models/expense-view.enum';
 import { OrgSettingsService } from 'src/app/core/services/org-settings.service';
@@ -41,7 +41,6 @@ import { LaunchDarklyService } from 'src/app/core/services/launch-darkly.service
 import { ShowAllApproversPopoverComponent } from 'src/app/shared/components/fy-approver/show-all-approvers-popover/show-all-approvers-popover.component';
 import { ReportApprovals } from 'src/app/core/models/platform/report-approvals.model';
 import * as Sentry from '@sentry/angular-ivy';
-import { ApprovalState } from 'src/app/core/models/platform/approval-state.enum';
 
 @Component({
   selector: 'app-my-view-report',
@@ -239,7 +238,7 @@ export class MyViewReportPage {
       (approver) => report.next_approver_user_ids?.[0] === approver.approver_user.id
     );
     const highestRankApprover = this.approvals.reduce(
-      (max, approver) => (approver.approver_order > max.approver_order ? approver : max),
+      (max, approver) => (approver.rank > max.rank ? approver : max),
       this.approvals[0]
     );
     this.approverToShow = filteredApprover.length === 1 ? filteredApprover[0] : highestRankApprover;
@@ -260,12 +259,7 @@ export class MyViewReportPage {
       map((report) => {
         this.setupComments(report);
         this.approvals = report?.approvals;
-        // filtering out disabled approvals from my view report page
-        this.approvals = report?.approvals?.filter((approval) =>
-          [ApprovalState.APPROVAL_PENDING, ApprovalState.APPROVAL_DONE].includes(approval.state)
-        );
-        if (this.showViewApproverModal) {
-          this.approvals.sort((a, b) => a.approver_order - b.approver_order);
+        if (this.approvals) {
           this.setupApproverToShow(report);
         }
         return report;
@@ -636,8 +630,8 @@ export class MyViewReportPage {
   }
 
   segmentChanged(event: SegmentCustomEvent): void {
-    if (isNumber(event?.detail?.value)) {
-      this.segmentValue = event.detail.value;
+    if (event?.detail?.value) {
+      this.segmentValue = parseInt(`${event.detail.value}`, 10);
     }
   }
 
