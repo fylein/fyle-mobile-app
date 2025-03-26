702a6b42d05c989205efad62158b120eb778501b
Aastha Bist <aastha.b@fyle.in>
2025-03-26 09:37:32 +0530

    Update show-all-approvers-popover.component.ts

--- src/app/shared/components/fy-approver/show-all-approvers-popover/show-all-approvers-popover.component.ts
+++ src/app/shared/components/fy-approver/show-all-approvers-popover/show-all-approvers-popover.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Platform, PopoverController } from '@ionic/angular';
+import { PopoverController } from '@ionic/angular';
 import { ApprovalState } from 'src/app/core/models/platform/approval-state.enum';
 import { ReportApprovals } from 'src/app/core/models/platform/report-approvals.model';
 
@@ -13,7 +13,7 @@ export class ShowAllApproversPopoverComponent {
 
   approvalState: typeof ApprovalState = ApprovalState;
 
-  constructor(private popoverController: PopoverController, public platform: Platform) {}
+  constructor(private popoverController: PopoverController) {}
 
   closePopover(): void {
     this.popoverController.dismiss();
