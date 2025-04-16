3e38dda1ce2fea7b935f5b81283cc62bb11b7a37
Devendra Singh Rana <132900359+devendrafyle@users.noreply.github.com>
2025-04-16 12:44:41 +0530

    fix: fix the show date for first comment added in reports (#3663)
    * fix: fix the show date for first comment added in reports
    
    * added fix for all the pages

--- src/app/fyle/my-view-report/my-view-report.page.ts
+++ src/app/fyle/my-view-report/my-view-report.page.ts
@@ -42,6 +42,7 @@ import { ShowAllApproversPopoverComponent } from 'src/app/shared/components/fy-a
 import { ReportApprovals } from 'src/app/core/models/platform/report-approvals.model';
 import * as Sentry from '@sentry/angular-ivy';
 import { ApprovalState } from 'src/app/core/models/platform/approval-state.enum';
+import { DateWithTimezonePipe } from 'src/app/shared/pipes/date-with-timezone.pipe';
 
 @Component({
   selector: 'app-my-view-report',
@@ -143,7 +144,8 @@ export class MyViewReportPage {
     private orgSettingsService: OrgSettingsService,
     private platformHandlerService: PlatformHandlerService,
     private spenderReportsService: SpenderReportsService,
-    private launchDarklyService: LaunchDarklyService
+    private launchDarklyService: LaunchDarklyService,
+    private dateWithTimezonePipe: DateWithTimezonePipe
   ) {}
 
   get Segment(): typeof ReportPageSegment {
@@ -223,8 +225,8 @@ export class MyViewReportPage {
       this.userComments.sort((a, b) => (a.created_at > b.created_at ? 1 : -1));
 
       for (let i = 0; i < this.userComments.length; i++) {
-        const prevCommentDt = dayjs(this.userComments[i - 1] && this.userComments[i - 1].created_at);
-        const currentCommentDt = dayjs(this.userComments[i] && this.userComments[i].created_at);
+        const prevCommentDt = this.dateWithTimezonePipe.transform(this.userComments?.[i - 1]?.created_at);
+        const currentCommentDt = this.dateWithTimezonePipe.transform(this.userComments?.[i]?.created_at);
         if (dayjs(prevCommentDt).isSame(currentCommentDt, 'day')) {
           this.userComments[i].show_dt = false;
         } else {
