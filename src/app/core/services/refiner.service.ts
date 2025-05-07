3071cfb5f067530b1774ca654501352910608f98
Vaibhav Singh <vaibhav.singh@fyle.in>
2025-05-07 15:46:49 +0530

    fix: Replace SPENDER role by FYLER in refiner service (#3677)

--- src/app/core/services/refiner.service.ts
+++ src/app/core/services/refiner.service.ts
@@ -266,7 +266,7 @@ export class RefinerService {
               region: `${this.getRegion(homeCurrency)} - ${homeCurrency}`,
             },
             source: `Mobile - ${device}`,
-            is_admin: eou?.ou?.roles?.some((role) => !['SPENDER', 'APPROVER'].includes(role)) ? 'T' : 'F',
+            is_admin: eou?.ou?.roles?.some((role) => !['FYLER', 'APPROVER'].includes(role)) ? 'T' : 'F',
             action_name: properties.actionName,
           }
         );
