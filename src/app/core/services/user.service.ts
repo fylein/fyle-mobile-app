9a33082729f907747605ed989497887f53bf5b44
aastha <aastha.b@fyle.in>
2025-05-29 10:13:02 +0530

    Minor

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { catchError, map, switchMap } from 'rxjs/operators';
-import { from, of, Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { from, Observable } from 'rxjs';
 import { ApiService } from './api.service';
 import { User } from '../models/user.model';
 import { UserProperty } from '../models/v1/user-property.model';
@@ -29,6 +27,10 @@ export class UserService {
 
   isPendingDetails(): Observable<boolean> {
     return from(this.authService.getEou()).pipe(map((eou) => eou.ou.status === 'PENDING_DETAILS'));
+  }
+
+  isUserActive(): Observable<boolean> {
+    return from(this.authService.getEou()).pipe(map((eou) => eou.ou.status === 'ACTIVE'));
   }
 
   getProperties(): Observable<UserProperty> {
