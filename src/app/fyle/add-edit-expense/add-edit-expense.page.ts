e2f14ca610fbb85e9fee413d1e7a9f8f3d7e556e
aastha <aastha.b@fyle.in>
2025-06-02 12:17:40 +0530

    Minor

--- src/app/fyle/add-edit-expense/add-edit-expense.page.ts
+++ src/app/fyle/add-edit-expense/add-edit-expense.page.ts
@@ -3313,9 +3313,6 @@ export class AddEditExpensePage implements OnInit {
 
     this.paymentModes$ = this.getPaymentModes();
 
-    // Show payment mode if it is not a CCC expense
-    this.showPaymentMode = !this.isCccExpense;
-
     this.initSplitTxn(orgSettings$);
 
     this.setupFilteredCategories();
@@ -3386,6 +3383,8 @@ export class AddEditExpensePage implements OnInit {
     this.etxn$.subscribe((etxn) => {
       this.isSplitExpense = this.getCheckSpiltExpense(etxn);
       this.isCccExpense = etxn?.tx?.corporate_credit_card_expense_group_id;
+      // Show payment mode if it is not a CCC expense
+      this.showPaymentMode = !this.isCccExpense;
       this.isExpenseMatchedForDebitCCCE = this.getDebitCCCExpense(etxn);
       this.canDismissCCCE = this.getDismissCCCExpense(etxn);
       this.canRemoveCardExpense = this.getRemoveCCCExpense(etxn);
