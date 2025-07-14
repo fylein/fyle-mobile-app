084cdb7994c40fc1e886e5de51dffd5615426618
Amit Pile <amitpile@Amits-MacBook-Air.local>
2025-07-14 18:40:49 +0530

    improving code coverage

--- src/app/core/mock-data/feature-config.data.ts
+++ src/app/core/mock-data/feature-config.data.ts
@@ -17,6 +17,19 @@ export const featureConfigOptInData: FeatureConfig<{ count: number }> = deepFree
   is_shared: true,
 });
 
+export const featureConfigEmailOptInData: FeatureConfig<boolean> = deepFreeze({
+  org_id: 'orwruogwnngg',
+  user_id: 'uswjwgnwwgo',
+  created_at: '2020-06-01T13:14:54.804+00:00',
+  updated_at: '2020-06-11T13:14:55.201598+00:00',
+  target_client: 'WEBAPP',
+  feature: 'DASHBOARD_EMAIL_OPT_IN_BANNER',
+  sub_feature: 'ALL_EXPENSES',
+  key: 'EMAIL_OPT_IN_BANNER_SHOWN',
+  value: false,
+  is_shared: true,
+});
+
 export const featureConfigWalkthroughFinishData: FeatureConfig<{ isShown: boolean; isFinished: boolean }> = deepFreeze({
   feature: 'DASHBOARD_NAVBAR_WALKTHROUGH',
   key: 'SHOW_NAVBAR_WALKTHROUGH',
