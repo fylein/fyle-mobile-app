690eba11c20b1d72664530ed4e8c0b70f525953e
MdFarhan <107925922+sret-farhan2021@users.noreply.github.com>
2025-08-04 23:20:12 +0530

    feat: Revert "fix: revert policy change (#3829)" (#3835)
    * Revert "fix: revert policy change (#3829)"
    
    This reverts commit 37280d67c8964034f3ce3ae25d1a96cba6a99c89.
    
    * minor fix

--- src/app/core/services/platform/v1/shared/expenses.service.ts
+++ src/app/core/services/platform/v1/shared/expenses.service.ts
@@ -27,6 +27,10 @@ export class ExpensesService {
     return expense.state && expense.state === ExpenseState.DRAFT;
   }
 
+  isExpenseUnreportable(expense: Expense): boolean {
+    return expense.state && expense.state === ExpenseState.UNREPORTABLE;
+  }
+
   isCriticalPolicyViolatedExpense(expense: Expense): boolean {
     return typeof expense.policy_amount === 'number' && expense.policy_amount < 0.0001;
   }
@@ -124,12 +128,17 @@ export class ExpensesService {
         (expense) =>
           !this.isCriticalPolicyViolatedExpense(expense) &&
           !this.isExpenseInDraft(expense) &&
+          !this.isExpenseUnreportable(expense) &&
           expense.id &&
           !this.doesExpenseHavePendingCardTransaction(expense),
       );
     } else {
       return expenses.filter(
-        (expense) => !this.isCriticalPolicyViolatedExpense(expense) && !this.isExpenseInDraft(expense) && expense.id
+        (expense) =>
+          !this.isCriticalPolicyViolatedExpense(expense) &&
+          !this.isExpenseInDraft(expense) &&
+          !this.isExpenseUnreportable(expense) &&
+          expense.id,
       );
     }
   }
@@ -338,6 +347,10 @@ export class ExpensesService {
 
       if (filters.state.includes(FilterState.POLICY_VIOLATED)) {
         stateOrFilter.push('and(is_policy_flagged.eq.true,or(policy_amount.is.null,policy_amount.gt.0.0001))');
+      }
+
+      if (filters.state.includes(FilterState.BLOCKED)) {
+        stateOrFilter.push('state.in.(UNREPORTABLE)');
       }
 
       if (filters.state.includes(FilterState.CANNOT_REPORT)) {
