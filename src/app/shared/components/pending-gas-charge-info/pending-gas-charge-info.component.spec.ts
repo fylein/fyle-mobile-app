93700e9abe9f901dce0d4a56e6c59c5a4d7e6cc9
Arjun <arjunmuraliknr@gmail.com>
2025-08-05 22:59:34 +0530

    minor

--- src/app/shared/components/pending-gas-charge-info/pending-gas-charge-info.component.spec.ts
+++ src/app/shared/components/pending-gas-charge-info/pending-gas-charge-info.component.spec.ts
@@ -7,7 +7,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { IconModule } from 'src/app/shared/icon/icon.module';
 import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 
-fdescribe('PendingGasChargeInfoComponent', () => {
+describe('PendingGasChargeInfoComponent', () => {
   let component: PendingGasChargeInfoComponent;
   let fixture: ComponentFixture<PendingGasChargeInfoComponent>;
   let modalControllerSpy: jasmine.SpyObj<ModalController>;
