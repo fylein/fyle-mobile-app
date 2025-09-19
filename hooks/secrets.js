55455f377ff5af3234d5b5524a0f7889cb1dfb4c
Arjun <arjunmuraliknr@gmail.com>
2025-09-19 17:13:54 +0530

    remove google credentials completely

--- hooks/secrets.js
+++ hooks/secrets.js
@@ -1,6 +1,4 @@
 const dotenv = require('dotenv');
-const { getGoogleCredentialsAndroid } = require('./utils/google-credentials-android');
-const { getGoogleCredentialsIos } = require('./utils/google-credentials-ios');
 const { getIosInfo } = require('./utils/ios-info');
 const { getProdEnvironment } = require('./utils/prod-environment');
 
@@ -9,8 +7,6 @@ dotenv.config();
 if(process.env.NATIVE_CONFIG) {
   const NATIVE_CONFIG = JSON.parse(process.env.NATIVE_CONFIG);
   module.exports = {
-    googleCredentialsAndroid: getGoogleCredentialsAndroid(NATIVE_CONFIG),
-    googleCredentialsIos: getGoogleCredentialsIos(NATIVE_CONFIG),
     iosInfo: getIosInfo()
   }
 }
