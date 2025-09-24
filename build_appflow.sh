8b8a86547d7235377c12dbd619965cd25e6aa436
Arjun <arjunmuraliknr@gmail.com>
2025-09-24 15:40:43 +0530

    modify appflow build script to remove sourcemaps (#3961)

--- build_appflow.sh
+++ build_appflow.sh
@@ -1,3 +1,13 @@
-ionic capacitor sync --prod --source-map
+# Build the web app with source maps
+ionic build --prod --source-map
+
+# Process and upload source maps to Sentry
 npm run sentry:sourcemaps
 sentry-cli releases --org fyle-technologies-private-limi -p "$SENTRY_PROJECT_NAME" files "$LIVE_UPDATE_APP_VERSION" upload-sourcemaps www
+
+# Remove source maps from www folder to reduce bundle size
+find www -name "*.js.map" -type f -delete
+find www -name "*.css.map" -type f -delete
+
+# Sync to native platforms without rebuilding
+ionic capacitor sync --no-build
