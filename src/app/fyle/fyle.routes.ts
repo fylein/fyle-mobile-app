721ec7d499d3a981fac01191564abad99a517ad8
Arjun <arjunmuraliknr@gmail.com>
2025-10-13 15:49:25 +0530

    minor

--- src/app/fyle/fyle.routes.ts
+++ src/app/fyle/fyle.routes.ts
@@ -137,10 +137,6 @@ const routes: Routes = [
     loadComponent: () =>
       import('./manage-corporate-cards/manage-corporate-cards.page').then((m) => m.ManageCorporateCardsPage),
   },
-  {
-    path: 'toast_demo',
-    loadComponent: () => import('../shared/components/toast-demo/toast-demo.component').then((m) => m.ToastDemoComponent),
-  },
 ];
 
 // Apply OptInGuard to all routes
