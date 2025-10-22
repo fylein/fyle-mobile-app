9923df5b180a7d759b19c3af678b27093b0ad3ba
Mohammed Farhan K <mohammed.farhan@fyle.in>
2025-10-22 11:29:44 +0530

    initial commit

--- src/app/core/mock-data/dwolla-customer.data.ts
+++ src/app/core/mock-data/dwolla-customer.data.ts
@@ -1,26 +1,5 @@
 import deepFreeze from 'deep-freeze-strict';
-
-export interface DwollaCustomer {
-  id: string;
-  created_at: Date;
-  updated_at: Date;
-  customer_id: string;
-  customer_email: string;
-  customer_added_by: string;
-  customer_verified: boolean;
-  beneficial_owner_added: boolean;
-  beneficial_owner_verified: boolean;
-  bank_account_added: boolean;
-  bank_account_verified: boolean;
-  bank_account_added_by: string;
-  customer_document_needed: boolean;
-  beneficial_owner_document_needed: boolean;
-  customer_retry: boolean;
-  customer_suspended: boolean;
-  micro_deposit_verification_status: string;
-  micro_deposit_verification_attempts: number;
-  beneficial_owner_retry: boolean;
-}
+import { DwollaCustomer } from '../models/dwolla-customer.model';
 
 export const suspendedDwollaCustomer: DwollaCustomer = deepFreeze({
   id: 'test-id',
