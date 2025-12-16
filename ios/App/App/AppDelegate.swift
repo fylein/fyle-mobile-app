1a04730586b60af86a22b09287406379b29d9fa3
aastha <aastha.b@fyle.in>
2025-12-16 17:36:28 +0530

    Minor

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Capacitor
-import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -50,7 +49,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
     // Called when the app was launched with a url. Feel free to add additional processing here,
     // but if you want the App API to support tracking app url opens, make sure to keep this call
-    FirebaseApp.configure()
     return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
   }
   
