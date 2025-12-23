07dae3203ec175b0149bdbc350ab00711e41d93b
aastha <aastha.b@fyle.in>
2025-12-23 15:52:04 +0530

    Minor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -317,30 +317,30 @@ export class AppComponent implements OnInit, AfterViewInit {
 
     this.gmapsService.loadLibrary();
 
-    PushNotifications.requestPermissions().then((result) => {
-      if (result.receive === 'granted') {
-        // Register with Apple / Google to receive push via APNS/FCM
-        PushNotifications.register();
-      } else {
-        // Show some error
-      }
-    });
+    // PushNotifications.requestPermissions().then((result) => {
+    //   if (result.receive === 'granted') {
+    //     // Register with Apple / Google to receive push via APNS/FCM
+    //     PushNotifications.register();
+    //   } else {
+    //     // Show some error
+    //   }
+    // });
 
-    PushNotifications.addListener('registration', (token: Token) => {
-      alert('Push registration success, token: ' + token.value);
-    });
+    // PushNotifications.addListener('registration', (token: Token) => {
+    //   alert('Push registration success, token: ' + token.value);
+    // });
 
-    PushNotifications.addListener('registrationError', (error: any) => {
-      alert('Error on registration: ' + JSON.stringify(error));
-    });
+    // PushNotifications.addListener('registrationError', (error: any) => {
+    //   alert('Error on registration: ' + JSON.stringify(error));
+    // });
 
-    PushNotifications.addListener('pushNotificationReceived', (notification: PushNotificationSchema) => {
-      alert('Push received: ' + JSON.stringify(notification));
-    });
+    // PushNotifications.addListener('pushNotificationReceived', (notification: PushNotificationSchema) => {
+    //   alert('Push received: ' + JSON.stringify(notification));
+    // });
 
-    PushNotifications.addListener('pushNotificationActionPerformed', (notification: ActionPerformed) => {
-      alert('Push action performed: ' + JSON.stringify(notification));
-    });
+    // PushNotifications.addListener('pushNotificationActionPerformed', (notification: ActionPerformed) => {
+    //   alert('Push action performed: ' + JSON.stringify(notification));
+    // });
   }
 
   getShowFooter(): void {
