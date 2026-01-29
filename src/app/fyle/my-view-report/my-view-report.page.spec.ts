fea78ae40f7ed5fa4e87893e6e4ae5c20dbb846e
Shrasty Rastogi <shrasty.r@fyle.in>
2026-01-30 00:42:04 +0530

    fix: minor

--- src/app/fyle/my-view-report/my-view-report.page.spec.ts
+++ src/app/fyle/my-view-report/my-view-report.page.spec.ts
@@ -70,7 +70,7 @@ import { ReportState } from 'src/app/core/models/platform/v1/report.model';
 })
 class MockExpensesCardComponent {}
 
-fdescribe('MyViewReportPage', () => {
+describe('MyViewReportPage', () => {
   let component: MyViewReportPage;
   let fixture: ComponentFixture<MyViewReportPage>;
   let activatedRoute: jasmine.SpyObj<ActivatedRoute>;
