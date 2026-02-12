1687e45fd5b52e3c6a18002d20a81705bcb12f13
Aniruddha Shriwant <aniruddhashriwant@gmail.com>
2026-02-12 13:12:18 +0530

    feat: migrating version/app api to platform (#4099)

--- src/app/core/services/app-version.service.spec.ts
+++ src/app/core/services/app-version.service.spec.ts
@@ -1,10 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { AppVersionService } from './app-version.service';
-import { ApiService } from './api.service';
+import { SpenderPlatformV1ApiService } from './spender-platform-v1-api.service';
 import { RouterApiService } from './router-api.service';
 import { LoginInfoService } from './login-info.service';
 import { AuthService } from './auth.service';
-import { appVersionData1 } from '../mock-data/app-version.data';
+import { appVersionData1, appVersionPostResponse, appVersionResponse } from '../mock-data/app-version.data';
 import { of } from 'rxjs';
 import { extendedDeviceInfoMockData } from '../mock-data/extended-device-info.data';
 import { apiEouRes } from '../mock-data/extended-org-user.data';
@@ -12,13 +12,13 @@ import { environment } from 'src/environments/environment';
 
 describe('AppVersionService', () => {
   let appVersionService: AppVersionService;
-  let apiService: jasmine.SpyObj<ApiService>;
+  let spenderPlatformV1ApiService: jasmine.SpyObj<SpenderPlatformV1ApiService>;
   let routerApiService: jasmine.SpyObj<RouterApiService>;
   let loginInfoService: jasmine.SpyObj<LoginInfoService>;
   let authService: jasmine.SpyObj<AuthService>;
 
   beforeEach(() => {
-    const apiServiceSpy = jasmine.createSpyObj('ApiService', ['get', 'post']);
+    const spenderPlatformV1ApiServiceSpy = jasmine.createSpyObj('SpenderPlatformV1ApiService', ['get', 'post']);
     const authServiceSpy = jasmine.createSpyObj('AuthService', ['getEou', 'refreshEou']);
     const routerApiServiceSpy = jasmine.createSpyObj('RouterApiService', ['post']);
     const loginInfoServiceSpy = jasmine.createSpyObj('LoginInfoService', ['getLastLoggedInVersion']);
@@ -26,8 +26,8 @@ describe('AppVersionService', () => {
       providers: [
         AppVersionService,
         {
-          provide: ApiService,
-          useValue: apiServiceSpy,
+          provide: SpenderPlatformV1ApiService,
+          useValue: spenderPlatformV1ApiServiceSpy,
         },
         {
           provide: AuthService,
@@ -44,7 +44,9 @@ describe('AppVersionService', () => {
       ],
     });
     appVersionService = TestBed.inject(AppVersionService);
-    apiService = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
+    spenderPlatformV1ApiService = TestBed.inject(
+      SpenderPlatformV1ApiService,
+    ) as jasmine.SpyObj<SpenderPlatformV1ApiService>;
     authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
     loginInfoService = TestBed.inject(LoginInfoService) as jasmine.SpyObj<LoginInfoService>;
     routerApiService = TestBed.inject(RouterApiService) as jasmine.SpyObj<RouterApiService>;
@@ -77,26 +79,33 @@ describe('AppVersionService', () => {
   });
 
   it('get(): should get app version', (done) => {
-    apiService.get.and.returnValue(of(appVersionData1));
+    spenderPlatformV1ApiService.get.and.returnValue(of(appVersionResponse));
 
     appVersionService.get('ios').subscribe((res) => {
-      expect(res).toEqual(appVersionData1);
-      expect(apiService.get).toHaveBeenCalledOnceWith('/version/app/IOS');
+      expect(res).toEqual(appVersionResponse);
+      expect(spenderPlatformV1ApiService.get).toHaveBeenCalledOnceWith('/mobile_app/versions', {
+        params: { order: 'created_at.desc', 'os->name': 'eq.IOS', limit: 1 },
+      });
       done();
     });
   });
 
   it('post(): should update the app version', (done) => {
-    apiService.post.and.returnValue(of(null));
+    spenderPlatformV1ApiService.post.and.returnValue(of(appVersionPostResponse));
 
-    const data = {
-      app_version: '5.50.0',
-      device_platform: 'ios',
-      platform_version: '5.50.0',
+    const payload = {
+      data: {
+        version: '5.50.0',
+        os: {
+          name: 'ios',
+          version: '5.50.0',
+        },
+      },
     };
 
-    appVersionService.post(data).subscribe((res) => {
-      expect(res).toBeNull();
+    appVersionService.post(payload).subscribe((res) => {
+      expect(res).toEqual(appVersionPostResponse);
+      expect(spenderPlatformV1ApiService.post).toHaveBeenCalledOnceWith('/mobile_app/versions', payload);
       done();
     });
   });
@@ -111,48 +120,58 @@ describe('AppVersionService', () => {
   });
 
   it('should load device info', (done) => {
-    spyOn(appVersionService, 'post').and.returnValue(of(null));
-    spyOn(appVersionService, 'get').and.returnValue(of(appVersionData1));
+    spyOn(appVersionService, 'post').and.returnValue(of(appVersionPostResponse));
+    spyOn(appVersionService, 'get').and.returnValue(of(appVersionResponse));
     spyOn(appVersionService, 'isVersionLower').and.returnValue(false);
 
     appVersionService.load(extendedDeviceInfoMockData);
-    const data = {
-      app_version: environment.LIVE_UPDATE_APP_VERSION,
-      device_platform: extendedDeviceInfoMockData.operatingSystem,
-      platform_version: extendedDeviceInfoMockData.osVersion,
+    const payload = {
+      data: {
+        version: environment.LIVE_UPDATE_APP_VERSION,
+        os: {
+          name: extendedDeviceInfoMockData.operatingSystem.toUpperCase(),
+          version: extendedDeviceInfoMockData.osVersion,
+        },
+      },
     };
 
     appVersionService.load(extendedDeviceInfoMockData);
-    expect(appVersionService.get).toHaveBeenCalledWith(extendedDeviceInfoMockData.operatingSystem);
+    expect(appVersionService.get).toHaveBeenCalledWith(extendedDeviceInfoMockData.operatingSystem.toUpperCase());
     expect(appVersionService.get).toHaveBeenCalledTimes(2);
-    expect(appVersionService.isVersionLower).toHaveBeenCalledWith(appVersionData1.app_version, data.app_version);
+    expect(appVersionService.isVersionLower).toHaveBeenCalledWith(appVersionData1.version, payload.data.version);
     done();
   });
 
   describe('load():', () => {
     it('should load device info when app version lower', (done) => {
-      spyOn(appVersionService, 'post').and.returnValue(of(null));
-      spyOn(appVersionService, 'get').and.returnValue(of({ ...appVersionData1, app_version: '1.10.1' }));
+      spyOn(appVersionService, 'post').and.returnValue(of(appVersionPostResponse));
+      spyOn(appVersionService, 'get').and.returnValue(
+        of({ ...appVersionResponse, data: [{ ...appVersionData1, version: '1.10.1' }] }),
+      );
       spyOn(appVersionService, 'isVersionLower').and.returnValue(true);
 
       appVersionService.load(extendedDeviceInfoMockData);
-      const data = {
-        app_version: environment.LIVE_UPDATE_APP_VERSION,
-        device_platform: extendedDeviceInfoMockData.operatingSystem,
-        platform_version: extendedDeviceInfoMockData.osVersion,
+      const payload = {
+        data: {
+          version: environment.LIVE_UPDATE_APP_VERSION,
+          os: {
+            name: extendedDeviceInfoMockData.operatingSystem.toUpperCase(),
+            version: extendedDeviceInfoMockData.osVersion,
+          },
+        },
       };
-      expect(appVersionService.get).toHaveBeenCalledOnceWith(extendedDeviceInfoMockData.operatingSystem);
-      expect(appVersionService.isVersionLower).toHaveBeenCalledWith('1.10.1', data.app_version);
+      expect(appVersionService.get).toHaveBeenCalledOnceWith(extendedDeviceInfoMockData.operatingSystem.toUpperCase());
+      expect(appVersionService.isVersionLower).toHaveBeenCalledWith('1.10.1', payload.data.version);
       done();
     });
 
     it('should load device info when app version not present', (done) => {
-      spyOn(appVersionService, 'post').and.returnValue(of(null));
-      spyOn(appVersionService, 'get').and.returnValue(of(null));
+      spyOn(appVersionService, 'post').and.returnValue(of(appVersionPostResponse));
+      spyOn(appVersionService, 'get').and.returnValue(of({ count: 0, offset: 0, data: [] }));
       spyOn(appVersionService, 'isVersionLower').and.returnValue(true);
 
       appVersionService.load(extendedDeviceInfoMockData);
-      expect(appVersionService.get).toHaveBeenCalledOnceWith(extendedDeviceInfoMockData.operatingSystem);
+      expect(appVersionService.get).toHaveBeenCalledOnceWith(extendedDeviceInfoMockData.operatingSystem.toUpperCase());
       done();
     });
   });
